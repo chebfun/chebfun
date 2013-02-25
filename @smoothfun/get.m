5eb82ea7265928509f4e22a9312ed2378510545b
nickhale <nick.p.hale@gmail.com>
2013-02-25 15:38:05 +0000

    Very basic get implementations for @onefun and @smoothfun.

---
+++ @smoothfun/get.m
@@ -0,0 +1,24 @@
+function out = get(f, prop)
+%GET    GET method for the SMOOTHFUN class
+%   P = GET(F,PROP) returns the property P specified in the string PROP from
+%   the fun F. Valid entries for the string PROP are:
+%       'VSCALE' - Vertical scale of F.
+%       'ISHAPPY' - Is F happy?
+%       'EPSLEVEL' - Happiness level of F.
+%       'POINTS' - Grid corresponding to F.
+%       'LVAL'
+%       'RVAL'
+
+% Copyright 2013 by The University of Oxford and The Chebfun Developers. 
+% See http://www.chebfun.org/ for Chebfun information.
+
+switch prop
+    case fieldnames(f)
+        % Allow access to any of F's properties via GET.
+        out = f.(prop);
+    case {'points', 'lval', 'rval', 'ishappy',' vscale', 'epslevel'}
+        out = get(f.funcheb, prop);
+    otherwise
+        error('CHEBFUN:SMOOTHFUN:GET:proname', ...
+            'Unknown property name ''%s'' for object of type smoothfun.', prop);
+end
