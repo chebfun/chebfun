b13307f900e6578017c86583b7d1f3e5fbe2ab32
Kuan Xu <xukuan2011@gmail.com>
2013-04-06 16:41:37 +0100

    Fix a bunch of minor blemishes in Chebtech2 related stuff found during the code  review process for the 'construction process'.

--- @chebtech1/make.m
+++ @chebtech1/make.m
@@ -1,6 +1,11 @@
 function f = make(varargin)
 %MAKE   Constructor shortcut for CHEBTECH1 objects.
 
+% See also CHEBTECH, CHEBTECH1.
+
+% Copyright 2013 by The University of Oxford and The Chebfun Developers.
+% See http://www.chebfun.org/ for Chebfun information.
+
 f = chebtech1(varargin{:}); 
 
 end
