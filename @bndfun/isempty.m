053e7fa28f70ca48fecf7a36e4e3c1a5e772308e
Asgeir Birkisson <asgeir@asgeir-mint.(none)>
2013-04-14 18:48:13 +0100

    Adding isempty, isequal, isfinite, isinf, isnan and isreal methods for BNDFUN.

---
+++ @bndfun/isempty.m
@@ -0,0 +1,19 @@
+function out = isempty(f)
+%ISEMPTY   True for an empty BNDFUN.
+%   ISEMPTY(F) returns TRUE if F is an empty BNDFUN and FALSE otherwise.
+
+% Copyright 2013 by The University of Oxford and The Chebfun Developers. 
+% See http://www.chebfun.org/ for Chebfun information.
+
+if ( numel(f) > 1 )
+    % An array cannot be empty.
+    out = false;
+elseif ( numel(f) == 1 )
+    % Check if the onefun is empty:
+    out = isempty(f.onefun);
+else 
+    % numel(f) == 0, so f must be empty.
+    out = true;
+end
+
+end
