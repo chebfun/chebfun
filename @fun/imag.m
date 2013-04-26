030aafe60654c23cc1799c612633e7e26899bd2e
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2013-04-26 12:56:52 +0100

    Moving the BNDFUN methods imag and real to the FUN level.

--- @fun/imag.m
+++ @fun/imag.m
@@ -1,5 +1,5 @@
-function f = real(f)
-%IMAG	Imaginary part of a BNDFUN.
+function f = imag(f)
+%IMAG	Imaginary part of a FUN.
 %   IMAG(F) is the imaginary part of F.
 %
 %   See also REAL, ISREAL, CONJ.
@@ -7,7 +7,7 @@ function f = real(f)
 % Copyright 2013 by The University of Oxford and The Chebfun Developers. 
 % See http://www.chebfun.org for Chebfun information.
 
-% Assign the onefun to be the real part of the onefun of the input:
-f.onefun = real(f.onefun);
+% Assign the onefun to be the imaginary part of the onefun of the input:
+f.onefun = imag(f.onefun);
 
 end
