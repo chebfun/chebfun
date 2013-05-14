5f0b814d58c6dc9550c05e7eb5e32126e985918d
Anthony P. Austin <austin@maths.ox.ac.uk>
2013-05-14 01:51:18 +0100

    Improve clarity of help text in @chebtech/clenshaw.m.
    Requested by Alex Townsend.

--- @chebtech/clenshaw.m
+++ @chebtech/clenshaw.m
@@ -1,11 +1,22 @@
 function y = clenshaw(x, c)
 %CLENSHAW   Clenshaw's algorithm for evaluating a Chebyshev polynomial.
-%   Y = CLENSHAW(X, C) evaluates the Chebyshev expansion
-%     Y = P_N(X) = C(1,1)*T_N(X) + ... + C(N,1)*T_1(X) + C(N+1,1)*T_0(X)
-%   using Clenshaw's algorithm. X must be a column vector.
+%   If C is a column vector, Y = CLENSHAW(X, C) evaluates the Chebyshev
+%   expansion
+%
+%     Y = P_N(X) = C(1)*T_N(X) + ... + C(N)*T_1(X) + C(N+1)*T_0(X)
+%
+%   using Clenshaw's algorithm.
 %
 %   If C is an (N+1) x M matrix, then CLENSHAW interprets each of the columns
-%   of C as coefficients of a degree N polynomial.
+%   of C as coefficients of a degree N polynomial and evaluates the M Chebyshev
+%   expansions
+%
+%     Y_m = P_N(X) = C(1,m)*T_N(X) + ... + C(N,m)*T_1(X) + C(N+1,m)*T_0(X)
+%
+%   for 1 <= m <= M, returning the results as columns of a matrix Y =
+%   [Y_1 ... Y_M].
+%
+%   In both cases, X must be a column vector.
 %
 % See also CHEBTECH.FEVAL, CHEBTECH.BARY.
 
