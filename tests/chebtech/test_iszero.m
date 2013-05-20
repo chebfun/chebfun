6d7d0afac490ec05c8c6c3d44f5c78c52186e6c8
Anthony P. Austin <austin@maths.ox.ac.uk>
2013-05-20 09:55:52 +0100

    Add second row to the pass variable in tests/chebtech/test_iszero.m
    Why did this pass on my machine without this last night?

--- tests/chebtech/test_iszero.m
+++ tests/chebtech/test_iszero.m
@@ -18,21 +18,21 @@ for n = 1:2
     f.values = [0 1 0 ; 0 0 NaN];
 
     % iszero([0;0]) = 1, iszero([1;0]) = 0, iszero([0;NaN]) = 0.
-    pass(1) = all(iszero(f) == [1 0 0]);
+    pass(n, 1) = all(iszero(f) == [1 0 0]);
 
     % iszero(0) = 1, iszero(NaN) = 0, iszero(1) = 0.
     f.values = [0 NaN 1];
-    pass(2) = all(iszero(f) == [1 0 0]);
+    pass(n, 2) = all(iszero(f) == [1 0 0]);
     f.values = [0 NaN 1]';
-    pass(3) = all(iszero(f) == 0);
+    pass(n, 3) = all(iszero(f) == 0);
 
     % iszero(0) = 1.
     f.values = zeros(3, 1);
-    pass(4) = all(iszero(f) == 1);
+    pass(n, 4) = all(iszero(f) == 1);
 
     % iszero(NaN) = 0.
     f.values = NaN;
-    pass(5) = all(iszero(f) == 0);
+    pass(n, 5) = all(iszero(f) == 0);
 end
 
 end
