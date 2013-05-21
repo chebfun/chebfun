24ff85679f0648f9a5a58c74685573d80ff632b8
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2013-05-21 15:39:43 +0100

    Moving BNDFUN times method to the FUN level.

--- @fun/times.m
+++ @fun/times.m
@@ -1,9 +1,9 @@
 function f = times(f, g, varargin)
-%.*   BNDFUN multiplication.
-%   F.*G multiplies BNDFUN objects F and G or a BNDFUN by a scalar if either
+%.*   FUN multiplication.
+%   F.*G multiplies FUN objects F and G or a FUN by a scalar if either
 %   F or G is a scalar.
 %
-%   If F is a array-valued BNDFUN, then F.*C is supported if C is a row
+%   If F is a array-valued FUN, then F.*C is supported if C is a row
 %   vector of doubles with the same number of columns as F.
 %
 % See also MTIMES, RDIVIDE.
@@ -11,19 +11,22 @@ function f = times(f, g, varargin)
 % Copyright 2013 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
 
-% BNDFUN * [] = []:
+% FUN * [] = []:
 if ( isempty(f) || isempty(g) )
     f = []; 
     return
 end
 
-if ( ~isa(f, 'bndfun') )        % Ensure F is a BNDFUN
-    f = times(g, f, varargin{:});
+if ( ~isa(f, 'fun') )
+    % If f is not a FUN, g must be
+    g.onefun = times(f, g.onefun, varargin{:});
+    % Swap arguments for output variable
+    f = g;
     return
-elseif ( isa(g, 'bndfun'))      % BNDFUN * BNDFUN
+elseif ( isa(g, 'fun'))      % FUN * FUN
     % Multiply the onefun objects of f and g together.
     f.onefun = times(f.onefun, g.onefun, varargin{:});
-elseif ( isa(g, 'double') )     % BNDFUN * double
+elseif ( isa(g, 'double') )     % FUN * double
     % Multiply the onefun of f with g.
     f.onefun = times(f.onefun, g, varargin{:});
 end
