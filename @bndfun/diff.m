ddc11135f1b9baa3e3064121cbaaa99ad6efdf24
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2013-06-05 14:44:10 +0100

    Fixing a bug in BNDFUN/DIFF, don't want to rescale when called with dim = 2. Spotted by KX.

--- @bndfun/diff.m
+++ @bndfun/diff.m
@@ -1,4 +1,4 @@
-function f = diff(f,k,varargin)
+function f = diff(f,k,dim)
 %DIFF	Derivative of a BNDFUN.
 %   DIFF(F) is the derivative of F and DIFF(F, K) is the K-th derivative.
 %
@@ -13,15 +13,28 @@ function f = diff(f,k,varargin)
 % Copyright 2013 by The University of Oxford and The Chebfun Developers. 
 % See http://www.chebfun.org for Chebfun information.
 
-if nargin < 2
+% Store number of input arguments
+narg = nargin;
+if ( narg < 2 || isempty(k) )
+    % Assume first derivative by default
     k = 1;
+    if narg < 3
+        % Assume dim = 1 by default
+        dim = 1;
+    end
 end
 
 % Rescaling factor, (b-a)/2, to the kth power
 rescaleFactork = (.5*diff(f.domain))^k;
 
-% Assign the onefun of the output to be the derivative of the onefun of the
-% input, and scale the result:
-f.onefun = diff(f.onefun,k,varargin{:})/rescaleFactork;
+% Assign the onefun of the output to be the output of the diff method of the
+% ONEFUN of the input. If we called diff with third argument equal to 2 (i.e.
+% dim = 2), we only wanted to compute difference between columns, in which case, 
+% we should not rescale the result.
+if ( dim == 1 )
+    f.onefun = diff(f.onefun,k,dim)/rescaleFactork;
+else
+    f.onefun = diff(f.onefun,k,dim);
+end
 
 end
