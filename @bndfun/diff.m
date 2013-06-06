49721ec8aad81cf60599c0f35083b47a997963fe
Kuan Xu <xukuan2011@gmail.com>
2013-06-06 14:31:31 +0100

    A minor fix in diff@bndfun for allowing for two input arguments.

--- @bndfun/diff.m
+++ @bndfun/diff.m
@@ -18,10 +18,11 @@ narg = nargin;
 if ( narg < 2 || isempty(k) )
     % Assume first derivative by default
     k = 1;
-    if narg < 3
-        % Assume dim = 1 by default
-        dim = 1;
-    end
+end
+
+if narg < 3
+    % Assume dim = 1 by default
+    dim = 1;
 end
 
 % Rescaling factor, (b-a)/2, to the kth power
@@ -29,7 +30,7 @@ rescaleFactork = (.5*diff(f.domain))^k;
 
 % Assign the onefun of the output to be the output of the diff method of the
 % ONEFUN of the input. If we called diff with third argument equal to 2 (i.e.
-% dim = 2), we only wanted to compute difference between columns, in which case, 
+% dim = 2), we only wanted to compute difference between columns, in which case,
 % we should not rescale the result.
 if ( dim == 1 )
     f.onefun = diff(f.onefun,k,dim)/rescaleFactork;
