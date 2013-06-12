d9c3f1ab53ec16a2c4798c0b6913f512023ae484
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2013-06-12 13:35:39 +0100

    Minor change to bndfun/test_cumsum, respecting 80 char line limit.

--- tests/bndfun/test_cumsum.m
+++ tests/bndfun/test_cumsum.m
@@ -30,26 +30,30 @@ f = bndfun(@(x) exp(x/10) - 1, dom, [], [], pref);
 F = cumsum(f);
 F_ex = @(x) 10*exp(x/10) - x - (10*exp(a/10) - a);
 err = feval(F, x) - F_ex(x);
-pass(1) = (norm(err, inf) < 5*f.onefun.vscale*tol) && (abs(feval(F, a)) < f.onefun.vscale*tol);
+pass(1) = (norm(err, inf) < 5*f.onefun.vscale*tol) && ...
+    (abs(feval(F, a)) < f.onefun.vscale*tol);
 
 f = bndfun(@(x) 1./(1 + x.^2), dom, [], [], pref);
 F = cumsum(f);
 F_ex = @(x) atan(x)-atan(a);
 err = feval(F, x) - F_ex(x);
-pass(2) = (norm(err, inf) < 5*f.onefun.vscale*tol) && (abs(feval(F, a)) < f.onefun.vscale*tol);
+pass(2) = (norm(err, inf) < 5*f.onefun.vscale*tol) && ...
+    (abs(feval(F, a)) < f.onefun.vscale*tol);
 
 f = bndfun(@(x) cos(1e4*x), dom, [], [], pref);
 F = cumsum(f);
 F_ex = @(x) (sin(1e4*x)-sin(1e4*a))/1e4;
 err = feval(F, x) - F_ex(x);
-pass(3) = (norm(err, inf) < f.onefun.vscale*tol) && (abs(feval(F, a)) < f.onefun.vscale*tol);
+pass(3) = (norm(err, inf) < f.onefun.vscale*tol) && ...
+    (abs(feval(F, a)) < f.onefun.vscale*tol);
 
 z = exp(2*pi*1i/6);
 f = bndfun(@(t) sinh(t*z), dom, [], [], pref);
 F = cumsum(f);
 F_ex = @(t) cosh(t*z)/z - (cosh(a*z)/z);
 err = feval(F, x) - F_ex(x);
-pass(4) = (norm(err, inf) < 10*f.onefun.vscale*tol) && (abs(feval(F, a)) < f.onefun.vscale*tol);
+pass(4) = (norm(err, inf) < 10*f.onefun.vscale*tol) && ...
+    (abs(feval(F, a)) < f.onefun.vscale*tol);
 
 %%
 % Check that applying cumsum() and direct construction of the antiderivative
@@ -59,7 +63,8 @@ f = bndfun(@(x) sin(4*x).^2, dom, [], [], pref);
 F = bndfun(@(x) 0.5*x - 0.0625*sin(8*x) - (a/2-sin(8*a)/16), dom, [], [], pref);
 G = cumsum(f);
 err = G - F;
-pass(5) = (std(err.onefun.values) < 10*f.onefun.vscale*tol) && (abs(feval(G, a)) < f.onefun.vscale*tol);
+pass(5) = (std(err.onefun.values) < 10*f.onefun.vscale*tol) && ...
+    (abs(feval(G, a)) < f.onefun.vscale*tol);
 
 %%
 % Check that diff(cumsum(f)) == f and that cumsum(diff(f)) == f up to a
@@ -71,16 +76,19 @@ err = feval(f, x) - feval(g, x);
 pass(6) = (norm(err, inf) < 3*g.onefun.vscale*f.onefun.vscale*tol);
 h = cumsum(diff(f));
 err = feval(f, x) - feval(h, x);
-pass(7) = (norm(err, inf) < 30*g.onefun.vscale*f.onefun.vscale*tol) && (abs(feval(h, a)) < g.onefun.vscale*f.onefun.vscale*tol);
+pass(7) = (norm(err, inf) < 30*g.onefun.vscale*f.onefun.vscale*tol) && ...
+    (abs(feval(h, a)) < g.onefun.vscale*f.onefun.vscale*tol);
 
 %%
 % Check operation for array-valued bndfun objects.
 
 f = bndfun(@(x) [sin(x) x.^2 exp(1i*x)], dom, [], [], pref);
-F_exact = bndfun(@(x) [(-cos(x)+cos(a)) (x.^3/3-a^3/3) ((exp(1i*x)-exp(1i*a))/1i)], dom, [], [], pref);
+F_exact = bndfun(@(x) [(-cos(x)+cos(a)) (x.^3/3-a^3/3) ...
+    ((exp(1i*x)-exp(1i*a))/1i)], dom, [], [], pref);
 F = cumsum(f);
 err = std(feval(F, x) - feval(F_exact, x));
-pass(8) = (norm(err, inf) < 2*max(f.onefun.vscale)*tol) && all(abs(feval(F, a) < 2*max(f.onefun.vscale)*tol));
+pass(8) = (norm(err, inf) < 2*max(f.onefun.vscale)*tol) && ...
+    all(abs(feval(F, a) < 2*max(f.onefun.vscale)*tol));
 
 %%
 % Check operation for second and third order cumsums.
@@ -89,12 +97,14 @@ F2_exact = bndfun(@(x) -sin(x)+x.*cos(a)+sin(a)-a*cos(a), ...
     dom, [], [], pref);
 F2 = cumsum(f,2);
 err = std(feval(F2, x) - feval(F2_exact, x));
-pass(9) = (norm(err, inf) < 5*(f.onefun.vscale)^2*tol) && abs(feval(F2, a) < (f.onefun.vscale)^2*tol);
+pass(9) = (norm(err, inf) < 5*(f.onefun.vscale)^2*tol) && ...
+    abs(feval(F2, a) < (f.onefun.vscale)^2*tol);
 
 F3_exact = bndfun(@(x) cos(x)+x.^2*cos(a)/2+x*(sin(a)-a*cos(a)) - ...
     (cos(a)/2-sin(a)), dom, [], [], pref);
 F3 = cumsum(f,3);
 err = std(feval(F3, x) - feval(F3_exact, x));
-pass(10) = (norm(err, inf) < (f.onefun.vscale)^3*tol) && abs(feval(F3, a) < (f.onefun.vscale)^3*tol);
+pass(10) = (norm(err, inf) < (f.onefun.vscale)^3*tol) && ...
+    abs(feval(F3, a) < (f.onefun.vscale)^3*tol);
 
 end
