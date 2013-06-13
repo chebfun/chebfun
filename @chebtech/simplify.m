b24bc748ea6e028c0eede7e137835ef157a4f09b
Anthony P. Austin <austin@maths.ox.ac.uk>
2013-06-13 10:21:17 +0100

    Change chebtech.simplify() to work relative to vscale.
    This should fix the problems with scaling and updating of values pointed
    out by Asgeir Birkission.  Also, the test for chebtech.simplify() has
    been rewritten to reflect the function's new behavior.

--- @chebtech/simplify.m
+++ @chebtech/simplify.m
@@ -1,17 +1,20 @@
 function f = simplify(f, tol)
 %SIMPLIFY   Zero small Chebyshev coefficients of a happy CHEBTECH object.
 %   G = SIMPLIFY(F) attempts to compute a 'simplified' version G of the happy
-%   CHEBTECH object F such that LENGTH(G) <= LENGTH(F) but ||G - F|| <
-%   F.EPSLEVEL. It does this by zeroing out all coefficients of F smaller in
-%   magnitude than the default CHEBTECH EPS preference and then removing all
-%   trailing zero coefficients. G.EPSLEVEL is set to the maximum of F.EPSLEVEL
-%   and the default CHEBTECH EPS.
+%   CHEBTECH object F such that LENGTH(G) <= LENGTH(F) but ||G - F|| is small
+%   in a relative sense:  ||G - F|| < G.EPSLEVEL*G.VSCALE.  It does this by
+%   zeroing out all coefficients of F that are relatively small; more
+%   precisely, it sets to zero all coefficients smaller in magnitude than the
+%   product of F.VSCALE and the default CHEBTECH EPS preference.  It then
+%   removes all trailing zero coefficients from F if there are any.  G.EPSLEVEL
+%   is set to the maximum of F.EPSLEVEL and the default CHEBTECH EPS.
 %
 %   If F is not happy, F is returned unchanged.
 %
-%   G = SIMPLIFY(F, TOL) does the same as above but uses TOL as the threshold
-%   level for deciding whether a coefficient is small enough to be zeroed.
-%   Here, G.EPSLEVEL is set to the maximum of F.EPSLEVEL and TOL.
+%   G = SIMPLIFY(F, TOL) does the same as above but uses TOL instead of the
+%   default CHEBTECH EPS preference as the relative threshold level for
+%   deciding whether a coefficient is small enough to be zeroed.  Here,
+%   G.EPSLEVEL is set to the maximum of F.EPSLEVEL and TOL.
 %
 % See also HAPPINESSCHECK.
 
@@ -34,16 +37,24 @@ if ( nargin < 2 )
     tol = pref.chebtech.eps;
 end
 
-% Zero all coefficients smaller than the tolerance:
-f.coeffs(abs(f.coeffs) < tol) = 0;
+% Zero all coefficients smaller than the tolerance relative to F.VSCALE:
+f.coeffs(bsxfun(@minus, abs(f.coeffs), tol*f.vscale) < 0) = 0;
 
-% Remove zero coefficients from the tail:
-[ignored, numCoeffsToRemove] = find(f.coeffs.' ~= 0, 1);
-if ( numCoeffsToRemove > 1 )
-    f = prolong(f, length(f) - numCoeffsToRemove + 1);
+% Check for trailing zero coefficients:
+[ignored, firstNonZeroRow] = find(f.coeffs.' ~= 0, 1);
+
+% If the whole thing's now zero, leave just one coefficient:
+if ( isempty(firstNonZeroRow) )
+    firstNonZeroRow = length(f);
 end
 
-% Update epslevel:
+% Remove trailing zeros:
+if ( firstNonZeroRow > 0 )
+    f.coeffs = f.coeffs(firstNonZeroRow:end, :);
+end
+
+% Update values and epslevel:
+f.values = f.chebpolyval(f.coeffs);
 f.epslevel = max(f.epslevel, tol);
 
 end
