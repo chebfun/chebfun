844c29b8148378238091fa9db93dd93c0a4acf48
nickhale <nick.p.hale@gmail.com>
2013-06-13 11:20:45 +0100

    Some minor changes.

--- @chebtech/simplify.m
+++ @chebtech/simplify.m
@@ -1,20 +1,20 @@
 function f = simplify(f, tol)
-%SIMPLIFY   Zero small Chebyshev coefficients of a happy CHEBTECH object.
-%   G = SIMPLIFY(F) attempts to compute a 'simplified' version G of the happy
-%   CHEBTECH object F such that LENGTH(G) <= LENGTH(F) but ||G - F|| is small
-%   in a relative sense:  ||G - F|| < G.EPSLEVEL*G.VSCALE.  It does this by
-%   zeroing out all coefficients of F that are relatively small; more
-%   precisely, it sets to zero all coefficients smaller in magnitude than the
-%   product of F.VSCALE and the default CHEBTECH EPS preference.  It then
-%   removes all trailing zero coefficients from F if there are any.  G.EPSLEVEL
-%   is set to the maximum of F.EPSLEVEL and the default CHEBTECH EPS.
+%SIMPLIFY  Zero small Chebyshev coefficients of a happy CHEBTECH object.
+%  G = SIMPLIFY(F) attempts to compute a 'simplified' version G of the happy
+%  CHEBTECH object F such that LENGTH(G) <= LENGTH(F) but ||G - F|| is small in
+%  a relative sense: ||G - F|| < G.EPSLEVEL*G.VSCALE. It does this by zeroing
+%  out all coefficients of F that are relatively small; more precisely, it sets
+%  to zero all coefficients smaller in magnitude than the product of F.VSCALE
+%  and the default CHEBTECH EPS preference. It then removes all trailing zero
+%  coefficients from F if there are any. G.EPSLEVEL is set to the maximum of
+%  F.EPSLEVEL and the default CHEBTECH EPS.
 %
-%   If F is not happy, F is returned unchanged.
+%  If F is not happy, F is returned unchanged.
 %
-%   G = SIMPLIFY(F, TOL) does the same as above but uses TOL instead of the
-%   default CHEBTECH EPS preference as the relative threshold level for
-%   deciding whether a coefficient is small enough to be zeroed.  Here,
-%   G.EPSLEVEL is set to the maximum of F.EPSLEVEL and TOL.
+%  G = SIMPLIFY(F, TOL) does the same as above but uses TOL instead of the
+%  default CHEBTECH EPS preference as the relative threshold level for deciding
+%  whether a coefficient is small enough to be zeroed. Here, G.EPSLEVEL is set
+%  to the maximum of F.EPSLEVEL and TOL.
 %
 % See also HAPPINESSCHECK.
 
@@ -26,7 +26,7 @@ if ( isempty(f) )
     return
 end
 
-% Do nothing to an unhappy CHEBTECH.  ([TODO]:  Is this the right thing to do?)
+% Do nothing to an unhappy CHEBTECH. ([TODO]: Is this the right thing to do?)
 if ( ~f.ishappy )
     return;
 end
@@ -55,6 +55,7 @@ end
 
 % Update values and epslevel:
 f.values = f.chebpolyval(f.coeffs);
+f.vscale = max(abs(f.values));
 f.epslevel = max(f.epslevel, tol);
 
 end
