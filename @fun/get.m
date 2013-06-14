7a6a92306f40ee07fb7e1714207727b71632bee5
nickhale <nick.p.hale@gmail.com>
2013-06-14 17:24:28 +0100

    Updating the GET() ethod for FUN.

--- @fun/get.m
+++ @fun/get.m
@@ -1,29 +1,34 @@
 function out = get(f, prop)
-%GET    GET method for the FUN class
+%GET   GET method for the BNDFUN class.
 %   P = GET(F,PROP) returns the property P specified in the string PROP from
 %   the fun F. Valid entries for the string PROP are:
-%       'DOMAIN'
-%       'MAPPING'
-%       'ONEFUN'
+%       'DOMAIN' - The domain of F.
+%       'MAPPING' - the map used by F.
+%       'ONEFUN' - The unmapped representation of F.
 %       'VSCALE' - Vertical scale of F.
-%       'ISHAPPY' - Is F happy?
 %       'EPSLEVEL' - Happiness level of F.
-%       'POINTS' - Grid corresponding to F.
-%       'LVAL'
-%       'RVAL'
+%       'LVAL' - Value of F at -1.
+%       'RVAL' - Value of F at +1.
 
 % Copyright 2013 by The University of Oxford and The Chebfun Developers. 
 % See http://www.chebfun.org/ for Chebfun information.
 
 switch prop
     case fieldnames(f)
-        % Allow access to any of F's properties via GET.
+        % Allow access to any of the properties of F via GET:
         out = f.(prop);
-    case 'points'
-         out = f.mapping.for(f.onefun.(prop));
-    case {'lval', 'rval', 'values', 'coeffs', 'ishappy', 'hscale', 'vscale', 'epslevel'}
-        out = get(f.onefun, prop);        
+    
+    case fieldnames(f.onefun)
+        % Allow access to any of the properties of F>onefun via GET:
+        out = f.onefun.(prop);
+
+    case {'lval', 'rval'}
+        % The value at -1:
+        out = get(f.onefun, prop);
+
     otherwise
-        error('CHEBFUN:FUN:GET:proname', ...
-            'Unknown property name ''%s'' for object of type fun.', prop);
+        error('CHEBFUN:ONEFUN:GET:proname', ...
+            'Unknown property name ''%s'' for object of type ONEFUN.', prop);
+end
+
 end
