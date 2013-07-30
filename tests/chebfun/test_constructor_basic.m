caa735568afdb592af96aefa21de3f986d9d4bf3
Anthony P. Austin <austin@maths.ox.ac.uk>
2013-07-30 14:11:53 +0100

    Modify basic chebfun constructor test to work with new branch structure.

--- tests/chebfun/test_constructor_basic.m
+++ tests/chebfun/test_constructor_basic.m
@@ -15,37 +15,40 @@ for j = 1:numel(FF);
     F = FF{j};
 
     % Test on [-1 1]:
+    % TODO:  Remove call to cell2mat() once we've figured out the vscale issue.
     f = chebfun(F, [-1, 1], pref);
     xx = linspace(-1, 1);
-    err = norm(f(xx) - F(xx), inf);
-    pass(j, k+1) = err < 10*max(f.epslevel.*f.vscale);
+    err = norm(feval(f, xx) - F(xx), inf);
+    pass(j, k+1) = err < 10*max(get(f, 'epslevel').*cell2mat(get(f, 'vscale')));
     pass(j, k+2) = err < 50*pref.chebfun.eps;
     k = k + 2;
 
     % Test on [-1 1] (no domain passed):
+    % TODO:  Remove call to cell2mat() once we've figured out the vscale issue.
     f = chebfun(F, pref);
     xx = linspace(-1, 1);
-    err = norm(f(xx) - F(xx), inf);
-    pass(j, k+1) = err < 10*max(f.epslevel.*f.vscale);
+    err = norm(feval(f, xx) - F(xx), inf);
+    pass(j, k+1) = err < 10*max(get(f, 'epslevel').*cell2mat(get(f, 'vscale')));
     pass(j, k+2) = err < 500*pref.chebfun.eps;
     k = k + 2;
 
     % Test on [0 10000]:
+    % TODO:  Remove call to cell2mat() once we've figured out the vscale issue.
     f = chebfun(F, [0, 10000], pref);
     xx = linspace(0, 10000);
-    err = norm(f(xx) - F(xx), inf);
-    pass(j, k+1) = err < 100*max(f.epslevel.*f.vscale);
+    err = norm(feval(f, xx) - F(xx), inf);
+    pass(j, k+1) = err < 100*max(get(f, 'epslevel').*cell2mat(get(f, 'vscale')));
     pass(j, k+2) = err < 100*hscale(f)*pref.chebfun.eps;
     k = k + 2;
 
     % Test on piecewise domain:
+    % TODO:  Remove call to cell2mat() once we've figured out the vscale issue.
     f = chebfun(F, [-1, 0, .5, sqrt(pi/4), 1], pref);
     xx = linspace(-1, 1);
-    err = norm(f(xx) - F(xx), inf);
-    pass(j, k+1) = err < 10*max(f.epslevel)*max(f.vscale);
+    err = norm(feval(f, xx) - F(xx), inf);
+    pass(j, k+1) = err < 10*max(max(get(f, 'epslevel'))*max(cell2mat(get(f, 'vscale'))));
     pass(j, k+2) = err < 100*pref.chebfun.eps;
     k = k + 2;
 end
-
 
 end
