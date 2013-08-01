2d3803a0eb78d243524733206083bbf1cd5912c4
nickhale <nick.p.hale@gmail.com>
2013-08-01 12:36:40 +0100

    Documenting @CHEBFUN/PREF().

--- @chebfun/pref.m
+++ @chebfun/pref.m
@@ -1,7 +1,71 @@
 function prefs = pref(varargin)
+%PREF   Preference settings for CHEBFUN.
+%   CHEBFUN.PREF(PREFNAME) returns the value corresponding to the preference
+%   named in the string PREFNAME.
+%
+%   P = CHEBFUN.PREF returns a structure P with a field P.FUN which contains the
+%   default FUN preferences as fields/values. This structure may be passed to
+%   the CHEBFUN constructor.
+%
+%   P = CHEBFUN.PREF(P) will check to see whether the input preference structure
+%   P already has a FUN field. If it does not, one is appended.
+%
+%   P = FUN.CHEBFUN('PREFNAME1', VAL1, 'PREFNAME2', VAL2, ...) returns the same
+%   structure as above, but with the default FUN preferences 'PREFNAME1',
+%   'PREFNAME2', replaced by the values in VAL1, VAL2, etc.
+%
+%   P = CHEBFUN.PREF(P, 'PREFNAME1', VAL1, 'PREFNAME2', VAL2, ...) appends a
+%   CHEBFUN preference field to P if required, and modifies the FUN properties
+%   'PREFNAME1' and 'PREFNAME2'.
+%
+%   Note that no checking of either the input PREFNAMEs or VALs takes place.
+%
+%   CHEBFUN PREFERENCES (case sensitive)
+%
+%    domain
+%      [-1,1]      - Default domain for FUN construction.
+%
+%     eps          -  Relative tolerance used in construction and subsequent
+%      [2^-52]        operations.
+%
+%    extrapolate   - Extrapolation at endpoints.
+%     [0, 'off']     If 'on', function values at endpoints maybe extrapolated
+%      1, 'on'       from interior values rather than sampled. Extrapolation is
+%                    used independently of this option if a function evaluation
+%                    returns NaN. In some cases, however, functions values at
+%                    end points maybe inaccurate or undefined, and enabling
+%                    extrapolation maybe helpful.
+%
+%    minsamples    - Minimum number of points used by the constructor. The 
+%       [9]          constructed CHEBFUN might be shorter as a result of
+%                    the SIMPLFY() command. Must be of the form 2^n+1.
+%
+%    maxdegree     - Maximum degree used by constructor in SPLITTING OFF mode.
+%      [2^16]
+%
+%    splitting     - Domain splitting option.
+%     [0, 'off']     If true, breakpoints between funs may be introduced where a
+%      1, 'on        discontinuity in a function or a low-order derivative is
+%                    detected, or if a global polynomial representation will be
+%                    too long. If false, breakpoints will be introduced only at
+%                    points where discontinuities are being created, e.g., by
+%                    ABS(F) at points where a CHEBFUN F passes through zero.
+%
+%    splitdegree   - Maximum degree used by constructor in SPLITTING "ON" mode.
+%       [128] 
+%
+%     maxlength    - Maximum number of points used in SPLITTING "ON" mode.
+%       [6000]
+%
+% See also CHEBFUN.
 
-% Copyright 2013 by The University of Oxford and The Chebfun Developers.
+% Copyright 2013 by The University of Oxford and The Chebfun Developers. 
 % See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
+
+% blowup - Blowup option:
+%        BLOWUP=0: bounded functions only
+%        BLOWUP=1: poles are permitted (integer order)
+%        BLOWUP=2: blowup of integer or non-integer orders permitted (experimental)
 
 classname = 'chebfun';
 
@@ -21,7 +85,7 @@ if ( isfield(prefs, classname) )  % It does, so either:
         p = prefs.(classname);    % b) Grab funcheb2 prefs
     end
 else                              % No funcheb2 prefs found, so make some:
-    p.blowup      = false;
+    %p.blowup      = false;
     p.eps         = 2^-52;
     p.extrapolate = true;
     p.domain      = [-1, 1];
@@ -29,7 +93,6 @@ else                              % No funcheb2 prefs found, so make some:
     p.maxdegree   = 65536;
     p.maxlength   = 6000;
     p.splitdegree = 129;
-    p.stats       = false;
 end
 % p is now the preference substructure relating to the FUN class.
 
