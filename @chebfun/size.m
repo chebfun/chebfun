242e134e324ddfee6d6919b29f1b9a788b6ba1f1
nickhale <nick.p.hale@gmail.com>
2013-08-01 12:03:49 +0100

    Adding a SIZE() method for @CHEBFUN.

---
+++ @chebfun/size.m
@@ -0,0 +1,50 @@
+function varargout = size(f, k)
+%SIZE   Size of a CHEBFUN.
+%   [S1, S2] = SIZE(F) returns S1, the number of piecewise smooth components of
+%   F, and S2, the number of columns in F. If S2 > 1, we say F is
+%   "array-valued".
+%
+%   S = SIZE(F) returns the same as above in a 1x2 vector, S = [S1, S2].
+%
+% See also LENGTH.
+
+% Copyright 2013 by The University of Oxford and The Chebfun Developers. 
+% See http://www.chebfun.org/ for Chebfun information.
+
+% Number of funs:
+numFuns = numel(f.funs);
+% Number of columns:
+numCols = size(f.funs{1}, 2);
+
+% Switch if F is transposed:
+if ( f.isTransposed )
+    tmp = numFuns;
+    numFuns = numCols;
+    numCols = tmp;
+end
+
+% Parse the output for consistency with MATLAB
+if ( nargout < 2 )
+    if ( nargin == 1 )
+        % Output a vector:
+        varargout{1} = [numFuns, numCols];
+    elseif ( k == 1 )
+        % Output first entry:
+        varargout{1} = numFuns;
+    elseif ( k == 2 )
+        % Output second entry:
+        varargout{1} = numCols;
+    elseif ( logical(round(k) - k) )
+        error('CHEBFUN:size:dim', ...
+            'Dimension argument must be a positive integer scalar within indexing range.');
+    else
+        % We do not allow tensors, so any further dimensions must have size 1.
+        varargout{1} = 1;
+    end
+else
+    % Two outputs:
+    varargout{1} = numFuns;
+    varargout{2} = numCols;
+end
+
+end
