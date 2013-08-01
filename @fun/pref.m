658d131c47a3e05a72253ff361647afe53a17f32
nickhale <nick.p.hale@gmail.com>
2013-08-01 09:34:13 +0100

    Fix to make FUN/PREF() pulll given preferences into the .misc field.

--- @fun/pref.m
+++ @fun/pref.m
@@ -1,33 +1,72 @@
 function prefs = pref(varargin)
-%PREF   Preference settings for FUN.
-%   FUN.PREF(PREFNAME) returns the value corresponding to the preference named
-%   in the string PREFNAME.
+%PREF   Preference settings for CHEBTECH.
+%   CHEBTECH.PREF(PREFNAME) returns the value corresponding to the preference
+%   named in the string PREFNAME.
 %
-%   P = FUN.PREF returns a structure P with a field P.FUN which contains the
-%   default FUN preferences as fields/values. This structure may be passed to
-%   the FUN constructor.
+%   P = CHEBTECH.PREF returns a structure P with a field P.CHEBTECH which contains
+%   the default CHEBTECH preferences as fields/values. This structure may be
+%   passed to the CHEBTECH constructor.
 %
-%   P = FUN.PREF(P) will check to see whether the input preference structure P
-%   already has a FUN field. If it does not, one is appended.
+%   P = CHEBTECH.PREF(P) will check to see whether the input preference structure
+%   P already has a CHEBTECH field. If it does not, one is appended.
 %
-%   P = FUN.PREF('PREFNAME1', VAL1, 'PREFNAME2', VAL2, ...) returns the same
-%   structure as above, but with the default FUN preferences 'PREFNAME1',
+%   P = CHEBTECH.PREF('PREFNAME1', VAL1, 'PREFNAME2', VAL2, ...) returns the same
+%   structure as above, but with the default CHEBTECH preferences 'PREFNAME1',
 %   'PREFNAME2', replaced by the values in VAL1, VAL2, etc.
 %
-%   P = FUN.PREF(P, 'PREFNAME1', VAL1, 'PREFNAME2', VAL2, ...) appends a FUN
-%   preference field to P if required, and modifies the FUN properties
-%   'PREFNAME1' and 'PREFNAME2'.
+%   P = CHEBTECH.PREF(P, 'PREFNAME1', VAL1, 'PREFNAME2', VAL2, ...) appends a
+%   CHEBTECH preference field to P if required, and modifies the CHEBTECH
+%   properties 'PREFNAME1' and 'PREFNAME2'.
 %
 %   Note that no checking of either the input PREFNAMEs or VALs takes place.
 %
-%   FUN PREFERENCES (case sensitive)
+%   CHEBTECH PREFERENCES (case sensitive)
 %
-%    domain
-%      [-1,1]      - Default domain for FUN construction.
+%     tech         -  Select the type of Chebyshev grid on which the function
+%                     is sampled.
+%      ['cheb2']   -  Use a grid of second-kind Chebyshev points.
+%       'cheb1'    -  Use a grid of first-kind Chebyshev points.
 %
 %     eps          -  Relative tolerance used in construction and subsequent
-%      [2^-52]        operations.
-
+%      [2^-52]        operations. See CHEBTECH.HAPPINESSCHECK for more details.
+%
+%     extrapolate
+%        true      -  Function values at endpoints may be extrapolated from
+%                     interior values rather than sampled.
+%       [false]    -  Do not extrapolate values at endpoints.
+%
+%     hscale       -  Horizontal scale. This preference can be used to ensure
+%        [1]          horizontal scale invariance when using the CHEBTECH
+%                     constructor to implicitly represent functions defined on
+%                     domains other than [-1, 1].
+%
+%     minSamples   -  Minimum number of points used by the constructor. Should
+%        [9]          be of the form 2^n+1 (if not, it is rounded as such).
+%
+%     maxSamples   -  Maximum number of points used by the constructor.
+%      [2^16+1]   
+%
+%         n        -  Fixed number of points used by constructor. NaN allows
+%       [NaN]         adaptive construction.
+%
+%     sampletest
+%       [true]     -  Tests the function at one more arbitrary point to
+%                     minimize the risk of missing signals between grid
+%                     points.
+%        false     -  Do not test.
+%
+%   refinementFunction - Define function for refining sample values.
+%     ['nested']       - Use the default process (nested evaluation).
+%      'resampling'    - Every function value is computed afresh as the
+%                        constructor tries grids of size 9, 17, 33, etc.
+%   function_handle    - A user-defined refinement. See REFINE.m
+%
+%   happinessCheck     - Define function for testing happiness.
+%     ['classic']      - Use the default process from Chebfun V4.
+%      'strict'        - Strict tolerance for coefficients.
+%      'loose'         - A looser tolerance for coefficients.
+%     function_handle  - A user defined happiness. See HAPPINESSCHECK.m
+%
 % See also CHEBTECH, CHEBTECH1, CHEBTECH2
 
 % Copyright 2013 by The University of Oxford and The Chebfun Developers. 
@@ -51,8 +90,8 @@ if ( isfield(prefs, classname) )  % It does, so either:
         p = prefs.(classname);    % b) Grab prefs for this class.
     end
 else                              % No prefs found for this class, so make some:
-    p.domain = [-1, 1];
-    p.eps    = 2^-52;
+    p.domain      = [-1, 1];
+    p.eps         = 2^-52;
 end
 % p is now the preference substructure relating to the current class.
 
@@ -63,8 +102,8 @@ else
 end
 % q is now the preference substructure for MISC preferences.
 
-for names = fieldnames(q)'
-    if isfield(p,names)
+for ( names = fieldnames(q)' )
+    if ( isfield(p,names) )
         field = names{:};
         p.(field) = q.(field);
         q = rmfield(q,field);
@@ -76,9 +115,14 @@ if ( numel(varargin) > 0 && isstruct(varargin{1}) ) % Yes
     r = varargin{1};
     varargin(1) = [];
     for names = fieldnames(r).'
-        if isfield(p,names)
+        if ( isfield(p,names ))
+            % Push field name to p.
             field = names{:};
             p.(field) = r.(field);
+        else
+            % Push field name to misc.
+            field = names{:};
+            q.(field) = r.(field);
         end
     end
 end
