8221dfaf1d945a76011d584966abc2f2bab2bade
Anthony P. Austin <austin@maths.ox.ac.uk>
2013-08-05 11:42:27 +0100

    Fixes for chebfun.overlap() test file.
    Use feval() for function evaluation instead of subsref() and remove
    unnecessary calls to max().

--- tests/chebfun/test_overlap.m
+++ tests/chebfun/test_overlap.m
@@ -10,8 +10,7 @@ g = chebfun(@sin, [-1 0 0.5 1], pref);
 
 [f2, g2] = overlap(f, g);
 xx = linspace(-1, 1);
-pass(1)  = norm(f(xx) - f2(xx), inf) < 10*max(f.epslevel.*f.vscale);
-pass(2)  = norm(g(xx) - g2(xx), inf) < 10*max(g.epslevel.*g.vscale);
-
+pass(1)  = norm(feval(f, xx) - feval(f2, xx), inf) < 10*f.epslevel.*f.vscale;
+pass(2)  = norm(feval(g, xx) - feval(g2, xx), inf) < 10*g.epslevel.*g.vscale;
 
 end
