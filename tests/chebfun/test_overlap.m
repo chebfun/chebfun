c8b63d96ba66aee43f04cb5d746722efbaab4d92
Anthony P. Austin <austin@maths.ox.ac.uk>
2013-08-06 12:49:50 +0100

    Improve test for chebfun.overlap().
    Check a few additional cases, including what happens for higher-order impulses.

--- tests/chebfun/test_overlap.m
+++ tests/chebfun/test_overlap.m
@@ -1,3 +1,5 @@
+% Test file for @chebfun/overlap.m.
+
 function pass = test_overlap(pref)
 
 % Grab some preferences:
@@ -5,12 +7,49 @@ if ( nargin == 0 )
     pref = chebfun.pref();
 end
 
+% Test empty input.
+f = chebfun();
+g = chebfun();
+[f2, g2] = overlap(f, g);
+pass(1) = isempty(f) && isempty(g);
+
+% Check behavior for input chebfuns with differing domains.
+f = chebfun(@sin, [-1 -.5 0 1], pref);
+g = chebfun(@sin, [-2 0 0.5 2], pref);
+
+try
+    [f2, g2] = overlap(f, g)
+    pass(2) = false;
+catch ME
+    pass(2) = strcmp(ME.identifier, 'CHEBFUN:overlap:domains');
+end
+
+% Check behavior in the basic case.
 f = chebfun(@sin, [-1 -.5 0 1], pref);
 g = chebfun(@sin, [-1 0 0.5 1], pref);
 
 [f2, g2] = overlap(f, g);
 xx = linspace(-1, 1);
-pass(1)  = norm(feval(f, xx) - feval(f2, xx), inf) < 10*f.epslevel.*f.vscale;
-pass(2)  = norm(feval(g, xx) - feval(g2, xx), inf) < 10*g.epslevel.*g.vscale;
+pass(3) = isequal(f2.domain, g2.domain) && ...
+    norm(feval(f, xx) - feval(f2, xx), inf) < 10*f.epslevel.*f.vscale && ...
+    norm(feval(g, xx) - feval(g2, xx), inf) < 10*g.epslevel.*g.vscale && ...
+    isequal(size(f2.impulses, 3), size(g2.impulses, 3));
+
+% Check correct behavior for higher-order impulses.  [TODO]:  Use a function
+% with real higher-order impulses instead of just faking them like this.
+f.impulses = cat(3, f.impulses, zeros(4, 1, 1));
+[f2, g2] = overlap(f, g);
+xx = linspace(-1, 1);
+pass(4) = isequal(f2.domain, g2.domain) && ...
+    norm(feval(f, xx) - feval(f2, xx), inf) < 10*f.epslevel.*f.vscale && ...
+    norm(feval(g, xx) - feval(g2, xx), inf) < 10*g.epslevel.*g.vscale && ...
+    isequal(size(f2.impulses, 3), size(g2.impulses, 3));
+
+[g2, f2] = overlap(g, f);
+xx = linspace(-1, 1);
+pass(5) = isequal(f2.domain, g2.domain) && ...
+    norm(feval(f, xx) - feval(f2, xx), inf) < 10*f.epslevel.*f.vscale && ...
+    norm(feval(g, xx) - feval(g2, xx), inf) < 10*g.epslevel.*g.vscale && ...
+    isequal(size(f2.impulses, 3), size(g2.impulses, 3));
 
 end
