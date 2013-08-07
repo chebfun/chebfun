b0c3dbaa4adf27db2e9444704686614190aa3f9b
nickhale <nick.p.hale@gmail.com>
2013-08-07 11:46:19 +0100

    Commenting LOG() part of this test, for speed.

--- tests/chebfun/test_constructor_splitting.m
+++ tests/chebfun/test_constructor_splitting.m
@@ -30,33 +30,33 @@ xx3 = linspace(f3.domain(1)+eps, f3.domain(end)-eps, 100);
 pass(3) = norm(feval(f3, xx3) - feval(F3, xx3), inf) < ...
     tol*max(f3.epslevel.*f3.vscale);
 
-% Test X*LOG(X) on [0 1]:
-F4 = @(x) x.*log(x);
-f4 = chebfun(F4, [0, 1], pref, 'splitting', 'on', 'blowup', 'off');
-xx4 = linspace(f4.domain(1)+eps, f4.domain(end)-eps, 100);
-pass(4) = norm(feval(f4, xx4) - feval(F4, xx4), inf) < ...
-    tol*max(f4.epslevel.*f4.vscale);
-
-% Test (-X)*LOG(-X) on [-1 0]:
-F5 = @(x) (-x).*log(-x);
-f5 = chebfun(F5, [-1, 0], pref, 'splitting', 'on', 'blowup', 'off');
-xx5 = linspace(f5.domain(1)+eps, f5.domain(end)-eps, 100);
-pass(5) = norm(feval(f5, xx5) - feval(F5, xx5), inf) < ...
-    tol*max(f5.epslevel.*f5.vscale);
-
-% Test (1-X)*LOG(1-X) on [0 1]:
-F6 = @(x) (1-x).*log(1-x);
-f6 = chebfun(F6, [0, 1], pref, 'splitting', 'on', 'blowup', 'off');
-xx6 = linspace(f6.domain(1)+eps, f6.domain(end)-eps, 100);
-pass(6) = norm(feval(f6, xx6) - feval(F6, xx6), inf) < ...
-    tol*max(f6.epslevel.*f6.vscale);
-pass(6) = pass(6) && numel(f6.funs) < 50;
-
-% Test (1-X^2)*LOG(1-X^2) on [-1 1]:
-F7 = @(x) (1-x.^2).*log(1-x.^2);
-f7 = chebfun(F7, [-1, 1], pref, 'splitting', 'on', 'blowup', 'off');
-xx7 = linspace(f7.domain(1)+eps, f7.domain(end)-eps, 100);
-pass(7) = norm(feval(f7, xx7) - feval(F7, xx7), inf) < ...
-    tol*max(f7.epslevel.*f7.vscale);
+% % Test X*LOG(X) on [0 1]:
+% F4 = @(x) x.*log(x);
+% f4 = chebfun(F4, [0, 1], pref, 'splitting', 'on', 'blowup', 'off');
+% xx4 = linspace(f4.domain(1)+eps, f4.domain(end)-eps, 100);
+% pass(4) = norm(feval(f4, xx4) - feval(F4, xx4), inf) < ...
+%     tol*max(f4.epslevel.*f4.vscale);
+% 
+% % Test (-X)*LOG(-X) on [-1 0]:
+% F5 = @(x) (-x).*log(-x);
+% f5 = chebfun(F5, [-1, 0], pref, 'splitting', 'on', 'blowup', 'off');
+% xx5 = linspace(f5.domain(1)+eps, f5.domain(end)-eps, 100);
+% pass(5) = norm(feval(f5, xx5) - feval(F5, xx5), inf) < ...
+%     tol*max(f5.epslevel.*f5.vscale);
+% 
+% % Test (1-X)*LOG(1-X) on [0 1]:
+% F6 = @(x) (1-x).*log(1-x);
+% f6 = chebfun(F6, [0, 1], pref, 'splitting', 'on', 'blowup', 'off');
+% xx6 = linspace(f6.domain(1)+eps, f6.domain(end)-eps, 100);
+% pass(6) = norm(feval(f6, xx6) - feval(F6, xx6), inf) < ...
+%     tol*max(f6.epslevel.*f6.vscale);
+% pass(6) = pass(6) && numel(f6.funs) < 50;
+% 
+% % Test (1-X^2)*LOG(1-X^2) on [-1 1]:
+% F7 = @(x) (1-x.^2).*log(1-x.^2);
+% f7 = chebfun(F7, [-1, 1], pref, 'splitting', 'on', 'blowup', 'off');
+% xx7 = linspace(f7.domain(1)+eps, f7.domain(end)-eps, 100);
+% pass(7) = norm(feval(f7, xx7) - feval(F7, xx7), inf) < ...
+%     tol*max(f7.epslevel.*f7.vscale);
 
 end
