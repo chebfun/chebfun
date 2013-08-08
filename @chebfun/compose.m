2d7e6b74e2f03425c40235101364bba72b119d20
nickhale <nick.p.hale@gmail.com>
2013-08-08 13:38:27 +0100

    Updating @CHEBFUN/COMPOSE() and @CHEBFUN/COMPOSECHEBFUNS().
    COMPOSECHEBFUNS() is incomplete as it requires @CHEBFUN/MINUS()
    and @CHEBFUN/MINANDMAX().
    
    It may also be worth placing COMPOSECHEBFUNS() in the COMPOSE.M
    M-file as it is not needed externally.

--- @chebfun/compose.m
+++ @chebfun/compose.m
@@ -1,23 +1,24 @@
 function f = compose(f, op, g, pref)
 %COMPOSE  Compostition of CHEBFUN objects.
-%   COMPOSE(F, OP) returns a CHEBFUN representing OP(F) where F is also a
-%   CHEBFUN object, and OP is a function handle.
+%   COMPOSE(F, OP) returns a CHEBFUN representing OP(F), where F is also a
+%   CHEBFUN object and OP is a function handle.
 %
-%   COMPOSE(F, OP, G) returns OP(F, G) where F and G are CHEBFUN objects, and OP
-%   is a function handle.
+%   COMPOSE(F, OP, G) returns OP(F, G), where F and G are CHEBFUN objects and OP
+%   is a function handle. The domains of F and G should be compatible.
 %
 %   COMPOSE(F, G) returns a CHEBFUN representing G(F), where both F and G are
 %   also CHEBFUN objects. If the range of F is not in [-1, 1] then an error is
 %   thrown.
 %
-%   COMPOSE(F, OP, PREF), COMPOSE(F, OP, G, PREF), or COMPOSE(F, G, PREF) uses
+%   COMPOSE(F, OP, PREF), COMPOSE(F, OP, G, PREF), and COMPOSE(F, G, PREF) use
 %   the options passed by the prefences structure PREF.
 %
-%   Note: If the location of required breakpoints in the output are known in
-%   advance, they should be applied to F and/or G using RESTRICT before the call
-%   to COMPOSE().
+%   Note 1: If the location of required breakpoints in the output are known in
+%   advance, they should be applied to F and/or G using RESTRICT() before the
+%   call to COMPOSE().
 %
-% See also RESTRICT.
+%   Note 2: Any higher-order impulse/delta function data in F and/or G is
+%   ignored by COMPOSE().
 
 % Copyright 2013 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org for Chebfun information.
@@ -35,51 +36,74 @@ function f = compose(f, op, g, pref)
 %                 |   pass/        \fail         /     |
 %                 --[next piece]    \--->[warning]-----^
 %
+% It is not expected that the user calls COMPOSE() directly. It is usually
+% accessed via other @CHEBFUN methods which use it for their implementation
+% (such a SIN()).
+%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% [TODO]: vscale?
+% [TODO]: vscale and tolerance?
 
-% Parse inputs:
+%% Parse inputs:
 numChebfuns = 2;
-if ( (nargin > 2) && isstruct(g) )
+if ( (nargin == 3) && isstruct(g) ) % compose(f, op, pref)
     pref = chebfun.pref(g);
     g = [];
     numChebfuns = 1;
-elseif ( (nargin < 4) )
+elseif ( (nargin < 4) )             % compose(f, op, g)
     pref = chebfun.pref();
 end
-if ( (nargin < 3) || isempty(g) )
+if ( (nargin < 3) || isempty(g) )   % compose(f, op) or compose(f, op, [], pref)
     numChebfuns = 1;
     g = [];
 end
+
+%% Special cases:
 
 % There is nothing to do for an empty chebfun!
 if ( isempty(f) )
     return
 end
 
-% Initialise impulses: (and overlap if there are two chebfuns)
-fDom = f.domain(1);
+% % Call the COMPOSECHEBFUNS method if OP is a CHEBFUN object:
+% if ( isa(op, 'chebfun') )
+%     f = composeChebfuns(f, g, pref);
+%     return
+% end
+
+%% Initialise:
+
+% Initialise impulses. Note that higher-order impulse data is destroyed by
+% compose, so we only require the first impulse 'sheet' (i.e., impulses(:,:,1)):
 if ( numChebfuns == 1 )
-    fImps = feval(op, f.impulses(1,1));
+    fevalImps = feval(op, f.impulses(:,:,1));
+    newImps = fevalImps(1,:);
 else
+    % Call OVERLAP() if there are two CHEBFUN inputs:
     [f, g] = overlap(f, g);
-    fImps = feval(op, f.impulses(1,1), g.impulses(1,1));
+    fevalImps = feval(op, f.impulses(:,:,1), g.impulses(:,:,1));
+    newImps = fevalImps(1,:);
 end
 
-% Number of piecewise smooth components:
-numFuns = numel(f.domain) - 1;
+% Number of piecewise intervals in f:
+numInts = numel(f.domain) - 1;
 
-% Initialise storage for output funs:
-fFuns = {};
+% Initialise storage for the output FUN cell:
+newFuns = {};
 
-% Merge preferences with fun.prefs:
+% Initialise new domain vector:
+newDom = f.domain(1);
+
+% Merge preferences with FUN.PREF();
 pref = fun.pref(pref, pref.chebfun);
 
-% Loop through the funs:
-for k = 1:numFuns
+% Suppress growing vector Mlint warnings (which are inevitable here):
+%#ok<*AGROW>
 
-    % Attempt to generate FUN objects using the FUN/COMPOSE().
+%% Loop through the each interval:
+for k = 1:numInts
+
+    % Attempt to generate FUN objects using FUN/COMPOSE().
     if ( isempty(g) )
         newFun = compose(f.funs{k}, op, [], pref);
     else
@@ -93,63 +117,65 @@ for k = 1:numFuns
         if ( ~isHappy )
             % Throw a warning if we're not happy:
             try
-                str = ['with function ' func2str(op)];
+                str = ['with function ', func2str(op)];
             catch ME %#ok<NASGU>
                 str = '';
             end
             warning('CHEBFUN:compose:resolve', ['Composition ', str, ...
-                ' not resolved using ' int2str(length(newFun)), ...
+                ' not resolved using ', int2str(length(newFun)), ...
                 ' points. Have you tried ''splitting on''?']);
         end
 
-        % Store new fun:
-        fFuns = [fFuns, {newFun}];                    %#ok<AGROW>
+        % Store new FUN in cell array:
+        newFuns = [newFuns, {newFun}];                   
         % Store new ends:
-        fDom = [fDom, f.domain(k+1)];                 %#ok<AGROW>
-        % Store new imps:
+        newDom = [newDom, f.domain(k+1)];
+        % Store new impulses: (Note, will only be a matrix - not a tensor)
         if ( isempty(g) )
-            fImps = [fImps, feval(op, f.impulses(1,k+1))]; %#ok<AGROW>
+            newImps = [newImps ; fevalImps(k+1,:)];
         else
-            fImps = [fImps, feval(op, f.impulses(1,k+1), g.impulses(1,k+1))]; %#ok<AGROW>
+            newImps = [newImps ; fevalImps(k+1,:)];
         end
 
     elseif ( pref.chebfun.splitting )
 
-        % If not happy and splitting is on, get a chebfun for that subinterval.
+        % If not happy and splitting is on, get a CHEBFUN for that subinterval:
         domk = f.domain(k:k+1);
         if ( numChebfuns == 1 )
-            newCfun = chebfun(@(x) feval(op, feval(f, x)), domk, pref);
+            newChebfun = chebfun(@(x) feval(op, feval(f, x)), domk, pref);
         else
-            newCfun = chebfun(@(x) feval(op, feval(f, x), feval(g, x)), ...
+            newChebfun = chebfun(@(x) feval(op, feval(f, x), feval(g, x)), ...
                 domk, pref);
         end
 
-        isHappy = get(newCfun, 'ishappy');
-        if ( ~isHappy )
-            % Throw a warning if we're not happy:
+        if ( ~get(newChebfun, 'ishappy') ) % Throw a warning if we're not happy:
             try
-                str = ['with function ' func2str(op)];
+                str = ['with function ', func2str(op)];
             catch ME %#ok<NASGU>
                 str = '';
             end
             warning('CHEBFUN:compose:resolve', ['Composition ', str, ...
-                ' not resolved using ' int2str(length(newFun)) ' points.']);
+                ' not resolved using ', int2str(length(newChebfun)), ...
+                ', points.']);
         end
 
-        % Store new funs:
-        fFuns = [fFuns, newCfun.funs];                %#ok<AGROW>
+        % Store new FUN objects:
+        newFuns = [newFuns, newChebfun.funs];
         % Store new ends:
-        fDom = [fDom, newCfun.domain(2:end)];     %#ok<AGROW>
-        % Store new imps:
-        fImps = [fImps, newCfun.impulses(1,2:end)];     %#ok<AGROW>
+        newDom = [newDom, newChebfun.domain(2:end)];
+        % Store new impulses; (Note, will only be a matrix - not a tensor)
+        newImps = [newImps ; newChebfun.impulses(2:end-1,:) ; ...
+            fevalImps(k+1,:) ];
 
     end
 
 end
 
-% Put the funs back into a chebfun:
-f.funs = fFuns;
-f.domain = fDom;
-f.impulses = fImps;
+%% Prepare output:
+
+% Put the FUN cell, domain, and impulses back into a CHEBFUN:
+f.funs = newFuns;
+f.domain = newDom;
+f.impulses = newImps;
 
 end
--- @chebfun/composeChebfuns.m
+++ @chebfun/composeChebfuns.m
@@ -1,31 +1,75 @@
 function h = composeChebfuns(f, g, pref)
-%COMPOSE chebfun composition
-%   COMPOSE(F,G) returns the composition of the chebfuns F and G, F(G). The
-%   range of G must be in the domain of F.
+%COMPOSECHEBFUNS   Chebfun composition.
+%   COMPOSECHEBFUNS(G, F) returns the composition of the CHEBFUN objects F and
+%   G, G(F). The range of F must be in the domain of Gor else an error is
+%   thrown. An equivalent syntax is G(F).
+%
+%   COMPOSECHEBFUN(F, G, PREF) uses the CHEBFUN preferences contained in the
+%   preference structure PREF.
+%
+%   Note 1: If the location of required breakpoints in the output are known in
+%   advance, they should be applied to F and/or G using RESTRICT() before the
+%   call to COMPOSE().
+%
+%   Note 2: Any higher-order impulse/delta function data in F and/or G is
+%   ignored by COMPOSECHEBFUNS().
 %
 %   Example:
-%           f = chebfun(@(x) 1./(1+100*x.^2));
-%           g = chebfun(@(x) asin(.99*x)/asin(.99));
-%           h = compose(f,g);
+%           f = chebfun(@(x) asin(.99*x)/asin(.99));
+%           g = chebfun(@(x) 1./(1+100*x.^2));
+%           h = compose(f, g);
+%
+% See also COMPOSE.
 
 % Copyright 2013 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org for Chebfun information.
 
+% Deal with the trivial empty case:
+if ( isempty(f) || isempty(g) )
+    h = chebfun();
+    return
+end
+
+% Obtain preferences:
 if ( nargin < 3 )
     pref = chebfun.pref();
 else
     pref = chebfun.pref(pref);
 end
 
-if ( ~isa(g,'chebfun') || ~isa(f, 'chebfun') )
-    warning('CHEBFUN:composeChebfuns:notachebfun', ...
-        'Inputs should be chebfun objects!');
+% If G is a function handle, we can simply call COMPOSE():
+if ( ~isa(g, 'chebfun') )
+    % Call COMPOSE():
     h = compose(f, g, pref);
     return
+elseif ( ~isa(f, 'chebfun') )
+    error('CHEBFUN:compose:NotAChebfun', ...
+        'Cannot compose a function handle and CHEBFUN in this way.');
 end
 
-% [TODO]
+% f must be a real-valued function:
+if ( ~isreal(f) )
+    error('CHEBFUN:compose:complex', 'F must be real valued to construct G(F).')
+%     warning('CHEBFUN:compose:complex', 'F SHOULD be real valued to construct G(F).');
+end
+
+% [TODO]: Requires MINANDMAX().
+% % Get epslevels and set a tolerance:
+% tol = 10*max(vscale(f).*epslevel(f), vscale(g).*epslevel(g));
+% hsf = hscale(f); 
+% % Find the range of F:
+% mmF = minandmax(f);
+% minF = min(mmF(:));
+% maxF = max(mmF(:));
+% % Range of f must be in the domain of g:
+% if ( g.domain(1) > minF + tol*hsf || g.domain(end) < maxF - tol*hsf )
+%     error('CHEBFUN:compose:domain', ...
+%         'Range of F, [%g, %g], must be in the domain of G, [%g, %g].', ...
+%         minF, maxF, g.domain(1), g.domain(end))
+% end
+
 % % Check to see if f or g is 'x';
+% % [TODO]: Is this worthwhile?
 % xg = chebfun(@(x) x, g.domain);
 % if ( norm(g - xg, 2) < get(g, 'epslevel') )
 %     h = f;
@@ -37,47 +81,31 @@ end
 %     return
 % end
 
-% Delta functions ?
-if ( size(f.impulses, 1) > 1 || size(g.impulses, 1) > 1 )
+% Delta functions:
+if ( size(f.impulses, 3) > 1 || size(g.impulses, 3) > 1 )
     warning('CHEBFUN:compose:imps',  ...
-        'Composition does not handle delta functions')
+        'Composition does not handle impulses / delta functions.')
 end
 
-% % g must be a real-valued function
-% if ~isreal(g)
-%     %     error('CHEBFUN:compose:complex', 'G must be real valued to construct F(G).')
-%     %     warning('CHEBFUN:compose:complex', 'G SHOULD be real valued to construct F(G).');
-%     % Experimental feature allows composition when G has a complex range.
-%     %   This is only really of any use when F is constructed from a
-%     %   polynomial otherwise approximation off the real line is awful.
-% end
-
-tol = 100*pref.chebfun.eps;
-
-% Range of g must be in the domain of f.
-mm = minandmax(g);
-if ( f.domain(1) > mm(1) + tol || f.domain(end) < mm(2) - tol && isreal(g) )
-    error('CHEBFUN:compose:domain', ...
-        'Range of G, [%g, %g], must be in the domain of F, [%g, %g].', ...
-        mm(1), mm(2), f.ends(1), f.ends(2))
-end
-
-% If f has breakpoints, find the corresponding x-points in the domain of g:
-bkpts = [];
-if ( numel(f.domain) > 2 )
-    bkf = f.domain(f.domain > mm(1) - tol & f.domain < mm(2) + tol);
-    for k = 1:length(bkf)
-        bkpts = [bkpts; roots(g - bkf(k))];
+% If g has breakpoints, find the corresponding x-points in the domain of f:
+newDom = f.domain;
+if ( numel(g.domain) > 2 )
+    gDom = g.domain(2:end-1);
+    for k = 1:length(gDom)
+        % [TODO]: This requires @CHEBFUN/MINUS.
+        r = roots(f - gDom(k));
+        newDom = [newDom, r(:).']; %#ok<AGROW>
     end
 end
-dom = union(g.domain, bkpts);
+newDom = unique(sort(newDom));
 
-% funs = {};
-for k = 1:length(dom)-1
-%     newfun = composeFuns(f.funs{k}, gg.funs{k},
-    gk = restrict(g, dom(k:k+1));
-    h = compose(gk, @(g) feval(f, g));
+% Restict f to the new domain:
+f = restrict(f, newDom);
+
+% Call compose:
+h = compose(f, @(f) feval(g, f));
+
+% Fix impulse values:
+h.impulses(:,:,1) = feval(g, feval(f, h.domain));
+
 end
-
-% Fix imps values
-h.impulses(1,:) = feval(f, feval(g, h.domain));
