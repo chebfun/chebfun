eb1c8989bb94e60f40899b521405cf778976e749
Anthony P. Austin <austin@maths.ox.ac.uk>
2013-08-13 15:21:39 +0100

    Implement chebfun.isfinite().
    Add a test for it, too.

--- @chebfun/isfinite.m
+++ @chebfun/isfinite.m
@@ -1,6 +1,24 @@
 function isf = isfinite(f)
+%ISFINITE   Test if a CHEBFUN is bounded.
+%   ISFINITE(F) returns FALSE if F has any infinite values and TRUE otherwise.
 
-% [TODO]: Implement this file!
+% Copyright 2013 by The University of Oxford and The Chebfun Developers.
+% See http://www.chebfun.org/ for Chebfun information.
+
+% Check for infinite breakpoint values or higher-order impulses.
+if ( any(any(~isfinite(f.impulses(:,:,1)))) || (size(f.impulses, 3) > 1) )
+    isf = false;
+    return
+end
+
+% Check for any infinite funs.
+for k = 1:numel(f.funs)
+    if ( ~isfinite(f.funs{k}) )
+        isf = false;
+        return
+    end
+end
+
 isf = true;
 
 end
