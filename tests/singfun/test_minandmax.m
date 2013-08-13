8e4b1f6e9a34ecc7b6609e1fb14773a257dc68bc
Kuan Xu <xukuan2011@gmail.com>
2013-08-13 20:29:23 +0100

    Finalizing test_minandmax.

--- tests/singfun/test_minandmax.m
+++ tests/singfun/test_minandmax.m
@@ -10,19 +10,14 @@ end
 % Set a tolerance.
 tol = 10*pref.singfun.eps;
 
-% Generate a few random points to use as test values.
-seedRNG(6178);
-d = 2;
-x = 2*(1-10^(-d)) * rand(100, 1) - (1-10^(-d));
-
 % The order of the exponents:
-a = 0.56;
-b = -0.56;
+a = 0.64;
+b = -0.64;
 c = 1.28;
 d = -1.28;
 
 % Pre-allocate pass matrix
-pass = zeros(1, 6);
+pass = zeros(1, 5);
 
 % fractional root at the left endpoint and the function value is bounded.
 f = singfun(@(x) (1+x).^a.*exp(x), [a 0], {'sing', 'none'}, pref);
@@ -36,37 +31,41 @@ pass(1) = (max([err_x err_y]) < tol*normest(f));
 % fractional pole at the left endpoint and the function value is unbounded.
 f = singfun(@(x) (1+x).^d.*sin(50*pi*x), [d+1 0], {'sing', 'none'}, pref);
 [y, x] = minandmax(f);
-y_exact = [-1 Inf];
+% The following exact answers are obtained using Mathematica.
+y_exact = [-92.48807414703726; Inf];
+x_exact = [-0.9717902218389038; -1];
 err_y = y(1) - y_exact(1);
-pass(2) = (abs(err_y) < tol*normest(f)) && (y(2) == Inf);
+err_x = x - x_exact; 
+pass(2) = (norm(err_x, inf) < tol*normest(f) && abs(err_y) < tol*normest(f)) && (y(2) == Inf);
 
 % fractional root at the right endpoint and the smooth part has no roots in [-1 1].
 f = singfun(@(x) (1-x).^c.*cos(x), [0 c], {'none', 'sing'}, pref);
-r = roots(f);
-r_exact = 1;
-err = r - r_exact;
-pass(3) = (norm(err, inf) < tol*normest(f));
+[y, x] = minandmax(f);
+% The exact maximum value and its location is found using Mathematica.
+y_exact = [0; 1.511345730595468];
+x_exact = [1; -0.6575681557708653];
+y_err = y - y_exact;
+x_err = x - x_exact;
+pass(3) = (norm(y_err, inf) < tol*normest(f) && norm(x_err, inf) < tol*normest(f));
 
 % no fractional pole but a root at the left endpoint.
 f = singfun(@(x) (1-x).^b.*(exp(x)-exp(1)), [0 1+b], {'none', 'sing'}, pref);
-r = roots(f);
-r_exact = 1;
-err = r - r_exact;
-pass(4) = (norm(err, inf) < tol*normest(f));
+[y, x] = minandmax(f);
+% The following exact answers are obtained using Mathematica.
+y_exact = [-1.727141310139675; 0];
+x_exact = [0.1651705232378299; 1];
+y_err = y - y_exact;
+x_err = x - x_exact;
+pass(4) = (norm(y_err, inf) < tol*normest(f) && norm(x_err, inf) < 5*tol*normest(f));
 
 % a combination of fractional pole and fractional root.
 f = singfun(@(x) (1+x).^b.*sin(x).*(1-x).^c, [b c], {'sing', 'sing'}, pref);
-r = roots(f);
-r_exact = [0 1];
-err = r - r_exact;
-pass(5) = (norm(err, inf) < tol*normest(f));
-
-% Check the case with roots close to endpoints.
-p = 1-1e-14;
-f = singfun(@(x) (1+x).^b.*sin(x-p).*(1-x).^b, [b b], {'sing', 'sing'}, pref);
-r = roots(f);
-r_exact = p;
-err = r - r_exact;
-pass(6) = (norm(err, inf) < tol*normest(f));
+[y, x] = minandmax(f);
+% The exact maximum value and its location is found using Mathematica.
+y_exact = [-Inf; 0.1636938399751735];
+x_exact = [-1; 0.3776091222310658];
+err_y = y(2) - y_exact(2);
+err_x = x - x_exact; 
+pass(5) = (norm(err_x, inf) < tol*normest(f) && abs(err_y) < tol*normest(f)) && (y(1) == -Inf);
 
 end
