e91212249be874d84abdf9287fbcb8a427e838f8
Anthony P. Austin <austin@maths.ox.ac.uk>
2013-08-13 16:38:20 +0100

    Add documentation to and clean up chebfun.iszero().

--- @chebfun/iszero.m
+++ @chebfun/iszero.m
@@ -1,18 +1,29 @@
 function out = iszero(f, tol)
+%ISZERO   True for zero CHEBFUN objects.
+%   ISZERO(F) returns logical TRUE if F is approximately zero everywhere and
+%   logical FALSE otherwise.  "Approximately zero" means that each of the FUNs
+%   of F is zero (per their ISZERO() function), the values of F at each of its
+%   breakpoints is smaller than VSCALE(F)*EPSLEVEL(F) in magnitude, and F has
+%   no higher-order impulses.
+%
+%   ISZERO(F, TOL) does the same but uses TOL instead of VSCALE(F)*EPSLEVEL(F)
+%   as the tolerance for checking the size of the breakpoint values of F.
 
-% Grab a preference:
+% Copyright 2013 by The University of Oxford and The Chebfun Developers.
+% See http://www.chebfun.org/ for Chebfun information.
+
+% Use default tolerance if none supplied.
 if ( nargin < 2 )
-    vs = get(f, 'vscale'); vs = max([vs{:}]);
-    el = max(get(f, 'epslevel'));
-    tol = el*vs;
+    tol = vscale(f)*epslevel(f)
 end
 
 % Impulses:
-out = all(f.impulses(:,1) < tol);
+out = all(all(abs(f.impulses(:,:,1)) < tol)) && (size(f.impulses, 3) <= 1);
 
 % Loop over each of the funs:
+% [TODO]:  We don't use tol here?
 k = 0;
-while ( k < numel(f.funs) && out )
+while ( (k < numel(f.funs)) && out )
     k = k + 1;
     out = iszero(f.funs{k});
 end
