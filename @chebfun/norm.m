1ba1017673302d34849935d5935c7717c6070b04
nickhale <nick.p.hale@gmail.com>
2013-08-14 14:38:55 +0100

    Initial implementation of @CHEBFUN/NORM. Much of this still requires @CHEBFUN/ABS() before it will work.

--- @chebfun/norm.m
+++ @chebfun/norm.m
@@ -1,19 +1,21 @@
-function [normF, normLoc] = norm(F, n)
-%NORM   Norm of a chebfun object.
-%   For scalar-valued chebfun objects:
+function [normF, normLoc] = norm(f, n)
+%NORM   Norm of a CHEBFUN object.
+%   For scalar-valued CHEBFUN objects:
 %       NORM(f) = sqrt(integral of abs(f)^2).
 %       NORM(f, 2) is the same as NORM(f).
 %       NORM(f, 'fro') is also the same as NORM(f).
 %       NORM(f, 1) = integral of abs(f).
+%       NORM(f, p) = (integral of abs(f)^p)^(1/p).
 %       NORM(f, inf) = max(abs(f)).
 %       NORM(f, -inf) = min(abs(f)).
 %
-%   For array-valued chebfun objects:
+%   For array-valued CHEBFUN objects:
 %       NORM(F) is the Frobenius norm, sqrt(sum(svd(F).^2)).
+%       NORM(F, 'fro') is the same as NORM(F).
 %       NORM(F, 1) is the maximum of the 1-norms of the columns of F.
 %       NORM(F, 2) is the largest singular value of F.
+%       NORM(f) = sqrt(integral of abs(f)^2).
 %       NORM(F, inf) is the maximum of the 1-norms of the rows of F.
-%       NORM(F, 'fro') is the same as NORM(F).
 %
 % Furthermore, the +\-inf norms for scalar-vaued chebfun objects may also return
 % a second input, giving the position where the max/min occurs. For array-valued
@@ -23,100 +25,116 @@ function [normF, normLoc] = norm(F, n)
 % Copyright 2013 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
 
-if ( nargin == 1 )
-    n = 'fro'; 	% Frobenius norm is the default
-end             % (2 norm would be much slower)
-
-
-normLoc = [];
-
-if ( isempty(F) )           % Empty chebfun has norm 0.
+% Empty chebfun has norm 0:
+if ( isempty(f) )           
     normF = 0;
-elseif ( size(F, 2) == 1 )  % A is a scalar-valued chebfun.
+    return
+end
+
+if ( nargin == 1 )
+    n = 'fro'; 	% Frobenius norm is the default (2 norm would be much slower)/
+end
+
+% Initialise:
+normLoc = [];
+numCols = size(f.funs{1}, 2);
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%% SCALAR-VALUED CHEBFUNS %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if ( numCols == 1 )
     switch n
         case 1
             if ( nargout == 2 )
                 error('CHEBFUN:norm:argout',...
                         'Cannot return two outputs for 1-norms');
             end
-            absA = abs(F);
-            normF = sum(absA);
+            normF = sum( abs(f) );
+            
         case {2, 'fro'}
             if ( nargout == 2 )
                 error('CHEBFUN:norm:argout',...
                         'Cannot return two outputs for ''fro''-norms');
             end
-            if ( F.isTransposed )
-                normF = sqrt(abs(F*F'));
-            else
-                normF = sqrt(abs(F'*F));
-            end
+            f.isTransposed = 0;
+            normF = sqrt( abs( sum( conj(f).*f ) ) );
+            
         case {inf, 'inf'}
-            if ( isreal(F) )
-                [normF, normLoc] = minandmax(F);
+            if ( isreal(f) )
+                [normF, normLoc] = minandmax(f);
                 [normF, idx] = max([-normF(1), normF(2)]);
                 normLoc = normLoc(idx);
             else
-                [normF, normLoc] = max(conj(F).*F);
+                [normF, normLoc] = max(conj(f).*f);
                 normF = sqrt(normF);
             end
+            
         case {-inf, '-inf'}
-            [normF, normLoc] = min(abs(F));
+            [normF, normLoc] = min(abs(f));
+            
         otherwise
             if ( isnumeric(n) && isreal(n) )
                 if ( nargout == 2 )
                     error('CHEBFUN:norm:argout',...
-                            'Cannot return two outputs for p-norms');
+                            'Cannot return two outputs for p-norms.');
                 end
                 if ( mod(n, 2) )
-                    normF = sum((conj(F).*F).^(n/2))^(1/n);
+                    normF = sum( (conj(f).*f).^(n/2) )^(1/n);
                 else
-                    normF = sum(abs(F).^n)^(1/n);
+                    normF = sum( abs(f).^n )^(1/n);
                 end
             else
-                error('CHEBFUN:norm:unknown', 'Unknown norm.');
+                error('CHEBFUN:norm:unknownNorm', ...
+                 'The only matrix norms available are 1, 2, inf, and ''fro''.');
             end
     end
-
-else                        % A is a vector-valued:
-    % [TODO]: Implement norms of vector-valued chebfun objects.
-    error('Norm not yet implemented')
+    
+%% %%%%%%%%%%%%%%%%%%%%%%%%%% ARRAY-VALUED CHEBFUNS %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+else 
+    
     switch n
         case 1
-            normF = zeros(numel(F));
-            for k = 1:numel(F)
-                normF(k) = norm(F(k), 1);
+            f = mat2cell(f);
+            normF = zeros(1, numel(f));
+            for k = 1:numel(f)
+                normF(k) = norm(f{k}, 1);
             end
-            [normF, normLoc] = max(norm(F));
+            [normF, normLoc] = max(normF);
+            
         case 2
-            if nargout == 2
+            if (nargout == 2 )
                 error('CHEBFUN:norm:argout',...
-                    'Cannot return two outputs for quasimatrix 2-norms');
+                    'Cannot return two outputs for quasimatrix 2-norms.');
             end
-            s = svd(F,0);
+            s = svd(f, 0);
             normF = s(1);
+            
         case 'fro'
-            % Find integration dimension: 1 if column, 2 if row
-            if nargout == 2
+            if ( nargout == 2 )
                 error('CHEBFUN:norm:argout', ...
-                    'Cannot return two outputs for quasimatrix ''fro''-norms');
+                    'Cannot return two outputs for quasimatrix ''fro''-norms.');
             end
-            dim = 1 + double(F(1).trans);
-            normF = sqrt( sum( sum(F.*conj(F),dim) ) );
-        case {'inf',inf}
-            [normF, normLoc] = max(sum(abs(F),2));
-        case {'-inf',-inf}
-            [normF, normLoc] = min(sum(abs(F),2));
+            % Find integration dimension: 1 if column, 2 if row:
+            f.isTransposed = 0;
+            normF = sqrt( sum( sum(f.*conj(f)) ) );
+            
+        case {'inf', inf}
+            f.isTransposed = 0;
+            [normF, normLoc] = max( sum(abs(f), 2) );
+            
+        case {'-inf', -inf}
+            f.isTransposed = 0;
+            [normF, normLoc] = min( sum(abs(f), 2) );
+            
         otherwise
-            if isnumeric(n) && isreal(n)
-%                 normA = max(sum((conj(A).*A).^(n/2)))^(1/n);
-%                 normA = max(sum(abs(A).^n))^(1/n);
-                [normF, normLoc] = max(sum(abs(F).^n));
+            if ( isnumeric(n) && isreal(n) )
+                f.isTransposed = 0;
+                [normF, normLoc] = max( sum( abs(f).^n, 2 ) );
                 normF = normF^(1/n);
             else
-                error('CHEBFUN:norm:unknown2','Unknown norm');
+                error('CHEBFUN:norm:unknownNorm', ...
+                 'The only matrix norms available are 1, 2, inf, and ''fro''.');
             end
     end
+    
 end
 
 % Discard possible imaginary rounding errors:
