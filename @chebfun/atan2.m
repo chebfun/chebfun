98c988c856d1ef720dd187a7a316f7dec3c8d556
Anthony P. Austin <austin@maths.ox.ac.uk>
2013-08-15 13:39:00 +0100

    Comment out chebfun.atan2().
    We can't work with this function yet, as it requires diff(), which is in a
    different branch.

--- @chebfun/atan2.m
+++ @chebfun/atan2.m
@@ -19,55 +19,58 @@ function p = atan2(y, x, pref)
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Grab some preferences:
-if ( nargin < 3)
-    pref = chebfun.pref();
-end
+error('CHEBFUN:atan2:notImplemented', ...
+    'atan2() not yet implemented for chebfun objects.');
 
-% We'll need to extrapolate here:
-pref.misc.extrapolate = true;
-tol = 1e-6*max(x.vscale, y.vscale);
-
-% There's no reason why we shouldn't keep breaks in both x and y:
-[x, y] = overlap(x, y);
-
-% Find discontinuities resulting from y = 0:
-ry = roots(y);
-% But not where x = 0:
-ry(feval(x, ry ) > tol) = [];
-% Or where dy/dt = 0: (avoid introcducing a break at a double root)
-ry(abs(feval(diff(y), ry)) < tol) = [];
-
-% Deal with the case when y has zero funs:
-rx = [];
-for k = 1:numel(y.funs);
-    % Here we look to see where x changes sign:
-    if ( iszero(y.funs{k}) )
-        rx = [ rx ; x.funs{k} ]; %#ok<AGROW>
-    end
-end
-
-% Collect all the roots together:
-r = [ ry ; rx ];
-
-% Introduce new breaks at the computed roots if required:
-if ( ~isempty(r) )
-    newDom = union(x.domain, r.');
-    x = restrict(x, newDom);
-    y = restrict(y, newDom);
-end
-
-% Do the composition:
-p = compose(x, @(x, y) atan2(y, x), y, pref);
-
-% Sort out the new impulses:
-if ( ~isempty(r) )
-    % Find roots that are breakpoints and evaluate there:
-    [r, ignored, idx] = intersect(r', p.domain);
-    % Set these impulses to zero if x(r) = y(r) = 0:
-    z = abs(feval(x, r)) < tol & abs(feval(y, r)) < tol;
-    p.impulses(1, idx(z)) = 0;    % Zero where x = y = 0.
-    p.impulses(1, idx(~z)) = pi;  % Pi elsewhere.
-end
+%% Grab some preferences:
+%if ( nargin < 3)
+%    pref = chebfun.pref();
+%end
+%
+%% We'll need to extrapolate here:
+%pref.misc.extrapolate = true;
+%tol = 1e-6*max(x.vscale, y.vscale);
+%
+%% There's no reason why we shouldn't keep breaks in both x and y:
+%[x, y] = overlap(x, y);
+%
+%% Find discontinuities resulting from y = 0:
+%ry = roots(y);
+%% But not where x = 0:
+%ry(feval(x, ry ) > tol) = [];
+%% Or where dy/dt = 0: (avoid introcducing a break at a double root)
+%ry(abs(feval(diff(y), ry)) < tol) = [];
+%
+%% Deal with the case when y has zero funs:
+%rx = [];
+%for k = 1:numel(y.funs);
+%    % Here we look to see where x changes sign:
+%    if ( iszero(y.funs{k}) )
+%        rx = [ rx ; x.funs{k} ]; %#ok<AGROW>
+%    end
+%end
+%
+%% Collect all the roots together:
+%r = [ ry ; rx ];
+%
+%% Introduce new breaks at the computed roots if required:
+%if ( ~isempty(r) )
+%    newDom = union(x.domain, r.');
+%    x = restrict(x, newDom);
+%    y = restrict(y, newDom);
+%end
+%
+%% Do the composition:
+%p = compose(x, @(x, y) atan2(y, x), y, pref);
+%
+%% Sort out the new impulses:
+%if ( ~isempty(r) )
+%    % Find roots that are breakpoints and evaluate there:
+%    [r, ignored, idx] = intersect(r', p.domain);
+%    % Set these impulses to zero if x(r) = y(r) = 0:
+%    z = abs(feval(x, r)) < tol & abs(feval(y, r)) < tol;
+%    p.impulses(1, idx(z)) = 0;    % Zero where x = y = 0.
+%    p.impulses(1, idx(~z)) = pi;  % Pi elsewhere.
+%end
 
 end
