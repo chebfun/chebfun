6378fec9a54e2c123f6692b943a5833d3ae43cff
nickhale <nick.p.hale@gmail.com>
2013-08-16 14:48:02 +0100

    Significant improvements to @CHEBUFN/SIGN() and @CHEBFUN/ABS().
    Note, these require some changes frmo upstream to work fully.

--- @chebfun/abs.m
+++ @chebfun/abs.m
@@ -17,50 +17,55 @@ if ( nargin < 2 )
     pref = chebfun.pref();
 end
 
-% Three cases: Real, imaginary, and complex.
+% Three cases: real, imaginary, and complex.
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% REAL CASE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if ( isreal(f) )
 
+    %% %%%%%%%%% LOCATE NEW BREAKS %%%%%%%%%
     % Abs is singular at roots, so locate these:
-    r = roots(f, 'nozerofun');
+    r = roots(f, 'nozerofun', 'nojump', 'noimps');
     
     % Since each column of an array-valued CHEBFUN must have the same breakpoints,
     % we simply take unique(r(:)) and remove any remaining NaNs.
     r = unique(r(:));
     r(isnan(r)) = [];
-
-    % Also discard any roots that are closer than the accuracy of the CHEBFUN:
+    
+    % Discard any roots which are closer than the accuracy of the CHEBFUN:
     el = epslevel(f);
     vs = vscale(f);
     hs = hscale(f);
-    r([false ; diff(r) < el*hs*vs]) = [];
+    rtol1 = el*hs*vs;
+    r([false ; diff(r) < rtol1]) = [];
+    
+    % Avoid introducing new breakpoints close to an existing ones:
+    rtol2 = el*vs*max(min(diff(f.domain)), 1);
+    r(any(abs(bsxfun(@minus, r, f.domain)) < rtol2, 2)) = [];
 
-    % Avoid adding new breaks where not needed:
-    if ( ~isempty(r) )
-        
-        % Choose a tolerance:
-        rtol = el*vs*max(min(diff(f.domain)), 1);
-
-        % Remove if sufficiently close to an existing break points:
-        [rem, ignored] = find(abs(bsxfun(@minus, r , f.domain)) < rtol);
-        r(rem) = [];
-
-    end
-
+    %% %%%%%%%%%% CREATE NEW FUNS %%%%%%%%%%
     % Get the domain with the new breakpoints: (union is not required, by above)
-    dom = sort([f.domain, r']);
+    oldDom = f.domain;
+    newDom = sort([f.domain, r.']);
 
     % Introduce these breakpoints to f:
-    f = restrict(f, dom);
-    f = simplify(f);
+    f = restrict(f, newDom);
 
-    % Call ABS on each of the funs: (result will be smooth)
+    % Call ABS on each of the FUNs: (result will be smooth)
     for k = 1:numel(f.funs)
         f.funs{k} = abs(f.funs{k});
     end
     
-    % Impulses are dealt with below.
+    %% %%%%%%%%%% TIDY THE OUTPUT %%%%%%%%%%
+    
+    % Take the absolute value of the impulses in the first row:
+    f.impulses(:,:,1) = abs(f.impulses(:,:,1));
+    
+    % Simplify the output:
+    f = simplify(f);
+    
+    % [TODO]: Do we want to do this?
+    [ignored, idx] = setdiff(newDom, oldDom);
+    f = merge(f, idx.'); 
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%% IMAGINARY CASE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 elseif ( isreal(1i*f) )
@@ -72,11 +77,15 @@ elseif ( isreal(1i*f) )
 else
 
     % SQRT will deal with introducing breakpoints.
-    f = sqrt(conj(f).*f, pref);
+    imps = f.impulses(:,:,1);
+    f = sqrt(real(conj(f).*f), pref);
+    
+    % Take the absolute value of the impulses in the first row:
+    f.impulses = abs(imp);
 
 end
 
-% Take the absolute value of the impulses in the first row:
-f.impulses(:,:,1) = abs(f.impulses(:,:,1));
+
+
 
 end
--- @chebfun/sign.m
+++ @chebfun/sign.m
@@ -1,19 +1,14 @@
-function g = sign(f, varargin)
+function f = sign(f, varargin)
 %SIGN    Sign function of a CHEBFUN.
 %   G = SIGN(F) returns a piecewise constant CHEBFUN G such that G(x) = 1 in the
 %   interval where F(x) > 0, G(x) = -1 in the interval where F(x) < 0 and G(x) =
 %   0 in the interval where F(x) = 0. Breakpoints in G are introduced at zeros
 %   of F.
 %
-%   G = SIGN(F, 'reassignImpulses') reassigns the impulses.
-%
 % See also ABS, HEAVISIDE, ROOTS.
 
 % Copyright 2013 by The University of Oxford and The Chebfun Developers.
 % See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
-
-% Copy g to f:
-g = f;
 
 % Deal with the empty case:
 if ( isempty(f) )
@@ -23,132 +18,72 @@ end
 % If f is not real, sign returns f?
 if ( ~isreal(f) )
     % [TODO]: Perhaps it should return:
-%     g = f./abs(f);
+%     out = f./abs(f);
     return
 end
-
-% % If f is a zero Chebfun, then return zero:
-% g.impulses = 0*g.impulses(:,:,1);
-% if ( iszero(g) )
-%     % Deal with any nonzero impulses:
-%     imps = f.impulses(:,:,1);
-%     if ( any(imps) )
-%         imps = sign(double(imps));
-%         idx = [true ; any(imps(2:end-1,:), 1) ; true];
-%         g = chebfun(0, f.domain(idx));
-%         g.impulses = imps(idx,:);
-%     else
-%         g = chebfun(0, f.domain([1, end]));
-%     end
-%     return
-% end
 
 % Set scales and tolerances:
 vs = vscale(f);
 hs = hscale(f);
 el = epslevel(f);
 tol = 100*el*vs;
-dom = f.domain;
-imps = f.impulses;
+oldDom = f.domain;
+oldImps = f.impulses;
 numFuns = numel(f.funs);
 numCols = size(f.funs{1}, 2);
 
+%% %%%%%%%%%%%%%%%%%%%%%%% DETERMINE BREAKPOINTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 % Compute the roots of the CHEBFUN:
-f.impulses = ones(size(imps(:,:,1)));
-r = roots(f, 'nozerofun');
+r = roots(f, 'nozerofun', 'noimps');
 
 % Since each column of an array-valued CHEBFUN must have the same breakpoints,
 % we simply take unique(r(:)) and remove any remaining NaNs.
 r = unique(r(:));
 r(isnan(r)) = [];
 
-% % Also discard any roots that are closer than the accuracy of the CHEBFUN:
-r([false ; diff(r) < el*hs*vs]) = [];
+% Discard any roots that are closer than the accuracy of the CHEBFUN:
+rtol = el*hs*vs;
+r([ false ; (diff(r) < rtol) ]) = [];
+% Also avoid introducing new breakpoints close to the ends of the domain:
+r(any(abs(bsxfun(@minus, r, oldDom([1,end]))) < rtol, 2)) = [];
 
 % Add the domain boundaries to the roots vector:
-if ( ~isempty(r) )
-    if ( abs(r(1) - dom(1)) > 1e-14*hs )
-        % Leftmost root is far from domain(1):
-        r = [ dom(1) ; r  ];
-    else
-        r(1) = dom(1);
-    end
-    if ( abs(r(end) - dom(end)) > 1e-14*hs )
-        % Rightmost root is far from domain(1):
-        r = [ r ; dom(end) ];
-    else
-        r(end) = dom(end);
-    end
-else
-    r = dom([1, end]).';
-end
+r = [f.domain(1) ; r ; f.domain(end)];
 
-% % [TODO]: Add this back in. prevents introducing a breakpoint in, say, x.^2.
-% % Check for double roots (double roots may be quite far apart!):
-% ind = find(diff(r) < 1e-7*hs)
-% count = 1;
-% while ( ~isempty(ind) && count < 3 )
-%     remove = false(size(r));
-%     for k = 1:length(ind)
-%         % Check whether a double root or two single roots very close:
-%         x = mean(r(ind(k):ind(k)+1));
-%         if ( abs(feval(f, x)) < tol )
-%             if ( ismember(r(ind(k)+1), dom) )
-%                 remove(ind) = true;
-%             else
-%                 remove(ind+1) = true;
-%             end
-%         end
-%     end
-%     r(remove) = [];
-%     count = count + 1;
-%     ind = find(diff(r) < 1e-7*hs);
-% end
+% Non-trivial impulses should _not_ be removed:
+lvals = feval(f, oldDom.', 'right');
+rvals = feval(f, oldDom.', 'left');
+% Check that the sign of the values is different _and_ values are far apart:
+idxl = sign(oldImps(:,:,1)) ~= sign(lvals) & abs(oldImps(:,:,1) - lvals) > 100*tol;
+idxr = sign(oldImps(:,:,1)) ~= sign(rvals) & abs(oldImps(:,:,1) - rvals) > 100*tol;
+% Look for a jump to left or right in any column: 
+idx = any(idxl | idxr, 2); % idx is st exists a non-trivial jump at oldDom(idx).
+% Append the new breakpoints with nontrival impulses to r and sort:
+[r2, ignored, idx2] = unique([oldDom(idx).' ; r]);
+% Location of existing breakpoints inside new r vector:
+idx2 = idx2(1:sum(idx));
 
-% Make sure that the domain of definition is not changed
-r([1 ; end]) = dom([1, end]);
-
-% Non-trivial impulses should not be removed (unless flag is present):
-if ( nargin == 1 )
-    lvals = zeros(length(domain), numCols);
-    for k = 1:numFuns
-        lvals(k,:) = get(f.funs{k}, 'lval');
-    end
-    lvals(numFuns+1,:) = get(f.funs{end}, 'rval');
-    rvals = zeros(length(domain), size(f.funs{1}, 2));
-    for k = 2:(numFuns+1)
-        rvals(k,:) = get(f.funs{k-1}, 'rval');
-    end
-    rvals(1,:) = get(f.funs{1}, 'lval');
-    idxl = abs(imps(:,:,1) - lvals) > 100*tol;
-    idxr = abs(imps(:,:,1) - rvals) > 100*tol;
-    idx = any(idxl | idxr, 2);
-    idx([1 end]) = 1;
-    r = sort(unique([r ; dom(idx).']));
-    [ignored, idx2] = intersect(r, dom(idx));
-end
+%% %%%%%%%%%%%%%%%%%%%%%%%% CONSTRUCT NEW CHEBFUN %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Evaluate at an arbitrary point between each root:
 c = 0.5912;
 nr = length(r);
 x = c*r(1:nr-1) + (1-c)*r(2:nr);
-vals = sign( feval(f, x) );
-
-% Reshape r as a row vector:
-r = r(:).';
+vals = sign( feval(f, x(:)) );
 
 % Build new CHEBFUN:
-ff = cell(1, nr-1);
+f.domain = r.';
+f.funs = cell(1, nr-1);
 for k = 1:nr-1
-    ff{k} = fun.constructor(vals(k,:), r(k:k+1));
+    f.funs{k} = fun.constructor(vals(k,:), f.domain(k:k+1));
 end
-g.funs = ff;
-g.domain = r;
-g.impulses = chebfun.jumpVals(ff);
 
-% Reassign impulses
-if ( nargin == 1 )
-    g.impulses(idx2,:) = sign(imps(idx,:,1));
-end
+% Reassign impulses:
+f.impulses = chebfun.jumpVals(f.funs);
+f.impulses(idx2,:) = sign(oldImps(idx,:,1));
+
+% Merge g to simplify unecessary breakpoints:
+f = merge(f, find(idx2));
 
 end
