24228ccec1d4f69d43396a2effac66947b37bb6a
Mohsin Javed <mhsnjvd@gmail.com>
2013-08-19 07:25:00 +0100

    improving code for the new sing order finder

--- @singfun/findSingExponents.m
+++ @singfun/findSingExponents.m
@@ -6,9 +6,55 @@ function exponents = findSingExponents(f, singType)
 
 % Copyright 2013 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org for Chebfun information.
+exponents(1) = findExponent(f, 'left');
+exponents(2) = findExponent(f, 'right');
 
-tol = singfun.pref.singfun.eps;
+end
 
-x1 = eps; x2 = 2*x1;
-exponents(1) = log(f(-1+x2)/f(-1+x1))/log(2);
-exponents(2) = log(f(1-x2)/f(1-x1))/log(2);
+
+
+function exponent = findExponent( f, singEnd)
+% Assume trivial-exponent
+exponent = 0;
+
+x = [1, 1-eps, 1-2*eps];
+% choose sample points based on the string passed in singEnd
+if ( strcmpi(singEnd, 'left') )
+    % reverse the sign of the grid points
+    x = -x;
+elseif ( strcmpi(singEnd, 'right' ) )
+    % no change
+else
+    error( 'CHEBFUN:SINGFUN:findSingExponents', 'end should be left or right' )
+end
+
+% end point value
+fx1 = f(x(1));
+
+% function values close to left endpoint
+fx2  = f(x(2));
+fx3  = f(x(3));    
+
+% if the function blows up 
+% Note: We assume a nan indicates a blow up. For example,
+%       consider f = @(x) 1./(1-x) - 3./(1-x).^1.5. Then
+%       f(1) = NaN, however, the function blows up in fact.
+if ( isinf(fx1) || isnan(fx1) )
+    if ( fx2 == 0 )
+        % function jumped from 0 to inf on the finest grid!
+        exponent = sign(fx1) * inf;
+    else
+        exponent = log(fx3/fx2)/log(2);
+    end
+end
+
+% if the function is zero at the endpoint
+if ( fx1 == 0 )
+    if ( fx2 == 0 )
+        % function is zero at the end point and at the point next to it.
+        exponent = 0;
+    else
+        exponent = log(fx3/fx2)/log(2);
+    end   
+end
+end
