e89f37d78effb6d579cb90beca2673bdf7648032
Anthony P. Austin <austin@maths.ox.ac.uk>
2013-08-29 11:14:42 +0100

    Test chebfun.overlap() for functions with differing numbers of breakpoints.
    Suggested by Georges Klein.

--- tests/chebfun/test_overlap.m
+++ tests/chebfun/test_overlap.m
@@ -25,7 +25,7 @@ catch ME
 end
 
 % Check behavior in the basic case.
-f = chebfun(@sin, [-1 -.5 0 1], pref);
+f = chebfun(@sin, [-1 -.5 0 0.5 1], pref);
 g = chebfun(@sin, [-1 0 0.5 1], pref);
 
 [f2, g2] = overlap(f, g);
@@ -37,7 +37,7 @@ pass(3) = isequal(f2.domain, g2.domain) && ...
 
 % Check correct behavior for higher-order impulses.  [TODO]:  Use a function
 % with real higher-order impulses instead of just faking them like this.
-f.impulses = cat(3, f.impulses, zeros(4, 1, 1));
+f.impulses = cat(3, f.impulses, zeros(5, 1, 1));
 [f2, g2] = overlap(f, g);
 xx = linspace(-1, 1);
 pass(4) = isequal(f2.domain, g2.domain) && ...
