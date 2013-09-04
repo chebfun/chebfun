b04fad33a064122fb4c2c6e0429acccd66129bce
Mohsin Javed <mhsnjvd@gmail.com>
2013-09-04 10:30:20 +0100

    moving singOP2SmoothOp inside the constructor

--- @singfun/singfun.m
+++ @singfun/singfun.m
@@ -133,7 +133,7 @@ classdef singfun
             end
                
             % update the operator based on the values in exponents.
-            smoothOp = singfun.singOp2SmoothOp(op, obj.exponents);
+            smoothOp = singOp2SmoothOp(op, obj.exponents);
             obj.smoothPart = singfun.constructSmoothPart(smoothOp, pref);
         end
     end
@@ -274,9 +274,6 @@ classdef singfun
         % method for finding fractional order singularities (+ve or -ve).
         barnchOrder = findSingOrder( op, SingEnd)
         
-        % method for converting a singular op to a smooth op
-        op = singOp2SmoothOp( op, exponents, tol )
-        
         % Retrieve and modify preferences for this class.
         prefs = pref(varargin)
         
@@ -363,5 +360,37 @@ if ( ~all(out) )
 end
 
 end
+
+function op = singOp2SmoothOp(op, exponents)
+%SINGOP2SMOOTHOP   Converts a singular operator to a smooth one by removing the 
+%   singularity(ies). 
+%   SINGOP2SMOOTHOP(OP, EXPONENTS) returns a smooth operator OP by removing the
+%   singularity(ies) at the endpoints -1 and 1. EXPONENTS are the order of the
+%   singularities. 
+%
+%   For examples, opNew = singOp2SmoothOp(opOld, [-a -b]) means 
+%   opNew = opOld.*(1+x).^a.*(1-x).^b
+%
+% See also SINGFUN.
+
+% Copyright 2013 by The University of Oxford and The Chebfun Developers.
+% See http://www.chebfun.org/ for Chebfun information.
+
+if ( all(exponents) )
+    % Both exponents are non trivial:
+    op = @(x) op(x)./((1 + x).^(exponents(1)).*(1 - x).^(exponents(2)));
+    
+elseif ( exponents(1) )
+    % (1+x) factor at the left end point:
+    op = @(x) op(x)./(1 + x).^(exponents(1));
+    
+elseif ( exponents(2) )
+    % (1-x) factor at the right end point:
+    op = @(x) op(x)./(1 - x).^(exponents(2));
+    
+end
+
+end
+
 
 
