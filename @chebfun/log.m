0314e0c3b79c70f0323c047408793b1a0d2d2880
Nick Hale <nick.p.hale@gmail.com>
2013-09-10 17:12:53 +0100

    Initial commit of @CHEBFUN/LOG() and related methods.
    Note, these offer no clever support for function which
    pass through the origin. THe help text gives some warning.
    
    SPLITTING ON preferences can be pass via log(f, pref),
    but this is as yet undocumented.

---
+++ @chebfun/log.m
@@ -0,0 +1,21 @@
+function g = log(f, pref)
+%LOG   Natural logarithm of a CHEBFUN.
+%   LOG(F) returns the natural logarithm of F. If F has an roots in its domain,
+%   then the representation is likely to be inaccurate.
+%
+% See also LOG1P, LOG2, LOG10, EXP, REALLOG.
+
+% Copyright 2013 by The University of Oxford and The Chebfun Developers. 
+% See http://www.chebfun.org/ for Chebfun information. 
+
+if ( nargin < 2 )
+    pref = chebfun.pref();
+end
+
+% Add breaks at the roots of f:
+f = addBreaksAtRoots(f);
+
+% Call COMPOSE():
+g = compose(f, @(x) log(x), [], pref);
+
+end
