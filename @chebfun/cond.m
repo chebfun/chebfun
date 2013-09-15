15d58cfb757fd6a00dcf006a4ec4d416b62e50b4
Nick Hale <nick.p.hale@gmail.com>
2013-09-15 16:25:57 +0100

    Adding COND() and PINV().

---
+++ @chebfun/cond.m
@@ -0,0 +1,16 @@
+function out = cond(f)
+%COND	Condition number of a CHEBFUN.
+%   COND(F) is the 2-norm condition number of F.
+%
+% See also SVD, RANK.
+
+% Copyright 2013 by The University of Oxford and The Chebfun Developers. 
+% See http://www.chebfun.org/ for Chebfun information.
+
+% Compute the singular values:
+s = svd(f, 0);
+
+% Compute the condition number:
+out = s(1)/s(end);
+
+end
