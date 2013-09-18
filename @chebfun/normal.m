ab8fd08e4558b93dcf1a7da7e7a789bd3a8e5ee4
Nick Hale <nick.p.hale@gmail.com>
2013-09-18 17:32:31 +0100

    Minor fix to NORMAL() in response to comments from AT.

--- @chebfun/normal.m
+++ @chebfun/normal.m
@@ -3,25 +3,28 @@ function n = normal(c, unit)
 %   N = NORMAL(C) returns the normal vector to the curve C as a CHEBFUN with two
 %   columns. The vector has the same magntiude as the curve's tangent vector.
 %
-%   N = NORMAL(C, 'unit') returns the unit normal vector to the curve C. N is a
-%   CHEBFUN with two columns.
+%   N = NORMAL(C, 'unit') returns the unit normal vector to the curve C. 
 
 % Copyright 2013 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
-
-% [TOD0]: This method requires HORZCAT() and quasimatrices.
 
 n = -1i * diff(c); 
 
 if ( nargin > 1 ) 
     if ( strcmpi(unit, 'unit') )
-        n = n./norm(n);
+        nrmn = norm(n);
+        if ( nrmn == 0 )
+            error('CHEBFUN:normal:zero', 'Normal vector is zero.'); 
+        else
+            n = n./nrmn;
+        end
     else
-        error('CHEBFUN:NORMAL', 'Second argument is not recognised.');
+        error('CHEBFUN:normal:args', 'Second argument is not recognised.');
     end
 end
 
 % Return a quasi-matrix. 
-n = [real(n), imag(n)];  
+% [TOD0]: This method requires HORZCAT() and quasimatrices.
+% n = [real(n), imag(n)];  
  
 end
