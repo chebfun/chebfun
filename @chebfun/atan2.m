7b153d8da7ddac30a219b0ed2294d7fc86db3960
Nick Hale <nick.p.hale@gmail.com>
2013-09-22 19:43:53 +0100

    Improveing @CHEBFUN/ATAN2() and ading a test.

--- @chebfun/atan2.m
+++ @chebfun/atan2.m
@@ -1,30 +1,34 @@
 function p = atan2(y, x, pref)
-%ATAN2    Four quadrant inverse tangent of a CHEBFUN.
+%ATAN2   Four quadrant inverse tangent of a CHEBFUN.
 %   ATAN2(Y, X) is the four quadrant arctangent of the real parts of the CHEBFUN
-%   objects X and Y.  -pi <= ATAN2(Y,X) <= pi.
+%   objects X and Y.  -pi <= ATAN2(Y, X) <= pi.
+%
+%   ATAN2 is defined as:
+%                  { atan(y/x),               x > 0
+%                  { atan(y/x) + pi,  y >= 0, x < 0
+%   atan2(y, x) =  { atan(y/x) - pi,  y < 0,  x < 0
+%                  { pi/2,            y > 0,  x = 0
+%                  { -pi/2,           y < 0,  x = 0
+%                  { 0,               y = 0,  x = 0
 %
 % See also ATAN, ATAN2D.
 
 % Copyright 2013 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org for Chebfun information.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% ATAN2 is defined as:
-%                { atan(y/x),               x > 0
-%                { atan(y/x) + pi,  y >= 0, x < 0
-% atan2(y, x) =  { atan(y/x) - pi,  y < 0,  x < 0
-%                { pi/2,            y > 0,  x = 0
-%                { -pi/2,           y < 0,  x = 0
-%                { 0,               y = 0,  x = 0
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% error('CHEBFUN:atan2:notImplemented', ...
-%     'atan2() not yet implemented for CHEBFUN objects.');
-
+%% Set up:
 % Grab some preferences:
 if ( nargin < 3)
     pref = chebfun.pref();
+end
+
+if ( ~isreal(y) || ~isreal(x) )
+    error('CHEBFUN:atan2:real', 'Inputs ust be real.');
+end
+
+if ( min(size(y)) > 1 || min(size(x)) > 1 )
+    error('CHEBFUN:atan2:array', ... % TODO: Add support for this.
+        'ATAN2 does not supprt array-valued CHEBFUN objects..');
 end
 
 % We'll need to extrapolate here:
@@ -34,32 +38,13 @@ tol = max(epslevel(y)*vscale(y), epslevel(x)*vscale(x));
 % There's no reason why we shouldn't keep breaks in both x and y:
 [x, y] = overlap(x, y);
 
-% Get the number of columns:
-numCols = min(size(x));
-
-%%
+%% Locate discontinuities:
 % Find discontinuities resulting from y = 0:
 ry = roots(y);
 
-% But not where x = 0:
-xr = feval(x, ry);
-% Deal with array-valued CHEBFUN objects:
-xr = xr(:, 1:numCols);
-% Remove unwanted roots:
-ry(xr > tol) = [];
+% But not where x > 0:
+ry(feval(x, ry) > tol) = [];
 
-% Or where dy/dt = 0: (avoid introcducing a break at a double root)
-ypr = feval(diff(y), ry);
-% Deal with array-valued CHEBFUN objects:
-ypr = ypr(:, 1:numCols);
-% Remove unwanted roots:
-ry(abs(ypr) < tol) = [];
-
-% Deal with array-valued CHEBFUN objects:
-ry = unique(ry(:));
-ry(isnan(ry)) = [];
-
-%%
 % Deal with the case when y has zero FUNs:
 rx = [];
 for k = 1:numel(y.funs);
@@ -68,11 +53,7 @@ for k = 1:numel(y.funs);
         rx = [ rx ; roots(x.funs{k}) ]; %#ok<AGROW>
     end
 end
-% Deal with array-valued CHEBFUN objects:
-rx = unique(rx(:));
-rx(isnan(rx)) = [];
 
-%%
 % Collect all the roots together:
 r = [ ry ; rx ];
 
@@ -83,33 +64,15 @@ if ( ~isempty(r) )
     y = restrict(y, newDom);
 end
 
-%%
+%% Compose:
 % Do the composition:
 p = compose(x, @(x, y) atan2(y, x), y, pref);
 
 % Sort out the new impulses:
 if ( ~isempty(r) )
-    % Find roots that are breakpoints and evaluate there:
-    [r, ignored, idx] = intersect(r', p.domain);
-    idx = repmat(idx, 1, numCols);
-    % Set these impulses to zero if x(r) = y(r) = 0:
-    z = abs(feval(x, r)) < tol & abs(feval(y, r)) < tol;
-    
-    idx1 = idx(z);
-    if ( ~isempty(idx1) )
-        % Zero where x = y = 0.
-        for k = 1:numCols
-            p.impulses(idx1(:,k),k,1) = 0;
-        end
-    end
-    
-    idx2 = idx(~z);
-    if ( ~isempty(idx2) )
-        % Pi elsewhere.
-        for k = 1:numCols
-            p.impulses(idx2(:,k),k,1) = pi;
-        end
-    end
+    % Set impulses to zero if x(r) = y(r) = 0:
+    idx = (abs(x.impulses(:,:,1)) < tol & abs(y.impulses(:,:,1)) < tol);
+    p.impulses(idx,1,1) = 0;  
 end
 
 end
