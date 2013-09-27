81700ac07c6594813aa4eb2b8590945f214cd98a
Nick Hale <nick.p.hale@gmail.com>
2013-09-27 11:03:34 +0100

    Correction to previous commit.

--- @chebmatrix/horzcat.m
+++ @chebmatrix/horzcat.m
@@ -1,11 +1,14 @@
 function C = horzcat(varargin)
 
+args = {};
 for k = 1:numel(varargin)
     if ( isa(varargin{k}, 'chebmatrix') )
-        varargin{k} = varargin{k}.blocks;
+        args = [args, varargin{k}.blocks];
+    else
+        args = [args, varargin(k)];
     end
 end
 
-C = chebmatrix( horzcat(varargin{:}) );
+C = chebmatrix( args );
 
 end
--- @chebmatrix/vertcat.m
+++ @chebmatrix/vertcat.m
@@ -1,12 +1,14 @@
 function C = vertcat(varargin)
 
-
+args = {};
 for k = 1:numel(varargin)
     if ( isa(varargin{k}, 'chebmatrix') )
-        varargin{k} = varargin{k}.blocks;
+        args = [args ; varargin{k}.blocks];
+    else
+        args = [args ; varargin(k)];
     end
 end
 
-C = chebmatrix( vertcat(varargin{:}) );
+C = chebmatrix( args );
 
 end
