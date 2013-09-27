808890030539ac01d55db6707dec6a54ed53d861
Kuan Xu <xukuan2011@gmail.com>
2013-09-27 15:40:04 +0100

    fixing the documentation.

--- @singfun/fliplr.m
+++ @singfun/fliplr.m
@@ -1,7 +1,5 @@
 function f = fliplr(f) %#ok<INUSD>
-%FLIPLR   Flip columns of an array-valued SINGFUN object.
-%   FLIPLR(F) flips the columns of an array-valued SINGFUN in the left/right
-%   direction. If F has only one column, then this function has no effect.
+%FLIPLR   SINGFUN object is not supported.
 
 % Copyright 2013 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org for Chebfun information.
