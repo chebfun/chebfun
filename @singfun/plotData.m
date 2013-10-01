9b15b9e48ce7ca5d47ced7e2040de8ebc8ca113e
Mohsin Javed <javed@maths.ox.ac.uk>
2013-10-01 17:28:12 +0100

    this commit is just to transmit local changes on plotData which is not finished yet.

--- @singfun/plotData.m
+++ @singfun/plotData.m
@@ -1,7 +1,14 @@
-function data = plotData(f)
+function data = plotData(f, g, h)
 %PLOTDATA   Useful data values for plotting a SINGFUN object.
 %   DATA = PLOTDATA(F) extracts PLOTDATA of the smooth part of F and then scales
-%   it by the singular factors given in the EXPONENTS of F
+%   it by the singular factors given in the EXPONENTS of F.
+%
+%   DATA = PLOTDATA(F, G) is similar but for plot calls of the form PLOT(F, G),
+%   where both F and G are SINGFUN objects. 
+% 
+%   DATA = PLOTDATA(F, G, H) is for plots of the form PLOT3(F, G, H). In this
+%   instance, DATA also contains fields zLine and zPoints for the data
+%   corresponding to H.
 %
 % See also PLOT.
 
@@ -9,12 +16,48 @@ function data = plotData(f)
 % See http://www.chebfun.org/ for Chebfun information.
 
 %%
-% Get plot data from the smooth part:
-data = plotData( f.smoothPart );
+if ( nargin == 1 )
+    g = [];
+    data = plotData(f.smoothPart);
+elseif (nargin == 2)
+    data = plotData(f.smoothPart, g.smoothPart);
+elseif (nargin == 3)
+    data = plotData(f.smoothPart, g.smoothPart, h.smoothPart);
+end
 
+if ( isempty(g) )       
+    % PLOT(F):
+    % Update the y-data:
+    data.yLine = data.yLine.*(data.xLine + 1).^f.exponents(1).*(1 - data.xLine).^f.exponents(2);
+    % Update sample point y-data:
+    data.yPoints = data.yPoints.*(data.xPoints + 1).^f.exponents(1).*(1 - data.xPoints).^f.exponents(2);
+elseif ( isa(g, 'singfun') )   
+    % PLOT(F, G)
+    
+   
+    
+    if ( isa(h, 'singfun') )
+        % PLOT3(F, G, H)
+   
+    
+    
+    else
+        error('CHEBFUN:SINGFUN:plotdata:DataType', ...
+            'Invalid data types.');
+        
+    end
+    
+else
+    error('CHEBFUN:SINGFUN:plotdata:DataType', ...
+        'Invalid data types.');    
+end
+% Scale data according to exponents.
+data = scaleData(data);
 % Update the y-data:
 x = data.xLine;
 y = data.yLine;
+z = data.zLine;
+
 y = y.*(x + 1).^f.exponents(1);
 y = y.*(1 - x).^f.exponents(2);
 data.yLine = y;
@@ -26,6 +69,4 @@ y = y.*(x + 1).^f.exponents(1);
 y = y.*(1 - x).^f.exponents(2);
 data.yPoints = y;
 
-end
 
-% NH: Need to also support PLOT(F, G, H).
