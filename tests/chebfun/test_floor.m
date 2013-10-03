006d269bcdeee6a17846380034326b4c4a0df514
Anthony P. Austin <austin@maths.ox.ac.uk>
2013-10-03 09:58:03 +0100

    Remove checks on impulse values in tests for fix(), floor(), and round().
    These functions are infinitey ill-conditioned at breakpoints, so we cannot hope
    to reliably compute them exactly right where breakpoints occur.  Thus, testing
    these values makes no sense.
    
    Fast example which demonstrates this (at least, on my workstation):
    
      >> f = chebfun(@(x) exp(x), [-1 -0.5 0.5 1]);
      >> g = round(f);
      >> [g.impulses round(exp(g.domain)).']
      ans =
           0     0
           1     0
           1     1
           2     2
           2     2
           3     3
           3     3

--- tests/chebfun/test_floor.m
+++ tests/chebfun/test_floor.m
@@ -17,35 +17,30 @@ pass(1) = isempty(floor(chebfun()));
 f = chebfun(@(x) sin(x), [-1 -0.5 0.5 1], pref);
 g = floor(f);
 g_exact = @(x) sign(x)/2 - 1/2;
-pass(2) = (norm(feval(g, x) - g_exact(x), inf) <= 10*vscale(g)*epslevel(g)) ...
-    && isequal(g.impulses, [-1 -1 0 0].');
+pass(2) = (norm(feval(g, x) - g_exact(x), inf) <= 10*vscale(g)*epslevel(g));
 
 f = chebfun(@(x) exp(x), [-1 -0.5 0.5 1], pref);
 g = floor(f);
 g_exact = @(x) floor(exp(x));
-pass(3) = (norm(feval(g, x) - g_exact(x), inf) <= 10*vscale(g)*epslevel(g)) ...
-    && isequal(g.impulses, [0 0 1 1 2 2].');
+pass(3) = (norm(feval(g, x) - g_exact(x), inf) <= 10*vscale(g)*epslevel(g));
 
 % Check complex-valued functions.
 f2 = 1i*f;
 g = floor(f2);
 g_exact = @(x) floor(1i*exp(x));
-pass(4) = (norm(feval(g, x) - g_exact(x), inf) <= 10*vscale(g)*epslevel(g)) ...
-    && isequal(g.impulses, 1i*[0 0 1 1 2 2].');
+pass(4) = (norm(feval(g, x) - g_exact(x), inf) <= 10*vscale(g)*epslevel(g));
 
 f3 = f + 1i*f;
 g = floor(f3);
 g_exact = @(x) floor(exp(x) + 1i*exp(x));
-pass(5) = (norm(feval(g, x) - g_exact(x), inf) <= 10*vscale(g)*epslevel(g)) ...
-    && isequal(g.impulses, (1 + 1i)*[0 0 1 1 2 2].');
+pass(5) = (norm(feval(g, x) - g_exact(x), inf) <= 10*vscale(g)*epslevel(g));
 
 % Check array-valued function.
 f = chebfun(@(x) [sin(x) exp(x)], [-1 -0.5 0.5 1], pref);
 g = floor(f);
 g_exact = @(x) floor([sin(x) exp(x)]);
 err = feval(g, x) - g_exact(x);
-pass(6) = (norm(err(:), inf) <= 10*vscale(g)*epslevel(g)) && ...
-    isequal(g.impulses, [-1 -1 0 0 0 0 ; 0 0 1 1 2 2].');
+pass(6) = (norm(err(:), inf) <= 10*vscale(g)*epslevel(g));
 
 % Check error conditions.
 try
--- tests/chebfun/test_round.m
+++ tests/chebfun/test_round.m
@@ -17,35 +17,30 @@ pass(1) = isempty(round(chebfun()));
 f = chebfun(@(x) sin(x), [-1 -0.5 0.5 1], pref);
 g = round(f);
 g_exact = @(x) round(sin(x));
-pass(2) = (norm(feval(g, x) - g_exact(x), inf) <= 10*vscale(g)*epslevel(g)) ...
-    && isequal(g.impulses, [-1 -1 0 0 1 1].');
+pass(2) = (norm(feval(g, x) - g_exact(x), inf) <= 10*vscale(g)*epslevel(g));
 
 f = chebfun(@(x) exp(x), [-1 -0.5 0.5 1], pref);
 g = round(f);
 g_exact = @(x) round(exp(x));
-pass(3) = (norm(feval(g, x) - g_exact(x), inf) <= 10*vscale(g)*epslevel(g)) ...
-    && isequal(g.impulses, [0 1 1 2 2 3 3].');
+pass(3) = (norm(feval(g, x) - g_exact(x), inf) <= 10*vscale(g)*epslevel(g));
 
 % Check complex-valued functions.
 f2 = 1i*f;
 g = round(f2);
 g_exact = @(x) round(1i*exp(x));
-pass(4) = (norm(feval(g, x) - g_exact(x), inf) <= 10*vscale(g)*epslevel(g)) ...
-    && isequal(g.impulses, 1i*[0 1 1 2 2 3 3].');
+pass(4) = (norm(feval(g, x) - g_exact(x), inf) <= 10*vscale(g)*epslevel(g));
 
 f3 = f + 1i*f;
 g = round(f3);
 g_exact = @(x) round(exp(x) + 1i*exp(x));
-pass(5) = (norm(feval(g, x) - g_exact(x), inf) <= 10*vscale(g)*epslevel(g)) ...
-    && isequal(g.impulses, (1 + 1i)*[0 1 1 2 2 3 3].');
+pass(5) = (norm(feval(g, x) - g_exact(x), inf) <= 10*vscale(g)*epslevel(g));
 
 % Check array-valued function.
 f = chebfun(@(x) [sin(x) exp(x)], [-1 -0.5 0.5 1], pref);
 g = round(f);
 g_exact = @(x) round([sin(x) exp(x)]);
 err = feval(g, x) - g_exact(x);
-pass(6) = (norm(err(:), inf) <= 10*vscale(g)*epslevel(g)) && ...
-    isequal(g.impulses, [-1 -1 -1 0 0 0 1 1 1; 0 1 1 1 2 2 2 3 3].');
+pass(6) = (norm(err(:), inf) <= 10*vscale(g)*epslevel(g));
 
 % Check error conditions.
 try
