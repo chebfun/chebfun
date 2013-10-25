566e0813c22283501631f1125564ba2ef1ac461e
Nick Hale <nick.p.hale@gmail.com>
2013-10-25 15:37:43 +0100

    Adding support for plot(f, [a, b]).

--- @chebfun/plot.m
+++ @chebfun/plot.m
@@ -70,13 +70,22 @@ jumpData = {};
 %#ok<*AGROW>
 
 % Check to see if the 'interval' flag has been set:
+intervalIsSet = 0;
 loc = find(strcmpi(varargin, 'interval'));
 if ( any(loc) )
     intervalIsSet = 1;
     interval = varargin{loc+1};
     varargin(loc:loc+1) = [];
 else
-    intervalIsSet = 0;
+    loc = find(cellfun(@(f) isa(f, 'chebfun'), varargin));
+    for k = 1:numel(loc)
+        if ( loc(k) < nargin && isnumeric(varargin{loc(k)+1}) )
+            interval = varargin{loc(k)+1};
+            varargin(loc(k)+1) = [];
+            intervalIsSet = true;
+            break
+        end
+    end
 end
     
 %%
