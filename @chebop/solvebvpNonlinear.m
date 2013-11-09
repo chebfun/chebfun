e518e046f0d17b47bfa01ef7f1e23339768f0d5b
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2013-11-09 13:22:30 +0000

    Missing file from yesterday commit for solving nonlinear BVPs.

---
+++ @chebop/solvebvpNonlinear.m
@@ -0,0 +1,107 @@
+function u = solvebvpNonlinear(N, rhs, pref)
+
+% Store preferences used in the Newton iteration in separate variables
+maxIter = pref.maxIter;
+discType = pref.discretizationType;
+delTol = pref.deltol;
+% plotMode determines whether we want to stop between plotting iterations
+plotMode = lower(pref.plotting);
+% Did the user request damped or undamped Newton iteration?
+damped = pref.damped;
+
+% NUMVARS indicate how many unknown function we seek.
+numVars = nargin(N.op) - 1;
+
+% Store the domain we're working with.
+dom = N.domain;
+
+% Initialise a zero ADCHEBFUN:
+zeroFun = chebfun(0, dom);
+u0 = cell(numVars, 1);
+for k = 1:numVars
+    u0{k} = zeroFun;
+end
+u0 = chebmatrix(u0);
+
+% Initialise the dependent variable:
+x = chebfun(@(x) x, dom);
+
+% Linearise
+[L, affine, isLinear] = linearise(N, x, u0);
+
+    function out = mynorm(f)
+        if ( isa(f, 'chebmatrix') )
+            out = max(cellfun(@(u) get(u, 'vscale'), f.blocks));
+        else
+            out = get(f, 'vscale');
+        end
+    end
+
+if ( ~isempty(N.init) )
+    u = N.init;
+    L = linearise(N, x, u);
+    res = N.op(x, u{:}) - rhs;
+    L.discretizationType = discType;
+    du = L\res;
+    %                 else
+    %                     u = makeGuess(L, x);
+    %                     L = linearise(N, x, u);
+    %                     res = N.op(x, u{:}) - rhs;
+    %                     du = L\res;
+else
+    u = u0;
+    L.discretizationType = discType;
+    du = L\(rhs - affine);
+end
+
+u = u - du;
+ub = u.blocks;
+res = N.op(x, ub{:}) - rhs;
+
+fprintf('Nonlinear equation. using Newton''s method:\n')
+fprintf('step\t normUpdate\t\t  normRes\n')
+normUpdate(1,1) = mynorm(du);
+normRes(1,1) = mynorm(res);
+fprintf(' %2.2d\t%16.16f\t %16.16f\n', 1, normUpdate(1,1), normRes(1,1))
+
+% Counter for number of Newton steps taken.
+newtonCounter = 0;
+
+% Variable that controls whether we want to stop the Newton iteration, either
+% because we converged, or the process has been identified to be nonconvergent.
+terminate = 0;
+while ( ~terminate ) && ( normRes(end) > 1e-12)
+    % Linearise around current solution:
+    L = linearise(N, x, ub, []); % flag to negate contraint RHSs.
+    % Solve the linearised system:
+    L.discretizationType = discType;
+    du = L\res;
+    % Append the Newton step:
+    u = u - du;
+    % Evaluate the residual
+    ub = u.blocks;
+    res = N.op(x, ub{:}) - rhs;
+    
+    % Update counter of Newton steps taken
+    newtonCounter = newtonCounter +1;
+    
+    % Stop if well converged, or stagnated:
+    normUpdate(newtonCounter,1) = mynorm(du);
+    normRes(newtonCounter,1) = mynorm(res);
+    fprintf(' %2.2d\t%16.16f\t %16.16f\n', newtonCounter, ...
+        normUpdate(newtonCounter,1), normRes(newtonCounter,1))
+    if ( normUpdate(newtonCounter,1) < 1e-12 )
+        break
+        %                     elseif (newt > 3 && normUpdate(newt) > 0.1*normUpdate(newt-3))
+        %                         warning('CHEBFUN:bvpsc','Newton iteration stagnated.')
+        %                         break
+    elseif (newtonCounter > maxIter)
+        warning('CHEBFUN:bvpsc','Newton iteration failed.')
+        break
+    end
+    
+
+    
+end
+
+end
