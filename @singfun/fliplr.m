b6a8d1645209401e72270186540073d7242cf9c3
Kuan Xu <xukuan2011@gmail.com>
2013-12-08 21:26:51 +0000

    Support filplr for singfun. Of course, this is only for single-column case and array-valued singfun is not supported.

--- @singfun/fliplr.m
+++ @singfun/fliplr.m
@@ -1,9 +1,12 @@
 function f = fliplr(f) %#ok<INUSD>
-%FLIPLR   FLIPLR is not supported for SINGFUN objects.
+%FLIPLR   Flip columns of an array-valued SINGFUN object. Since SINGFUN does not
+%support array-valued SINGFUN which has more than one column, what it returns
+%will the input SINGFUN itself.
+%
+%   FLIPLR(F) flips the columns of an array-valued CHEBTECH in the left/right
+%   direction. Since F has to be of one column, this function has no effect.
 
 % Copyright 2013 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org for Chebfun information.
 
-error('CHEBFUN:SINGFUN:fliplr:notpossible', ...
-      'SINGFUN objects can not be flipped from left to right.')
 end
