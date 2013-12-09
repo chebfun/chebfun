63172376c4f6aca2210a9c5b5f19ab224c519522
Hadrien Montanelli <montanelli@maths.ox.ac.uk>
2013-12-09 19:12:00 +0000

    Adding a test for POLYFIT and LEGPOLY, with breakpoints and row CHEBFUN.

--- tests/chebfun/test_legpoly.m
+++ tests/chebfun/test_legpoly.m
@@ -10,7 +10,7 @@ end
 
 % Test 1 confirms that the elements of the array-valued chebfun p 
 % are Legendre polynomials: 
-p = legpoly(900:1100, [-1 1]);
+p = legpoly(900:1100, [-1 0 1]);
 xx = linspace(-1, 1, 1000);
 P = legendre(900, xx); 
 err = norm(feval(p(:,1), xx) - P(1, :), inf);
@@ -21,7 +21,7 @@ err = norm(p'*p - diag(diag(p'*p)));
 pass(2) = err < 5e1*epslevel(p)*vscale(p);
 
 % Test 3 confirms othonormality:
-p = legpoly(900:1100, [-1 1], 'normalize');
+p = legpoly(900:1100, [-1 0 1], 'normalize');
 err = norm(p'*p - eye(201));
 pass(3) = err < 5e3*epslevel(p)*vscale(p);
 
@@ -47,31 +47,31 @@ pass(6) = err < 5e3*epslevel(p)*vscale(p);
 %% Test method 1 on [0 10000]:
 
 % Test 7 confirms orthogonality:
-p = legpoly(900:1100, [0 10000], 'normalize');
+p = legpoly(900:1100, [0 100 10000], 'normalize');
 err = norm(p'*p - diag(diag(p'*p)));
 pass(7) = err < 5e4*epslevel(p)*vscale(p);
 
 % Test 8 confirms othonormality:
-p = legpoly(900:1100, [0 10000], 'normalize');
+p = legpoly(900:1100, [0 50 10000], 'normalize');
 err = norm(p'*p - eye(201));
 pass(8) = err < 5e4*epslevel(p)*vscale(p);
 
 %% Test method 2 on [-1 1]:
 
 % Test 9 confirms that p is a Legendre polynomial: 
-p = legpoly(40, [-1 1]);
+p = legpoly(40, [-1 0.2 1]);
 xx = linspace(-1, 1, 1000);
 P = legendre(40, xx);
 err = norm(feval(p, xx) - P(1, :), inf);
 pass(9) = err < 50*epslevel(p)*vscale(p);
 
 % Test 10 confirms orthogonality:
-p = legpoly(1:100, [-1 1]);
+p = legpoly(1:100, [-1 -0.2 0.3 1]);
 err = norm(p'*p - diag(diag(p'*p)));
 pass(10) = err < 10*epslevel(p)*vscale(p);
 
 % Test 11 confirms othonormality:
-p = legpoly(1:100, [-1 1], 'normalize');
+p = legpoly(1:100, [-1 0.145 1], 'normalize');
 err = norm(p'*p - eye(100));
 pass(11) = err < 100*epslevel(p)*vscale(p);
 
@@ -98,26 +98,26 @@ pass(14) = err < 100*epslevel(p)*vscale(p);
 %% Test method 2 on [0 10000]:
 
 % Test 15 confirms orthogonality:
-p = legpoly(1:100, [0 10000]);
+p = legpoly(1:100, [0 155 3333 10000]);
 err = norm(p'*p - diag(diag(p'*p)));
 pass(15) = err < 5e4*epslevel(p)*vscale(p);
 
 % Test 16 confirms othonormality:
-p = legpoly(1:100, [0 10000], 'normalize');
+p = legpoly(1:100, [0 3333 10000], 'normalize');
 err = norm(p'*p - eye(100));
 pass(16) = err < 5e3*epslevel(p)*vscale(p);
 
 %% Test method 3 on [-1 1]:
 
 % Test 17 confirms that p is a Legendre polynomial: 
-p = legpoly(1500, [-1 1]);
+p = legpoly(1500, [-1 -0.5 -0.3 1]);
 xx = linspace(-1, 1, 1000);
 P = legendre(1500, xx);
 err = norm(feval(p, xx) - P(1, :), inf);
 pass(17) = err < 1e4*epslevel(p)*vscale(p);
 
 % Test 18 confirms normaliztion:
-p = legpoly(1500, [-1 1], 'normalize');
+p = legpoly(1500, [-1 0 1], 'normalize');
 err = norm(p'*p-1);
 pass(18) = err < 1e4*epslevel(p)*vscale(p);
 
@@ -138,17 +138,17 @@ pass(20) = err < 1e4*epslevel(p)*vscale(p);
 %% Test method 2 on [-1 1] with a row input N:
 
 % Test 21 confirms orthogonality:
-p = legpoly([1;2;3;4;5;6;7;8;9;10], [-1 1]);
+p = legpoly([1;2;3;4;5;6;7;8;9;10], [-1 0.2 1]);
 err = norm(p*p' - diag(diag(p*p')));
 pass(21) = err < 5*epslevel(p)*vscale(p);
 
 % Test 22 confirms othonormality:
-p = legpoly([1;2;3;4;5;6;7;8;9;10], [-1 1], 'normalize');
+p = legpoly([1;2;3;4;5;6;7;8;9;10], [-1 0.6 1], 'normalize');
 err = norm(p*p' - eye(10));
 pass(22) = err < 10*epslevel(p)*vscale(p);
 
 % Test 23 checks empty case:
-p = legpoly([], [-1 1], 'normalize');
+p = legpoly([], [-1 0.66 1], 'normalize');
 pass(23) = isempty(p);
 
 end
