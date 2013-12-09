a7b1605326f516a77c8dc30aa91ffb88dddbd0bb
Kuan Xu <xuk@maths.ox.ac.uk>
2013-12-09 20:39:33 +0000

    Adding sign function to singfun and test it from chebfun level.

---
+++ @singfun/sign.m
@@ -0,0 +1,31 @@
+function g = sign(f, pref)
+%SIGN   Signum of a SINGFUN object.
+%   SIGN(F) returns the sign of F, where F is a SINGFUN object with no 
+%   roots in its domain. If F has roots, then SIGN(F) will return garbage
+%   with no warning.
+%
+%   For the nonzero elements of complex F, SIGN(F) = F./ ABS(F).
+%
+% See also ABS.
+
+% Copyright 2013 by The University of Oxford and The Chebfun Developers.
+% See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
+
+% Extract the smoothPart of F:
+g = f.smoothPart;
+    
+if ( isreal(f) )
+    % Pick a random point:
+    arbitraryPoint = 0.1273881594;
+
+    g.values = sign(feval(g, arbitraryPoint));
+    g.coeffs = g.values;
+else
+    if ( nargin == 1 )
+        pref = chebtech.techPref();
+    end
+    pref.extrapolate = 1;
+    g = compose(g, @(x) x./abs(x), [], pref);
+end
+
+end
