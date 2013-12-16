0e229867f87d222b332874867010e9a207d79420
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2013-12-16 18:07:55 +0000

    Introducing help text to the chebop class.

--- @chebop/chebop.m
+++ @chebop/chebop.m
@@ -1,6 +1,109 @@
 classdef (InferiorClasses = {?double}) chebop
 %CHEBOP  CHEBOP class for representing operators on functions defined on [a,b].
-   
+%
+% N = CHEBOP(OP) creates a chebop object N with operator defined by OP, which
+% should be a handle to a function, (often created using an anonymous function),
+% that accepts a chebfun or a chebmatrix consisting of chebfuns and scalars as
+% input and returns a chebfun (or chebmatrix). The first input argument to OP is
+% the independent variable X, while all others represent dependent functions of
+% X; if only one input argument is accepted by OP, it is the dependent variable.
+% In case of coupled systems, the function OP must return vertically, not
+% horizontally, concatenated elements.
+% Examples:
+%
+%   @(x, u) x.*diff(u) + u;                             % one dependent variable
+%   @(x, u, v, w) [ u.*diff(v); diff(u,2)+w; diff(w)-v ];     % 3 dependent vars
+%   @(x, u) [ u(:,1)-diff(u(:,2)); diff(u(:,1)).*u(:,2) ];          % chebmatrix
+%   @(u) diff(u,2) - exp(u);                  % no explicit independent variable
+%   @(u) [ diff(u(:,2))-u(:,1), diff(u(:,1)) ];        % no independent variable
+%
+% The number of elements in the output chebmatrix should equal the number of
+% dependent variables, whether specified as names or chebmatrix elements. For
+% systems of equations not given in chebmatrix form, the first input argument is
+% always the independent variable.
+%
+% By default, the operator acts on chebfuns defined on the domain [-1,1].
+% CHEBOP(OP, D), for a vector D, gives a different domain, with breakpoints (if
+% any) described by D.
+%
+% CHEBOP(OP, D, LBC, RBC) specifies boundary conditions for functions at the
+% left and right endpoints of the domain D. Possible values for LBC and RBC are:
+%
+%   []          : No condition (for only assigning LBC or RBC in constructor).
+%   scalar      : All variables equal the given value.
+%   'dirichlet' : All variables equal zero.
+%   'neumann'   : All variables have derivative zero.
+%   function    : A function handle that must accept all dependent variables as 
+%                 given in OP and return a chebfun or chebmatrix. All elements
+%                 of the result are evaluated at the endpoint, and for the
+%                 solution of the BVP, they are made to equal zero.
+%
+% A boundary condition function may be nonlinear; it must not accept the
+% independent variable X as an input. Again, in case of systems, the function
+% describing the boundary conditions must return vertically concatenated
+% elements. Some examples:
+%
+%   @(u) diff(u) - 2;               % set u' = 2 at the appropriate endpoint
+%   @(u, v, w) [ u - 1; w ];        % set u = 1 and w = 0 at the endpoint
+%   @(u) u(:,2) - diff(u(:,2));     % set u_2 - (u_2)' = 0 at the endpoint
+%   @(u, v, w) u.*v - diff(w)       % set u*v=w' at the endpoint
+%
+% CHEBOP(OP, D, BC) gives boundary or other side conditions in an alternate
+% form. Choices for BC are:
+%
+%   scalar      : All variables equal the given value at both endpoints.
+%   'dirichlet' : All variables equal zero at both endpoints.
+%   'neumann'   : All variables have derivative zero at both endpoints.
+%   'periodic'  : Impose periodicity on all dependent variables.
+%   function    : See below.
+%
+% Note that the 'dirichlet' and 'neumann' keywords impose behavior that may not
+% be identical to the common understanding of Dirichlet or Neumann conditions in
+% every problem.
+%
+% When BC is passed in the CHEBOP call, the more specialized fields LBC and RBC
+% are ignored.
+%
+% If BC is given a function handle, then each condition must give points
+% explicitly or otherwise evaluate to a scalar. The function handle must return
+% a column vector, not a row vector. Example:
+%    @(x, u) [ u(1) - u(0); sum(x.*u) ] % set u(1) = u(0), and definite integral
+%                                         of xu over the whole interval = 0
+%
+% BCs can also be assigned to the chebop N after it has been constructed,
+% by N.lbc = ..., N.rbc = ..., and N.bc = ... . This will overwrite the BCs
+% currently stored in the field being assigned to, but not the other
+% fields).
+%
+% CHEBOP(OP,...,'init',U) provides a chebfun/chebmatrix as a starting point for
+% nonlinear iterations or a PDE solution. See CHEBOP/SOLVEBVP and CHEBOP/PDE15S
+% for details.
+%
+% N = CHEBOP(..., 'dim', DIM) where DIM is an integer informs the chebop that it
+% operates on chebmatrices of dimension "(Inf x Dim) x 1".
+%
+% Note that many fields can be set after the chebop object N is created:
+% N.op, N.lbc, N.rbc, N.bc, N.init can all be assigned new values. For
+% example:
+%
+%    N = chebop(-5,5);  % Constructs an empty chebop on the interval [-5,5]
+%    N.op = @(x,u) 0.01*diff(u,2) - x.*u;
+%    N.bc = 'dirichlet';
+%    plot(N\1)
+%
+% There is some support for solving systems of equations containing unknown
+% parameters without the need to introduce extra equations into the system.
+% For example, y''+x.*y+p = 0, y(-1) = 1, y'(-1) = 1, y(1) = 1 can be
+% solved via
+%    N = chebop(@(x,y,p)diff(y,2)+x.*y+p,@(y,p)[y-1,diff(y)],@(y,p)y-1);
+%    plot(N\0)
+% This syntax will work whenever p is not differentiated within N.op, i.e.,
+% something like @(x,y,p) diff(p*diff(y)) will require a second equation
+% explicitly enforcing that diff(p) = 0.
+%
+% TODO: Revisit help text on parameter problems.
+%
+% See also chebop/mtimes, chebop/mldivide, cheboppref.   
     properties ( GetAccess = 'public', SetAccess = 'public' )
         domain = [];    % Domain of the operator
         op = [];        % The operator
