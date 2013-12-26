c9c08a6d5c62983cbb0b8f64cd08b5d69850b9fb
Kuan Xu <xukuan2011@gmail.com>
2013-12-26 14:04:32 +0000

    Substantial improvements in plot relevant methods, as suggested by AB.

--- @chebfun/plotData.m
+++ @chebfun/plotData.m
@@ -23,67 +23,178 @@ function data = plotData(f, g, h)
 
 % Initialise the output structure:
 data = struct('xLine', [], 'yLine', [], 'xPoints', [], 'yPoints', [], ...
-    'xJumps', [], 'yJumps', []);
+    'xJumps', [], 'yJumps', [], 'yLim', []);
 
-if ( nargin == 1 || isempty(g) )
+% Initialize the variable INTERVAL:
+interval = [];
+
+if ( nargin == 1 || ( ( nargin == 2 ) && ( isnumeric(g) ) ) )
     % PLOT(F)
     
+    % Grab the interval:
+    if ( ~isempty(g) )
+        interval = g;
+    end
+    
+    % Overhead:
+    nFuns = numel(f.funs);
+    ymax = zeros(1,nFuns);
+    ymin = zeros(1,nFuns);
+
     % Loop over each FUN for Line and Points data:
-    for k = 1:numel(f.funs)
+    for k = 1:nFuns
         % Get the data from the FUN:
         dataNew = plotData(f.funs{k});
         myNaN = NaN(1, size(dataNew.yLine, 2)); % Array of NaNs.
+        
+        if ( k == 1 )
+            dataNew.xJumps(1) = [];
+            dataNew.yJumps(1,:) = [];
+        end
+        
+        if ( k == nFuns )
+            dataNew.xJumps(end) = [];
+            dataNew.yJumps(end,:) = [];
+        end
+        
+        if ( any( ~isreal( dataNew.yLine ) ) )
+            % Deal with complex-valued functions:
+            
+            % Assign x to be the real part, and y to be the imaginary part:
+            dataNew.xLine = real(dataNew.yLine);
+            dataNew.yLine = imag(dataNew.yLine);
+            dataNew.xPoints = real(dataNew.yPoints);
+            dataNew.yPoints = imag(dataNew.yPoints);
+            dataNew.xJumps = real(dataNew.yJumps);
+            dataNew.yJumps = imag(dataNew.yJumps);
+            
+        elseif ( ~isempty(interval) && size(dataNew.xLine, 2) == 1 ...
+                && ( ( f.funs{k}.domain(1) < interval(1) ) || ...
+                ( interval(2) < f.funs{k}.domain(2) ) ) )
+            
+            % Deal with 'interval' flag.
+            idx = dataNew.xLine < interval(1) | dataNew.xLine > interval(end);
+            dataNew.xLine(idx) = [];
+            dataNew.yLine(idx,:) = [];
+            idx = dataNew.xPoints < interval(1) | dataNew.xPoints > interval(end);
+            dataNew.xPoints(idx) = [];
+            dataNew.yPoints(idx,:) = [];
+            idx = dataNew.xJumps < interval(1) | dataNew.xJumps > interval(end);
+            dataNew.xJumps(idx) = [];
+            dataNew.yJumps(idx,:) = [];
+        end
+        
         % Insert a NaN (or array of NaNs) and append new data to array:
         data.xLine = [data.xLine ; NaN ; dataNew.xLine];
         data.yLine = [data.yLine ; myNaN ; dataNew.yLine];
         data.xPoints = [data.xPoints ; NaN ; dataNew.xPoints];
         data.yPoints = [data.yPoints ; myNaN ; dataNew.yPoints];
+        data.xJumps = [data.xJumps ; NaN ; dataNew.xJumps];
+        data.yJumps = [data.yJumps ; myNaN ; dataNew.yJumps];
+        
+        % If any of the boundary values is positively infinite, then set the
+        % ylim for this piece to be the minimum of 10 and the largest yLine 
+        % data:
+        ymax(k) = max(max(dataNew.yLine));
+        
+        if ( dataNew.yLim(2) == Inf )
+            ymax(k) = min(10, ymax(k));
+        end
+        
+        % If any of the boundary values is negatively infinite, then set the
+        % ylim for this piece to be the maximum of -10 and the smallest yLine 
+        % data:
+        ymin(k) = min(min(dataNew.yLine));
+        
+        if ( dataNew.yLim(1) == -Inf )
+            ymin(k) = max(-10, ymin(k)); 
+        end
+
     end
     
-    % Return NaNs if there are no jumps:
-    data.xJumps = NaN;
-    data.yJumps = myNaN;
-    
-    % Loop over each FUN for Jumps data:
-    for k = 1:(numel(f.funs) - 1)
-        data.xJumps = [data.xJumps ; NaN ; f.funs{k}.domain(2) ; ...
-            f.funs{k+1}.domain(1)];
-        data.yJumps = [data.yJumps ; myNaN ; get(f.funs{k}, 'rval') ; ...
-            get(f.funs{k+1}, 'lval')];
-    end
-    
-elseif ( nargin == 2 )
+    % Take the maximum ymax to be the upper ylim for the entire CHEBFUN, while 
+    % take the minimum ymin to be the lower ylim. Then store yLim in data:
+    data.yLim = [min(ymin) max(ymax)];
+
+elseif ( nargin == 2 || ( ( nargin == 3 ) && ( isnumeric(h) ) ) )
     % PLOT(F, G)
+    
+    % Grab the interval:
+    if ( ~isempty(h) )
+        interval = h;
+    end
     
     % [TODO]: Fix this once OVERLAP() is implemented.
     if ( all( f.domain ~= g.domain ) )
         [f, g] = overlap(f, g);
     end
     
-    % Loop over each FUN for Line and Points data:
-    for k = 1:numel(f.funs)
+    % Overhead:
+    nFuns = numel(f.funs);
+    ymax = zeros(1,nFuns);
+    ymin = zeros(1,nFuns);
+    
+    for k = 1:nFuns
         % Get the data from the FUN objects:
         dataNew = plotData(f.funs{k}, g.funs{k});
-        myNaN = NaN(1, size(dataNew.yLine, 2)); % Array of NaNs.
+        
+        % Discard the unnecessary jump data:
+        if ( k == 1 )
+            dataNew.xJumps(1) = [];
+            dataNew.yJumps(1,:) = [];
+        end
+        
+        if ( k == nFuns )
+            dataNew.xJumps(end) = [];
+            dataNew.yJumps(end,:) = [];
+        end
+        
+        if ( ~isempty(interval) && size(dataNew.xLine, 2) == 1 ...
+                && ( ( f.funs{k}.domain(1) < interval(1) ) || ...
+                ( interval(2) < f.funs{k}.domain(2) ) ) )
+            
+            % Deal with 'interval' flag.
+            idx = dataNew.xLine < interval(1) | dataNew.xLine > interval(end);
+            dataNew.xLine(idx) = [];
+            dataNew.yLine(idx,:) = [];
+            idx = dataNew.xPoints < interval(1) | dataNew.xPoints > interval(end);
+            dataNew.xPoints(idx) = [];
+            dataNew.yPoints(idx,:) = [];
+            idx = dataNew.xJumps < interval(1) | dataNew.xJumps > interval(end);
+            dataNew.xJumps(idx) = [];
+            dataNew.yJumps(idx,:) = [];
+        end
+
+        % Array of NaNs:
+        myNaN = NaN(1, size(dataNew.yLine, 2)); 
+        
         % Insert a NaN (or array of NaNs) and append new data to array:
         data.xLine = [data.xLine ; myNaN ; dataNew.xLine];
         data.yLine = [data.yLine ; myNaN ; dataNew.yLine];
         data.xPoints = [data.xPoints ; myNaN ; dataNew.xPoints];
         data.yPoints = [data.yPoints ; myNaN ; dataNew.yPoints];
+        data.xJumps = [data.xJumps ; NaN ; dataNew.xJumps];
+        data.yJumps = [data.yJumps ; myNaN ; dataNew.yJumps];
     end
     
-    % Return NaNs if there are no jumps:
-    data.xJumps = NaN;
-    data.yJumps = myNaN;
+    % If any of the boundary values is positively infinite, then set the
+    % ylim for this piece to be 10:
+    ymax(k) = max(max(dataNew.yLine));
     
-    % Loop over each FUN for Jumps data:
-    for k = 1:(numel(f.funs) - 1)
-        % Append [oldData, NaN, rval_k, lval_{k+1}]:
-        data.xJumps = [data.xJumps ; myNaN ; get(f.funs{k}, 'rval') ; ...
-            get(f.funs{k+1}, 'lval')];
-        data.yJumps = [data.yJumps ; myNaN ; get(g.funs{k}, 'rval') ; ...
-            get(g.funs{k+1}, 'lval')];
+    if ( dataNew.yLim(2) == Inf )
+        ymax(k) = min(10, ymax(k));
     end
+    
+    % If any of the boundary values is negatively infinite, then set the
+    % ylim for this piece to be -10:
+    ymin(k) = min(min(dataNew.yLine));
+    
+    if ( dataNew.yLim(1) == -Inf )
+        ymin(k) = max(-10, ymin(k));
+    end
+    
+    % Store ylim:
+    data.yLim = [min(ymin) max(ymax)];
     
 else
     % PLOT(F, G, H)
@@ -97,13 +208,15 @@ else
     if ( any( f.domain ~= g.domain ) )
         [f, g] = overlap(f, g);
     end
+    
     if ( any( g.domain ~= h.domain ) )
         [g, h] = overlap(g, h);
         [h, f] = overlap(h, f);
     end
     
     % Loop over each FUN for Line and Points data:
-    for k = 1:numel(f.funs)
+    nFuns = numel(f.funs);
+    for k = 1:nFuns
         % Get the data from the FUN objects:
         dataNew = plotData(f.funs{k}, g.funs{k}, h.funs{k});
         myNaN = NaN(1, size(dataNew.yLine, 2)); % Array of NaNs.
@@ -121,7 +234,7 @@ else
     data.yJumps = myNaN;
     
     % Loop over each FUN for Jumps data:
-    for k = 1:(numel(f.funs) - 1)
+    for k = 1:(nFuns - 1)
         % Append [oldData, NaN, rval_k, lval_{k+1}]:
         data.xJumps = [data.xJumps ; myNaN ; get(f.funs{k}, 'rval') ; ...
             get(f.funs{k+1}, 'lval')];
@@ -132,6 +245,5 @@ else
     end
     
 end
-
 
 end
