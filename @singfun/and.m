aac950bd4cfb2f22697a00d21af00be88374d84f
Kuan Xu <xuk@maths.ox.ac.uk>
2013-12-28 20:39:31 +0000

    Corrections in documentation.

--- @singfun/and.m
+++ @singfun/and.m
@@ -1,13 +1,11 @@
 function h = and(f, g)
 %&   SINGFUN logical AND.
 %   F & G performs a logical AND of two SINGFUN objects F and G and returns a
-%   SINGFUN containing elements set to either logical 1 (TRUE) or logical 0
-%   (FALSE). An element of the output SINGFUN is set to 1 if both input
-%   SINGFUN objects have a non-zero element at that point, otherwise it is set
-%   to 0.  F and G must either be identically zero or have roots in their
-%   domains.  If this is not the case, garbage is returned with no warning.
-%   Note that no matter what singularities F and G have, the returned
-%   object H is a SMOOTHFUN without any singularity.   
+%   SMOOTHFUN which is set to either logical 1 (TRUE) or logical 0 (FALSE).
+%   The output SMOOTHFUN is set to 1 if both input SINGFUN objects have a 
+%   non-zero element at that point, otherwise it is set to 0.  F and G must 
+%   either be identically zero or have roots in their domains.  If this is not 
+%   the case, garbage is returned with no warning.   
 
 % Copyright 2013 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org for Chebfun information.
--- @singfun/logical.m
+++ @singfun/logical.m
@@ -1,6 +1,6 @@
 function f = logical(f)
 %LOGICAL   SINGFUN logical.
-%   LOGICAL(F) returns a SINGFUN which evaluates to one at all points where F
+%   LOGICAL(F) returns a SMOOTHFUN which evaluates to one at all points where F
 %   is nonzero and zero otherwise.  F cannot have any roots in its domain.  If
 %   F does have roots, then LOGICAL(F) will return garbage with no warning. F
 %   may be complex.
