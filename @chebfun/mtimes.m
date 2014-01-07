fa4c367d78a3c1f6353fcbec6d761ca06e383290
Nick Hale <nick.p.hale@gmail.com>
2014-01-07 19:17:51 +0000

    Quasimatrix tests for MTIMES() and PLUS().

--- @chebfun/mtimes.m
+++ @chebfun/mtimes.m
@@ -44,20 +44,20 @@ elseif ( isnumeric(g) )     % CHEBFUN * double
 
     else
         % QUASIMATRIX case:
-
         numCols = numel(f);
-        if ( length(g) ~= numCols || min(size(g)) ~= 1 )
+        if ( length(g) ~= numCols && min(size(g)) ~= 1 )
             error('CHEBFUN:plus:dims', 'Matrix dimensions must agree.');
         end
         % Transpose g if f is a row CHEBFUN:
         if ( f(1).isTransposed )
             g = g.';
         end
-        s = f(1)*g(1);
+        s = f(1)*g(1,:);
         % Loop over the columns:
         for k = 2:numCols
-            s = s + f(k).*g(k);
+            s = s + f(k).*g(k,:);
         end
+        f = s;
     end
 
 elseif ( ~isa(g, 'chebfun') )
@@ -68,30 +68,32 @@ elseif ( ~isa(g, 'chebfun') )
 else                        % CHEBFUN' * CHEBFUN
 
     % We can't do MTIMES() on two CHEBFUNs that have the same transpose state.
-    if ( f.isTransposed == g.isTransposed )
-        if ( ~all(size(f) == size(g)) )
+    if ( f(1).isTransposed == g(1).isTransposed )
+        if ( numColumns(f) ~= numColumns(g) )
             error('CHEBFUN:mtimes:dims', ...
                 ['Matrix dimensions must agree. Use f.*g to multiply ' ...
-                 'two chebfun objects.']);
+                 'two CHEBFUN objects.']);
         else
             error('CHEBFUN:mtimes:dims', ...
                 'Matrix dimensions must agree.');
         end
     end
 
-    if ( f.isTransposed && ~g.isTransposed ) % Row times column.
-        % Overlap:
-        [f, g] = overlap(f, g);
-
-        % Compute the inner product (we call CONJ() here because INNERPRODUCT()
-        % is semilinear in the first factor, and we want to undo that):
-        S = 0;
-        for k = 1:numel(f.funs)
-            S = S + innerProduct(conj(f.funs{k}), g.funs{k});
-        end
-
-        % Output in f:
-        f = S;
+    if ( f(1).isTransposed && ~g(1).isTransposed ) % Row times column.
+        
+        f = innerProduct(conj(f), g);
+%         % Overlap:
+%         [f, g] = overlap(f, g);
+% 
+%         % Compute the inner product (we call CONJ() here because INNERPRODUCT()
+%         % is semilinear in the first factor, and we want to undo that):
+%         S = 0;
+%         for k = 1:numel(f.funs)
+%             S = S + innerProduct(conj(f.funs{k}), g.funs{k});
+%         end
+% 
+%         % Output in f:
+%         f = S;
     else                                     % Column times row.
         % [TODO]:  Implement (and document) this once we have CHEBFUN2.
         error('CHEBFUN:mtimes:colTimesRow', ...
