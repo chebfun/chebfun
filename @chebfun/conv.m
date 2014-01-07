d0e54b0a1a5ccd0d33064802a9c69e71424c23d5
Anthony P. Austin <austin@maths.ox.ac.uk>
2014-01-07 17:03:47 +0000

    Minor style fixes for chebfun.conv() and its test.

--- @chebfun/conv.m
+++ @chebfun/conv.m
@@ -1,21 +1,21 @@
 function h = conv(f, g)
 %CONV   Convolution of CHEBFUN objects.
-% H = CONV(F, G) produces the convolution of CHEBFUN objects F and G:
+%   H = CONV(F, G) produces the convolution of CHEBFUN objects F and G:
+%                     -
+%                    /
+%           H(x) =   |    F(t) G(x - t) dt,
+%                    /
 %                   -
-%                  /
-%         H(x) =   |    F(t) G(x-t) dt,
-%                  /
-%                 -
-% defined for x in [a+c, b+d], where F.domain is [a,b] and G.domain is [c, d].
-% The integral is taken over all t for which the integrand is defined: max(a,
-% x-d) <= t <= min(b, x-c), and the breakpoints of H are all pairwise sums of
-% the breakpoints of F and G.
+%   defined for x in [a+c, b+d], where F.domain is [a,b] and G.domain is [c, d].
+%   The integral is taken over all t for which the integrand is defined: max(a,
+%   x-d) <= t <= min(b, x-c), and the breakpoints of H are all pairwise sums of
+%   the breakpoints of F and G.
 %
-% Example:
-%   f = chebfun(1/2); g = f;
-%   subplot(2, 2, 1), plot(f)
-%   for j = 2:4, g = conv(f, g); subplot(2, 2, j), plot(g), end
-%   figure, for j = 1:4, subplot(2, 2, j), plot(g), g = diff(g); end
+%   Example:
+%     f = chebfun(1/2); g = f;
+%     subplot(2, 2, 1), plot(f)
+%     for j = 2:4, g = conv(f, g); subplot(2, 2, j), plot(g), end
+%     figure, for j = 1:4, subplot(2, 2, j), plot(g), g = diff(g); end
 
 % Copyright 2013 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
@@ -30,7 +30,8 @@ if ( xor(f.isTransposed, g.isTransposed) )
     error('CHEBFUN:conv:transposed', 'CHEBFUN dimensions do not agree.');
 end
 
-if ( min(size(f)) > 1 || min(size(g)) > 1 ) % TODO
+% TODO:  Support for array-valued CHEBFUNs / quasimatrices.
+if ( min(size(f)) > 1 || min(size(g)) > 1 )
     error('CHEBFUN:conv:quasi', 'No support for array-valued CHEBFUN objects.');
 end
 
@@ -47,10 +48,10 @@ end
 
 % Find all breakpoints in the convolution:
 [A, B] = meshgrid(f.domain, g.domain);
-dom = unique( A(:) + B(:) ).';
+dom = unique(A(:) + B(:)).';
 
 % Coalesce breaks that are close due to roundoff:
-dom( diff(dom) < 10*eps*max(abs(dom([1,end]))) ) = [];
+dom(diff(dom) < 10*eps*max(abs(dom([1,end])))) = [];
 dom(isnan(dom)) = [];
 
 if ( any(isinf(dom)) )
@@ -70,16 +71,10 @@ p.techPrefs.extrapolate = true;
 p.techPrefs.resampling = false;
 p.techPrefs.sampletest = false;
 
-% For convenience:
-a = f.domain(1); 
-b = f.domain(end); 
-c = g.domain(1); 
-d = g.domain(end);
-
 % Construct FUNS:
 funs = cell(1, length(dom)-1);
 for k = 1:length(dom)-1  
-    newFun = bndfun(@(x) convIntegral(x, a, b, c, d, f, g), dom(k:k+1), vs, hs, p);
+    newFun = bndfun(@(x) convIntegral(x, f, g), dom(k:k+1), vs, hs, p);
     vs = max(get(newFun, 'vscale'), vs); 
     funs{k} = newFun;
 end
@@ -93,18 +88,28 @@ h.isTransposed = f.isTransposed;
 
 end
 
-function out = convIntegral(x, a, b, c, d, f, g)
+function out = convIntegral(x, f, g)
+%CONVINTEGRAL   Evaluate convolution integral.
+%   Y = CONVINTEGRAL(X, F, G) evaluates the convolution of the CHEBFUNs F and G
+%   at the points X.
+
+a = f.domain(1);
+b = f.domain(end);
+c = g.domain(1);
+d = g.domain(end);
+
 out = 0*x;
 for k = 1:length(x)
     A = max(a, x(k) - d); 
     B = min(b, x(k) - c);
     if ( A < B )
         ends = union(x(k) - g.domain, f.domain);
-        dom = [A, ends(A < ends & ends < B),  B];
+        dom = [A, ends((A < ends) & (ends < B)), B];
         for j = 1:length(dom)-1
             out(k) = out(k) + integral(@(t) feval(f, t).*feval(g, x(k) - t), ...
                 dom(j), dom(j+1), 'AbsTol', 1e-15, 'RelTol', 1e-15);
         end
     end
 end
+
 end
--- tests/chebfun/test_conv.m
+++ tests/chebfun/test_conv.m
@@ -1,3 +1,5 @@
+% Test file for @chebfun/conv.m.
+
 function pass = test_conv(pref)
 
 % Grab some preferences
@@ -6,31 +8,31 @@ if ( nargin == 0 )
 end
 
 % Construct a few CHEBFUN objects for the tests.
-f = chebfun('x', [-1 1]);
-g = chebfun('sin(5*x)', [2 4]);
-h = chebfun('cos(2*x)', [-3 1]);
-p = chebfun('sin(15*x)', [-1 1]);
-q = chebfun('exp(cos(3*x))', [-1 1]);
+f = chebfun(@(x) x, [-1 1]);
+g = chebfun(@(x) sin(5*x), [2 4]);
+h = chebfun(@(x) cos(2*x), [-3 1]);
+p = chebfun(@(x) sin(15*x), [-1 1]);
+q = chebfun(@(x) exp(cos(3*x)), [-1 1]);
 
-%% 1. test the commutativity
+%% 1. Test the commutativity.
 H1 = conv(f, g);
 H2 = conv(g, f);
 pass(1) = norm(H1 - H2) < 100*epslevel(H1);
 
-%% 2. test the associativity
+%% 2. Test the associativity.
 H1 = conv(conv(f, g), h);
 H2 = conv(f, conv(g, h));
 pass(2) = norm(H1 - H2) < 100*epslevel(H1);
 
-%% 3. test the distributivity
+%% 3. Test the distributivity.
 H1 = conv(f, (p + q));
 H2 = conv(f, p) + conv(f, q);
 pass(3) = norm(H1 - H2) < 100*epslevel(H1);
 
-%% 4. test B-splines 
-% Generates piecewise polynomial cardinal B-splines by a process of convolution,
-% with CONV, then estimates the error compared to the exact solution B, using
-% Chebfun's NORM command. For more on generating B-splines using convolution,
+%% 4. Test B-splines.
+% Generates piecewise polynomial cardinal B-splines by a process of
+% convolution, with CONV, then estimates the error compared to the exact
+% solution B, using NORM. For more on generating B-splines using convolution,
 % see e.g. http://en.wikipedia.org/wiki/B-spline
 B = (1/6)*chebfun( {@(x) (2+x).^3, ...
     @(x)1 + 3*(1+x) + 3*(1+x).^2 - 3*(1+x).^3, ...
@@ -38,15 +40,17 @@ B = (1/6)*chebfun( {@(x) (2+x).^3, ...
     @(x)(2-x).^3}, -2:2 );
 s = chebfun(1, [-.5 .5]);
 f = s;
-for k = 1:3, f = conv(f, s); end
-pass(4) = norm( f - B ) < 100*epslevel(f)*vscale(f);
+for k = 1:3
+    f = conv(f, s);
+end
+pass(4) = norm(f - B) < 100*epslevel(f)*vscale(f);
 
-%% 5. test more splines
+%% 5. Test more splines.
 f = chebfun(1/2); 
 g = f;
-for j = 2:4, g = conv(f, g); end
-% pass(5) = numel(g.domain) == 5 && all(g.domain == -4:2:4) && ...
-%     abs(feval(g, 1) - 23/96) < 100*epslevel(g);
+for j = 2:4
+    g = conv(f, g);
+end
 pass(5) = abs(feval(g, 1) - 23/96) < 100*epslevel(g);
 
 end
