81d6e892dc5133f8c0fb1c55603a6b2d790458ad
Nick Hale <nick.p.hale@gmail.com>
2014-01-08 14:03:35 +0000

    Minor change to HORZCAT that was missedin rebasing.

--- @chebfun/horzcat.m
+++ @chebfun/horzcat.m
@@ -68,7 +68,7 @@ if ( any(diff(cellfun(@(d) length(d), allDomainsCell))) )
     differentBreakpoints = true;
 else
     tol = max(cellfun(@(f) hscale(f).*epslevel(f), varargin));
-    if ( any(cellfun(@(d) any(d - domainEnds) > tol, allDomainsCell)) )
+    if ( any(cellfun(@(d) any(d - domain1) > tol, allDomainsCell)) )
         differentBreakpoints = true;
     end
 end
