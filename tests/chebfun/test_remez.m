7cb71c407729cf9a5c14b644df24dd42450af711
Anthony P. Austin <austin@maths.ox.ac.uk>
2014-01-08 17:30:47 +0000

    Expand test for chebfun.remez() to cover rational case.
    We can't do much with this, as the algorithm isn't very robust, but this ought
    to suffice for now.

--- tests/chebfun/test_remez.m
+++ tests/chebfun/test_remez.m
@@ -3,6 +3,10 @@
 
 function pass = remeztest(pref)
 
+% Generate a few random points to use as test values.
+seedRNG(6178);
+xx = 2 * rand(100, 1) - 1;
+
 x = chebfun(@(x) x, [-1 1]);
 
 % Test known exact answer.
@@ -12,10 +16,21 @@ pbest = remez(f, 1);
 err = norm(pbest - pexact);
 pass(1) = err < 1e-10;
 
-% Test remez and cf.
+% Test remez() and cf().
 f = exp(sin(exp(x)));
 pcf = cf(f, 7);
 pbest = remez(f, 7);
 pass(2) = norm(pcf - pbest) < 0.0003;
+
+% Test rational mode.
+f = ((x + 3).*(x - 0.5))./(x.^2 - 4);
+[pbest, qbest, rbest] = remez(f, 2, 2);
+pass(3) =  norm(f(xx) - rbest(xx), inf) < 1e-10;
+
+% Test an example with a breakpoint.
+x = chebfun(@(x) x, [-1 0 1]);
+f = ((x - 3).*(x + 0.2).*(x - 0.7))./((x - 1.5).*(x + 2.1));
+[pbest, qbest, rbest] = remez(f, 3, 2);
+pass(4) =  norm(f(xx) - rbest(xx), inf) < 1e-10;
 
 end
