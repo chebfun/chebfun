e8a7ac7585c13e370396a62dc3308d57f772f410
Anthony P. Austin <austin@maths.ox.ac.uk>
2014-01-10 18:59:31 +0000

    A few more minor style changes.

--- @chebfun/chebellipseplot.m
+++ @chebfun/chebellipseplot.m
@@ -1,30 +1,29 @@
-function varargout = chebellipseplot(u,varargin)
+function varargout = chebellipseplot(u, varargin)
 %CHEBELLIPSEPLOT   Plot the Bernstein (aka Chebyshev) ellipses.
+%   CHEBELLIPSEPLOT(U) plots Bernstein ellipses in the complex plane for each
+%   piecewise part of U, with foci at points in U.domain and semi-minor and
+%   major axes summing to rho(k) = C*exp(abs(log(EPS))/N(k)), where C is the
+%   appropriate scaling for the interval [U.ends(k) U.ends(k+1)] and EPS is the
+%   EPSLEVEL of U.
 %
-%   CHEBELLIPSEPLOT(U,EPS) plots ellipses in the complex plane for each 
-%   piecewise part of U, with foci at points in U.domain and semi-minor
-%   and major axes summing to rho(k) = C*exp(abs(log(EPS))/N(k)), where
-%   C is the appropriate scaling for the interval [U.ends(k) U.ends(k+1)]
-%   and EPS is the EPSLEVEL of U.
+%   CHEBELLIPSEPLOT(U, EPS) allows a user-specified EPS.
 %
-%   CHEBELLIPSEPLOT(U,EPS) allows a user-specified EPS.
-%
-%   CHEBELLIPSEPLOT(U,K) and CHEBELLIPSEPLOT(U,EPS,K) plot ellipses for
+%   CHEBELLIPSEPLOT(U, K) and CHEBELLIPSEPLOT(U, EPS, K) plot ellipses for
 %   the funs of U indexed by the vector K. If U is a quasimatrix, only
 %   the first column/row is considered.
 %
-%   CHEBELLIPSEPLOT(U,...,S) allows plotting options to be passed. For
-%   example, for black lines one may write CHEBELLIPSEPLOT(U,'k-').
+%   CHEBELLIPSEPLOT(U, ..., S) allows plotting options to be passed. For
+%   example, for black lines one may write CHEBELLIPSEPLOT(U, 'k-').
 %
-%   CHEBELLIPSEPLOT(U,...,'legends',0) prevents the legends being
+%   CHEBELLIPSEPLOT(U, ..., 'legends', 0) prevents the legends being
 %   displayed on the plot.
 %
-%   CHEBELLIPSEPLOT(U,...,'numpts',N) plots each ellipse using N points.
+%   CHEBELLIPSEPLOT(U, ..., 'numpts', N) plots each ellipse using N points.
 %
 %   H = CHEBELLIPSEPLOT(U) returns a handle H to the figure.
 %
 %   Example:
-%       u = chebfun({ @sin @cos @tan @cot }, [-2,-1,0,1,2]);
+%       u = chebfun({@sin, @cos, @tan, @cot}, [-2, -1, 0, 1, 2]);
 %       chebellipseplot(u, sqrt(eps), '--');
 
 % Copyright 2014 by The University of Oxford and The Chebfun Developers.
@@ -80,12 +79,12 @@ end
 
 % Error if index exceeds dimensions.
 if ( any(k > length(u.funs)) )
-    error( 'CHEBFUN:chebellipseplot:outofbounds', ...
+    error( 'CHEBFUN:chebellipseplot:outOfBounds', ...
         'Input chebfun has only %d pieces', length(u.funs) );
 end
 
 % The unit circle.
-c = exp(2*pi*1i*linspace(0,1,numpts));
+c = exp(2*pi*1i*linspace(0, 1, numpts));
 
 % A cell array of ellipse coordinates to be plotted.
 UK = {};
@@ -94,7 +93,7 @@ for j = k
     endsk = uk.domain;
     rhok = exp(abs(log(ee)) / length(uk));
     ek = .5*sum(endsk) + .25*diff(endsk)*(rhok*c + 1./(rhok*c));
-    UK = [ UK, { real(ek), imag(ek) }, varargin ]; % Add the variable args.
+    UK = [UK, {real(ek), imag(ek)}, varargin]; % Add the variable args.
 end
 
 % Plot the ellipses.
