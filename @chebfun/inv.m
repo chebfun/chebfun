197894bf1d6d65e7959f224eff5d785a85887ecb
Anthony P. Austin <austin@maths.ox.ac.uk>
2014-01-12 13:34:51 +0000

    Refactor inv() and inv2().
    inv2.m was basically just a copy of inv.m with just the body of the
    fInverse() routine changed.  All code has now been moved into inv.m, and
    you can select between the different options for fInverse via a new
    'algorithm' flag.  The body of inv2() has been replaced with a call to
    inv() with this flag set appropriately.
    
    All-in-all, this results in about 50-60 fewer lines of code than the
    previous configuration.

--- @chebfun/inv.m
+++ @chebfun/inv.m
@@ -1,141 +1,235 @@
 function g = inv(f, varargin)
 %INV   Invert a CHEBFUN.
-%   INV(F) will attempt to invert the monotonic chebfun F. If F has zero
-%   derivatives at its endpoints, then it is advisable to turn Splitting ON.
+%   FINV = INV(F) attempts to compute the inverse of the monotonic CHEBFUN F.
 %
-%   INV(F, 'SPLITTING', 'ON') turns Splitting ON locally for the inv command.
+%   FINV =INV(F, PREF) uses the preferences specified by the structure or
+%   CHEBPREF object PREF when constructing the inverse.
 %
-%   INV(F, 'EPS', TOL) will construct with the relative tolerance set by TOL. If
-%   no tolerance is passed, TOL = chebfunpref('eps') is used.
+%   FINV = INV(..., 'SPLITTING', 'ON') enables breakpoint detection locally for
+%   INV.  Setting this option (or the equivalent preference in PREF) is
+%   particularly advisable if F has zero derivatives at its endpoints.
 %
-%   INV(F, 'MONOCHECK', 'ON'/'OFF') turns the check for monotonicity ON or OFF
-%   respectively. It is OFF by default.
+%   FINV = INV(..., 'EPS', TOL) will construct with the relative tolerance set
+%   by TOL.  If no tolerance is passed, TOL = EPSLEVEL(F) is used.
 %
-%   G = INV(F, 'RANGECHECK', 'ON'/'OFF') enforces that the range of G exactly
-%   matches the domain of F (by adding a linear function) or not. RANGECHECK OFF
-%   is the default behaviour.
+%   FINV = INV(..., 'MONOCHECK', 'ON') enables an optional check for
+%   monotonicity.
 %
-%   Any of the preferences above can be used in tandem.
+%   FINV = INV(..., 'RANGECHECK', 'ON') enforces that the range of FINV exactly
+%   matches the domain of F (by adding a linear function).
 %
-% Example: 
-%   x = chebfun('x');
-%   f = sign(x) + x;
-%   g = inv(f, 'splitting', true);
+%   FINV = INV(..., 'ALGORITHM', ALGSTR) selects the algorithm used to compute
+%   the values of the inverse of F.  Possible values for ALGSTR are:
+%      'ROOTS'  - Compute the inverse using ROOTS().
+%      'NEWTON' - Compute the inverse using a Newton iteration.
+%   The default algorithm is 'ROOTS'.  For a more concise syntax for accessing
+%   the 'NEWTON' algorithm, see INV2.
 %
-%   Note, this function is experimental and slow! INV may be the better
-%   choice for piecewise functions, where as INV2 is good for smooth
-% f unctions.
+%   Any of the name-value option pairs listed above can be used in tandem.
+%
+%   Example:
+%      x = chebfun('x');
+%      f = sign(x) + x;
+%      g = inv(f, 'splitting', 'on');
+%
+%   NB:  This function is experimental and slow!  Use of the 'ROOTS' algorithm
+%   (default) may be the better choice for piecewise functions, whereas the
+%   'NEWTON' algorithm is good for smooth functions.
 %
 % See also INV2.
 
-% Copyright 2013 by The University of Oxford and The Chebfun Developers. 
+% Copyright 2014 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
-
-% [TODO]: This method requires a test.
 
 % No quasimatrix support:
 if ( min(size(f)) > 1 )
-    error('CHEBFUN:inv:noquasi', 'No support for array0valued CHEBFUN objects.');
+    error('CHEBFUN:inv:noquasi', ...
+        'INV does not support array-valued CHEBFUN objects or quasimatrices.');
 end
 
-% Default options:
-if ( nargin > 1 && isa(varargin{1}, 'chebpref') )
-    p = varargin{1};
-    varargin{1} = [];
-else
-    p = chebpref;
-end
-splitYesNo = p.enableSingularityDetection;
-tol = epslevel(f);
-monoCheck = false;
-rangeCheck = false;
-
-% Turn splitting on if F is piecewise:
-if ( length(f.domain) > 2 && ~splitYesNo )
-    splitYesNo = true;
-end
-
-% Parse input
-while ( numel(varargin) > 1 )
-    if ( strcmpi(varargin{1}, 'splitting') )
-        splitYesNo = checkOnOff(varargin{2});
-    elseif ( strcmpi(varargin{1}, 'eps') )
-        tol = varargin{2};
-    elseif ( strcmpi(varargin{1}, 'monocheck') )
-        monoCheck = checkOnOff(varargin{2});
-    elseif ( strcmpi(varargin{1}, 'rangecheck') )
-        rangeCheck = checkOnOff(varargin{2});
-    else
-        error('CHEBFUN:inv:inputs', ...
-            [varargin{1}, 'is an unrecognised input to INV().']);
-    end
-    varargin(1:2) = [];
-end
+% Parse the inputs:
+[tol, opts, pref] = parseInputs(f, varargin{:});
 
 % Compute the derivative:
 fp = diff(f);
 
 % Monotonicity check:
-if ( monoCheck )
-    tPoints = roots(fp);
-    if ( ~isempty(tPoints) )
-        endtest = zeros(length(tPoints), 1);
-        for k = 1:length(tPoints)
-            endtest(k) = min(abs(tPoints(k) - f.domain));
-        end
-        if ( any(endtest > 100*abs(feval(f, tPoints))*tol) )
-            error('CHEBFUN:inv:notmonotonic', 'chebfun F must be monotonic its domain.');
-        elseif ( ~splitYesNo )
-            warning('CHEBFUN:inv:singularendpoints', ['F is monotonic, but ', ...
-                'INV(F) has singular endpoints. Suggest you try ''splitting on''.']);
-        end
-    end
+if ( opts.monoCheck )
+    doMonoCheck(f, fp, tol, pref.enableBreakpointDetection);
 end
 
 % Compute the inverse:
 gDomain = minandmax(f).';
-x = chebfun(@(x) x, gDomain);
-g = chebfun(@(x) fInverse(f, x), gDomain, 'resampling', 0, 'splitting', splitYesNo, 'eps', tol);
+if ( opts.algorithm == 1 )     % Algorithm based on ROOTS.
+    g = chebfun(@(x) fInverseRoots(f, x), gDomain, pref);
+elseif ( opts.algorithm == 2 ) % Newton iteration algorithm.
+    g = chebfun(@(x) fInverseNewton(f, fp, x, tol), gDomain, pref);
+end
 
 % Scale so that the range of g is the domain of f:
-if ( rangeCheck )
-    [gRange, gx] = minandmax(g);
-    g = g + (gx(2)-x)*(f.domain(1) - gRange(1))/diff(gx) ...
-          + (x-gx(1))*(f.domain(end) - gRange(2))/diff(gx);
+if ( opts.rangeCheck )
+    g = adjustRange(g, f.domain);
 end
-  
-    function y = fInverse(f, x)
-        % FINVERSE(F, X) attemptes to return the values of F^{-1}(X) using
-        % CHEBFUN/ROOTS().
-        y = zeros(length(x), 1);
-        % Vectorise:
-        for j = 1:length(x)
-            temp = roots(f - x(j));
-            if ( length(temp) ~= 1 )
-                fvals = feval(f, f.domain);
-                err = abs(fvals - x(j));
-                [temp, k] = min(err);
-                if ( err(k) > 100*tol*abs(fvals(k)))
-                    error('CHEBFUN:inv:notmonotonic2', 'Chebfun must be monotonic.');
-                end
-            end
-            y(j, 1) = temp;
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Input parsing.
+
+function [tol, opts, pref] = parseInputs(f, varargin)
+
+% Default options:
+tol = epslevel(f);
+opts.monoCheck = false;
+opts.rangeCheck = false;
+opts.algorithm = 1;
+
+% Parse preference input:
+if ( (nargin > 1) && isa(varargin{1}, 'chebpref') )
+    pref = varargin{1};
+    varargin(1) = [];
+else
+    pref = chebpref();
+end
+
+% Enable breakpoint detection if F is piecewise:
+if ( length(f.domain) > 2 )
+    p.enableBreakpointDetection = true;
+end
+
+% Parse name/value pairs.
+while ( numel(varargin) > 1 )
+    if ( strcmpi(varargin{1}, 'splitting') )
+        pref.enableBreakpointDetection = checkOnOff(varargin{2});
+    elseif ( strcmpi(varargin{1}, 'eps') )
+        tol = varargin{2};
+    elseif ( strcmpi(varargin{1}, 'monocheck') )
+        opts.monoCheck = checkOnOff(varargin{2});
+    elseif ( strcmpi(varargin{1}, 'rangecheck') )
+        opts.rangeCheck = checkOnOff(varargin{2});
+    elseif ( strcmpi(varargin{1}, 'algorithm') )
+        if ( strcmpi(varargin{2}, 'roots') )
+            opts.algorithm = 1;
+        elseif ( strcmpi(varargin{2}, 'newton') )
+            opts.algorithm = 2;
+        else
+            error('CHEBFUN:inv:badAlgo', ...
+                'Unrecognized value for ''algorithm'' input.');
         end
+    else
+        error('CHEBFUN:inv:inputs', ...
+            [varargin{1} ' is an unrecognised input to INV().']);
     end
+    varargin(1:2) = [];
+end
+
+% Assign preferences:
+pref.techPrefs.resampling = 1;
+pref.techPrefs.eps = tol;
+pref.techPrefs.minsamples = length(f);
+pref.techPrefs.sampleTest = 0;
 
 end
 
 function value = checkOnOff(value)
-    if ( ischar(value) )
-        % If ON or OFF used -> change to true or false.
-        if ( strcmpi(value,'on') )
-            value = true;
-        elseif ( strcmpi(value,'off') )
-            value = false; 
-        end
-    else
-        value = logical(value);
+%CHECKONOFF   Convert 'on' and 'off' strings to their logical equivalents.
+
+if ( ischar(value) )
+    if ( strcmpi(value,'on') )
+        value = true;
+    elseif ( strcmpi(value,'off') )
+        value = false;
+    end
+else
+    value = logical(value);
+end
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Monotonicity and range checks.
+
+function doMonoCheck(f, fp, tol, splitYesNo)
+
+tPoints = roots(fp);
+if ( ~isempty(tPoints) )
+    endtest = zeros(length(tPoints), 1);
+    for k = 1:length(tPoints)
+        endtest(k) = min(abs(tPoints(k) - f.domain));
+    end
+    if ( any(endtest > 100*abs(feval(f, tPoints))*tol) )
+        error('CHEBFUN:inv:notMonotonic', ...
+            'F must be monotonic on its domain.');
+    elseif ( ~splitYesNo )
+        warning('CHEBFUN:inv:singularEndpoints', ...
+            ['F is monotonic, but its inverse has singular endpoints. ' ...
+             'Enabling breakpoint detection is advised.']);
     end
 end
 
+end
 
+function g = adjustRange(g, fDomain)
+
+x = chebfun(@(x) x, g.domain);
+[gRange, gx] = minandmax(g);
+g = g + (gx(2) - x)*(fDomain(1) - gRange(1))/diff(gx) ...
+      + (x - gx(1))*(fDomain(end) - gRange(2))/diff(gx);
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function y = fInverseRoots(f, x)
+%FINVERSEROOTS(F, X)   Compute F^{-1}(X) using CHEBFUN.ROOTS().
+
+y = zeros(length(x), 1);
+% Vectorise:
+for j = 1:length(x)
+    temp = roots(f - x(j));
+    if ( length(temp) ~= 1 )
+        fvals = feval(f, f.domain);
+        err = abs(fvals - x(j));
+        [temp, k] = min(err);
+        if ( err(k) > 100*tol*abs(fvals(k)))
+            error('CHEBFUN:inv:notmonotonic2', 'f must be monotonic.');
+        end
+    end
+    y(j, 1) = temp;
+end
+
+end
+
+function y = fInverseNewton(f, fp, x, tol)
+%FINVERSENEWTON(F, X)   Compute F^{-1}(X) using a Newton iteration.
+
+tol = tol/5;
+y = zeros(length(x), 1);
+
+% If the sample points are dense enough in the construction domain that our
+% initial guesses scheme for the Newton iteration will work (see below), use
+% that.  Otherwise, compute the inverse with ROOTS.
+if ( length(x) >= length(f) )
+    t = f.domain(1);
+    for j = 1:length(x);
+        % Do Newton iteration with solution for previous sample point as the
+        % starting guess:
+        ft = feval(f, t) - x(j);
+        counter = 0;
+        while ( abs(ft) > tol )
+            fpt = feval(fp, t);
+            t = t - ft./fpt;
+            ft = feval(f, t) - x(j);
+            counter = counter + 1;
+            if ( counter > 10 )
+                % TODO:  Issue a warning here?
+                break;
+            end
+        end
+        y(j,1) = t;
+    end
+else
+    y = fInverseRoots(f, x);
+end
+
+end
--- @chebfun/inv2.m
+++ @chebfun/inv2.m
@@ -1,173 +1,21 @@
 function g = inv2(f, varargin)
-%INV2  Invert a CHEBFUN.
-%   INV2(F) will attempt to invert the monotonic chebfun F. If F has zero
-%   derivatives at its endpoints, then it is advisable to turn Splitting ON.
+%INV2   Invert a CHEBFUN.
+%   FINV = INV2(F, ...) is a shortcut for FINV = INV(F, ..., 'ALGORITHM',
+%   'NEWTON').  For other valid input parameters, se INV.
 %
-%   INV2(F,'SPLITTING','ON') turns Splitting ON locally for the inv command.
+%   Example:
+%      f = chebfun(@(x) tanh(7*x)./tanh(7) + 1, [-.5, .5]);
+%      g = inv2(f, 'splitting', 'off', 'rangecheck', 'off', 'monocheck', 'off');
 %
-%   INV2(F,'EPS',TOL) will construct with the relative tolerance set by TOL. If
-%   no tolerance is passed, TOL = 100*chebfunpref('eps') is used. EPS should be
-%   set to at least a factor of 100 larger than the accuracy of F.
-%
-%   INV2(F,'MONOCHECK','ON'/'OFF') turns the check for monotonicity ON or OFF
-%   respectively. It is OFF by default.
-%
-%   G = INV2(F,'RANGECHECK','ON'/'OFF') enforces that the range of G exactly
-%   matches the domain of F (by adding a linear function) or not. RANGECHECK OFF
-%   is the default behaviour.
-%
-%   Any of the preferences above can be used in tandem.
-%
-% Example:
-%   f = chebfun(@(x) tanh(7*x)./tanh(7) + 1, [-.5, .5]);
-%   g = inv2(f, 'splitting', 'off', 'rangecheck', 'off', 'monocheck', 'off');
-%
-% Note, this function is experimental and slow! INV may be the better choice for
-% piecewise functions, where as INV2 is good for smooth functions.
+%   NB:  This function is experimental and slow!  Use of INV with the default
+%   'ROOTS' algorithm may be the better choice for piecewise functions, whereas
+%   the 'NEWTON' algorithm used by INV2 is good for smooth functions.
 %
 % See also INV.
 
-% Copyright 2013 by The University of Oxford and The Chebfun Developers. 
+% Copyright 2014 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
 
-% [TODO]: This method requires a test.
+g = inv(f, varargin{:}, 'algorithm', 'newton');
 
-% No quasimatrix support!
-if ( min(size(f)) > 1)
-    error('CHEBFUN:inv:noquasi','No support for array-valued CHEBFUN objects.');
-end
-
-% Default options:
-if ( nargin > 1 && isa(varargin{1}, 'chebpref') )
-    p = varargin{1};
-    varargin{1} = [];
-else
-    p = chebpref;
-end
-splitYesNo = p.enableSingularityDetection;
-tol = epslevel(f);
-monoCheck = false;
-rangeCheck = false;
-
-% Turn splitting on if F is piecewise:
-if ( length(f.domain) > 2 && ~splitYesNo )
-    splitYesNo = true;
-end
-
-% Parse input
-while ( numel(varargin) > 1 )
-    if ( strcmpi(varargin{1}, 'splitting') )
-        splitYesNo = checkOnOff(varargin{2});
-    elseif ( strcmpi(varargin{1}, 'eps') )
-        tol = varargin{2};
-    elseif ( strcmpi(varargin{1}, 'monocheck') )
-        monoCheck = checkOnOff(varargin{2});
-    elseif ( strcmpi(varargin{1}, 'rangecheck') )
-        rangeCheck = checkOnOff(varargin{2});
-    else
-        error('CHEBFUN:inv:inputs', ...
-            [varargin{1}, 'is an unrecognised input to INV().']);
-    end
-    varargin(1:2) = [];
-end
-
-% Compute the derivative:
-fp = diff(f);
-
-% Monotonicity check:
-if ( monoCheck )
-    tPoints = roots(fp);
-    if ( ~isempty(tPoints) )
-        endtest = zeros(length(tPoints), 1);
-        for k = 1:length(tPoints)
-            endtest(k) = min(abs(tPoints(k) - f.domain));
-        end
-        if ( any(endtest > 100*abs(feval(f, tPoints))*tol) )
-            error('CHEBFUN:inv2:notmonotonic', 'chebfun F must be monotonic its domain.');
-        elseif ( ~splitYesNo )
-            warning('CHEBFUN:inv:singularendpoints', ['F is monotonic, but ', ...
-                'INV2(F) has singular endpoints. Suggest you try ''splitting on''.']);
-        end
-    end
-end
-
-% Assign preferences:
-p.techPrefs.resampling = 1;
-p.enableBreakpointDetection = splitYesNo;
-p.techPrefs.eps = tol;
-p.techPrefs.minsamples = length(f);
-p.techPrefs.sampleTest = 0;
-
-% Compute the inverse:
-gDomain = minandmax(f).';
-x = chebfun('x', gDomain);
-g = chebfun(@(x) fInverse(f, fp, x, tol), gDomain, p );
-
-% Scale so that the range of g is the domain of f:
-if ( rangeCheck )
-    [gRange, gx] = minandmax(g);
-    g = g + (gx(2) - x)*(f.domain(1) - gRange(1))/diff(gx) ...
-          + (x - gx(1))*(f.domain(end) - gRange(2))/diff(gx);
-end
-
-end
-
-function y = fInverse(f, fp, x, tol)
-    tol = tol/5;
-    y = zeros(length(x), 1);
-
-    if ( length(x) >= length(f) )
-        % Newton iteration:
-        t = f.domain(1);
-        for j = 1:length(x);
-            ft = feval(f, t) - x(j);
-            counter = 0;
-            while ( abs(ft) > tol ) % Newton step
-                fpt = feval(fp, t);
-                t = t - ft./fpt;
-                ft = feval(f, t) - x(j);
-                counter = counter + 1;
-                if ( counter > 10 )
-                    %error('No convergence')
-                    break;
-                end
-            end
-            y(j,1) = t;
-        end
-    else
-        y = fInverseRoots(f, x);
-    end
-end    
-    
-
-function y = fInverseRoots(f, x)
-    % FINVERSE(F, X) attemptes to return the values of F^{-1}(X) using
-    % CHEBFUN/ROOTS().
-    y = zeros(length(x), 1);
-    % Vectorise:
-    for j = 1:length(x)
-        temp = roots(f - x(j));
-        if ( length(temp) ~= 1 )
-            fvals = feval(f, f.domain);
-            err = abs(fvals - x(j));
-            [temp, k] = min(err);
-            if ( err(k) > 100*tol*abs(fvals(k)))
-                error('CHEBFUN:inv:notmonotonic2', 'Chebfun must be monotonic.');
-            end
-        end
-        y(j, 1) = temp;
-    end
-end
-
-function value = checkOnOff(value)
-    if ( ischar(value) )
-        % If ON or OFF used -> change to true or false.
-        if ( strcmpi(value,'on') )
-            value = true;
-        elseif ( strcmpi(value,'off') )
-            value = false; 
-        end
-    else
-        value = logical(value);
-    end
 end
