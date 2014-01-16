5142daa3b0f7e873d46293d3dba35b9343f4929b
Hadrien Montanelli <montanelli@maths.ox.ac.uk>
2014-01-16 18:45:46 +0000

    Minor modifications testclasse.make -> bndfun, pass(n, .) -> pass(.).

--- tests/fun/test_max.m
+++ tests/fun/test_max.m
@@ -7,57 +7,53 @@ if ( nargin < 1 )
     pref = chebpref();
 end
 
-for n = 1:1 %[TODO]: unbndfun
-    if ( n == 1 )
-        testclass = bndfun();
-        dom = [-2 7];
-    else
-        testclass = unbndfun();
-    end
+% Set a domain for BNDFUN.
+dom = [-2 7];
+    
+%% 
+% Spot-check the extrema for a few BNDFUN.
+pass(1) = test_spotcheck_max(@(x) sin(10*x), dom, 1, pref);
+pass(2) = test_spotcheck_max(@airy, dom, 0.535656656015700, pref);
+pass(3) = test_spotcheck_max(@(x) -1./(1 + x.^2), dom, -.02, pref);
+pass(4) = test_spotcheck_max(@(x) (x/10).^3.*cosh(x/10), ...
+    dom, 0.7^3*cosh(0.7), pref);
 
-    %%
-    % Spot-check the extrema for a few functions.
-    pass(n, 1) = test_spotcheck_max(testclass, @(x) sin(10*x), dom, 1, pref);
-    pass(n, 2) = test_spotcheck_max(testclass, @airy, dom, 0.535656656015700, pref);
-    pass(n, 3) = test_spotcheck_max(testclass, @(x) -1./(1 + x.^2), dom, -.02, pref);
-    pass(n, 4) = test_spotcheck_max(testclass, @(x) (x/10).^3.*cosh(x/10), ...
-        dom, 0.7^3*cosh(0.7), pref);
+%%
+% Check operation for array-valued BNDFUN inputs.
+fun_op = @(x) [sin(10*x) airy(x) (x/10).^3.*cosh(x/10)];
+f = bndfun(fun_op, dom, [], [], pref);
+[y, x] = max(f);
+exact_max = [1 0.535656656015700 0.7^3*cosh(0.7)];
+fx = [sin(10*x(1)) airy(x(2)) (x(3)/10).^3.*cosh(x(3)/10)];
+tol = 10*get(f, 'vscale')*get(f, 'epslevel');
+pass(5) = (all(abs(y - exact_max) < tol) && ...
+    all(abs(fx - exact_max) < tol));
 
-    %%
-    % Check operation for array-valued inputs.
-    fun_op = @(x) [sin(10*x) airy(x) (x/10).^3.*cosh(x/10)];
-    f = testclass.make(fun_op, dom, [], [], pref);
-    [y, x] = max(f);
-    exact_max = [1 0.535656656015700 0.7^3*cosh(0.7)];
-    fx = [sin(10*x(1)) airy(x(2)) (x(3)/10).^3.*cosh(x(3)/10)];
-    tol = 10*get(f, 'vscale')*get(f, 'epslevel');
-    pass(n, 5) = (all(abs(y - exact_max) < tol) && ...
-               all(abs(fx - exact_max) < tol));
+%%
+% Test for complex-valued BNDFUN.
+pass(6) = test_spotcheck_max(@(x) (x/2).*(exp(1i*(x/2))+1i*sin(x/2)), ...
+    dom, -3.277598405517787 - 2.455482593827339i, pref);
 
-    %%
-    % Test for complex-valued fun objects.
-    pass(n, 6) = test_spotcheck_max(testclass, ...
-        @(x) (x/2).*(exp(1i*(x/2))+1i*sin(x/2)), dom, ...
-         -3.277598405517787 - 2.455482593827339i, pref);
-
-    fun_op = @(x) [((x-2).^2/4+1).*exp(1i*(x/2)) ... 
-                  -((x+1).^2/4+1).*exp(1i*(x/2))];
-    f = testclass.make(fun_op, dom, [], [], pref);
-    [y, x] = max(f);
-    exact_max = [-6.789310982858273-2.543178400749744i 15.919763683943538+5.963314870723537i];
-    fx = [((x(1)-2).^2/4+1).*exp(1i*(x(1)/2)) ... 
-         -((x(2)+1).^2/4+1).*exp(1i*(x(2)/2))];
-    tol = get(f, 'vscale')*get(f, 'epslevel');
-    pass(n, 7) = (all(abs(y - exact_max) < tol) && ...
-                  all(abs(fx - exact_max) < tol));
+fun_op = @(x) [((x-2).^2/4+1).*exp(1i*(x/2)) ... 
+    -((x+1).^2/4+1).*exp(1i*(x/2))];
+f = bndfun(fun_op, dom, [], [], pref);
+[y, x] = max(f);
+exact_max = [-6.789310982858273-2.543178400749744i 15.919763683943538+5.963314870723537i];
+fx = [((x(1)-2).^2/4+1).*exp(1i*(x(1)/2)) ... 
+    -((x(2)+1).^2/4+1).*exp(1i*(x(2)/2))];
+tol = get(f, 'vscale')*get(f, 'epslevel');
+pass(7) = (all(abs(y - exact_max) < tol) && ...
+    all(abs(fx - exact_max) < tol));
+      
+%% 
+% [TODO]: Run a few tests for UNBNDFUN.
 end
 
-end
+%%
+% Spot-check the results for a given BNDFUN.
+function result = test_spotcheck_max(fun_op, dom, exact_max, pref)
 
-% Spot-check the results for a given function.
-function result = test_spotcheck_max(testclass, fun_op, dom, exact_max, pref)
-
-f = testclass.make(fun_op, dom, [], [], pref);
+f = bndfun(fun_op, dom, [], [], pref);
 [y, x] = max(f);
 fx = fun_op(x);
 tol = 10*get(f, 'vscale')*get(f, 'epslevel');
@@ -65,3 +61,6 @@ result = (all(abs(y - exact_max) < tol) && ...
           all(abs(fx - exact_max) < tol));
 
 end
+
+%%
+% [TODO]: Spot-check the results for a given UNBNDFUN.
--- tests/fun/test_plus.m
+++ tests/fun/test_plus.m
@@ -7,7 +7,7 @@ if ( nargin < 1 )
     pref = chebpref();
 end
 
-% Set a domain
+% Set a domain for BNDFUN.
 dom = [-2 7];
 
 % Generate a few random points to use as test values.
@@ -19,21 +19,18 @@ alpha = -0.194758928283640 + 0.075474485412665i;
 
 %%
 % Check operation in the face of empty arguments.
-
 f = bndfun();
 g = bndfun(@(x) x, dom, [], [], pref);
 pass(1) = (isempty(f + f) && isempty(f + g) && isempty(g + f));
 
-%%
+%% 
 % Check addition with scalars.
-
 f_op = @(x) sin(x);
 f = bndfun(f_op, dom, [], [], pref);
 pass(2:3) = test_add_function_to_scalar(f, f_op, alpha, x);
 
-%%
-% Check addition of two bndfun objects.
-
+%% 
+% Check addition of two BNDFUN objects.
 f_op = @(x) zeros(size(x));
 f = bndfun(f_op, dom, [], [], pref);
 pass(4:5) = test_add_function_to_function(f, f_op, f, f_op, x);
@@ -53,9 +50,8 @@ g_op = @(t) sinh(t*exp(2*pi*1i/6));
 g = bndfun(g_op, dom, [], [], pref);
 pass(10:11) = test_add_function_to_function(f, f_op, g, g_op, x);
 
-%%
-% Check operation for array-valued bndfun objects.
-
+%% 
+% Check operation for array-valued BNDFUN objects.
 f_op = @(x) [zeros(size(x)) zeros(size(x)) zeros(size(x))];
 f = bndfun(f_op, dom, [], [], pref);
 pass(12:13) = test_add_function_to_function(f, f_op, f, f_op, x);
@@ -68,6 +64,7 @@ g_op = @(x) [cosh(x) airy(1i*x) sinh(x)];
 g = bndfun(g_op, dom, [], [], pref);
 pass(16:17) = test_add_function_to_function(f, f_op, g, g_op, x);
 
+%% 
 % This should fail with a dimension mismatch error.
 g_op = @(x) sin(x);
 g = bndfun(g_op, dom, [], [], pref);
@@ -78,9 +75,8 @@ catch ME
     pass(18) = strcmp(ME.message, 'Matrix dimensions must agree.');
 end
 
-%%
+%% 
 % Check that direct construction and PLUS give comparable results.
-
 tol = 10*eps;
 f = bndfun(@(x) x, dom, [], [], pref);
 g = bndfun(@(x) cos(x) - 1, dom, [], [], pref);
@@ -88,10 +84,9 @@ h1 = f + g;
 h2 = bndfun(@(x) x + cos(x) - 1, dom, [], [], pref);
 pass(19) = norm(feval(h1, x) - feval(h2, x), inf) < 2*tol;
 
-%%
+%% 
 % Check that adding a BNDFUN to an unhappy BNDFUN gives an unhappy
 % result.
-
 f = bndfun(@(x) cos(x + 1), dom);    % Happy
 g = bndfun(@(x) sqrt(x + 1), dom);   % Unhappy
 h = f + g;  % Add unhappy to happy.
@@ -99,8 +94,8 @@ pass(20) = (~get(g, 'ishappy')) && (~get(h, 'ishappy'));
 h = g + f;  % Add happy to unhappy.
 pass(21) = (~get(g, 'ishappy')) && (~get(h, 'ishappy'));
 
-%% Test on singular function:
-
+%% 
+% Test on singular BNDFUN.
 pow = -1;
 op1 = @(x) (x - dom(2)).^pow.*sin(x);
 op2 = @(x) (x - dom(2)).^pow.*cos(3*x);
@@ -114,26 +109,30 @@ h_exact = op(x);
 pass(22) = ( norm(vals_h-h_exact, inf) < 1e1*max(get(f, 'epslevel'), get(g, 'epslevel'))*...
     norm(h_exact, inf) );
 
+%% 
+% [TODO]: Run a few tests for UNBNDFUN.
 end
 
+%% 
 % Test the addition of a BNDFUN F, specified by F_OP, to a scalar ALPHA using
 % a grid of points X in [a  b] for testing samples.
 function result = test_add_function_to_scalar(f, f_op, alpha, x)
-    g1 = f + alpha;
-    g2 = alpha + f;
-    result(1) = isequal(g1, g2);
-    g_exact = @(x) f_op(x) + alpha;
-    result(2) = norm(feval(g1, x) - g_exact(x), inf) < ...
-        10*max(get(g1, 'vscale'))*get(g1, 'epslevel');
+g1 = f + alpha;
+g2 = alpha + f;
+result(1) = isequal(g1, g2);
+g_exact = @(x) f_op(x) + alpha;
+result(2) = norm(feval(g1, x) - g_exact(x), inf) < ...
+    10*max(get(g1, 'vscale'))*get(g1, 'epslevel');
 end
 
+%% 
 % Test the addition of two BNDFUN objects F and G, specified by F_OP and
 % G_OP, using a grid of points X in [-1  1] for testing samples.
 function result = test_add_function_to_function(f, f_op, g, g_op, x)
-    h1 = f + g;
-    h2 = g + f;
-    result(1) = isequal(h1, h2);
-    h_exact = @(x) f_op(x) + g_op(x);
-    result(2) = norm(feval(h1, x) - h_exact(x), inf) <= ...
-        10*max(get(h1, 'vscale'))*get(h1, 'epslevel');
+h1 = f + g;
+h2 = g + f;
+result(1) = isequal(h1, h2);
+h_exact = @(x) f_op(x) + g_op(x);
+result(2) = norm(feval(h1, x) - h_exact(x), inf) <= ...
+    10*max(get(h1, 'vscale'))*get(h1, 'epslevel');
 end
