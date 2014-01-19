bc666d46d6ff2a6da7766de2b05d82de247be3ee
Anthony P. Austin <austin@maths.ox.ac.uk>
2014-01-19 14:16:14 +0000

    Coding style fixes for new chebfun.conv().

--- @chebfun/conv.m
+++ @chebfun/conv.m
@@ -1,22 +1,23 @@
 function h = conv(f, g)
 %CONV   Convolution of CHEBFUN objects.
-% H = CONV(F, G) produces the convolution of CHEBFUN objects F and G:
-%                   - 
-%                  /
-%         H(x) =   |    F(t) G(x-t) dt,  x in [a + c, b + d]
-%                  /
-%                 -
-% where domain(F) is [a, b] and domain(G) is [c, d]. The integral is taken over
-% all t for which the integrand is defined: max(a, x - d) <= t <= min(b, x - c).
-% The breakpoints of H are all pairwise sums of the breakpoints of F and G.
+%   H = CONV(F, G) produces the convolution of CHEBFUN objects F and G:
+%                     - 
+%                    /
+%           H(x) =   |    F(t) G(x-t) dt,  x in [a + c, b + d]
+%                    /
+%                   -
+%   where domain(F) is [a, b] and domain(G) is [c, d]. The integral is taken
+%   over all t for which the integrand is defined: max(a, x - d) <= t <= min(b,
+%   x - c).  The breakpoints of H are all pairwise sums of the breakpoints of F
+%   and G.
 %
-% Note that CONV only supports piecewise-smooth functions on bounded domains.
+%   Note that CONV only supports piecewise-smooth functions on bounded domains.
 %
-% Example:
-%   f = chebfun(1/2); g = f;
-%   subplot(2, 2, 1), plot(f)
-%   for j = 2:4, g = conv(f, g); subplot(2, 2, j), plot(g), end
-%   figure, for j = 1:4, subplot(2,2,j), plot(g), g = diff(g); end
+%   Example:
+%     f = chebfun(1/2); g = f;
+%     subplot(2, 2, 1), plot(f)
+%     for j = 2:4, g = conv(f, g); subplot(2, 2, j), plot(g), end
+%     figure, for j = 1:4, subplot(2,2,j), plot(g), g = diff(g); end
 
 % Copyright 2013 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
@@ -62,7 +63,7 @@ function h = conv(f, g)
 %  a+c     b+c             fl  a+d     b+d
 %
 % The final piece is computed via further parallelogram subdivision starting
-% from the right (B), and a smaller subdivisin in both f and g (C).
+% from the right (B), and a smaller subdivision in both f and g (C).
 % Contributions from D and E are discarded, as they have already been counted
 % above. Complexity O(m^2 + n^2)
 %   ________________
@@ -79,7 +80,7 @@ function h = conv(f, g)
 % Total complexity: O( R*(m*n + n*n) + m*m )
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% TODO: Force size(B) = [n, min(m, n)] ifor the interior convolutions?
+% TODO: Force size(B) = [n, min(m, n)] for the interior convolutions?
 % TODO: Refactor so that most of this lives at the FUN and/or CHEBTECH level?
 
 % Return empty for an empty input:
@@ -89,7 +90,7 @@ if ( isempty(f) || isempty(g) )
 end
 
 % No support for quasimatrices:
-if ( min(size(f)) > 1 || min(size(g)) > 1 )
+if ( (min(size(f)) > 1) || (min(size(g)) > 1) )
     error('CHEBFUN:conv:quasi', 'No support for array-valued CHEBFUN objects.');
 end
 
@@ -103,13 +104,13 @@ end
 [c, d] = domain(g);
 
 % Ensure that g is the signal (i.e., on the larger domain) and f is the filter:
-if ( b-a > d-c )
+if ( (b - a) > (d - c) )
     h = conv(g, f);
     return
 end
     
 % Deal with piecewise CHEBFUN objects.
-if ( numel(f.funs) > 1 || numel(g.funs) > 1 )
+if ( (numel(f.funs) > 1) || (numel(g.funs) > 1) )
     h = 0;
     % Loopover each of the interactions:
     for j = 1:numel(f.funs)
@@ -131,17 +132,17 @@ if ( ~isa(f.onefun, 'chebtech') || ~isa(g.onefun, 'chebtech') )
         'CONV() only supports smooth CHEBFUN objects on bounded domains.')
 end
 
-% Useful things..
-m = length(f); n = length(g);                % Lengths of f anf g
+% Useful things:
+m = length(f);                               % Length of f
+n = length(g);                               % Length of g
 numPatches = floor((d - c) / (b - a));       % Number of patches required
 x = chebpts(n, [b + c, a + d], 1);           % Chebyshev grid for interior piece
 y = 0*x;                                     % Initialise values in interior
 map = @(x, a, b) (x-a)/(b-a) - (b-x)/(b-a);  % Map from [a, b] --> [-1, 1]
-f_leg = chebtech.cheb2leg(get(f, 'coeffs'));  % Legendre coefficients of f
-
+f_leg = chebtech.cheb2leg(get(f, 'coeffs')); % Legendre coefficients of f
 
 % Restrict g:
-doms = c + (b-a)*(0:numPatches);
+doms = c + (b - a)*(0:numPatches);
 g_restricted = restrict(g, doms);
 if ( ~iscell(g_restricted) )
     g_restricted = {g_restricted};
@@ -159,23 +160,23 @@ for k = 1:numPatches
     [hLegL, hLegR] = easyConv(f_leg, gk_leg);      % Convolution on this domain
     
     % The left triangle for the kth patch:
-    idx = dk_left <= x & x < dk_mid; % Locate the grid values in [dkl, dkr]:
+    ind = (dk_left <= x) & (x < dk_mid); % Locate the grid values in [dkl, dkr]:
     if ( k == 1 ) % First piece:
-        hLegL = chebtech.leg2cheb(flipud(hLegL));  % Chebyshev coeffs of left tri.
+        hLegL = chebtech.leg2cheb(flipud(hLegL));  % Cheb. coeffs of left tri.
         h_left = chebfun(hLegL, [dk_left, dk_mid], 'coeffs'); % Make CHEBFUN
     else          % Subsequent left pieces
-        z = map(x(idx), dk_left, dk_mid);          % Map grid points to [-1, 1]
+        z = map(x(ind), dk_left, dk_mid);          % Map grid points to [-1, 1]
         tmp = clenshawLegendre(z, hLegL);          % Evaluate via recurrence
-        y(idx) = y(idx) + tmp;                     % Append
+        y(ind) = y(ind) + tmp;                     % Append
     end
     
     % The right triangle for the kth patch:
     if ( k < numPatches ) % Not needed for the final patch!
         % Locate the grid values in [dkl, dkr]:
-        idx = dk_mid <= x & x < dk_right;
-        z = map(x(idx), dk_mid, dk_right);
+        ind = (dk_mid <= x) & (x < dk_right);
+        z = map(x(ind), dk_mid, dk_right);
         tmp = clenshawLegendre(z, hLegR);
-        y(idx) = y(idx) + tmp;
+        y(ind) = y(ind) + tmp;
     end
 end
 
@@ -195,7 +196,7 @@ else
     %     fl a+d     b+d
 
     finishLocation = a + c + numPatches*(b - a);   % Where patches got to. (fl) 
-    gk = restrict(g, d-[(b-a) 0]);                 % g on appropriate domain        
+    gk = restrict(g, d - [(b-a) 0]);               % g on appropriate domain
     gk_leg = chebtech.cheb2leg(get(gk, 'coeffs')); % Legendre coeffs
     [hLegL, hLegR] = easyConv(f_leg, gk_leg);      % Conv on A and B
     hLegR = chebtech.leg2cheb(flipud(hLegR));      % Cheb coeffs on A
@@ -204,22 +205,22 @@ else
     % Remainder piece: (between fl and a+d)
     remainderWidth = d + a - finishLocation; % b+d-fl-(b-a)
     if ( remainderWidth > 0 )
-        idx = finishLocation <= x;           % Discard D and E
+        ind = finishLocation <= x;           % Discard D and E
         
         % B: (Coeffs were computed above)
-        z = map(x(idx), d - b + 2*a, d + a); % Map grid to [-1, 1]
+        z = map(x(ind), d - b + 2*a, d + a); % Map grid to [-1, 1]
         tmp = clenshawLegendre(z, hLegL);    % Evaluate via recurrence
-        y(idx) = tmp;                        % Store
+        y(ind) = tmp;                        % Store
         
         % C: 
         fk = restrict(f, b + [-remainderWidth, 0]);     % Restrict f
         fk_leg = chebtech.cheb2leg(get(fk, 'coeffs'));  % Legendre coeffs
         gk = restrict(g, [finishLocation, d + a] - b);  % Restrict g
-        gk_leg = chebtech.cheb2leg(get(gk, 'coeffs'));   % Legendre coeffs
+        gk_leg = chebtech.cheb2leg(get(gk, 'coeffs'));  % Legendre coeffs
         [ignored, hLegR] = easyConv(fk_leg, gk_leg);    % Conv 
-        z = map(x(idx), finishLocation, d + a);         % Map to [-1, 1]
+        z = map(x(ind), finishLocation, d + a);         % Map to [-1, 1]
         tmp = clenshawLegendre(z, hLegR);               % Eval via recurrence
-        y(idx) = y(idx) + tmp*remainderWidth/(b - a);   % Scale and append
+        y(ind) = y(ind) + tmp*remainderWidth/(b - a);   % Scale and append
     end
     
     % Convert values to coeffs (we don't want to construct a chebteh1)
@@ -242,7 +243,6 @@ h = join(h_left, h_mid, h_right)*(b-a)/2;
 
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [gammaL, gammaR] = easyConv(alpha, beta)
@@ -286,18 +286,27 @@ gammaR = rec(M, -alpha, beta, 1); % Chebyshev coeffs for the right piece
         scl(2:2:end) = -scl(2:2:end);
         
         % First column of B:
-        vNew = M*alpha; v = vNew;
+        vNew = M*alpha;
+        v = vNew;
         gamma = beta(1)*vNew;
-        beta_scl = scl.*beta; beta_scl(1) = 0;
+        beta_scl = scl.*beta;
+        beta_scl(1) = 0;
         gamma(1) = gamma(1) + vNew(1:nb).'*beta_scl;
         
         % The scalar case is trivial:
-        if ( length(beta) == 1 ), return, end
+        if ( length(beta) == 1 )
+                return
+        end
         
         % Second column of B:
-        vNew = M*v + sgn*v; vOld = v; v = vNew; vNew(1) = 0;
+        vNew = M*v + sgn*v;
+        vOld = v;
+        v = vNew;
+        vNew(1) = 0;
+
         gamma = gamma + beta(2)*vNew;
-        beta_scl = -beta_scl*((2-.5)/(2-1.5)); beta_scl(2) = 0;
+        beta_scl = -beta_scl*((2 - 0.5)/(2 - 1.5));
+        beta_scl(2) = 0;
         gamma(2) = gamma(2) + vNew(1:nb).'*beta_scl;
         
         % Loop over remaining columns using recurrence:
@@ -307,7 +316,8 @@ gammaR = rec(M, -alpha, beta, 1); % Chebyshev coeffs for the right piece
             gamma = gamma + vNew*beta(k);    % Append to g
             
             % Recurrence is unstable for j < k. Correct for upper-tri part:
-            beta_scl = -beta_scl*((k-.5)/(k-1.5)); beta_scl(k) = 0;
+            beta_scl = -beta_scl*((k-.5)/(k-1.5));
+            beta_scl(k) = 0;
             gamma(k) = gamma(k) + vNew(1:nb).'*beta_scl;
             
             vOld = v;
@@ -322,32 +332,40 @@ gammaR = rec(M, -alpha, beta, 1); % Chebyshev coeffs for the right piece
         % Initialise B and scl:
         nb = length(beta);
         B = zeros(numel(alpha), nb);
-        scl = 1./(2*(1:nb).'-1); scl(2:2:end) = -scl(2:2:end);
+        scl = 1./(2*(1:nb).'-1);
+        scl(2:2:end) = -scl(2:2:end);
         
         % First 2 columns of B:
-        vNew = M*alpha; v = vNew;
+        vNew = M*alpha;
+        v = vNew;
         B(:,1) = vNew;
-        idx2 = 1:nb;
-        B(1,:) = vNew(idx2).*scl(idx2);
+        ind2 = 1:nb;
+        B(1,:) = vNew(ind2).*scl(ind2);
         
         % The scalar case is trivial:
-        if ( length(beta) == 1 ), return, end
+        if ( length(beta) == 1 )
+            return
+        end
         
-        vNew = M*v + sgn*v; vOld = v; v = vNew;  B(:,2) = vNew;
-        scl = -scl*(2-.5)/(2-1.5);
-        idx2 = 3:nb;
-        B(2,idx2) = vNew(idx2).*scl(idx2);
+        vNew = M*v + sgn*v;
+        vOld = v;
+        v = vNew;
+        B(:,2) = vNew;
+
+        scl = -scl*(2 - .5)/(2 - 1.5);
+        ind2 = 3:nb;
+        B(2,ind2) = vNew(ind2).*scl(ind2);
         
         % Loop over remaining columns using recurrence:
         for k = 3:nb
             vNew = (2*k-3) * ( M * v ) + vOld;
-            idx1 = (k-1):numel(alpha);
-            B(idx1, k) = vNew(idx1);
+            ind1 = (k-1):numel(alpha);
+            B(ind1, k) = vNew(ind1);
             vNew(1:k-2) = 0;
             % Recurrence is unstable for j < k. Correct for upper-tri part:
             scl = -scl*(k-.5)/(k-1.5);
-            idx2 = (k+1):nb;
-            B(k,idx2) = vNew(idx2).*scl(idx2);
+            ind2 = (k+1):nb;
+            B(k,ind2) = vNew(ind2).*scl(ind2);
             
             vOld = v;
             v = vNew;
@@ -359,26 +377,27 @@ gammaR = rec(M, -alpha, beta, 1); % Chebyshev coeffs for the right piece
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function val = clenshawLegendre(x, alpha) 
 % Evaluate a Legendre expansion with coefficient alpha at x. 
+
 n = length(alpha); 
 b_old = 0; 
 b_cur = 0; 
 for k = (n-1):-1:1
-  b_new = alpha(k+1) + (2*k+1)/(k+1)*x.*b_cur - (k+1)/(k+2)*b_old;
+  b_new = alpha(k+1) + (2*k + 1)/(k + 1)*x.*b_cur - (k + 1)/(k + 2)*b_old;
   b_old = b_cur; 
   b_cur = b_new; 
 end
 val = alpha(1) + x.*b_cur - .5*b_old; 
+
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function h = myplus(f, g)
 % Modified PLUS() which pads with zeros to fulfil domain requirements.
+
 if ( isnumeric(f) )
     h = f + g;
 else
@@ -390,4 +409,5 @@ else
     hTmp = restrict(h, [c, d]);
     h = defineInterval(h, [c, d], hTmp + g); % h{c, d} = h{c, d} + g;
 end
+
 end
