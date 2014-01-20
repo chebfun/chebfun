c3990327eabfe23bd4eb2e6c7d4d79f8968301f0
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2014-01-20 22:54:11 +0000

    Coding style convention update to previous commit.

--- tests/adchebfun/test_trig3.m
+++ tests/adchebfun/test_trig3.m
@@ -1,16 +1,18 @@
-% Test file for ADCHEBFUN trigonometric and related functions, part 3
+% Test file for ADCHEBFUN trigonometric and related functions, part 3/4
 
 function pass = test_trig3
 
 % List of trigonometric functions to test.
-% trigFunctions = {@acos, @acosd, @acosh, @acot, @acotd, @acoth, @acsc, ...
-%     @acscd, @acsch, @asec, @asecd, @asech, @asin, @asind, @asinh, @atan, ...
-%     @atand, @atanh, @cos, @cosd, @cosh, @cot, @cotd, @coth, @csc, @cscd, ...
-%     @csch, @sec, @secd, @sech, @sin, @sind, @sinh, @tan, @tand, @tanh, @mysinc};
-
 trigFunctions = {@atand, @atanh, @cos, @cosd, @cosh, @cot, @cotd, @coth, ...
     @csc, @cscd};
 
+% How many iterations we want to in the Taylor testing
+numSteps = 4;
+
+% Tolerance for Taylor testing
+tol = 1e-2;
+
+% Initialise vector with pass information
 pass = zeros(2, numel(trigFunctions));
 
 % Do the tests.
@@ -18,18 +20,19 @@ for k = 1:numel(trigFunctions)
     % Call the taylorTesting method
     try
         % First, check that the computed function values match what we expect
-        pass(1,k) = ( valueTesting(trigFunctions{k}) == 0 );
+        pass(1, k) = ( valueTesting(trigFunctions{k}) == 0 );
         
-        [order1, order2] = taylorTesting(trigFunctions{k},4);
-        % We expect all elements of ORDER1 to be close to 1, and of ORDER2 to be close
-        % to 2.
-        pass(2,k) = ( max(abs(order1-1)) < .01 ) & ( max(abs(order2-2)) < .01 );
-    catch ME
+        [order1, order2] = taylorTesting(trigFunctions{k}, numSteps);
+        % We expect all elements of ORDER1 to be close to 1, and of ORDER2 to be
+        % close to 2.
+        pass(2, k) = ( max(abs(order1 - 1)) < tol ) & ...
+            ( max(abs(order2 - 2)) < tol );
+    catch
         % Denote that an error happened when computing derivative, because the
         % version of development we branched off in Git didn't include the
         % required methods
-        pass(1,k) = -1;
-        pass(2,k) = -1;
+        pass(1, k) = -1;
+        pass(2, k) = -1;
     end
 end
 
