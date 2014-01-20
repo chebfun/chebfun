b799a3da51afd257497726b1bd9bec28dcb0f167
Anthony P. Austin <austin@maths.ox.ac.uk>
2014-01-20 21:11:42 +0000

    Add primitive automated test for plot().
    This addresses #122 on the GitHub issues tracker.  We can't actually check that
    the plots are accurate without a human in the loop (or some much more
    sophisticated processing), but we can at the very least check that none of the
    syntaxes triggers a crash.
    
    Currently, plotting scalar chebfuns against array-valued ones (tests 20 and 21)
    causes a crash, while plotting scalar chebfuns against quasimatrices (tests 18
    and 19) does not.  This is a bug which demands fixing.

---
+++ tests/chebfun/test_plot.m
@@ -0,0 +1,90 @@
+% Tests for chebfun plotting functions.
+
+function pass = test_plot(pref)
+
+if ( nargin < 1 )
+    pref = chebpref();
+end
+
+% Real scalar functions.
+fsr1 = chebfun(@sin, [-1 0 1], pref);
+fsr2 = chebfun(@cos, [-1 0.5 1], pref);
+fsr3 = chebfun(@exp, [-1 -0.5 1], pref);
+
+% Real array-valued functions.
+far1 = chebfun(@(x) [sin(x) cos(x) exp(x)], [-1 0 1], pref);
+far2 = chebfun(@(x) [sin(x) 2*cos(x) 3*exp(x)], [-1 -0.5 1], pref);
+far3 = chebfun(@(x) [3*sin(x) 2*cos(x) exp(x)], [-1 0.5 1], pref);
+
+% Real quasimatrices.
+fqr1 = cheb2quasi(far1);
+fqr2 = cheb2quasi(far2);
+fqr3 = cheb2quasi(far3);
+
+% Complex functions.
+fsc = chebfun(@(x) exp(1i*x), [-1 0 1], pref);
+fac = chebfun(@(x) [exp(1i*x) 2*exp(1i*x) 3*exp(1i*x)], [-1 0.5 1], pref);
+fqc = cheb2quasi(fac);
+
+% Obviously, we can't check if the plots are correct without human
+% intervention, so all these tests are meant to do is make sure none of the
+% plotting functions crash.
+
+hfig = figure('Visible', 'off');
+
+% Plots of real scalar functions.
+pass(1) = doesNotCrash(@() plot(fsr1));
+pass(2) = doesNotCrash(@() plot(fsr1, fsr2));
+pass(3) = doesNotCrash(@() plot3(fsr1, fsr2, fsr3));
+
+% Plots of real array-valued functions.
+pass(4) = doesNotCrash(@() plot(far1));
+pass(5) = doesNotCrash(@() plot(far1, far2));
+pass(6) = doesNotCrash(@() plot3(far1, far2, far3));
+
+% Plots of real quasimatrices.
+pass(7) = doesNotCrash(@() plot(fqr1));
+pass(8) = doesNotCrash(@() plot(fqr1, fqr2));
+pass(9) = doesNotCrash(@() plot3(fqr1, fqr2, fqr3));
+
+% Plots which mix array-valued functions and quasimatrices.
+pass(10) = doesNotCrash(@() plot(far1, fqr2));
+pass(11) = doesNotCrash(@() plot(fqr1, far2));
+
+pass(12) = doesNotCrash(@() plot3(far1, far2, fqr3));
+pass(13) = doesNotCrash(@() plot3(far1, fqr2, far3));
+pass(14) = doesNotCrash(@() plot3(fqr1, far2, far3));
+pass(15) = doesNotCrash(@() plot3(fqr1, fqr2, far3));
+pass(16) = doesNotCrash(@() plot3(fqr1, far2, fqr3));
+pass(17) = doesNotCrash(@() plot3(far1, fqr2, fqr3));
+
+% Plots of quasimatrices against scalar functions.
+pass(18) = doesNotCrash(@() plot(fsr1, fqr2));
+pass(19) = doesNotCrash(@() plot(fqr1, fsr2));
+pass(20) = doesNotCrash(@() plot(fsr1, far2));
+pass(21) = doesNotCrash(@() plot(far1, fsr2));
+
+% Plots of complex functions.
+pass(22) = doesNotCrash(@() plot(fsc));
+pass(23) = doesNotCrash(@() plot(fac));
+pass(24) = doesNotCrash(@() plot(fqc));
+
+% Check plot flags and other options.
+pass(25) = doesNotCrash(@() plot(fsr1, 'numpts', 100));
+pass(26) = doesNotCrash(@() plot(fsr2, 'interval', [-0.5 0.5]));
+pass(27) = doesNotCrash(@() plot(fsr2, [-0.5 0.5]));
+
+close(hfig);
+
+end
+
+function pass = doesNotCrash(fn)
+
+try
+    fn();
+    pass = true;
+catch ME;
+    pass = false;
+end
+
+end
