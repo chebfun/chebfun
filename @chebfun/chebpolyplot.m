chebfun/chebfun

commit d6a167483621430eb695fba6dc91831c3b43497a

Further simplify code for dealing with zeros in chebfun.chebpolyplot().
Suggestions from NH.

@chebfun/chebpolyplot.m
@@ -47,18 +47,13 @@ numcols = size(c{1}, 2);
 % Get vscale, epslevel data:
 v = get(f, 'vscale-local');
 e = get(f, 'epslevel-local');
-ve = v.*repmat(e, 1, numcols);
+ve = bsxfun(@times, v, e);
 
 % Add a tiny amount to zeros to make plots look nicer:
+minve = min(ve, [], 1);
 for k = 1:numfuns
-    % If the minimum of (vscale)*(epslevel) is zero, use the smallest nonzero
-    % coefficient.  Otherwise, use the smaller of these two values.
-    if (min(ve(k, :)) == 0)
-        zeroLevel = min(c{k}(logical(c{k}(:))));
-    else
-        zeroLevel = min(min(ve(k, :)), min(c{k}(logical(c{k}(:)))));
-    end
-    c{k}(~c{k}) = zeroLevel;
+    % Use smaller of min. of (vscale)*(epslevel) and the smallest nonzero coeff.
+    c{k}(~c{k}) = min(minve(k), min(c{k}(logical(c{k}(:)))));
 end
 
 % Shape it:

commit c99dea8a19f1076b6eb37feff0c3300515213920

Fix bugs in chebpolyplot() for array-valued chebfuns.
 - When taking the minimum of vscale.*epslevel in deciding the size of the
   perturbation to be made to identically zero coefficients, the minimum needs
   to be taken across each row, not down each column, for things to work out
   correctly.  (This bug was introduced in d6a16748, but nobody noticed!)

   Also replaced a use of bsxfun() with a .* operation, since (local) epslevels
   are now vectors.

 - Fixed an issue with the code setting the colors for each plot.  The old code
   crashed when making chebpolyplots for array-valued chebfuns with multiple
   funs (though it worked fine for such chebfuns with only one fun).

@chebfun/chebpolyplot.m
@@ -105,10 +105,10 @@ numCols = size(c{1}, 2);
 % Get vscale, epslevel data:
 v = get(f, 'vscale-local');
 e = get(f, 'epslevel-local');
-ve = bsxfun(@times, v, e);
+ve = v.*e;
 
 % Add a tiny amount to zeros to make plots look nicer:
-minve = min(ve, [], 1);
+minve = min(ve, [], 2);
 for k = 1:numFuns
     % Use smaller of min. of (vscale)*(epslevel) and the smallest nonzero coeff.
     c{k}(~c{k}) = min(minve(k), min(c{k}(logical(c{k}(:)))));
@@ -128,13 +128,17 @@ doEpsLevel = ~any(noEpsLevel);
 % Plot the coeffs:
 % h1 = semilogy(data{:}, varargin{:}, 'color', col);
 h1 = semilogy(data{:}, varargin{:});
-for k = 1:numel(h1)
-    if ( size(col, 1) == numel(h1) )
-        c = col(k,:);   
-    else
-        c = col;
+
+% Set the colors (loop over columns if we have an array-valued CHEBFUN):
+for j = 1:numCols
+    % The figure handles in the h1 vector are stored such that plots for all
+    % the first funs are listed first, ordered by column, followed by the
+    % second funs, etc.  Thus, the plot of the first fun in the first column is
+    % at index 1, the second fun in the first column is at 1 + numCols, the
+    % third fun in the first column is at 1 + 2*numCols, etc.
+    for k = j:numCols:(numFuns*numCols)
+        set(h1(k), 'color', col(j,:));
     end
-    set(h1(k), 'color', c);
 end
 hold on
 