2d2a040b784b94c1482f49bb27dc6efddb9756da
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2014-01-22 10:53:21 +0000

    Code review and more documentation for BLOCKFUNCTION.

--- @blockFunction/blockFunction.m
+++ @blockFunction/blockFunction.m
@@ -1,18 +1,18 @@
 classdef blockFunction
-    %BLOCKFUNCTION   Convert linear operator to callable function.
-    %   This class is not intended to be called directly by the end user.
-    %
-    %   See also LINOP, CHEBOP, CHEBOPPREF.
+%BLOCKFUNCTION   Convert linear operator to callable function.
+%   This class is not intended to be called directly by the end user.
+%
+%   See also LINOP, CHEBOP, CHEBOPPREF.
+
+% Copyright 2013 by The University of Oxford and The Chebfun Developers.
+% See http://www.chebfun.org/ for Chebfun information.
     
-    % Copyright 2013 by The University of Oxford and The Chebfun Developers.
-    % See http://www.chebfun.org/ for Chebfun information.
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Developer notes
-    %
-    % This class converts a LINBLOCK object into a callable function suitable
-    % for application to a CHEBFUN.
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Developer notes
+%
+% This class converts a LINBLOCK object into a callable function suitable
+% for application to a CHEBFUN.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
     properties ( Access=public )
         % This property is assigned the callable function that does the
@@ -25,17 +25,20 @@ classdef blockFunction
     methods
         
         function A = blockFunction(varargin)
+        % BLOCKFUNCTION     Constructor of BLOCKFUNCTION objects.
+        
             % When called with no arguments, the returned object causes the
             % block's stack to be evaluated with these methods to produce
             % coefficients.
+            % TODO: This comment doesn't make much sense to me (AB 22/1/14)
             if ( isempty(varargin{1}) )
                 pref = cheboppref;
                 A.domain = pref.domain;  % TODO: not clear this is ever used...
                 return
                 
                 
-                % Calling the constructor with a linBlock argument initiates the
-                % process of evaluating the stack with a dummy object of this class.
+            % Calling the constructor with a linBlock argument initiates the
+            % process of evaluating the stack with a dummy object of this class.
             elseif ( isa(varargin{1}, 'linBlock') )
                 % Convert the given linBlock to its function form by
                 % evaluating its stack.
@@ -45,59 +48,39 @@ classdef blockFunction
                 A = L.stack( dummy );
                 
                 
-                % If the constructor is called with data, just make a regular object
-                % out of it.
+            % If the constructor is called with data, just make a regular object
+            % out of it.
             else
                 A.func = varargin{1};
             end
         end
         
-        
-        function C = mtimes(A, B)
-            % Interpret as composition.
-            C = blockFunction( @(z) A.func(B.func(z)) );
-        end
-        
-        function C = plus(A, B)
-            C = blockFunction( @(z) A.func(z) + B.func(z) );
-        end
-        
-        function C = uminus(A)
-            C = blockFunction( @(z) -A.func(z) );
-        end
-        
-        function A = uplus(A)
+        function C = cumsum(A, m)
+        %CUMSUM   
+        %
+        % Returns a BLOCKFUNCTION corresponding to a call to chebfun/cumsum
+            C = blockFunction( @(u) cumsum(u, m) );
         end
         
         function D = diff(A, m)
-            D = blockFunction( @(z) diff(z,m) );
-        end
-        
-        function C = cumsum(A, m)
-            C = blockFunction( @(u) cumsum(u,m) );
+        %DIFF   
+        %
+        % Returns a BLOCKFUNCTION corresponding to a call to chebfun/diff
+            D = blockFunction( @(z) diff(z, m) );
         end
         
         function I = eye(A)
+        %EYE
+        %
+        % Returns a BLOCKFUNCTION corresponding to a function that simply
+        % returns the input chebfun.
             I = blockFunction( @(z) z );
         end
         
-        function Z = zeros(A)
-            Z = blockFunction( @(z) chebfun(0,A.domain) );
-        end
-        
-        function z = zero(A)
-            z = blockFunction( @(u) 0 );
-        end
-        
-        function F = mult(A, f)
-            F = blockFunction( @(z) times(f,z) );
-        end
-        
-        function S = sum(A)
-            S = blockFunction( @(z) sum(z) );
-        end
-        
         function E = feval(A, location, direction)
+        %FEVAL
+        %
+        % Returns a BLOCKFUNCTION corresponding to a call to chebfun/feval.    
             if ( direction < 0 )
                 E = blockFunction( @(u) feval(u, location, -1) );
             elseif ( direction > 0 )
@@ -106,11 +89,62 @@ classdef blockFunction
                 E = blockFunction( @(u) feval(u, location) );
             end
         end
-        
+ 
         function F = inner(A, f)
+        %INNER
+        %
+        % Returns a BLOCKFUNCTION corresponding to a call to chebfun/inner.  
             F = blockFunction( @(z) mtimes(f', z) );
         end
+                               
+        function C = mtimes(A, B)
+        %*      Composition of BLOCKFUNCTION objects
+            C = blockFunction( @(z) A.func(B.func(z)) );
+        end
+
+        function F = mult(A, f)
+        %MULT 
+        %
+        % Returns a BLOCKFUNCTION corresponding to a call to chebfun/times.
+            F = blockFunction( @(z) times(f, z) );
+        end
+                
+        function C = plus(A, B)
+        %-     Addition of BLOCKFUNCTION objects.
+            C = blockFunction( @(z) A.func(z) + B.func(z) );
+        end
+
+        function S = sum(A)
+        %SUM
+        %
+        % Returns a BLOCKFUNCTION corresponding to a call to chebfun/sum.
+            S = blockFunction( @(z) sum(z) );
+        end
         
+        function C = uminus(A)
+        %-     Unary minus of BLOCKFUNCTION.
+            C = blockFunction( @(z) -A.func(z) );
+        end
+        
+        function A = uplus(A)
+        %-     Unary plus of BLOCKFUNCTION.
+        end      
+
+        function z = zero(A)
+        %ZERO
+        %
+        % Returns a BLOCKFUNCTION corresponding to a function that returnins a
+        % zero scalar.
+            z = blockFunction( @(u) 0 );
+        end
+        
+        function Z = zeros(A)
+        %ZEROS
+        %
+        % Returns a BLOCKFUNCTION corresponding to a function that returns the
+        % zero chebfun.
+            Z = blockFunction( @(z) chebfun(0, A.domain) );
+        end
         
     end
     
