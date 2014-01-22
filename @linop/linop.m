c93e5f452bb046f83febd169d89949f4bdfd21f2
Toby Driscoll <driscoll@udel.edu>
2014-01-22 13:51:34 -0500

    Added linop.primitive* static methods to make it easier to get the standard operators.

--- @linop/linop.m
+++ @linop/linop.m
@@ -51,9 +51,45 @@ classdef (InferiorClasses = {?chebfun, ?operatorBlock, ?functionalBlock}) linop 
         function L = linop(M)
             L = L@chebmatrix(M);
         end
-        
+    end
+    
+    methods ( Static )
+        function [Z,I,D,C] = primitiveOperators(domain)
+%LINOP.PRIMITIVEOPERATORS Frequently used operator blocks.
+%   [Z,I,D,C] = LINOP.PRIMITIVEOPERATORS(DOMAIN) returns a zero, identity,
+%   differentiation, and indefinite integration operator, respecitvely, on
+%   the given domain.
+%
+%   Note that the 'linop' is just a convenient name. The outputs are *not*
+%   linops.
+%
+%   See also OPERATORBLOCK.
+            
+            Z = operatorBlock.zeros(domain);
+            I = operatorBlock.eye(domain);
+            D = operatorBlock.diff(domain,1);
+            C = operatorBlock.cumsum(domain,1);
+        end
+
+        function [z,E,s] = primitiveFunctionals(domain)
+%LINOP.PRIMITIVEFUNCTIONALS Frequently used functional blocks.
+%   [Z,E,S] = LINOP.PRIMITIVEFUNCTIONALS(DOMAIN) returns frequently used
+%   functional blocks on the given domain.
+%
+%   Z is the zero functional. E(X) is an evaluation functional at the point
+%   X. E(X,DIR) adds a direction (+1/-1) to the evaluation. S is a definite
+%   integration functional.
+%
+%   Note that the 'linop' is just a convenient name. The outputs are *not*
+%   linops.
+%
+%   See also OPERATORBLOCK.
+            
+            z = functionalBlock.zero(domain);
+            E = functionalBlock.eval(domain);
+            s = functionalBlock.sum(domain);
+        end
     end
 
-        
 end
 
