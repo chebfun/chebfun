899db77fe3e3d469fb801033ef0102389280b145
Toby Driscoll <driscoll@udel.edu>
2014-01-21 21:53:12 -0500

    Updates to linop documentation.

--- @linop/mpower.m
+++ @linop/mpower.m
@@ -1,4 +1,10 @@
 function B = mpower(A,pow)
+%^         Repeated application of a linop.
+%   B = A^m, for positive integer m, returns a linop that is the mth power
+%   of A. Note that boundary conditions are not carried into the result. 
+
+%  Copyright 2013 by The University of Oxford and The Chebfun Developers.
+%  See http://www.chebfun.org for Chebfun information.
 
 B = linop( mpower@chebmatrix(A,pow) );
 
