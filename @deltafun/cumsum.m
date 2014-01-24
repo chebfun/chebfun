72321f5e8b35429ce9da40dc3d15b936e0ba3178
Mohsin Javed <mhsnjvd@gmail.com>
2014-01-24 17:39:22 +0000

    fix for cumsum, in progress.

--- @deltafun/cumsum.m
+++ @deltafun/cumsum.m
@@ -26,42 +26,75 @@ deltaLoc = f.location;
 
 if ( isempty(deltaLoc) || isempty(deltaMag) )
     g = cumsum(f.funPart);    
-else    
-    g = {};
+else
+    % Clean up delta functions:
+    f = simplify(f);
+    
+    % If f does not have a funPart, construct one matching the delta
+    % function domain:    
+    if ( isempty(f.funPart) )
+        a = deltaLoc(1);
+        if ( length(deltaLoc) < 2 )
+            a = a-1;
+            b = a+2;
+        else
+            b = deltaLoc(end);
+        end
+        f.funPart = fun.constructor(0, [a, b]);
+    end
+        
+    % Get tolerance:
     pref = chebpref();
     deltaTol = pref.deltaPrefs.deltaTol;
-    idx = abs(deltaMag(1,:)) >= deltaTol;
+        
+    % Determine locations where jumps are to be introduced:
+    idx = abs(deltaMag(1,:)) >= deltaTol;    
     newBreaks = deltaLoc(idx);
+    
+    % Determine the value of jumps, and remove them from the delta function
+    % matrix:
     jumpVals = deltaMag(1, idx);
-    breakPts = union(f.funPart.domain, newBreaks);
+    dom = f.funPart.domain;    
+    % If there is no delta function at the left end point, introduce a jump of
+    % size 0:
+    if ( deltaLoc(1) > dom(1) )
+        jumpVals = [0, jumpVals];
+    end
+    
+    % If there is no delta function at the right end point, introduce a jump of
+    % size 0:
+    if ( deltaLoc(end) < dom(2) )
+        jumpVals = [jumpVals, 0];
+    end
+    
+    % Remove the delta functions that have been integrated and simplify the
+    % deltafunction matrix:
+    deltaMag(1, :) = 0*deltaMag(1,:);                
+    [deltaMag, deltaLoc] = deltafun.cleanColumns(deltaMag, deltaLoc);
+    
+    % Calculate the cumulative jump vector, entries of this vector will be used
+    % to off-set the cumsum by the correct value.
+    cumJump = cumsum(jumpVals);
+    
+    % Add end points to existing break points:
+    breakPts = sort(union(f.funPart.domain, newBreaks));
+    
+    % Get a cell array of funs:
     funParts = restrict(f.funPart, breakPts);
+    
     for k = 1:numel(funParts)
-        fk = cumsum(funParts{k}) + (k-1);
-        dk = fk.domain;
-        idx = deltaLoc >= dk(1) & deltaLoc <= dk(2);
+        % Integrate the funPart and add the appropriate jump:
+        fk = cumsum(funParts{k}) + cumJump(k);
+        domaink = fk.domain;
+        % Notice the strict inequalities below, since end-point delta functions
+        % have already been handled:
+        idx = (deltaLoc > domaink(1)) & (deltaLoc < domaink(2));
         lk = deltaLoc(idx);
-        mk = deltaMag(2:end, idx);
-        if ( isempty(idx) || all(mk == 0) )
+        if ( isempty(idx) )
             g{k} = fk;
         else
-            g{k} = deltafun(fk, dk, lk);
-        end
-        
-    end
-    
-    % Integrate the funPart:
-    if ( isempty(f.funPart) )
-        g.funPart = fun.constructor(0);
-    else                
-        g.funPart = cumsum(f.funPart);
-    end
-    
-    % Get the tolerance:
-    pref = chebpref();
-    deltaTol = pref.deltaPrefs.deltaTol;
-    
-    jumpVals = deltaMag(1, :);
-    idx = abs(jumpVals) > deltaTol; 
-    jumpVals = jumpVals(idx);
-    locations = deltaLoc(idx);            
+            mk = deltaMag(:, idx);    
+            g{k} = deltafun(fk, mk, lk);
+        end        
+    end    
 end
