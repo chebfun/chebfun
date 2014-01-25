6b80e72e111237b2edd0b4e03b9db887189c4a63
Townsend <townsend@maths.ox.ac.uk>
2014-01-25 11:55:38 +0000

    Finishing plotting commands.

--- @chebfun2/plot.m
+++ @chebfun2/plot.m
@@ -1,4 +1,4 @@
-function varargout = plot(f,varargin)
+function varargout = plot( f, varargin )
 %PLOT surface plot of a chebfun2
 %
 % PLOT(F) if F is a real-valued chebfun2 then this is the surface plot and
@@ -45,13 +45,13 @@ function varargout = plot(f,varargin)
 myish = ishold;
 if ( ~isempty(varargin) )
     % See if first set of option makes it a pivot plot.
-    if ( length(varargin{1})<5 )
+    if ( length(varargin{1}) < 5 )
 %% Column, row, pivot plot
 
         % Only option with <=3 letters is a colour, marker, line
-        ll = regexp(varargin{1},'[-:.]+','match');
-        cc = regexp(varargin{1},'[bgrcmykw]','match');  % color
-        mm = regexp(varargin{1},'[.ox+*sdv^<>ph]','match');  % marker
+        ll = regexp( varargin{1}, '[-:.]+', 'match' );
+        cc = regexp( varargin{1}, '[bgrcmykw]', 'match' );  % color
+        mm = regexp( varargin{1}, '[.ox+*sdv^<>ph]', 'match' );  % marker
         
         if ( ~isempty(ll) )
             if ( strcmpi(ll{1},'.') )
@@ -63,9 +63,12 @@ if ( ~isempty(varargin) )
             end
         end
         plotline = ~isempty(ll);  % plot row and col pivot lines?
-        %         if isempty(ll), ll{1}= [];end
-        if isempty(mm), mm{1}= '.';end
-        if isempty(cc), cc{1}= 'b';end
+        if ( isempty(mm) )
+            mm{1}= '.';
+        end
+        if ( isempty(cc) ) 
+            cc{1}= 'b';
+        end
         % Plot the crosses and pivots on domain.
         
         % Get domain.
@@ -82,47 +85,52 @@ if ( ~isempty(varargin) )
         ne = dom(4) +  ht/4;
         
         % plot the square domain
-        rectangle('Position',[x y w ht],LW,2); hold on;
-        axis([sw se nw ne]); axis equal;
+        rectangle('Position', [x y w ht], LW, 2); hold on;
+        axis( [sw se nw ne] ); axis equal;
         
         % Plot pivots
         crosses = f.pivotLocations;
-        defaultopts = {'MarkerSize',7};
-        extraopts = {'Marker',mm{:},'LineStyle','none','Color',cc{:}};
-        if( length(varargin) < 2 )
-            h=plot(crosses(:,1),crosses(:,2),extraopts{:},defaultopts{:});
+        defaultopts = { 'MarkerSize', 7 };
+        extraopts = { 'Marker', mm{:}, 'LineStyle', 'none', 'Color', cc{:} };
+        if ( length(varargin) < 2 )
+            h = plot( crosses(:,1), crosses(:,2), extraopts{:}, defaultopts{:} );
         else
-            h=plot(crosses(:,1),crosses(:,2),extraopts{:},defaultopts{:},varargin{2:end});
+            h = plot(crosses(:,1), crosses(:,2), extraopts{:},...
+                                      defaultopts{:}, varargin{2:end} );
         end
-        if plotline
+        if ( plotline )
             opts = {}; 
-            if ~isempty(ll)
-                opts = {opts{:},'LineStyle',ll{:},'color',cc{:}};
+            if ( ~isempty(ll) )
+                opts = { opts{:}, 'LineStyle', ll{:}, 'color', cc{:} };
             end
             %plot column lines:
-            line([crosses(:,1) crosses(:,1)].',[nw+ht/8 ne-ht/8],opts{:});
+            line( [crosses(:,1) crosses(:,1)].', [nw+ht/8 ne-ht/8], opts{:} );
             %plot row lines:
-            line([sw+w/8 se-w/8],[crosses(:,2),crosses(:,2)].',opts{:});
+            line( [sw+w/8 se-w/8], [crosses(:,2), crosses(:,2)].', opts{:} );
         end
         hold off
     else
 %% Standard surface plot 
 
-        h=surf(f,'facecolor','interp',varargin{:});
+        h=surf(f, 'facecolor', 'interp', varargin{:});
     end
 else
-    if isreal(f)
-        h=surf(f,'facecolor','interp');
+    if ( isreal( f ) )
+        h=surf(f, 'facecolor', 'interp');
     else
 %% Phase Protrait plot 
         % The following is a slightly modified version of Wegert's code 
         % from p345 of his book.  
-        dom = f.domain; nx = 500; ny = 500;
-        x = linspace(dom(1),dom(2),nx); y = linspace(dom(3),dom(4),ny);
-        [xx yy]=meshgrid(x,y); zz = xx+1i*yy;
-        f=feval(f,xx,yy);
-        h = surf(real(zz),imag(zz),0*zz,angle(-f));
-        set(h,'EdgeColor','none');
+        dom = f.domain; 
+        nx = 500; 
+        ny = 500;
+        x = linspace(dom(1), dom(2), nx); 
+        y = linspace(dom(3), dom(4), ny);
+        [xx, yy]=meshgrid(x,y); 
+        zz = xx + 1i*yy;
+        f = feval(f, xx, yy);
+        h = surf( real(zz), imag(zz), 0*zz, angle(-f) );
+        set(h, 'EdgeColor', 'none');
         caxis([-pi pi]), colormap hsv(600)
         view(0,90), axis equal, axis off        
     end
@@ -130,8 +138,11 @@ end
 
 %%
 
-if ~myish, hold off, end
-if nargout > 0, varargout = {h}; end
-
+if ( ~myish )
+    hold off
+end
+if ( nargout > 0 )
+    varargout = { h }; 
+end
 
 end
--- @chebfun2/quiver3.m
+++ @chebfun2/quiver3.m
@@ -1,11 +1,11 @@
-function varargout = quiver3(Z,F,varargin)
+function varargout = quiver3( Z, F, varargin )
 %QUIVER3  3-D quiver plot of a chebfun2v at data mapped by a chebfun2.
 %
-% QUIVER3(Z,F) plots velocity vectors at the equally spaced surface points
+% QUIVER3(Z, F) plots velocity vectors at the equally spaced surface points
 % specified by the chebfun2 Z. We use Z to map a uniform grid. F should be
 % a chebfun2v.
 %
-% QUIVER3(X,Y,Z,F) plots velocity vectors at (x,y,z), where X, Y, Z are
+% QUIVER3(X, Y, Z, F) plots velocity vectors at (x,y,z), where X, Y, Z are
 % chebfun2 objects which we use to to map a uniform grid. F should be a
 % chebfun2v.
 %
@@ -26,7 +26,7 @@ function varargout = quiver3(Z,F,varargin)
 
 numpts = 20; 
 
-if ( ~isa(Z,'chebfun2') )
+if ( ~isa(Z, 'chebfun2') )
     error('CHEBFUN2:QUIVER3:INPUT','First argument to this command should be chebfun2.');
 end
 
@@ -50,7 +50,7 @@ varargin = argin;
 
 
 
-if ( isa(F,'chebfun2v') )              % quiver(Z,F,...)
+if ( isa(F, 'chebfun2v') )              % quiver(Z,F,...)
     zz = new_data_locations(Z, numpts);
     h = quiver3(zz, F, varargin{:});
 elseif ( nargin > 3 )
@@ -59,29 +59,29 @@ elseif ( nargin > 3 )
         yy = new_data_locations(F, numpts);
         zz = new_data_locations(varargin{1}, numpts);
         h = quiver3( xx, yy, zz, varargin{2}, varargin{3:end} );
-    elseif ( isa(Z,'chebfun2') && isa(F,'chebfun2') &&...
-            isa(varargin{1},'chebfun2') &&...
-            isa(varargin{2},'chebfun2') &&...
-            ( nargin ==4 || ~isa(varargin{3},'chebfun2')) )
+    elseif ( isa(Z, 'chebfun2') && isa(F, 'chebfun2') &&...
+            isa(varargin{1}, 'chebfun2') &&...
+            isa(varargin{2}, 'chebfun2') &&...
+            ( nargin ==4 || ~isa(varargin{3}, 'chebfun2')) )
         FF = vertcat( vertcat(F, varargin{1}), varargin{2}) ;
-        h = quiver3( Z, FF );  % call quiver3(Z,F,...)
+        h = quiver3( Z, FF );     % call quiver3(Z,F,...)
     elseif ( ( nargin > 5 ) && isa(Z,'chebfun2') && isa(F,'chebfun2') &&...
-            isa(varargin{1},'chebfun2') &&...
-            isa(varargin{2},'chebfun2') &&...
-            isa(varargin{3},'chebfun2') &&...
-            isa(varargin{4},'chebfun2') &&...
-            ( nargin == 6 || ~isa(varargin{5},'chebfun2')) )
+            isa(varargin{1}, 'chebfun2') &&...
+            isa(varargin{2}, 'chebfun2') &&...
+            isa(varargin{3}, 'chebfun2') &&...
+            isa(varargin{4}, 'chebfun2') &&...
+            ( nargin == 6 || ~isa(varargin{5}, 'chebfun2')) )
         FF = vertcat( vertcat(varargin{2}, varargin{3}), varargin{4} );
         h = quiver3(Z, F, varargin{1}, FF, varargin{5:end} );  % call quiver3(X,Y,Z,F,...)
     else
-        error('CHEBFUN2:QUIVER:INPUTS','Unrecognised input arguments.');
+        error('CHEBFUN2:QUIVER:INPUTS', 'Unrecognised input arguments.');
     end
 else
-    error('CHEBFUN2:QUIVER:INPUTS','Unrecognised input arguments.');
+    error('CHEBFUN2:QUIVER:INPUTS', 'Unrecognised input arguments.');
 end
 
 if ( nargout > 0 )
-    varargout = {h};
+    varargout = { h };
 end
 
 end
