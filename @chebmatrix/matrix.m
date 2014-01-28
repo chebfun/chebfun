0f5bb444879b421cfdb3621d2a671f95299cc7f9
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2014-01-28 20:47:06 +0000

    Next batch of code review of CHEBMATRIX class.

--- @chebmatrix/matrix.m
+++ @chebmatrix/matrix.m
@@ -1,9 +1,11 @@
-function M = matrix(A,varargin)
+function M = matrix(A, varargin)
 %MATRIX  Discretize a chebmatrix as an ordinary matrix.
-%   M = MATRIX(A,DIM) discretizes each block in the chebmatrix A using the
-%   dimension vector DIM for all functions.
+%   M = MATRIX(A, DIM) discretizes each block in the chebmatrix A using the
+%   dimension vector DIM for all functions. In case the domain of A has
+%   breakpoints, the vector DIM must specify the desired discretisation
+%   dimension for each subinterval.
 %
-%   M = MATRIX(A,DIM,DOMAIN) replaces the 'native' domain of A with DOMAIN.
+%   M = MATRIX(A, DIM, DOMAIN) replaces the 'native' domain of A with DOMAIN.
 %   Usually this would be done to introduce a breakpoint.
 %
 %   The discretization type is controlled by the 'prefs' property of A.
@@ -12,18 +14,18 @@ function M = matrix(A,varargin)
 %     d = [0 1];
 %     A = [ operatorBlock.eye(d), operatorBlock.diff(d) ];
 %     A.prefs.discretization = @colloc2;
-%     matrix(A,5)
+%     matrix(A, 5)
 %     A.prefs.discretization = @ultraS;
-%     matrix(A,5)
+%     matrix(A, 5)
 %
-%   See also CHEBOPPREF, CHEBDISCRETIZATION. 
+%   See also CHEBOPPREF, CHEBDISCRETIZATION, CHEBDISCRETIZATION/MATRIX. 
 
 %  Copyright 2013 by The University of Oxford and The Chebfun Developers.
 %  See http://www.chebfun.org for Chebfun information.
 
 % TODO: Confirm with TAD why this was A.prefs.discretizatino, calling a
 % dependent property? AB, 27/1/14.
-d = A.prefs.discretisation(A,varargin{:});
+d = A.prefs.discretisation(A, varargin{:});
 M = matrix(d);
 
 end
--- @chebmatrix/mtimes.m
+++ @chebmatrix/mtimes.m
@@ -5,20 +5,27 @@ function C = mtimes(A, B)
 %  See http://www.chebfun.org for Chebfun information.
 
 if ( isnumeric(A) )  % suspect these aren't necessary...historical...
+    % TODO: Can we remove this then? AB, 28/01/14?
     C = scalartimes(B, A);
 elseif ( isnumeric(B) )
     C = scalartimes(A, B);
 else
+    % If either input is a singe entity of a LINBLOCK, need to wrap in a cell to
+    % simplify code below,
     if ( isa(A, 'linBlock') )
         A = chebmatrix({A});
     elseif ( isa(B, 'linBlock') )
         B = chebmatrix({B});
     end
+    
+    % Setup before we can start composition
     [m, n] = size(A);
     Adata = A.blocks;   % needed to avoid subsref call later
     Bdata = B.blocks;
     [n, p] = size(B);
     C = cell(m, p);
+    
+    % Do the composition
     for i = 1:m
         for j = 1:p
             % It's tricky to just start a sum with "zero", because we don't know
@@ -30,16 +37,18 @@ else
             C{i, j} = u;
         end
     end
+    % Convert the resulting cell C to a chebmatrix to be returned.
     C = chebmatrix(C);
 end
 end
 
-function C = scalartimes(A,z)
-[m,n] = size(A);
-C = cell(m,n);
+function C = scalartimes(A, z)
+% SCALARTIMES   Multiplying blocks in a CHEBMATRIX with a scalar.
+[m, n] = size(A);
+C = cell(m, n);
 for i = 1:m
     for j = 1:n
-        C{i,j} = z*A.blocks{i,j};
+        C{i, j} = z*A.blocks{i, j};
     end
 end
 C = chebmatrix(C);
