6a24162737e0d5745c85cc5806e4b919fc485910
Toby Driscoll <driscoll@udel.edu>
2014-01-29 09:57:29 -0500

    Addressed main directory and chebmatrix TODOs.

--- @chebmatrix/iszero.m
+++ @chebmatrix/iszero.m
@@ -1,8 +1,6 @@
 function out = iszero(f)
 % For future use. 
 
-% TODO: Implement this properly (for linearity detection).
-
 %  Copyright 2013 by The University of Oxford and The Chebfun Developers.
 %  See http://www.chebfun.org for Chebfun information.
 
