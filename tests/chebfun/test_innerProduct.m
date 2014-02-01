9b938cafde6ddeaaf14ec72deaa380d15ccb2dfc
Kuan Xu <xukuan2011@gmail.com>
2014-02-01 18:01:35 +0000

    Tests.

--- tests/chebfun/test_innerProduct.m
+++ tests/chebfun/test_innerProduct.m
@@ -10,7 +10,7 @@ end
 % Set a domain
 dom = [-2 7];
 
-%% Test on singular function: piecewise smooth chebfun - splitting on.
+%% Test for singular function: piecewise smooth chebfun - splitting on.
 
 pow1 = -0.3;
 pow2 = -0.5;
@@ -26,5 +26,54 @@ I = innerProduct(f,g);
 I_exact = 0.35838148154346034 - 0.26037938759089226i;
 pass(1) = ( abs(I-I_exact) < 2e1*max(get(f, 'epslevel'), get(g, 'epslevel'))*...
     abs(I_exact) );
+
+%% Test for functions defined on unbounded domains:
+
+%% Functions on [-inf inf]:
+
+% Set the domain:
+dom = [-Inf Inf];
+
+opf = @(x) 2-exp(-x.^2);
+opg = @(x) exp(-x.^2);
+f = chebfun(opf, dom);
+g = chebfun(opg, dom);
+
+I = innerProduct(f, g);
+IExact = (sqrt(pi)*(4 - sqrt(2)))/2;
+err = abs(I - IExact);
+pass(2) = err < 2e6*max(get(f,'epslevel')*get(f,'vscale'), ...
+    get(g,'epslevel')*get(g,'vscale'));
+
+%% Functions on [a inf]:
+
+% Set the domain:
+dom = [1 Inf];
+
+opf = @(x) x;
+opg = @(x) exp(-x);
+pref = chebpref();
+pref.singPrefs.exponents = [0 1];
+f = chebfun(opf, dom, pref);
+g = chebfun(opg, dom);
+I = innerProduct(f, g);
+IExact = 2*exp(-1);
+err = abs(I - IExact);
+pass(3) = err < 1e8*max(get(f,'epslevel')*get(f,'vscale'), ...
+    get(g,'epslevel')*get(g,'vscale'));
+
+%% Functions on [-inf b]:
+
+% Set the domain:
+dom = [-Inf -3*pi];
+
+opf = @(x) 1./x;
+opg = @(x) 2./x;
+f = chebfun(opf, dom);
+g = chebfun(opg, dom);
+I = innerProduct(f, g);
+IExact = 2/(3*pi);
+err = abs(I - IExact);
+pass(4) = err < 1e5*get(f,'epslevel')*get(f,'vscale');
 
 end
