a3a3218f4e09f9dbf790de3328f62c7d5e3cc4ee
Townsend <townsend@maths.ox.ac.uk>
2014-02-02 10:03:48 +0000

    Adding LU command for column quasimatrices.

--- @chebfun/lu.m
+++ @chebfun/lu.m
@@ -1,28 +1,65 @@
-% TODO: 
+function [L, U, p] = lu( A , varargin )
+%LU  LU factorization of a quasimatrix. 
+% 
+% [L, U] = lu(A) stores an upper-triangular matrix U and a "psychologically"
+% lower triangular quasimatrix in L so that A = L*U. Here, A is a column 
+% quasimatrix. 
+%
+% [L, U, p] = lu(A) returns the pivoting information so that L(p,:) is a lower
+% triangular matrix. 
+%
+% For more information about the factorization: 
+% A. Townsend and L. N. Trefethen, Continuous analogues of matrix
+% factorizations, submitted, 2014. 
+%
+% See also QR. 
 
-f = chebfun2(@(x,y) cos(x.*y) + x); 
-C = f.cols; 
-[Q, R] = qr( C ); 
+% Copyright 2013 by The University of Oxford and The Chebfun Developers.
+% See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
 
-L = C; 
-U = zeros( eye( size(L, 2) ) ); 
-% Do GE with partial pivoting: 
-for j = 1 : size(Q, 2)
-    % find maximum absolute entry: 
-    [vals, pos] = minandmax( L(:, j) );
-    [mx, idx] = max( abs( vals ) ); 
-    pos = pos( idx );
-    
-    % Do one step of GE: 
-    L = L - L(:, j) * L(pos, :) / mx; 
-    
-    % Store upper-triangular part: 
-    U = U + L(pos, :); 
-    
-    % store pivot locations: 
-    pivots( j ) = pos; 
+% Empty check: 
+if ( isempty( A ) ) 
+    L = chebfun; 
+    U = []; 
+    p = []; 
+    return
 end
 
+if ( A(1).isTransposed )
+    error('CHEBFUN:LU:SIZES',...
+        'CHEBFUN LU works only for column CHEBFUN objects.');
+end
 
-roots(L)
+L = A; 
+U = zeros( size(L, 2) ); 
+p = zeros( size(L, 2), 1);
+% Do GE with partial pivoting: 
+for j = 1 : size(A, 2)
+    Acol = extractColumns(A, j);
+    
+    % find maximum absolute entry: 
+    [vals, pos] = minandmax( Acol );
+    [ignored, idx] = max( abs( vals ) ); 
+    pos = pos( idx );
+    mx = feval( Acol, pos ); 
+    Arow = feval(A, pos); 
+    
+    % Store upper-triangular part: 
+    U( j, : ) = Arow; 
+    if ( j == 1 ) 
+        L = Acol / mx; 
+    else
+        L = horzcat(L, Acol / mx ); 
+    end
+    
+    % Do one step of GE: 
+    A = A - Acol * Arow / mx; 
+    
+    % store pivot locations: 
+    p( j ) = pos; 
+end
 
+% Make upper-triangular even with rounding errors:
+U = triu( U );
+
+end
---
+++ tests/chebfun/test_chebfun_lu.m
@@ -0,0 +1,31 @@
+function pass = test_chebfun_lu( pref ) 
+% Test Chebfun LU command. 
+
+if ( nargin == 0) 
+    pref = chebpref; 
+end
+
+tol = pref.cheb2Prefs.eps; 
+j = 1;
+
+% Check accuracy on [-1,1]
+x = chebfun(@(x) x); 
+A = [1 x x.^2 x.^3 x.^4 x.^5]; 
+[L, U, p] = lu( A ); 
+
+pass(j) = norm( triu(U) - U ) < tol; j = j + 1; 
+pass(j) = norm( A - L * U ) < tol; j = j + 1; 
+pass(j) = norm( diag( L(p,:) ) - ones(size(L,2),1) ) < 10*tol; j = j + 1; 
+pass(j) = norm( tril(L(p,:)) - L(p,:)) < 10*tol; j = j + 1; 
+
+% Check accuracy on [-2,3]
+x = chebfun(@(x) x, [-2 3]); 
+A = [1 x x.^2 x.^3 x.^4 x.^5]; 
+[L, U, p] = lu( A ); 
+
+pass(j) = norm( triu(U) - U ) < tol; j = j + 1; 
+pass(j) = norm( A - L * U ) < 100*tol; j = j + 1; 
+pass(j) = norm( diag( L(p,:) ) - ones(size(L,2),1) ) < 10*tol; j = j + 1; 
+pass(j) = norm( tril(L(p,:)) - L(p,:)) < 10*tol; j = j + 1; 
+
+end
