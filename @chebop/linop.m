00b91efc437dddc33245434639dc9213593a43b7
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2014-02-05 17:44:32 +0000

    Minor documentation improvements.

--- @chebop/linop.m
+++ @chebop/linop.m
@@ -1,12 +1,12 @@
 function [L, f] = linop(N)
 %LINOP Converts a chebop to a linop
 % L = LINOP(N) converts a chebop N to a linop L if N is a linear operator.
-% If N is not linear, then an error message is returned.
+% If N is not linear, an error message is returned.
 %
 % [L F] = LINOP(N) returns also the affine part F of the linear chebop N
 % such that L*u + F(x) = N.op(x,u).
 %
-% See also LINOP, CHEBOP/LINEARISE, CHEBOP/ISLINEAR, CHEBOP/DIFF
+% See also LINOP, CHEBOP/LINEARIZE, CHEBOP/ISLINEAR
 
 % Copyright 2011 by The University of Oxford and The Chebfun Developers. 
 % See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
@@ -20,10 +20,11 @@ linCheck = 1;
 % We need the entire operator (including BCs) to be linear
 isLinear = all(isLinear);
 
+% TODO: Remove once linearity detection is ready.
 warning('Linearity detection for chebops is not yet fully implemented');
 
 % Throw an error is the chebop is nonlinear
 if ~( isLinear )
-    error('CHEBOP:linop:nonlinear',...
+    error('CHEBFUN:CHEBOP:linop:nonlinear',...
         'Chebop does not appear to be a linear operator.')
 end
