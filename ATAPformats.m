38d2a1c49c7cc469598ad9cd2970b7deb3846718
Nick Hale <nick.p.hale@gmail.com>
2014-02-06 16:14:34 +0000

    Adding a 'reset' option to ATAPformats.
    ATAPformats('reset') or ATAPformats('factory') will
    revert everything to the default setting.

--- ATAPformats.m
+++ ATAPformats.m
@@ -1,3 +1,4 @@
+function ATAPformats(flag)
 %ATAPFORMATS   Set default formats for Trefethen's book 
 %              "Approximation Theory and Approximation Practice"
 %
@@ -6,18 +7,44 @@
 % appearance. It is included in this directory as part of the Chebfun
 % distribution so that readers of the book need only download Chebfun to be able
 % to execute each section.
+%
+% ATAPFORMATS('reset') will reset these defaults to the MATLAB default state.
 
 % Copyright 2014 by The University of Oxford and The Chebfun Developers. 
 % See http://www.chebfun.org/ for Chebfun information.
 
-close all
-clear all 
-set(0, 'defaultfigureposition', [380 320 540 200])
-set(0, 'defaultaxeslinewidth', 0.9)
-set(0, 'defaultaxesfontsize', 8)
-set(0, 'defaultlinelinewidth', 1.1)
-set(0, 'defaultpatchlinewidth', 1.1)
-set(0, 'defaultlinemarkersize', 15); 
-format compact
-format long
-chebpref.setDefaults('factory');
+persistent defaultfigureposition
+if ( isempty(defaultfigureposition) )
+    defaultfigureposition = get(0, 'defaultfigureposition');
+    mlock();
+end
+
+if ( nargin == 0 )
+    % Change to the ATAP settings.
+
+    close all 
+    set(0, 'defaultfigureposition', [380 320 540 200])
+    set(0, 'defaultaxeslinewidth', 0.9)
+    set(0, 'defaultaxesfontsize', 8)
+    set(0, 'defaultlinelinewidth', 1.1)
+    set(0, 'defaultpatchlinewidth', 1.1)
+    set(0, 'defaultlinemarkersize', 15); 
+    format compact
+    format long
+    chebpref.setDefaults('factory');
+    
+elseif ( any(strcmpi(flag, {'reset', 'factory'})) )
+    % Revert to MATLAB defaults
+
+    close all
+    % The default options (harcoded). Matlab 2014a Mac OSX.
+    set(0, 'defaultfigureposition', defaultfigureposition)
+    set(0, 'defaultaxeslinewidth', 0.5)
+    set(0, 'defaultaxesfontsize', 10)
+    set(0, 'defaultlinelinewidth', 0.5)
+    set(0, 'defaultpatchlinewidth', 0.5)
+    set(0, 'defaultlinemarkersize', 6); 
+    
+end
+
+end    
