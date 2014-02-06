4fc66f3d95365cef974673f0252cfe5bc873e975
Nick Hale <nick.p.hale@gmail.com>
2014-02-06 09:43:31 +0000

    Minor changes noticed during a merge with development.

--- @chebtech/imag.m
+++ @chebtech/imag.m
@@ -2,18 +2,18 @@ function f = imag(f)
 %IMAG   Imaginary part of a CHEBTECH.
 %   IMAG(F) is the imaginary part of F.
 %
-%   See also REAL, ISREAL, CONJ.
+% See also REAL, ISREAL, CONJ.
 
 % Copyright 2013 by The University of Oxford and The Chebfun Developers. 
 % See http://www.chebfun.org for Chebfun information.
 
 % Compute the imaginary part of the values:
 f.values = imag(f.values);
-f.vscale = max(abs(f.values(:)));
+f.vscale = max(abs(f.values), [], 1);
 
 if ( ~any(f.values(:)) )
     % Input was real, so output a zero CHEBTECH:
-    f = f.make(zeros(1, size(f.values, 2)), f.vscale, f.hscale);
+    f = f.make(zeros(1, size(f.values, 2)), 0, f.hscale);
     f.ishappy = 1;
 else
     % Compute imaginary part of the coefficients:
