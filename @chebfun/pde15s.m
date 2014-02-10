7703aa1bf8771a56b73fdd981adfa69c9f30319f
Nick Hale <nick.p.hale@gmail.com>
2014-02-10 10:39:37 +0000

    Adding support for the old syntax.
    Updating tests for new syntax.

--- @chebfun/pde15s.m
+++ @chebfun/pde15s.m
@@ -286,6 +286,7 @@ else
         tmp = chebdouble(ones(1, SYSSIZE));
         sizeOp = size(op(tmp, 0, mean(DOMAIN)));
         leftNonlinBCLocs = 1:max(sizeOp);
+        bc.left = [];
         bc.left.op = {@(n) zeros(max(sizeOp), SYSSIZE*n)}; % Dummy entries.
         BCRHS = num2cell(zeros(1, max(sizeOp)));
         leftNonlinBCFuns = op;
@@ -324,8 +325,8 @@ else
         tmp = chebdouble(ones(1, SYSSIZE));
         sizeOp = size(op(tmp, 0, mean(DOMAIN)));
         rightNonlinBCLocs = 1:max(sizeOp);
-        bc.right.op = {@(n) zeros(max(sizeOp), SYSSIZE*n)};  % Dummy entries.
-        bc.right.val = zeros(max(sizeOp),1);
+        bc.right = [];
+        bc.right.op = {@(n) zeros(max(sizeOp), SYSSIZE*n)};
         BCRHS = [BCRHS num2cell(zeros(1, max(sizeOp)))];
         rightNonlinBCFuns = op;
     else
@@ -660,15 +661,20 @@ function outfun = parseFun(inFun)
 
 global SYSSIZE
 
-% We don't accept only time or space as input args (i.e., both or nothing).
+inFun = backCompat(inFun);
+
+% V4: We don't accept only time or space as input args (i.e., both or nothing).
+% V5: Actually, we now accept @(u, x) diff(u, 2) + sin(x).*u (for chebops).
 Nind = nargin(inFun) - SYSSIZE;
 if ( Nind == 0 )
     outfun = @(u, t, x) conv2cell(inFun, u);
+elseif ( Nind == 1 )
+    outfun = @(u, t, x) conv2cell(inFun, u, x);
 elseif ( Nind == 2 )
     outfun = @(u, t, x) conv2cell(inFun, u, t, x);
 else
     error('CHEBFUN:pde15s:inputs_ind', ['Incorrect number of independant ' ...
-        'variables in input function. (Must be 0 or 2).']);
+        'variables in input function. (Must be 0, 1, or 2).']);
 end
 
 end
@@ -721,4 +727,119 @@ elseif ( isnumeric(in) )
 else
     out = in;
 end
+end
+
+function outFun = backCompat(inFun)
+% In V4 PDE15S required the user to pass in dummy function handles for the
+% differential operators. For example, u'' + u' + sum(u) would have needed
+%  pdefun = @(u, t, x, diff, sum) diff(u, 2) + diff(u) + sum(u).
+% V5 deals with this in a different way (by using the chebdouble class), but we
+% would still like to support the old syntax (at least for now).
+%
+% To do this, we parse the input function for the strings 'diff', 'Diff', and
+% 'D'. If we find any of these, we assume the old syntax is being used, and
+% redefine the function handle appropriately. Of course, this is not fool-proof,
+% but it should work most of the time.
+
+% Determine the variable names:
+str = func2str(inFun);
+parLoc = strfind(str, ')');
+varList = str(3:parLoc(1)-1);
+commaLoc = strfind(varList, ',');
+varNames = cell(1, numel(commaLoc)+1);
+for k = 1:numel(commaLoc)
+    varNames{k} = varList(1:commaLoc(k)-1);
+    varList(1:commaLoc(k)) = [];
+    commaLoc = commaLoc - commaLoc(k); 
+end
+varNames{numel(commaLoc)+1} = varList;
+
+% Look for 'diff', 'Diff', or 'D':
+diffLoc = cellfun(@(v) any(strcmp(v, {'diff', 'Diff', 'D'})), varNames);
+idx = find(diffLoc);
+if ( isempty(idx) )
+    % None present - new syntax!
+    outFun = inFun;
+    return
+end
+
+% Get the new list of variables:
+newVarList = varNames{1};
+for k = 2:idx-1
+    newVarList = [newVarList, ',', varNames{k}]; %#ok<AGROW>
+end
+
+% Compile the new function string:
+funList = {'@diff', '@sum', '@cumsum', '@fred'};
+newStr = ['@(', newVarList, ')' 'inFun(' newVarList];    
+for k = 0:(nargin(inFun) - idx)
+    newStr = [newStr, ',', funList{k+1}]; %#ok<AGROW>
+end
+newStr = [newStr, ')'];
+
+% Make the new function handle:
+outFun = eval(newStr);
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% TODO: DIFFMAT and CUMSUMMAT are in @colloc2/private/. Below should be removed.
+
+function D = diffmat(N,k)
+
+%  Copyright 2013 by The University of Oxford and The Chebfun Developers.
+%  See http://www.chebfun.org for Chebfun information.
+% DIFFMAT  Chebyshev differentiation matrix
+% D = DIFFMAT(N) is the matrix that maps function values at N Chebyshev
+% points to values of the derivative of the interpolating polynomial at
+% those points.
+%
+% D = DIFFMAT(N,K) is the same, but for the Kth derivative.
+%
+% The matrices are computed using the 'hybrid' formula of Schneider &
+% Werner [1] and Welfert [2] proposed by Tee [3].
+
+% Copyright 2011 by The University of Oxford and The Chebfun Developers.
+% See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
+
+% References:
+%  [1] Schneider, C. and Werner, W., "Some new aspects of rational
+%   interpolation", Math. Comp. (47) 285--299, 1986.
+%  [2] Welfert, B. D., "Generation of pseudospectral matrices I", SINUM,
+%   (34) 1640--1657.
+%  [3] Tee, T. W., "An adaptive rational spectral method for differential
+%   equations with rapidly varying solutions", Oxford DPhil Thesis, 2006.
+
+if nargin < 2, k = 1; end
+if N == 0, D = []; return, end
+if N == 1, D = 0; return, end
+
+% construct Chebyshev grid and weights
+x = chebtech2.chebpts(N);
+w = [.5 ; ones(N-1,1)]; w(2:2:end) = -1; w(N) = .5*w(N);
+
+ii = (1:N+1:N^2)';              % indices of diagonal
+Dx = bsxfun(@minus,x,x');       % all pairwise differences
+Dx(ii) = Dx(ii) + 1;            % add identity
+Dxi = 1./Dx;                    % reciprocal
+Dw = bsxfun(@rdivide,w.',w);    % pairwise divisions
+Dw(ii) = Dw(ii) - 1;            % subtract identity
+
+% k = 1
+D = Dw .* Dxi;
+D(ii) = 0; D(ii) = - sum(D,2);              % negative sum trick
+
+if k == 1, return, end
+
+% k = 2
+D = 2*D .* (repmat(D(ii),1,N) - Dxi);
+D(ii) = 0; D(ii) = - sum(D,2);              % negative sum trick
+
+% higher orders
+for n = 3:k
+    D = n*Dxi .* (Dw.*repmat(D(ii),1,N) - D);
+    D(ii) = 0; D(ii) = - sum(D,2);          % negative sum trick
+end
+
 end
--- tests/misc/test_pde15s.m
+++ tests/misc/test_pde15s.m
@@ -6,7 +6,7 @@ end
 
 pass = 1;
 
-return
+end
 
 %% pde15s_demos
 % Some demos of pde15s (and pdesolve)
@@ -16,7 +16,7 @@ close all
 d = [-1, 1]; x = chebfun('x', d);
 u = exp(3*sin(pi*x));
 plot(u); hold on
-f = @(u, D) -D(u);
+f = @(u) -diff(u);
 opts = pdeset('eps', 1e-4, 'abstol', 1e-4, 'reltol', 1e-4, 'plot', 1);
 pde15s(f, 0:.05:2, u, 'periodic', opts);
 
@@ -24,7 +24,7 @@ pde15s(f, 0:.05:2, u, 'periodic', opts);
 close all
 d = [-1, 1]; x = chebfun('x', d);
 u = exp(3*sin(pi*x));
-f = @(u, t, x, D) -(1+0.3*sin(pi*x)).*D(u) + 1e-6*D(u, 2);
+f = @(u, t, x) -(1+0.3*sin(pi*x)).*diff(u) + 1e-6*diff(u, 2);
 opts = pdeset('eps', 1e-4, 'abstol', 1e-4, 'reltol', 1e-4, 'plot', 1);
 pde15s(f, 0:.05:2, u, 'periodic', opts);
 
@@ -32,7 +32,7 @@ pde15s(f, 0:.05:2, u, 'periodic', opts);
 close all
 d = [-1, 1]; x = chebfun('x', d);
 u = (1-x.^2).*exp(-30*(x+.5).^2);
-f = @(u, t, x, D) -D(u)+.002*D(u, 2);
+f = @(u, t, x) -diff(u)+.002*diff(u, 2);
 opts = pdeset('eps', 1e-4, 'abstol', 1e-4, 'reltol', 1e-4, 'plot', 1);
 pde15s(f, 0:.05:2, u, 'dirichlet', opts);
 
@@ -41,7 +41,7 @@ close all
 E = 1e-1;
 d = [-3*pi/4, pi]; x = chebfun('x', d);
 u = sin(2*x);
-f = @(u, t, x, D) E*D(u, 2)+D(u);
+f = @(u, t, x) E*diff(u, 2)+diff(u);
 lbc = {'neumann', 0};
 rbc = {'dirichlet', 1};
 bc = struct; bc.left = lbc; bc.right = rbc;
@@ -54,9 +54,9 @@ close all
 E = 1e-1;
 d = [-3*pi/4, pi]; x = chebfun('x', d);
 u = sin(2*x);
-f = @(u, t, x, D) E*D(u, 2)+D(u);
+f = @(u, t, x) E*diff(u, 2)+diff(u);
 lbc = 'neumann';
-rbc = @(u, t, x, D) u - .1*sin(t);
+rbc = @(u, t, x) u - .1*sin(t);
 bc = struct; bc.left = lbc; bc.right = rbc;
 opts = pdeset('holdPlot', 'on');
 tt = linspace(0, 3, 101);
@@ -66,14 +66,14 @@ uu = pde15s(f, tt, u, bc, opts);
 close all
 d = [-1, 1]; x = chebfun('x', d);
 u = (1-x.^2).^2.*(1+sin(12*x))/2;
-f = @(u, D) u.*(1-u) + 5e-4*D(u, 2);
+f = @(u) u.*(1-u) + 5e-4*diff(u, 2);
 pde15s(f, 0:0.1:5, u, 'neumann');
 
 %% Allen-Cahn 
 close all
 d = [-1, 1]; x = chebfun('x', d);
 u = .53*x-.47*sin(1.5*pi*x);
-f = @(u, D) u.*(1-u.^2) + 5e-4*D(u, 2);
+f = @(u) u.*(1-u.^2) + 5e-4*diff(u, 2);
 bc.left = struct('op', 'dirichlet', 'val', -1);
 bc.right = struct('op', 'dirichlet', 'val', 1);
 opts = pdeset('Ylim', [-1.1 1.1]);
@@ -83,18 +83,17 @@ pde15s(f, 0:0.1:5, u, bc, opts);
 close all
 d = [-1, 1]; x = chebfun('x', d);
 u = (1-x.^2).*exp(-30*(x+.5).^2);
-f = @(u, D) -D(u.^2)+.01*D(u, 2);
+f = @(u) -diff(u.^2)+.01*diff(u, 2);
 pde15s(f, 0:.05:3, u, 'dirichlet');
 
 %% KS
 close all
 d = [-1, 1]; x = chebfun('x', d);
-I = eye(d); D = diff(d);
 u = 1 + 0.5*exp(-40*x.^2);
 bc = struct;
-bc.left = @(u, D) [u-1 ; D(u)];
-bc.right = @(u, D) [u-1 ; D(u)];
-f = @(u, D) u.*D(u)-D(u, 2)-0.006*D(u, 4);
+bc.left = @(u) [u-1 ; diff(u)];
+bc.right = @(u) [u-1 ; diff(u)];
+f = @(u) u.*diff(u)-diff(u, 2)-0.006*diff(u, 4);
 u = pde15s(f, 0:.01:.5, u, bc);
 
 %% Cahn-Hilliard - not working!
@@ -105,9 +104,9 @@ u = cos(pi*x)-exp(-6*pi*x.^2);
 plot(u)
 opts = pdeset('eps', 1e-4, 'abstol', 1e-4, 'reltol', 1e-4, 'plot', 1);
 bc = struct;
-bc.left = @(u, D) [u+1 ; D(u)];
-bc.right = @(u, D) [u+1 ; D(u)];
-f = @(u, t, x, D) -D(u, 4) + D(u.^3, 2)-D(u, 2);
+bc.left = @(u) [u+1 ; diff(u)];
+bc.right = @(u) [u+1 ; diff(u)];
+f = @(u, t, x) -diff(u, 4) + diff(u.^3, 2)-diff(u, 2);
 tt = linspace(0, .001, 101);
 uu = pde15s(f, tt, u, bc, opts);
 
@@ -121,9 +120,9 @@ uu = pde15s(f, tt, u, bc, opts);
 close all
 d = [-1, 1]; x = chebfun('x', d);
 u = [ chebfun(1, d)  chebfun(1, d) ];
-f = @(u, v, t, x, D) [ -u + (x + 1).*v + 0.1*D(u, 2) , ...
-                        u - (x + 1).*v + 0.2*D(v, 2) ];
-bc.left = @(u, v, D) [D(u), D(v)];  bc.right = bc.left;   % New way
+f = @(u, v, t, x) [ -u + (x + 1).*v + 0.1*diff(u, 2) , ...
+                     u - (x + 1).*v + 0.2*diff(v, 2) ];
+bc.left = @(u, v) [diff(u), diff(v)];  bc.right = bc.left;   % New way
 opts = pdeset('plot', 1, 'N', 32);
 uu = pde15s(f, 0:.05:3, u, bc, opts);
 
@@ -133,9 +132,9 @@ uu = pde15s(f, 0:.05:3, u, bc, opts);
 close all
 d = [-1, 1]; x = chebfun('x', d);
 u = [ 1-erf(10*(x+0.7)) , 1 + erf(10*(x-0.7)) , chebfun(0, d) ];
-f = @(u, v, w, D)      [ 0.1*D(u, 2) - 100*u.*v , ...
-                      0.2*D(v, 2) - 100*u.*v , ...
-                     .001*D(w, 2) + 2*100*u.*v ];                  
+f = @(u, v, w)      [ 0.1*diff(u, 2) - 100*u.*v , ...
+                      0.2*diff(v, 2) - 100*u.*v , ...
+                     .001*diff(w, 2) + 2*100*u.*v ];                  
 bc = 'neumann';     
 uu = pde15s(f, 0:.1:3, u, bc);
 
@@ -145,10 +144,11 @@ close all
 % Crazy nonlinear boundary conditions
 d = [-1, 1]; x = chebfun('x', d);
 u = [ chebfun(1, d)  chebfun(1, d) ];
-f = @(u, v, t, x, D) [ -u + (x + 1).*v + 0.1*D(u, 2) , ...
-                    u - (x + 1).*v + 0.2*D(v, 2) ];
-bc.left =  @(u, v, t, x, D) [D(u)+t*sin(u)./v,  D(v)];
-bc.right = @(u, v, t, x, D) [D(u),              D(v).*v+sin(5*pi*t)];
+f = @(u, v, t, x) [ -u + (x + 1).*v + 0.1*diff(u, 2) , ...
+                     u - (x + 1).*v + 0.2*diff(v, 2) ];
+bc = struct;
+bc.left =  @(u, v, t, x) [diff(u)+t*sin(u)./v,  diff(v)];
+bc.right = @(u, v, t, x) [diff(u),              diff(v).*v+sin(5*pi*t)];
 uu = pde15s(f, 0:.05:1, u, bc);
 
 %%
@@ -157,10 +157,8 @@ close all
 % Maxwell's Equations
 d = [-1, 1]; x = chebfun('x', d);
 u = exp(-20*x.^2) .* sin(14*x);  u = [u -u];
-D = diff(d); I = eye(d); Z = zeros(d);
-f = @(u, v, D) [D(v) ; D(u)];
-bc.left = @(u, v, D) u; bc.right = @(u, v, D) v;        % New way
+f = @(u, v) [diff(v) ; diff(u)];
+bc.left = @(u, v) u; bc.right = @(u, v) v;        % New way
 opt = pdeset('eps', 1e-6, 'Ylim', pi/2*[-1 1], 'AbsTol', 1e-6, 'RelTol', 1e-6);
 uu = pde15s(f, 0:.05:2, u, bc, opt, 64);
 
-end
