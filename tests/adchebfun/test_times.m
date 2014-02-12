a98d6855d6f3fd359f27131016d86d64d044b90c
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2014-02-12 14:43:49 +0000

    Tidying up of tests.

--- tests/adchebfun/test_times.m
+++ tests/adchebfun/test_times.m
@@ -2,11 +2,9 @@
 
 function pass = test_times
 
-% Functions to be tested
-funcList = {@times};
 
 % Initialise pass vector
-pass = zeros(2*length(funcList), 5);
+pass = zeros(2, 5);
 
 % Steps to be taken in Taylor testing
 numSteps = 6;
@@ -15,37 +13,34 @@ numSteps = 6;
 tolOrder = 1e-2;
 tolDiff = 1e-14;
 
-% Loop through functions
-for funcCounter = 1:length(funcList)
-    % Temporarily store the function handle we're working with:
-    func = funcList{funcCounter};
-    
-    % Compare values
-    err = adchebfun.valueTestingBinary(func);
-    
-    % Confirm that the error returned is zero
-    pass(2*(funcCounter-1) + 1, :) = ( err == 0 );
-    
-    % Taylor testing
-    [order1, order2, nDiff2] = adchebfun.taylorTestingBinary(func, numSteps);
-    
-    % We expect all elements of ORDER1 to be close to 1. Since the methods being
-    % tested in this case are all linear, ORDER2 will be noise. However, since
-    % the methods are indeed linear, we should expect nDiff2 to have values all
-    % close to machine epsilon, which we can use to check for the correctness of
-    % the derivative computed.
-    
-    % Check whether we get the expected results for linear and nonlinear
-    % operations.
-    linearOpResults = ( ((max(abs(order1(:, 2:end))) - 1) < tolOrder) & ...
-        (max(abs(nDiff2(:, 2:end))) < tolDiff) );
-    
-    nonlinearOpResults = ( (max(abs(order1(:, 1) - 1)) < tolOrder) && ...
-        (max(abs(order2(:, 1) - 2)) < tolOrder) );
-    
-    % Concatenate results
-    pass(2*(funcCounter-1) + 2, :) = [nonlinearOpResults, linearOpResults];
-    
-end
+% The function handle we're working with:
+func = @times;
+
+% Compare values
+err = adchebfun.valueTestingBinary(func);
+
+% Confirm that the error returned is zero
+pass(1, :) = ( err == 0 );
+
+% Taylor testing
+[order1, order2, nDiff2] = adchebfun.taylorTestingBinary(func, numSteps);
+
+% We expect all elements of ORDER1 to be close to 1. Since the methods being
+% tested in this case are all linear, ORDER2 will be noise. However, since
+% the methods are indeed linear, we should expect nDiff2 to have values all
+% close to machine epsilon, which we can use to check for the correctness of
+% the derivative computed.
+
+% Check whether we get the expected results for linear and nonlinear
+% operations.
+linearOpResults = ( ((max(abs(order1(:, 2:end))) - 1) < tolOrder) & ...
+    (max(abs(nDiff2(:, 2:end))) < tolDiff) );
+
+nonlinearOpResults = ( (max(abs(order1(:, 1) - 1)) < tolOrder) && ...
+    (max(abs(order2(:, 1) - 2)) < tolOrder) );
+
+% Concatenate results
+pass(2, :) = [nonlinearOpResults, linearOpResults];
+
 
 end
