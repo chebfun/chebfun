27a32d0e34b9aa505065b27c2d5f03e4b50c6fc4
Nick Hale <nick.p.hale@gmail.com>
2014-02-12 10:07:02 +0000

    Revert to the Matlab 'factory' values.
    http://undocumentedmatlab.com/blog/getting-default-hg-property-values/

--- ATAPformats.m
+++ ATAPformats.m
@@ -8,43 +8,37 @@ function ATAPformats(flag)
 % distribution so that readers of the book need only download Chebfun to be able
 % to execute each section.
 %
-% ATAPFORMATS('reset') will reset these defaults to the MATLAB default state.
+% ATAPFORMATS('factory') will reset these defaults to the MATLAB default state.
 
 % Copyright 2014 by The University of Oxford and The Chebfun Developers. 
 % See http://www.chebfun.org/ for Chebfun information.
-
-persistent defaultfigureposition
-if ( isempty(defaultfigureposition) )
-    defaultfigureposition = get(0, 'defaultfigureposition');
-    mlock();
-end
 
 if ( nargin == 0 )
     % Change to the ATAP settings.
 
     evalin('caller', 'clear all')
     close all 
-    set(0, 'defaultfigureposition', [380 320 540 200])
-    set(0, 'defaultaxeslinewidth', 0.9)
-    set(0, 'defaultaxesfontsize', 8)
-    set(0, 'defaultlinelinewidth', 1.1)
-    set(0, 'defaultpatchlinewidth', 1.1)
+    set(0, 'defaultfigureposition', [380 320 540 200]);
+    set(0, 'defaultaxeslinewidth',  0.9);
+    set(0, 'defaultaxesfontsize',   8);
+    set(0, 'defaultlinelinewidth',  1.1);
+    set(0, 'defaultpatchlinewidth', 1.1);
     set(0, 'defaultlinemarkersize', 15); 
     format compact
     format long
     chebpref.setDefaults('factory');
     
 elseif ( any(strcmpi(flag, {'reset', 'factory'})) )
-    % Revert to MATLAB defaults
+    % Revert to MATLAB factory values.
 
     close all
-    % The default options (harcoded). Matlab 2014a Mac OSX.
-    set(0, 'defaultfigureposition', defaultfigureposition)
-    set(0, 'defaultaxeslinewidth', 0.5)
-    set(0, 'defaultaxesfontsize', 10)
-    set(0, 'defaultlinelinewidth', 0.5)
-    set(0, 'defaultpatchlinewidth', 0.5)
-    set(0, 'defaultlinemarkersize', 6); 
+    set(0, 'defaultfigureposition', get(0, 'factoryfigureposition'));
+    set(0, 'defaultaxeslinewidth',  get(0, 'factoryaxeslinewidth'));
+    set(0, 'defaultaxesfontsize',   get(0, 'factoryaxesfontsize'));
+    set(0, 'defaultlinelinewidth',  get(0, 'factorylinelinewidth'));
+    set(0, 'defaultpatchlinewidth', get(0, 'factorypatchlinewidth'));
+    set(0, 'defaultlinemarkersize', get(0, 'factorylinemarkersize')); 
+    chebpref.setDefaults('factory');
     
 end
 
