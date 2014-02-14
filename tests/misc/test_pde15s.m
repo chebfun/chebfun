fa5617587b10016509c1c2f132c20e8973e981af
Anthony P. Austin <austin@maths.ox.ac.uk>
2014-02-14 11:30:08 +0000

    Move PDE15s files to new branch based directly off of development.
    pde15s() no longer depends on linop, so there's no sense making it remain that
    way causing it to be held up while linop gets finished.
    
    This work was done predominantly by Nick Hale and was reveiwed by Asgeir
    Birkisson.

---
+++ tests/misc/test_pde15s.m
@@ -0,0 +1,179 @@
+% function pass = test_pde15s(pref)
+% 
+% if ( nargin == 0 )
+%     pref = chebpref();
+% end
+% 
+% pass = 1;
+% 
+% end
+
+%% pde15s_demos
+% Some demos of pde15s (and pdesolve)
+
+%% Advection
+close all
+d = [-1, 1]; x = chebfun('x', d);
+u = exp(3*sin(pi*x));
+plot(u); hold on
+f = @(u) -diff(u);
+opts = pdeset('eps', 1e-4, 'abstol', 1e-4, 'reltol', 1e-4, 'plot', 1);
+uu = pde15s(f, 0:.05:2, u, 'periodic', opts);
+
+
+%% Nonuniform Advection
+close all
+d = [-1, 1]; x = chebfun('x', d);
+u = exp(3*sin(pi*x));
+f = @(u, t, x) -(1+0.3*sin(pi*x)).*diff(u) + 1e-6*diff(u, 2);
+opts = pdeset('eps', 1e-4, 'abstol', 1e-4, 'reltol', 1e-4, 'plot', 1);
+uu = pde15s(f, 0:.05:2, u, 'periodic', opts);
+
+%% Advection-diffusion
+close all
+d = [-1, 1]; x = chebfun('x', d);
+u = (1-x.^2).*exp(-30*(x+.5).^2);
+f = @(u, t, x) -diff(u)+.002*diff(u, 2);
+opts = pdeset('eps', 1e-4, 'abstol', 1e-4, 'reltol', 1e-4, 'plot', 1);
+uu = pde15s(f, 0:.05:2, u, 'dirichlet', opts);
+waterfall(uu), shg
+
+%% Advection-diffusion2
+close all
+E = 1e-1;
+d = [-3*pi/4, pi]; x = chebfun('x', d);
+u = sin(2*x);
+f = @(u, t, x) E*diff(u, 2)+diff(u);
+lbc = {'neumann', 0};
+rbc = {'dirichlet', 1};
+bc = struct; bc.left = lbc; bc.right = rbc;
+opts = pdeset('HoldPlot', 'on');
+tt = linspace(0, 5, 21);
+uu = pde15s(f, tt, u, bc, opts);
+
+%% Advection-diffusion3 (Time depended rhs bc)
+close all
+E = 1e-1;
+d = [-3*pi/4, pi]; x = chebfun('x', d);
+u = sin(2*x);
+f = @(u, t, x) E*diff(u, 2)+diff(u);
+lbc = 'neumann';
+rbc = @(u, t, x) u - .1*sin(t);
+bc = struct; bc.left = lbc; bc.right = rbc;
+opts = pdeset('holdPlot', 'on');
+tt = linspace(0, 3, 101);
+uu = pde15s(f, tt, u, bc, opts);
+
+%% Allen-Cahn
+close all
+d = [-1, 1]; x = chebfun('x', d);
+u = (1-x.^2).^2.*(1+sin(12*x))/2;
+f = @(u) u.*(1-u) + 5e-4*diff(u, 2);
+pde15s(f, 0:0.1:5, u, 'neumann');
+
+%% Allen-Cahn 
+close all
+d = [-1, 1]; x = chebfun('x', d);
+u = .53*x-.47*sin(1.5*pi*x);
+f = @(u) u.*(1-u.^2) + 5e-4*diff(u, 2);
+bc.left = struct('op', 'dirichlet', 'val', -1);
+bc.right = struct('op', 'dirichlet', 'val', 1);
+opts = pdeset('Ylim', [-1.1 1.1]);
+pde15s(f, 0:0.1:5, u, bc, opts);
+
+%% Burgers
+close all
+d = [-1, 1]; x = chebfun('x', d);
+u = (1-x.^2).*exp(-30*(x+.5).^2);
+f = @(u) -diff(u.^2)+.01*diff(u, 2);
+pde15s(f, 0:.05:3, u, 'dirichlet');
+
+%% KS
+close all
+d = [-1, 1]; x = chebfun('x', d);
+u = 1 + 0.5*exp(-40*x.^2);
+bc = struct;
+bc.left = @(u) [u-1 ; diff(u)];
+bc.right = @(u) [u-1 ; diff(u)];
+f = @(u) u.*diff(u)-diff(u, 2)-0.006*diff(u, 4);
+u = pde15s(f, 0:.01:.5, u, bc);
+
+%% Cahn-Hilliard - not working!
+close all
+E = 1e-1;
+d = [-1, 1]; x = chebfun('x', d);
+u = cos(pi*x)-exp(-6*pi*x.^2);
+plot(u)
+opts = pdeset('eps', 1e-4, 'abstol', 1e-4, 'reltol', 1e-4, 'plot', 1);
+bc = struct;
+bc.left = @(u) [u+1 ; diff(u)];
+bc.right = @(u) [u+1 ; diff(u)];
+f = @(u, t, x) -diff(u, 4) + diff(u.^3, 2)-diff(u, 2);
+tt = linspace(0, .001, 101);
+uu = pde15s(f, tt, u, bc, opts);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% A selection of PDE system examples
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%
+
+close all
+d = [-1, 1]; x = chebfun('x', d);
+u = [ chebfun(1, d)  chebfun(1, d) ];
+f = @(u, v, t, x) [ -u + (x + 1).*v + 0.1*diff(u, 2) , ...
+                     u - (x + 1).*v + 0.2*diff(v, 2) ];
+bc.left = @(u, v) [diff(u), diff(v)];  bc.right = bc.left;   % New way
+opts = pdeset('plot', 1, 'N', 32);
+uu = pde15s(f, 0:.05:3, u, bc, opts);
+
+
+%%
+
+close all
+d = [-1, 1]; x = chebfun('x', d);
+u = [ 1-erf(10*(x+0.7)) , 1 + erf(10*(x-0.7)) , chebfun(0, d) ];
+f = @(u, v, w)      [ 0.1*diff(u, 2) - 100*u.*v , ...
+                      0.2*diff(v, 2) - 100*u.*v , ...
+                     .001*diff(w, 2) + 2*100*u.*v ];                  
+bc = 'neumann';     
+uu = pde15s(f, 0:.1:3, u, bc);
+
+%%
+
+close all
+% Crazy nonlinear boundary conditions
+d = [-1, 1]; x = chebfun('x', d);
+u = [ chebfun(1, d)  chebfun(1, d) ];
+f = @(u, v, t, x) [ -u + (x + 1).*v + 0.1*diff(u, 2) , ...
+                     u - (x + 1).*v + 0.2*diff(v, 2) ];
+bc = struct;
+bc.left =  @(u, v, t, x) [diff(u)+t*sin(u)./v,  diff(v)];
+bc.right = @(u, v, t, x) [diff(u),              diff(v).*v+sin(5*pi*t)];
+uu = pde15s(f, 0:.05:1, u, bc);
+
+%%
+
+close all
+% Maxwell's Equations
+d = [-1, 1]; x = chebfun('x', d);
+u = exp(-20*x.^2) .* sin(14*x);  u = [u -u];
+f = @(u, v) [diff(v) ; diff(u)];
+bc.left = @(u, v) u; bc.right = @(u, v) v;        % New way
+opt = pdeset('eps', 1e-6, 'Ylim', pi/2*[-1 1], 'AbsTol', 1e-6, 'RelTol', 1e-6);
+uu = pde15s(f, 0:.05:2, u, bc, opt, 64);
+
+%% 
+%chebop-style synatx
+
+close all
+x = chebfun('x');
+u = 1 + 0.5*exp(-40*x.^2);
+bcc = @(u, x) [u(-1)-1 ; feval(diff(u),-1) ; u(1)-1 ; feval(diff(u),1)];
+f = @(u) u.*diff(u) - diff(u, 2) - 0.006*diff(u, 4);
+opts = pdeset('Ylim', [-30 30], 'PlotStyle', {'LineWidth', 2});
+uu = pde15s(f, 0:.025:.5, u, bcc, opts);
+surf(uu, 0:.025:.5)
+
+
