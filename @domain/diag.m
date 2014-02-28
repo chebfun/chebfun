bd2c612c5097fb71259730861d23e51fe9bf2c14
Toby Driscoll <driscoll@udel.edu>
2014-02-28 14:24:05 -0500

    Introduced domain class linop constructors for backward compatibility.

---
+++ @domain/diag.m
@@ -0,0 +1,10 @@
+function F = diag(f,d)
+%DIAG      Pointwise multiplication operator.
+%   This function is deprecated. Use OPERATORBLOCK.MULT.
+
+% Copyright 2013 by The University of Oxford and The Chebfun Developers.
+% See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
+
+F = linop( operatorBlock.diag(f) );
+
+end
