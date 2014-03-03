4ade89c60c0b7b3d0d880fb6dfb818d545c3d53a
hrothgar <hrothgarrrr@gmail.com>
2014-03-03 18:19:38 +0000

    Adding cases to test_oldschool.m.

--- tests/linop/test_oldschool.m
+++ tests/linop/test_oldschool.m
@@ -6,16 +6,17 @@ function pass = test_oldschool
 d = domain(-1,1);
 
 %% Just test whether these will execute.
-D = diff(d);
-I = eye(d);
-F = diag( chebfun('x',d([1 end])) ,d );
-C = cumsum(d);
-s = sum(d);
-E = feval(d,0,'left');
-Z = zeros(d);
+pass(1) = doesNotCrash( @() diff(d) );
+pass(2) = doesNotCrash( @() eye(d) );
+pass(3) = doesNotCrash( @() diag(chebfun('x',d([1 end])), d) );
+pass(4) = doesNotCrash( @() cumsum(d) );
+pass(5) = doesNotCrash( @() sum(d) );
+pass(6) = doesNotCrash( @() feval(d, 0, 'left') );
+pass(7) = doesNotCrash( @() zeros(d) );
 
 %% Test the feval-style instantiation syntax.
-pass(1) = norm( D(8) - matrix(D,8) ) < 2e-14
+D = diff(d);
+pass(8) = norm( D(8) - matrix(D,8) ) < 2e-14;
 
 %% Test boundary condition syntax
 A = D^2;
@@ -26,6 +27,15 @@ A = addConstraint(A,e(d(end))*D,0);
 A1 = matrix(A,10);   % first two rows hold BCs
 correct = A0; 
 correct([1 end],:) = A1(1:2,:);  % classic row replacement
-pass(2) = norm( correct - feval(A,10,'oldschool') ) < 2e-14
+pass(9) = norm( correct - feval(A,10,'oldschool') ) < 2e-14;
 
+end
+
+function pass = doesNotCrash(fn)
+try
+    fn();
+    pass = true;
+catch ME
+    pass = false;
+end
 end
