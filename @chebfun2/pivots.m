dd38bfafa1e9f1db60ea7dfe50fcfed90695022c
hrothgar <hrothgarrrr@gmail.com>
2014-03-06 15:21:31 +0000

    Resolving conflict in pivots.m

--- @chebfun2/pivots.m
+++ @chebfun2/pivots.m
@@ -1,32 +1,31 @@
-function p = pivots(f, varargin)
-%PIVOTS  pivot values of a chebfun2
-% 
-% PIVOTS(F) returns the pivot values taken during in the constructor by the 
-%  GE algorithm. 
-% 
-% PIVOTS(F, 'normalize'), returns the normalised pivot values.  These
-% numbers are scaled so that the columns and rows have unit 2-norm.
+function p = pivots(f, str)
+%PIVOTS   Pivot values of a CHEBFUN2.
+%   PIVOTS(F) returns the pivot values taken during in the constructor by the GE
+%   algorithm.
+%
+%   PIVOTS(F, 'normalize'), returns the normalised pivot values. These numbers
+%   are scaled so that the columns and rows have unit 2-norm.
 
 % Copyright 2013 by The University of Oxford and The Chebfun Developers.
 % See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
 
-if ( nargin < 2 )
+% Get the pivot values:
+p = f.pivotValues;  
+
+if ( nargin > 1 )
     
-    p = f.pivotValues;  % Return the pivot values. 
-    p = p(:);            % make column vector
-    
-elseif ( strcmpi(varargin{1},'normalise') ... 
-                               || strcmpi(varargin{1},'normalize') )
-                           
-    cscl = norm( f.cols );   
-    rscl = norm( f.rows );
-    p = f.pivotValues; 
-    p = p.*cscl.*rscl; % normalized pivots. 
-    p = p(:);   % make column vector.
-    
-else
-    
-    error('CHEBFUN2:PIVOTS:InPuts','Unrecognised second argument');
+    if ( strcmpi(str, 'normalise') || strcmpi(str,'normalize') )
+        cscl = norm( f.cols );
+        rscl = norm( f.rows );
+        % Normalized pivots:
+        p = p.*cscl.*rscl;  
+    else
+        error('CHEBFUN2:PIVOTS:Inputs', 'Unrecognised second argument.');
+    end
     
 end
+
+% Make column vector:
+p = p(:);               
+
 end
