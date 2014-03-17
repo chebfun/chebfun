92eeed5074269662e1e629bb577dbfdb5f695338
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2014-03-17 16:15:59 +0000

    Tidying up ADCHEBFUN tests. In particular, unifying the testing mechanisms for most unary operators into a single static method, making the code easier to maintain.

--- tests/adchebfun/test_erf.m
+++ tests/adchebfun/test_erf.m
@@ -2,33 +2,10 @@
 
 function pass = test_erf
 
-% List of trigonometric functions to test.
+% List of functions related to the error function to test.
 funcList = {@erf, @erfc, @erfcinv, @erfcx, @erfinv};
 
-% Tolerance for Taylor testing
-tol = 1e-2;
-
-% Initialise vector with pass information
-pass = zeros(3, numel(funcList));
-
-% Do the tests.
-for k = 1:numel(funcList)   
-    
-    % Call the valueTesting method, which also returns linearity information
-    [err, lin] = adchebfun.valueTesting(funcList{k});
-    
-    % First, check that the computed function values match what we expect
-    pass(1, k) = ( err == 0 );
-    
-    % Call the taylorTesting method
-    [order1, order2] = adchebfun.taylorTesting(funcList{k});
-    % We expect all elements of ORDER1 to be close to 1, and of ORDER2 to be
-    % close to 2.
-    pass(2, k) = ( max(abs(order1 - 1)) < tol ) & ...
-        ( max(abs(order2 - 2)) < tol );
-    
-    % Check that we received the correct linearity information
-    pass(3, k) = ( lin == 0 );
-end
+% Call the ADCHEBFUN testUnary() method to do the tests.
+pass = adchebfun.testUnary(funcList);
 
 end
--- tests/adchebfun/test_trig4.m
+++ tests/adchebfun/test_trig4.m
@@ -6,30 +6,7 @@ function pass = test_trig4
 funcList = {@csch, @sec, @secd, @sech, @sin, @sinc, @sind, @sinh, @tan, ...
     @tand, @tanh};
 
-% Tolerance for Taylor testing
-tol = 1e-2;
-
-% Initialise vector with pass information
-pass = zeros(3, numel(funcList));
-
-% Do the tests.
-for k = 1:numel(funcList)   
-    
-    % Call the valueTesting method, which also returns linearity information
-    [err, lin] = adchebfun.valueTesting(funcList{k});
-    
-    % First, check that the computed function values match what we expect
-    pass(1, k) = ( err == 0 );
-    
-    % Call the taylorTesting method
-    [order1, order2] = adchebfun.taylorTesting(funcList{k});
-    % We expect all elements of ORDER1 to be close to 1, and of ORDER2 to be
-    % close to 2.
-    pass(2, k) = ( max(abs(order1 - 1)) < tol ) & ...
-        ( max(abs(order2 - 2)) < tol );
-    
-    % Check that we received the correct linearity information
-    pass(3, k) = ( lin == 0 );
-end
+% Call the ADCHEBFUN testUnary() method to do the tests.
+pass = adchebfun.testUnary(funcList);
 
 end
