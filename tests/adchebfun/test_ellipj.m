d8411778084d5cb6ba41440025fe64ab6a7d2f5c
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2014-03-24 13:55:27 +0000

    Linearity testing for ADCHEBFUN/ellipj. This concludes linearity testing for ADCHEBFUN.

--- tests/adchebfun/test_ellipj.m
+++ tests/adchebfun/test_ellipj.m
@@ -21,15 +21,21 @@ pass = zeros(2, numel(ellipFunctions));
 
 % Do the tests.
 for k = 1:numel(ellipFunctions)
+    % Call the valueTesting method, which also returns linearity information
+    [err, lin] = adchebfun.valueTesting(ellipFunctions{k});
+
     % First, check that the computed function values match what we expect
-    pass(1, k) = ( adchebfun.valueTesting(ellipFunctions{k}, 3) == 0 );
+    pass(1, k) = ( err == 0 );
     
     % Call the taylorTesting method
     [order1, order2] = adchebfun.taylorTesting(ellipFunctions{k}, numSteps, 3);
     % We expect all elements of ORDER1 to be close to 1, and of ORDER2 to be
     % close to 2.
     pass(2, k) = ( max(abs(order1 - 1)) < tol ) & ...
-        ( max(abs(order2 - 2)) < tol );    
+        ( max(abs(order2 - 2)) < tol );
+    
+    % Check that we received the correct linearity information
+    pass(3, k) = ( lin == 0 );
 end
 
 end
