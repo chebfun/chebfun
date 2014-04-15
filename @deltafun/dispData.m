7ae363a3d2514a79e70ad948669e948fe3cfbc61
Anthony P. Austin <austin@maths.ox.ac.uk>
2014-04-15 17:29:34 +0100

    Call funPart.dispData() in deltafun.dispData().
    This fixes a bug wherein calling disp() on a chebfun with a deltafun based on a
    singfun would not print the singfun exponent information.  deltafun.dispData()
    now retrieves all display data from the lower layers, as it needs to.

--- @deltafun/dispData.m
+++ @deltafun/dispData.m
@@ -1,18 +1,18 @@
 function data = dispData(f)
 %DISPDATA   Useful DELTAFUN information for DISPLAY at higher levels.
 %   DATA = DISPDATA(F) extracts useful information from the given DELTAFUN F and
-%   the information DATA will be used by DISPLAY at higher levels. Currently,
-%   the only information it extracts is exponents.
+%   the information DATA will be used by DISPLAY at higher levels.
 
 % Copyright 2014 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
 
+data = dispData(f.funPart);
+
 if ( anyDelta(f) )    
-    data{1}.name = 'deltas';
-    data{1}.data.deltaMag = f.deltaMag;
-    data{1}.data.deltaLoc = f.deltaLoc;
-else
-    data{1} = [];
+    deltaData.name = 'deltas';
+    deltaData.data.deltaMag = f.deltaMag;
+    deltaData.data.deltaLoc = f.deltaLoc;
+    data = [data {deltaData}];
 end
 
 end
