427b106995f70458b23d8ec1ed33373742e3ca90
Townsend <townsend@maths.ox.ac.uk>
2014-04-20 13:16:43 +0100

    Adding getvscl.m

---
+++ @chebtech/getvscl.m
@@ -0,0 +1,13 @@
+function vscale = getvscl( f )
+% GETVSCL(F) returns an estimate of the vertical scale of a function. 
+% 
+% VSCALE = GETVSCL(F) estimates the vertical scale (also known as the dynamical
+% range) of a function. This is required because a chebtech does not store its
+% interpolation data. If F is an array-valued chebtech with K columns, then 
+% VSCALE is a row vector of length K. 
+
+% compute values
+values = f.coeffs2vals( f.coeffs ); 
+vscale = max(abs(values), [], 1);
+
+end
