d09b627bbf808cd19c82e3c2016f21f56100b329
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2014-04-26 14:56:10 +0100

    Removing unecessary changes to cheboppref and initial guesses.

--- tests/chebop/test_scalarODE_breakpoints.m
+++ tests/chebop/test_scalarODE_breakpoints.m
@@ -1,29 +1,32 @@
-function [pass, u1, u2, info1, info2] = test_scalarODE_breakpoints
+function [pass, u1, u2, info1, info2] = test_scalarODE_breakpoints(pref)
+
+if ( nargin == 0 )
+    pref = cheboppref;
+end
 
 % Setup
 dom = [0 1 2 pi];
-p = cheboppref;
-p.plotting = 'off';
-p.display = 'off';
-p.damped = 1;
+pref.plotting = 'on';
+pref.display = 'iter';
+pref.damped = 1;
 
 N = chebop(@(x,u) diff(u,2) + sin(u-.2), dom);
-N.lbc = @(u) u-2; N.rbc = @(u) u - 3;
-N.init = chebfun(@(x) x/pi + 2, dom);
+N.lbc = @(u) u-2;
+N.rbc = @(u) u - 3;
 rhs = 0;
 
 %% Try different discretizations
 % Start with collocation -- no further action required
-[u1, info1] = solvebvp(N, rhs, p);
+[u1, info1] = solvebvp(N, rhs, pref);
 
 %% Change to ultraS
-p.discretization = @ultraS;
-[u2, info2] = solvebvp(N, rhs, p);
+pref.discretization = @ultraS;
+[u2, info2] = solvebvp(N, rhs, pref);
 
 %% Did we pass? 
 % To pass, both residuals have to be small, but we should not expect u1 and u2
 % to be identical!
-tol = p.errTol;
+tol = pref.errTol;
 pass(1) = normest(N(u1)) < tol;
 pass(2) = normest(N(u2)) < tol;
 pass(3) = ( norm(u1-u2) ~= 0 );
--- tests/chebop/test_scalarODE_sign.m
+++ tests/chebop/test_scalarODE_sign.m
@@ -1,11 +1,10 @@
-function [pass, u1, u2, info1, info2] = test_scalarODE_sign
+function [pass, u1, u2, info1, info2] = test_scalarODE_sign(pref)
 
 % Setup
+if ( nargin == 0 )
+    pref = cheboppref;
+end
 dom = [-1 .5 1];
-p = cheboppref;
-p.plotting = 'off';
-p.display = 'off';
-p.damped = 1;
 
 N = chebop(@(x,u) diff(u,2) + sign(x).*sin(u), dom);
 N.lbc = @(u) u - 2;
@@ -14,16 +13,16 @@ rhs = 0;
 
 %% Try different discretizations
 % Start with collocation -- no further action required
-[u1, info1] = solvebvp(N, rhs, p);
+[u1, info1] = solvebvp(N, rhs, pref);
 
 %% Change to ultraS
-p.discretization = @ultraS;
-[u2, info2] = solvebvp(N, rhs, p);
+pref.discretization = @ultraS;
+[u2, info2] = solvebvp(N, rhs, pref);
 
 %% Did we pass? 
 % To pass, both residuals have to be small, but we should not expect u1 and u2
 % to be identical!
-tol = p.errTol;
+tol = pref.errTol;
 pass(1) = norm(N(u1)) < tol;
 pass(2) = norm(N(u2)) < tol;
 pass(3) = ( norm(u1 - u2) ~= 0 );
