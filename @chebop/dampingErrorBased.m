81c47774eaa59dd91b7c620fbc92d8aa46b065e6
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2014-04-27 13:24:43 +0100

    Massive overhaul of CHEBOP nonlinear solver, moving damping into a separate method, makes the code much more tractable.

---
+++ @chebop/dampingErrorBased.m
@@ -0,0 +1,151 @@
+function [u, dampingInfo] = dampingErrorBased(N, u, rhs, delta, L, disc, dampingInfo)
+%DAMPINGERRORBASED      Finds the step-size for damped Newton method.
+%
+% This algorithm finds the step-size lambda used in the damped Newton iteration.
+% It is affine invariant, and error controlled, that is, it seeks to minimize
+% the error in the SOLUTION SPACE, not the RESIDUAL SPACE.
+%
+% For further details, see
+%   [1] P. Deuflhard. Newton Methods for Nonlinear Problems. Springer, 2004.
+%
+%   [2] A. Birkisson. Numerical Solution of Nonlinear Boundary Value Problems
+%         for Ordinary Differential Equations in the Continuous Framework.
+%         Dphil Thesis, Oxford, 2013.
+
+% Extract info from the dampingInfo struct
+errTol = dampingInfo.errTol;
+lambda = dampingInfo.lambda;
+lambdaMin = dampingInfo.lambdaMin;
+newtonCounter = dampingInfo.newtonCounter;
+normDelta = dampingInfo.normDelta;
+normDeltaBar = dampingInfo.normDeltaBar;
+normDeltaOld = dampingInfo.normDeltaOld;
+deltaBar = dampingInfo.deltaBar;
+
+%TODO: should not really need x here, but just evaluate the chebop
+x = dampingInfo.x;
+
+% Monitors whether we want to accept the current steplength
+accept = 0;
+
+% Indicates whether we are in prediction or correction mode (i. e. whether we
+% are finding the first value of lambda at a given Newton step, or whether we
+% are correcting the value initially predicted for that step).
+initPrediction = 1;
+
+% Iterate until we find a step-size lambda that we accept
+while ( ~accept )
+    
+    % Check whether we want to predict a value for lambda. Can only do so once
+    % we have taken one Newton step, as it is based on information obtained from
+    % the previous step
+    if newtonCounter > 0 && initPrediction
+        % Compute a prediction value
+        mu = (normDeltaOld*normDeltaBar)/...
+            (mynorm(deltaBar-delta, 'fro')*normDelta)*lambda;
+        lambda = min(1,mu);
+        % Indicate that we will now be in correction mode until next
+        % Newton step.
+        initPrediction = 0;
+    end
+    
+    if lambda < lambdaMin
+        disp('Convergence failure')
+        % Take full Newton step
+        u = u + delta;
+        accept = 1;
+        normDeltaOld = normDelta;
+        initPrediction = 1;
+        lambda = 1;
+        continue
+    end
+    
+    % Take a trial step
+    uTrial = u + lambda*delta;
+    
+    uTrialb = uTrial.blocks;
+    
+    % TODO: This is a hack, we shouldn't need this!
+    NopTrial = N.op(x, uTrialb{:});
+    
+    if (isa(NopTrial, 'chebfun') && size(NopTrial, 2) > 1)
+        NopTemp = {};
+        for NopCounter = 1:size(NopTrial, 2)
+            NopTemp{NopCounter} = NopTrial(:, NopCounter);
+        end
+        NopTrial = chebmatrix(NopTemp');
+    end
+    
+    deResFunTrial = NopTrial - rhs;
+    
+    % Compute a simplified Newton step, using the current derivative of
+    % the operator, but with a new right-hand side.
+    [deltaBar, disc] = linsolve(L, deResFunTrial, disc);
+    
+    % TODO: Why are we doing this twice?
+    
+    % We had two output arguments above, need to negate deltaBar
+    %             deltaBar = -deltaBar;
+    deltaBar = -(L\deResFunTrial);    % Old fashion, to be removed
+    % TODO: We also need to update the values of the RHS for the BCs
+    % here!
+    
+    
+    
+    % The norm of the simplified Newton step is used to compute a
+    % contraction factor
+    normDeltaBar = mynorm(deltaBar);
+    
+    % Contraction factor
+    cFactor = normDeltaBar/normDelta;
+    
+    muPrime = (.5*normDelta*lambda^2)/...
+        (mynorm(deltaBar-(1-lambda)*delta,'fro'));
+    
+    if cFactor >=1
+        lambda = min(muPrime,.5*lambda);
+        continue;
+    end
+    
+    lambdaPrime = min(1,muPrime);
+    
+    if lambdaPrime == 1 && normDeltaBar < errTol
+        u = uTrial + deltaBar;
+        newtonCounter
+        terminate = 1;
+        break
+    end
+    
+    % Switch to pure Newton if we are experiencing good convergence
+    if lambdaPrime == 1 && cFactor < .5
+        dampingInfo.damped = 0;
+    end
+    
+    
+    if lambdaPrime >= 4*lambda
+        lambda = lambdaPrime;
+        continue;
+    end
+    
+    % If we get all the way here, accept iterate
+    accept = 1;
+    
+end
+
+% Return uTrial, and update the dampingInfo structur
+u = uTrial;
+dampingInfo.lambda = lambda;
+dampingInfo.cFactor = cFactor;
+dampingInfo.normDeltaBar = normDeltaBar;
+dampingInfo.deltaBar = deltaBar;
+
+
+end
+
+function out = mynorm(f, type)
+if ( isa(f, 'chebmatrix') )
+    out = max(cellfun(@(u) get(u, 'vscale'), f.blocks));
+else
+    out = get(f, 'vscale');
+end
+end
--- @chebop/solvebvpNonlinear.m
+++ @chebop/solvebvpNonlinear.m
@@ -8,30 +8,24 @@ function [u, info] = solvebvpNonlinear(N, rhs, L, u0, res, pref, displayInfo)
 %   res:    Residual of ODE at initial guess
 %   pref:   Cheboppref preference structure
 %
-% References: TODO: Fill out info for Deuflhard, DPhil thesis, and other
-% references as appropriate
 
-% TODO: Replace with real values further down in code (temp values to test
-% display methods)
-errEst = NaN;
-errEstDE = NaN;
-errEstBC = .5678;
 % Store preferences used in the Newton iteration in separate variables
 maxIter  = pref.maxIter;
 errTol   = pref.errTol;
-% Did the user request damped or undamped Newton iteration?
-damped = pref.damped;
+
+% Did the user request damped or undamped Newton iteration? Start in mode
+% requested (later on, the code can switch between modes).
+prefDamped = pref.damped;
+damped = prefDamped;
+
 % Minimum allowed steplength
 lambdaMin = pref.lambdaMin;
 
 % Assign initial guess to u
 u = u0;
 
-% Store the domain we're working with.
-dom = N.domain;
-
-% Initialise the dependent variable:
-x = chebfun(@(x) x, dom);
+% Initialise the independent variable:
+x = chebfun(@(x) x, N.domain);
 
 % Print info to command window, and/or show plot of progress
 [displayFig, displayTimer] = displayInfo('init', u0, pref);
@@ -44,130 +38,57 @@ newtonCounter = 0;
 terminate = 0;
 
 % Store a vector with information about the norm of the Newton updates
-normDeltaVec = zeros(maxIter,1);
+normDeltaVec = zeros(maxIter, 1);
 
 % Initial damping parameter
 lambda = 1;
 
 % Need to subtract the rhs from the residual passed in
 res = res - rhs;
+
+% Some initializations
+dampingInfo.errTol = errTol;
+dampingInfo.normDeltaOld = [];
+dampingInfo.normDeltaBar = [];
+dampingInfo.lambda = lambda;
+dampingInfo.lambdaMin = lambdaMin;
+dampingInfo.newtonCounter = newtonCounter;
+dampingInfo.deltaBar = [];
+dampingInfo.damped = damped;
+dampingInfo.x = x;
+
+% Start the Newton iteration!
 while ( ~terminate )
+    
     % Compute a Newton update
     [delta, disc] = linsolve(L, res, pref);
-    % We had two output arguments above, need to negate delta
+    
+    % We had two output arguments above, need to negate delta.
     delta = -delta;
 
     % Store the norm of the update
     normDelta = mynorm(delta);
     
+    dampingInfo.normDelta = normDelta;
+    
     % Are we in damped mode?
     if ( damped )
-        % Find a step-length lambda using an affine covariant algorithm due to
-        % Deuflhard [!!!reference], also described in [!!!thesis]
         
-        % Monitors whether we want to accept the current steplength
-        accept = 0;
+        % Find the next Newton iterate (the method finds the step-size, then
+        % takes the damped Newton and returns the next iterate)
+        [u, dampingInfo] = dampingErrorBased(N, u, rhs, delta, ...
+            L, disc, dampingInfo);
         
-        % Indicates whether we are in prediction or correction mode (i. e.
-        % whether we are finding the first value of lambda at a given Newton
-        % step, or whether we are correcting the value initially predicted for
-        % that step).
-        initPrediction = 1;
-        while ( ~accept )       % Iterate until lambda is accepted
-            % Check whether we want to predict a value for lambda. Can only do
-            % so once we have taken one Newton step, as it is based on
-            % information obtained from the previous step
-            if newtonCounter > 0 && initPrediction
-                % Compute a prediction value
-                mu = (normDeltaOld*nrmDeltaBar)/(mynorm(deltaBar-delta,'fro')*normDelta)*lambda;
-                lambda = min(1,mu);
-                % Indicate that we will now be in correction mode until next
-                % Newton step.
-                initPrediction = 0;
-            end
-            
-            if lambda < lambdaMin
-                disp('Convergence failure')
-                % Take full Newton step
-                u = u + delta;
-                accept = 1;
-                normDeltaOld = normDelta;
-                initPrediction = 1;
-                lambda = 1;
-                continue
-            end
-            
-            % Take a trial step
-            uTrial = u + lambda*delta;
-            
-            uTrialb = uTrial.blocks;
-            
-            % TODO: This is a hack, we shouldn't need this!
-            NopTrial = N.op(x, uTrialb{:});
-            
-            if (isa(NopTrial, 'chebfun') && size(NopTrial, 2) > 1)
-                NopTemp = {};
-                for NopCounter = 1:size(NopTrial, 2)
-                    NopTemp{NopCounter} = NopTrial(:, NopCounter);
-                end
-                NopTrial = chebmatrix(NopTemp');
-            end
-            
-            deResFunTrial = NopTrial - rhs;
-            
-            % Compute a simplified Newton step, using the current derivative of
-            % the operator, but with a new right-hand side.
-            [deltaBar, disc] = linsolve(L, deResFunTrial, disc);
-            
-            % We had two output arguments above, need to negate deltaBar
-%             deltaBar = -deltaBar;
-            deltaBar = -(L\deResFunTrial);    % Old fashion, to be removed
-            % TODO: We also need to update the values of the RHS for the BCs
-            % here!
-            
-
-            
-            % The norm of the simplified Newton step is used to compute a
-            % contraction factor
-            nrmDeltaBar = mynorm(deltaBar);
-            
-            % Contraction factor
-            cFactor = nrmDeltaBar/normDelta;
-            
-            muPrime = (.5*normDelta*lambda^2)/...
-                (mynorm(deltaBar-(1-lambda)*delta,'fro'));
-            
-            if cFactor >=1
-                lambda = min(muPrime,.5*lambda);
-                continue;
-            end
-            
-            lambdaPrime = min(1,muPrime);
-            
-            if lambdaPrime == 1 && nrmDeltaBar < errTol
-                u = uTrial + deltaBar;
-                newtonCounter
-                terminate = 1;
-                break
-            end
-            
-            % Switch to pure Newton if we are experiencing good convergence
-            if lambdaPrime == 1 && cFactor < .5
-                damped = 0;
-            end
-            
-            
-            if lambdaPrime >= 4*lambda
-                lambda = lambdaPrime;
-                continue;
-            end
-            
-            % If we get all the way here, accept iterate
-            accept = 1;
-        end
-        u = uTrial;
-        
+        % If we're in damped mode, we don't get an error estimate
         errEst = NaN;
+        
+        % Extract info from the dampingInfo struct, used for printing
+        lambda = dampingInfo.lambda;
+        cFactor = dampingInfo.cFactor;
+        
+        % Do we want to keep Newton in damped mode?
+        damped = dampingInfo.damped;
+        
     else    % We are in undamped phase
         % Update lambda so that we will print correct information
         lambda = 1;
@@ -183,7 +104,9 @@ while ( ~terminate )
             cFactor = normDelta/normDeltaOld;
             
             if cFactor >= 1
-                damped = 1; % Have to resort back to damped
+                % Have to resort back to damped (but only if the user wanted
+                % damped Newton in the first place).
+                damped = prefDamped;
                 continue
             end
 %             contraFactor(newtonCounter-1) = 2*cFactor;
@@ -198,13 +121,16 @@ while ( ~terminate )
     
     % Evaluate the residual
     ub = u.blocks;
+    
     % Update counter of Newton steps taken
     newtonCounter = newtonCounter +1;
+    dampingInfo.newtonCounter = newtonCounter;
         
     % Store information about the norm of the updates
     normDeltaVec(newtonCounter) = normDelta;
     % Need to store the norm of the current update to use in damping strategy
     normDeltaOld = normDelta;
+    dampingInfo.normDeltaOld = normDeltaOld;
     
     % Print info to command window, and/or show plot of progress
     displayInfo('iter', u, delta, newtonCounter, normDelta, cFactor, ...
@@ -231,6 +157,10 @@ while ( ~terminate )
         L.prefs = pref;
     end
 end
+
+% TODO: Replace with real values further down in code (temp values to test
+% display methods)
+errEstBC = .5678;
 
 % Show final information
 displayInfo('final', u, delta, newtonCounter, errEstDE, errEstBC, displayFig, ...
