6c4e34241cc237dd7693d5ffd738f7c2365c3d67
Mohsin Javed <mhsnjvd@gmail.com>
2014-04-30 11:41:42 +0100

    v4 code copied to @deltafun/conv.m

--- @deltafun/conv.m
+++ @deltafun/conv.m
@@ -28,61 +28,60 @@ deltaLocG = g.deltaLoc;
 
 hFun = conv(f.funPart, g.funPart);
 
-   if(isfimps)
-        [m n] = size(fimps);
-        % loop through the imps matrix
-        for i = 1:m
-            for j = 1:n
-                if(abs(fimps(i,j)) > 100*eps)
-                    % take appropriate derivative and shift the function
-                    gshift = newdomain(diff(g,i-1),[g.ends(1)+f.ends(j) g.ends(end)+f.ends(j)]);
-                    % pad with zero chebfuns on either side
-                    l = chebfun( 0, [h.ends(1) gshift.ends(1) ] );
-                    r = chebfun( 0, [gshift.ends(end) h.ends(end) ] );
-                    gshift = chebfun( [ l; gshift; r ], [ h.ends(1) gshift.ends(1) gshift.ends(end) h.ends(end) ] );
-                    % scale by the impulse value and add
-                    h = h + fimps(i,j)*gshift;
-                end                     
+if( ~isempty(deltaLocF))
+    [m n] = size(deltaMagF);
+    % loop through the delta function matrix
+    for i = 1:m
+        for j = 1:n
+            % Take appropriate derivative and shift the function
+            gshift = newdomain(diff(g,i-1),[g.ends(1)+f.ends(j) g.ends(end)+f.ends(j)]);
+            % pad with zero chebfuns on either side
+            l = chebfun( 0, [h.ends(1) gshift.ends(1) ] );
+            r = chebfun( 0, [gshift.ends(end) h.ends(end) ] );
+            gshift = chebfun( [ l; gshift; r ], [ h.ends(1) gshift.ends(1) gshift.ends(end) h.ends(end) ] );
+            % scale by the impulse value and add
+            h = h + fimps(i,j)*gshift;
+            
+        end
+    end
+end
+
+% if g has delta funtions, do the same as above
+isgimps = any(any(abs(gimps)>100*eps));
+if(isgimps)
+    [m n] = size(gimps);
+    for i = 1:m
+        for j = 1:n
+            if(abs(gimps(i,j)) > 100*eps)
+                fshift = newdomain(diff(f,i-1),[f.ends(1)+g.ends(j) f.ends(end)+g.ends(j)]);
+                l = chebfun( 0, [h.ends(1) fshift.ends(1) ] );
+                r = chebfun( 0, [fshift.ends(end) h.ends(end) ] );
+                fshift = chebfun( [ l; fshift; r ], [ h.ends(1) fshift.ends(1) fshift.ends(end) h.ends(end) ] );
+                h = h + gimps(i,j)*fshift;
             end
         end
     end
-    
-    % if g has delta funtions, do the same as above 
-    isgimps = any(any(abs(gimps)>100*eps));
-    if(isgimps)
-        [m n] = size(gimps);
-        for i = 1:m
-           for j = 1:n
-               if(abs(gimps(i,j)) > 100*eps)
-                   fshift = newdomain(diff(f,i-1),[f.ends(1)+g.ends(j) f.ends(end)+g.ends(j)]);
-                   l = chebfun( 0, [h.ends(1) fshift.ends(1) ] );
-                   r = chebfun( 0, [fshift.ends(end) h.ends(end) ] );
-                   fshift = chebfun( [ l; fshift; r ], [ h.ends(1) fshift.ends(1) fshift.ends(end) h.ends(end) ] );
-                   h = h + gimps(i,j)*fshift;
-               end
-           end
-        end
-    end
-         
-    % if both f and g have delta functions
-    if(isfimps && isgimps)
-        [m n] = size(fimps);
-        [p q] = size(gimps);
-        himps = zeros(m+p-1,length(h.ends));
-        for i=1:m
-            for j=1:n
-                if(abs(fimps(i,j))>100*eps)
-                    % find the locations of shifted ends in h.ends
-                    [xx yy] = meshgrid(h.ends,f.ends(j)+g.ends);
-                    idx = find(sum(~(abs(xx-yy)>100*eps)));
-                    % place the scaled and shifted impulses in himps
-                    himps(i:i+p-1,idx) = himps(i:i+p-1,idx) + fimps(i,j)*gimps;
-                end
+end
+
+% if both f and g have delta functions
+if(isfimps && isgimps)
+    [m n] = size(fimps);
+    [p q] = size(gimps);
+    himps = zeros(m+p-1,length(h.ends));
+    for i=1:m
+        for j=1:n
+            if(abs(fimps(i,j))>100*eps)
+                % find the locations of shifted ends in h.ends
+                [xx yy] = meshgrid(h.ends,f.ends(j)+g.ends);
+                idx = find(sum(~(abs(xx-yy)>100*eps)));
+                % place the scaled and shifted impulses in himps
+                himps(i:i+p-1,idx) = himps(i:i+p-1,idx) + fimps(i,j)*gimps;
             end
         end
-        % append delta functions to the imps of h
-        h.imps = [ h.imps; himps ];    
     end
+    % append delta functions to the imps of h
+    h.imps = [ h.imps; himps ];
+end
 end
 
 
