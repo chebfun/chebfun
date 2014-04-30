93d4a25931d390d640ef90a7f0ffd897377f0966
Townsend <townsend@maths.ox.ac.uk>
2014-04-30 08:50:10 +0100

    Adding display data.

---
+++ @fourtech/dispData.m
@@ -0,0 +1,11 @@
+function data = dispData(f)
+%DISPDATA   Useful information for DISPLAY at higher levels.
+%   DATA = DISPDATA(F) extracts useful information from the given FOURTECH F and
+%   the information DATA will be used by DISPLAY at higher levels.
+
+% Copyright 2014 by The University of Oxford and The Chebfun Developers.
+% See http://www.chebfun.org/ for Chebfun information.
+
+data = [];
+
+end
