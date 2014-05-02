4ebc7e1c7f727804d8bc9e92b94a9c0e4f4bb360
Mohsin Javed <mhsnjvd@gmail.com>
2014-05-02 23:10:43 +0100

    adding test for deltafun/conf.m

---
+++ tests/deltafun/test_conv.m
@@ -0,0 +1,29 @@
+% Test file for @deltafun/conv.m
+
+function pass = test_conv(pref)
+
+if (nargin < 1)
+    pref = chebpref();
+end
+
+%%
+tol = pref.deltaPrefs.deltaTol;
+
+d = deltafun();
+f = fun.constructor(@(x) x);
+d1 = deltafun(f, [], []);
+d2 = deltafun(bndfun(0), 1, 0);
+
+pass(1) = isempty(conv(d, d)) && isempty(conv(d, d1)) && isempty(conv(d1, d));
+
+%%
+g = conv(d1, d2);
+g = g{1};
+pass(2) = max(abs(minandmax(g-f))) < tol;
+
+%%
+g = conv(d1, diff(d2));
+g = g{1};
+pass(3) = max(abs(minandmax(g-1))) < tol;
+
+%%
