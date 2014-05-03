87d6669c22e616f472c42a4301a5e66ffa97092d
Mohsin Javed <mhsnjvd@gmail.com>
2014-05-04 00:38:26 +0100

    redefining min and max for deltafunctions. These are practical definitions.

--- @deltafun/minandmax.m
+++ @deltafun/minandmax.m
@@ -14,13 +14,40 @@ function [vals, pos] = minandmax(f)
 if ( ~anyDelta(f) )
     
     % The function has no delta functions:
-    [vals, pos] = minandmax(f.smoothPart);
+    [vals, pos] = minandmax(f.funPart);
     
 else
-    
-    % If there are delta functions, maxima and minima are undefined:
-    vals = [NaN; NaN];
-    pos  = [NaN; NaN];
+    % If there are delta functions, maxima and minima are inf or -inf:
+    f = simplifyDeltas(f);
+    deltaFuns = f.deltaMag(1, :);
+    deltaLoc = f.deltaLoc;
+    if ( isempty(deltaFuns) )
+        % This means higher order deltas, return NaNs
+        vals = [NaN; NaN];
+        % Return the location of the first occurance of a higher order delta:
+        pos  = [deltaLoc(1); deltaLoc(1)];
+    else
+        % This means at least one delta function so maxima and minima are inf 
+        % and -inf regardless of the sign of the delta function.
+        vals = [-inf, inf];        
+        posIdx = deltaFuns > 0;
+        negIdx = deltaFuns < 0;
+        
+        if ( isempty(negIdx) )
+            locMin = deltaLoc(1);
+        else
+            locMin = deltaLoc(negIdx);
+            locMin = locMin(1);
+        end
+        
+        if ( isempty(posIdx) )
+            locMax = deltaLoc(1);
+        else
+            locMax = deltaLoc(posIdx);
+            locMax = locMax(1);
+        end
+        pos  = [locMin; locMax];
+    end
 end
   
 end
