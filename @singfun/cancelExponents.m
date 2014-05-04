10e4ee5d739c4ed05020d4b09c84c4eb4328e2b6
Nick Hale <nick.p.hale@gmail.com>
2014-05-04 10:21:34 +0200

    Minor tidying.

--- @singfun/cancelExponents.m
+++ @singfun/cancelExponents.m
@@ -1,11 +1,11 @@
 function f = cancelExponents(f)
 %CANCELEXPONENTS  Cancel the exponents of a SINGFUN.
-%   F = SIMPLIFYEXPONENTS(F) returns a SINGFUN whose smoothPart has no vanishing 
-%   value at the boundaries if the corresponding exponents are negative. 
-%   Boundary roots are tried to be extracted off from the smoothPart at the 
-%   corresponding boundary to cancel the partial or entire exponents. 
+%   F = CANCELEXPONENTS(F) returns a SINGFUN whose smoothPart has no vanishing
+%   value at the boundaries if the corresponding exponents are negative.
+%   Boundary roots are extracted off from the smoothPart at the corresponding
+%   boundary to cancel the negative exponents as far as possible.
 %
-%  See also EXTRACTBOUNDARYROOTS.
+% See also EXTRACTBOUNDARYROOTS.
 
 % Copyright 2014 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
@@ -14,10 +14,10 @@ function f = cancelExponents(f)
 exps = get(f, 'exponents');
 
 % Grab boundary values:
-boundaryVals = [get(f.smoothPart,'lval') get(f.smoothPart,'rval')];
+boundaryVals = [get(f.smoothPart,'lval'),  get(f.smoothPart,'rval')];
 
 % Tolerance:
-tol = 100*get(f.smoothPart, 'vscale')*get(f.smoothPart, 'epslevel');
+tol = 100*get(f.smoothPart, 'vscale').*get(f.smoothPart, 'epslevel');
 
 % Boundaries with negative exponent and vanishing value:
 ind  = ( exps < 0 ) & ( abs(boundaryVals) < repmat(tol, 1, 2) );
