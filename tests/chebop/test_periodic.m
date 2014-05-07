f51c69742b6c087eba6dd1d827e41cc3a690d442
Hrothgar <hrothgar@maths.ox.ac.uk>
2014-05-07 12:04:34 +0100

    Sorting eigenvalues in chebop/test_periodic for consistency across machines (again).

--- tests/chebop/test_periodic.m
+++ tests/chebop/test_periodic.m
@@ -46,9 +46,11 @@ B.op = @(x, u, v) [v + u ; diff(v)];
 [V, D] = eigs(A, B, 5, 0, pref);
 e = diag(D);
 
-% Sort the eigenvalues to ensure things will work on all machines
-[ignore, idx] = sort(real(e));
-e = e(idx);
+% Sort the eigenvalues to ensure things will work on all machines.
+% `sort(x)` sorts complex values by abs() and then by angle(). In order have
+% consistent sorting, we move all the eigenvalues up into the first quadrant
+% before sorting them.
+sort(e+(10+10i)) - (10+10i);
 
 err(3) = norm(real(e) - [0 0 1 1 1].', inf) + ...
     norm(imag(e) - [-1 1 0 -1 1].', inf);
@@ -59,4 +61,3 @@ err(4) = norm(V{1}(pi) - V{1}(pi), inf) + norm(V{2}(pi) - V{2}(pi), inf);
 pass = err < tol;
 
 end
-
