176f419056aeb4eb88ec835577df8f363fb124ec
Nick Hale <nick.p.hale@gmail.com>
2014-05-17 18:28:17 +0200

    Updating CHEBPOLYVALM() for V5 and adding a test.

--- chebpolyvalm.m
+++ chebpolyvalm.m
@@ -1,37 +1,46 @@
-function p = chebpolyvalm(c,A)
-% CHEBPOLYVALM Evaluate polynomial with matrix argument. 
+function p = chebpolyvalm(p, A)
+%CHEBPOLYVALM   Evaluate polynomial with matrix argument. 
+%   Y = CHEBPOLYVALM(P, X), when P is a vector of length N+1 whose elements are
+%   the Chebyshev coefficients of a polynomial, is the value of the polynomial
+%   evaluated with matrix argument X. X must be a square matrix.
+%       Y = P(1)*T_N(X) + P(2)*T_{N-1}(X) + ... + P(N)*T_1(X) + P(N+1)*I
 %
-% Y = CHEBPOLYVALM(P,X), when P is a vector of length N+1 whose elements 
-% are the Chebyshev coefficients of a polynomial, is the value of the
-% polynomial evaluated with matrix argument X.  X must be a square matrix. 
-%
-%  Y = P(1)*T_N(X) + P(2)*T_{N-1}(X) + ... + P(N)*T_1(X) + P(N+1)*I
-%
-% Warning: The matrix X must have a spectrum close to [-1,1], and the
-% matrix X should not be too non-normal. 
+%   Warning: The matrix X must have a spectrum close to [-1, 1], and the matrix
+%   X should not be too non-normal.
 % 
 % See also POLYVALM, CHEBPOLYVAL.  
 
-% Copyright 2013 by The University of Oxford and The Chebfun Developers. 
-% See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
+% Copyright 2014 by The University of Oxford and The Chebfun Developers. 
+% See http://www.chebfun.org/ for Chebfun information.
 
-[n m]=size(A); 
-if (n ~= m)  % square matrix check.
-   error('CHEBPOLYVALM:SQUARE','Matrix must be square'); 
+% Check inputs:
+[n, m] = size(A); 
+if ( n ~= m )
+   error('CHEBFUN:chebpolyvalm:square', 'Matrix must be square'); 
+end
+if ( ~isvector(p) )
+    error('CHEBFUN:chebpolyvalm:vector', 'P must be a vector.'); 
 end
 
-% flip the coefficients. 
-c = c(end:-1:1); 
+% Flip the coefficients. 
+p = p(end:-1:1); 
 
+% Initialise:
+p(1) = 2*p(1);
+n = length(p); 
+BOld = zeros(m); 
+B = zeros(m); 
+BNew = B; 
+I = eye(m);
 
-c(1) = 2*c(1);
-n = length(c); Bold = zeros(m); B=zeros(m); Bnew=B; 
 % Clenshaw's method.
 for k = n:-1:1     
-    Bold=B; B=Bnew; 
-    Bnew = c(k)*eye(m) + 2*A*B - Bold;
+    BOld = B; 
+    B = BNew; 
+    BNew = p(k)*I + 2*A*B - BOld;
 end
-% correct at end.
-p = .5*(Bnew - Bold);
+
+% Correct at end.
+p = .5*(BNew - BOld);
 
 end
