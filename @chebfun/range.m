e57152af5ae0f40d71aafb79e1cde52e4f8ec811
Anthony P. Austin <austin@maths.ox.ac.uk>
2014-05-19 16:38:32 +0100

    Add test file for chebfun.range().

--- @chebfun/range.m
+++ @chebfun/range.m
@@ -13,8 +13,6 @@ function r = range(f, dim)
 % Copyright 2014 by The University of Oxford and The Chebfun Developers. 
 % See http://www.chebfun.org/ for Chebfun information.
 
-% TODO: This method needs a test.
-
 if ( isempty(f) )
     r = [];
     return
