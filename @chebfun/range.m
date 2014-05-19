99cc449181cb3286d1795a32af35798abcc3ab1d
Anthony P. Austin <austin@maths.ox.ac.uk>
2014-05-19 16:17:54 +0100

    Several fixes for chebfun.range().
     - Minor changes to the documentation.
    
     - Use numColumns instead of numel to determine number of columns.  This
       is necessary to make range() work for array-valued chebfuns; the
       latter will only give the correct result for quasimatrices.
    
     - Fix off-by-one error:  dim >= 3 instead of dim > 3.
    
     - Rewrote the code for handling computing the range along the
       continuous dimension to respect the fact that minandmax(f) always
       returns a matrix with two rows, even if f is a row chebfun.
    
     - Rewrote the code for handling the range across the discrete dimension
       to respect the fact that, when inside a class, the overloaded
       chebfun.subsref() is not called in favor of the built-in one and
       that, internally, quasimatrices are stored in a singly-indexed array.
       (NB:  This change assumes that minandmax() returns a quasimatrix and
       not an array-valued chebfun, which it currently does.)

--- @chebfun/range.m
+++ @chebfun/range.m
@@ -1,17 +1,17 @@
 function r = range(f, dim)
 %RANGE   Range of CHEBFUN.
-%   R = RANGE(F) returns the range R = max(F) - min(F) of the CHEBFUN F.
+%   R = RANGE(F) returns the range R = MAX(F) - MIN(F) of the CHEBFUN F.
 %
 %   R = RANGE(F, DIM) operates along the dimension DIM of the quasimatrix F. If
 %   DIM represents the continuous variable, then R is a vector. If DIM
-%   represents the discrete dimension, then R is a quasimatrix. The default for
+%   represents the discrete dimension, then R is a CHEBFUN. The default for
 %   DIM is 1, unless F has a singleton dimension, in which case DIM is the
 %   continuous variable.
 %
 % See also CHEBFUN/MINANDMAX.
 
 % Copyright 2014 by The University of Oxford and The Chebfun Developers. 
-% See http://www..chebfun.org/ for Chebfun information.
+% See http://www.chebfun.org/ for Chebfun information.
 
 % TODO: This method needs a test.
 
@@ -21,7 +21,7 @@ if ( isempty(f) )
 end
 
 if ( nargin < 2 )
-    if ( numel(f) > 1 )
+    if ( numColumns(f) > 1 )
         % Dim 1 by default.
         dim = 1;
     else
@@ -30,7 +30,7 @@ if ( nargin < 2 )
     end
 end
 
-if ( dim > 3 )
+if ( dim >= 3 )
     % Consistent with MATLAB?
     r = 0*f;
     return
@@ -41,31 +41,16 @@ r = minandmax(f, [], dim);
 
 if ( isnumeric(r) )
     % Range along continuous dimension.
-    
-    if ( numel(f) == 1 )
-        % Scalar f:
-        r = diff(r.');  
-    elseif ( ~f(1).isTransposed )
-        % Column quasimatrix:
-        r = diff(r);    
-    else   
-        % Row quasimatrix:
-        r = diff(r, 1, 2); 
+    r = diff(r);
+
+    % Handel row inputs:
+    if ( f(1).isTransposed )
+        r = r.';
     end
     
 else
     % Range across discrete dimension.
-    
-    if ( ~r(1).isTransposed )
-        % Column CHEBFUN:
-        r = r(:,2) - r(:,1);
-    
-    else
-        % Row CHEBFUN:
-        r = r(2,:) - r(1,:);
-        
-    end
-    
+    r = r(2) - r(1);
 end
 
 end
