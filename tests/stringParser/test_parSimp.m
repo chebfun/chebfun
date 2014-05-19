77b4759bd7ad37032a14ad44f06927ca0c2e0d90
Nick Hale <nick.p.hale@gmail.com>
2014-05-19 10:56:25 +0200

    Adding a test for the parSimp() (the parenthesis simplification routine).
    Since this method is a private static method of the ```@stringParser``` class, we can't call it directly to test it. To get around this I added a second hidden, static, but pulblic method which tests this file. Presumably there's a more sensible way of testing private methods?

---
+++ tests/stringParser/test_parSimp.m
@@ -0,0 +1,9 @@
+function pass = test_parSimp(pref)
+
+% Copyright 2014 by The University of Oxford and The Chebfun Developers. 
+% See http://www.chebfun.org/ for Chebfun information.
+
+pass = stringParser.test_parSimp();
+
+end
+    
