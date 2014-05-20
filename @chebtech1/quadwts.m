8ed1acf90eb3d771dde6ed1af78875b8c467b63e
Nick Hale <nick.p.hale@gmail.com>
2014-05-20 15:34:35 +0200

    Use NH's algorithm for computing CC weights.

--- @chebtech1/quadwts.m
+++ @chebtech1/quadwts.m
@@ -4,15 +4,13 @@ function w = quadwts(n)
 %
 % See also CHEBPTS, BARYWTS.
 
-%   References:
-%
-%   [1] Joerg Waldvogel, "Fast construction of the Fejer and Clenshaw-Curtis
-%   quadrature rules", BIT Numerical Mathematics 46 (2006), pp 195-202.
-%
-%   [2] Joerg Waldvogel, www.chebfun.org/and_beyond/programme/slides/wald.pdf
+% Copyright 2014 by The University of Oxford and The Chebfun Developers.
+% See http://www.chebfun.org/ for Chebfun information.
 
-%   Copyright 2014 by The University of Oxford and The Chebfun Developers. 
-%   See http://www.chebfun.org for Chebfun information.
+% References:
+%   [1] Joerg Waldvogel, "Fast construction of the Fejer and Clenshaw-Curtis
+%       quadrature rules", BIT Numerical Mathematics 46 (2006), pp 195-202.
+%   [2] Joerg Waldvogel, www.chebfun.org/and_beyond/programme/slides/wald.pdf
 
 % Quadrature weights for Chebyshev points of 1st kind (also known as Fejer nodes
 % of the 1st kind): cos(k*pi/n), where k = 1/2,3/2,...,n-1/2 (see references
--- @chebtech2/quadwts.m
+++ @chebtech2/quadwts.m
@@ -6,23 +6,28 @@ function w = quadwts(n)
 % See also CHEBPTS, BARYWTS.
 
 % Copyright 2014 by The University of Oxford and The Chebfun Developers.
-% See http://www.chebfun.org for Chebfun information.
+% See http://www.chebfun.org/ for Chebfun information.
 
-%   References:
-%    [1] Joerg Waldvogel, "Fast construction of the Fejer and Clenshaw-Curtis
-%     quadrature rules", BIT Numerical Mathematics 46 (2006), pp 195-202.
-%    [2] Joerg Waldvogel, www.chebfun.org/and_beyond/programme/slides/wald.pdf
+% References:
+%   [1] Joerg Waldvogel, "Fast construction of the Fejer and Clenshaw-Curtis
+%        quadrature rules", BIT Numerical Mathematics 46 (2006), pp 195-202.
+%   [2] Joerg Waldvogel, www.chebfun.org/and_beyond/programme/slides/wald.pdf
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DEVELOPER NOTE:
+%  We use a varient of Waldvogel's algorithm, due to Nick Hale. (See his SIAM
+%  2013 talk.)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if ( n == 0 )                      % Special case (no points!)
     w = [];
 elseif ( n == 1 )                  % Special case (single point)
     w = 2;
 else                               % General case
-    n = n-1;
-    u0 = 1/(n^2 - 1 + mod(n, 2));  % Boundary weights
-    L = 0:n-1;                     % Auxiliary vector 1
-    r = 2./(1 - 4*min(L, n-L).^2); % Auxiliary vector 2
-    w = [ ifft(r-u0), u0 ];        % Clenshaw-Curtis weights
+    c = 2./[1, 1-(2:2:(n-1)).^2];  % Exact integrals of T_k (even)
+    c = [c, c(floor(n/2):-1:2)];   % Mirror for DCT via FFT
+    w = ifft(c);                   % Interior weights
+    w([1,n]) = w(1)/2;             % Boundary weights
 end
 
 end
