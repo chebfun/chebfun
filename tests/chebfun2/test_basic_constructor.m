0b45f2e39f59aa6259db144b04e00312d89be546
Townsend <townsend@maths.ox.ac.uk>
2014-05-22 10:26:49 +0100

    Indexing error in chebtest.

--- tests/chebfun2/test_basic_constructor.m
+++ tests/chebfun2/test_basic_constructor.m
@@ -45,5 +45,5 @@ g = @(x,y) exp(-((x-0.2).^2+(y-0.33).^2)./max(1 - ((x-0.2).^2 + (y-0.33).^2),0))
 f = chebfun2(g,[-pi,pi,-pi,pi]);
 [xx,yy] = meshgrid(linspace(-pi,pi,101));
 err = g(xx,yy)-f(xx,yy);
-pass(7) = ( norm(err(:),inf ) < 2e3*tol );
+pass(8) = ( norm(err(:),inf ) < 2e3*tol );
 end
