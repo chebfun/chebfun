924f9ccb2baf5ae1bdb152d7f2004150ea47faea
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2014-05-30 13:01:40 +0100

    Moving exporting steps from subclasses to the superclass.

--- @chebguiExporter/toFile.m
+++ @chebguiExporter/toFile.m
@@ -1,21 +1,34 @@
-function toFile(e, guifile)
+function toFile(exporter, guifile)
 
 [filename, pathname] = uiputfile( ...
     {'*.m', 'M-files (*.m)'; '*.*',  'All Files (*.*)'}, ...
-    'Save as', e.defaultFileName);
+    'Save as', exporter.defaultFileName);
 
 fullFileName = [pathname, filename];
 
 fid = fopen(fullFileName, 'wt');
 
 % Extract the necessary info for export to an .m file from the GUIFILE object:
-expInfo = exportInfo(e, guifile);
+expInfo = exportInfo(exporter, guifile);
 
-writeHeader(e, fid, filename)
+writeHeader(exporter, fid, filename)
 
 if ( filename ~= 0 )     % User did not press cancel
-    chebgui2mfile(e, guifile, fid, expInfo)
-            
+    % Print description of the problem:
+    exporter.printDescription(fid, expInfo)
+    
+    % Print the problem set-up:
+    exporter.printSetup(fid, expInfo, guifile)
+    
+    % Print the options set-up:
+    exporter.printOptions(fid, expInfo)
+    
+    % Print the solution step:
+    exporter.printSolver(fid, expInfo)
+    
+    % Print the post-solution process:
+    exporter.printPostSolver(fid, expInfo)
+    
     % Open the new file in the editor
     open(fullFileName)
 end
