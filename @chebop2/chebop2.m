6d87eeff701d3e48df5c140d682be5d2b0d6b19d
Townsend <townsend@maths.ox.ac.uk>
2014-06-02 09:29:59 +0100

    Adding user help text and comment to the chebop2.m file.

--- @chebop2/chebop2.m
+++ @chebop2/chebop2.m
@@ -1,21 +1,31 @@
-%CHEBOP2 constructor 
+classdef chebop2
+%CHEBOP2  CHEBOP2 class for representing partial differential equations
+% 
+% Class used to solve PDEs defined on rectangular domains that have 
+% unique and globally smooth solutions. 
 % 
 % N = CHEBOP2(@(u) op(u)) constructs an operator N representing the
 % operator given in @(u)op(u) acting on functions of two variables on
 % [-1,1] by [-1,1]. 
 %
-% N = CHEBOP2(@(u) op(u),[a b c d]) constructs an operator N acting on
+% N = CHEBOP2(@(u) op(u), [a b c d]) constructs an operator N acting on
 % functions of two variables defined on [a,b] by [c,d]. 
 % 
 % N = CHEBOP2(@(x,y,u) op(x,y,u),...) constructs a variable coefficient PDE
 % operator. 
+% 
+% Boundary conditions are imposed via the syntax N.lbc, N.rbc, N.ubc, and
+% N.dbc. For example to solve Poisson with Dirichlet conditions try: 
+% 
+% Example: 
+%    N = chebop2(@(u) diff(u,2,1) + diff(u,2,2)); 
+%    N.lbc = 0; N.rbc = 0; N.ubc = 0; N.dbc = 0; 
+%    u = N \ 1; 
 
 % Copyright 2013 by The University of Oxford and The Chebfun2 Developers.
 % See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
 
 
-classdef chebop2
-    
     properties ( GetAccess = 'public', SetAccess = 'public' )
         domain = [];    % Domain of the operator
         op = [];        % The operator
@@ -36,53 +46,64 @@ classdef chebop2
         U
         S               %explict low rank form. 
         V     
-        %coeffcell={};   % cell array for storing variable coefficients
     end
     
     methods
         
-        function N = chebop2(varargin)
-            N = ctor(N, varargin{:});
+        function N = chebop2( varargin )
+            N = constructor( N, varargin{:} );
         end
         
     end
 
-    methods ( Static = true )
-        X = BartelsStewart(A,B,C,D,E,xsplit,ysplit);
+    methods ( Static = true, Hidden = true )
+        % Matrix equation solver: AXB^T + CXD^T = E. xsplit, ysplit = 1 if
+        % the even and odd modes (coefficients) decouple. 
+        X = BartelsStewart(A, B, C, D, E, xsplit, ysplit);
+        
+        % Use automatic differentiation to pull out the coeffs of the 
+        % operator: 
+        deriv = chebfun2deriv( op );
+        
+        % This is used to discretize the linear constrains:
+        [bcrow, bcvalue] = constructbc(bcArg, bcpos,...
+                                                een, bcn, dom, scl, order);
+        
+        % This is used to discretize the PDE:
+        [CC, rhs, bb, gg, Px, Py, xsplit, ysplit] =...
+                                 constructDiscretisation(N, f, m, n, flag);
+        
+        % Convert all the different user inputs for bc into uniform format:
         bc = createbc(bcArg, ends);
+        
         T = spconvermat(n,lam,k);
+        
         M = MultMat(a,bn,varargin);
+        
         diffMat = spdiffmat(n,k,varargin);
-        [bcrow,bcvalue]=constructbc(bcArg,bcpos,een,bcn,dom,scl,order);
-        deriv= chebfun2deriv( op );
-        a = truncate(a,tol);
-        [CC,rhs,bb,gg,Px,Py,xsplit,ysplit]=constructDiscretisation(N,f,m,n,flag)
+        
+        % Remove trailing coefficients.
+        a = truncate(a, tol);
+              
     end
 end
 
-function N = ctor(N, varargin)
+function N = constructor( N, varargin )
 % Constructing the chebop2 object.
 
 % Get chebfun2 preferences
 prefs = chebfunpref(); 
 tol = prefs.cheb2Prefs.eps;
 
-% maximum differential order in the PDE.
-% maxorder = 2;
-
-% Set up empty arrays for coefficients. 
-% C = cell(maxorder+1);
-% A = zeros(maxorder+1); 
-
 % If empty input arguments then return an empty chebop2 object.
-if ( isempty(varargin) )
-    return;
+if ( isempty( varargin ) )
+    return
 end
 
 % What domain is the operator defined on?
-if ( numel(varargin) > 1 )
+if ( numel( varargin ) > 1 )
     ends = varargin{2};      % second argument should be a domain.
-    if ( length(ends) == 4 )
+    if ( length( ends ) == 4 )
         if ( (diff(ends(1:2)) > 0 )&& (diff(ends(3:4)) > 0 ) )  % valid domain?
             domain = ends;
         else
@@ -92,14 +113,14 @@ if ( numel(varargin) > 1 )
         error('CHEBOP2:CONSTRUCTOR:INPUT','Argument should be a domain given by four doubles')
     end
 else
-    if isa(varargin{1},'function_handle') 
+    if ( isa( varargin{1}, 'function_handle' ) )
         % pick the default domain
         rect1 = [-1,1]; 
         rect2 = [-1,1];
         domain = [rect1 rect2];
-    elseif isa(varargin{1},'double')
+    elseif ( isa( varargin{1},'double' ) )
         N = chebop2(@(u) u, varargin{1});  % set up identity operator on the domain. 
-        return;
+        return
     else
         error('CHEBOP2:INPUTS','First argument is not an operator or domain.')
     end
@@ -115,9 +136,9 @@ if ( isa(varargin{1},'function_handle') )
     
     if ( nargin(op) == 1 )     % The PDE has constant coefficients.
     
-%         %OLD way! % 
-        % Use an elimination procedure to extract out the constant
-        % coefficients of the PDE.
+%   OLD way! ( Use elimination )
+% Use an elimination procedure to extract out the constant
+% coefficients of the PDE.
 %         for jj = 0:maxorder
 %             
 %             for kk = 0:maxorder
@@ -139,68 +160,34 @@ if ( isa(varargin{1},'function_handle') )
         
       try 
         %  NEW way! % 
-        A = chebop2.chebfun2deriv(op); 
-        A = rot90(A,2);
+        A = chebop2.chebfun2deriv( op ); 
+        A = rot90( A, 2 );
       catch 
           % Trust that the user has formed the chebfun2 objects outside of
           % chebop2. 
-          u = adchebfun2(chebfun2(@(x,y) x.*y, domain)); 
-          v = op(u); 
+          u = adchebfun2( chebfun2(@(x,y) x.*y, domain) ); 
+          v = op( u ); 
           A = v.der.derCell;
       end
-    elseif (nargin(op)==2 )
+    elseif ( nargin(op) == 2 )
         error('Did you intend to have @(x,y,u)?')
-    elseif ( nargin(op) == 3 ) % The coefficients of the PDE are now variable coefficient.
+    elseif ( nargin(op) == 3 ) 
+        % The coefficients of the PDE are now variable coefficient.
         
-        % setup a chebfun2 on the right domain 
-        u = adchebfun2(chebfun2(@(x,y) x.*y, domain)); 
-        x = chebfun2(@(x,y) x, domain); 
-        y = chebfun2(@(x,y) y, domain);
+        % Setup a chebfun2 on the right domain 
+        u = adchebfun2( chebfun2(@(x,y) x.*y, domain) ); 
+        x = chebfun2( @(x,y) x, domain ); 
+        y = chebfun2( @(x,y) y, domain );
         % apply it to the operator 
-        v = op(x,y,u); 
+        v = op( x, y, u ); 
         A = v.der.derCell;  % cell array of variable coefficients. 
-        
-        
-%         for jj=0:maxorder
-%             
-%             for kk=0:maxorder
-%                 % Test with the function x^j*y^k to eliminate terms with
-%                 % derivatives of j+1, k+1 and higher. Now we have variable
-%                 % coefficients so we are left with a variable coefficient
-%                 % to approximate.
-%                 
-%                 const = factorial(jj).*factorial(kk);
-%                 test = chebfun2(@(x,y) (x.^jj.*y.^kk)/const, domain);
-%                 
-%                 % First evaluate on a 5 by 5 grid to see if the variable
-%                 % coefficient is actually a constant.
-%                 x = chebpts(5); F = zeros(5);
-%                 for j = 1:length(x)
-%                     for k = 1:length(x)
-%                         F(j,k) = feval(op(x(j),x(k),test),0,0);
-%                     end
-%                 end
-%                 
-%                 % If the variable coefficient is likely to be a constant
-%                 % then it is faster to check for this case.
-%                 if ( norm(F - mean(mean(F))) < 10 * tol )
-%                     A(jj+1,kk+1) = mean(mean(F));  % constant is the mean
-%                     C(jj+1,kk+1) = {[]};
-%                 else
-%                     % We have no choice but to call the constructor to work
-%                     % out the variable coefficient.
-%                     A(jj+1,kk+1) = 1; % Elimination procedure to get coeffs.
-%                     C(jj+1,kk+1) = {chebfun2(@(x,y) feval(op(x,y,test),0,0),'vectorize')};
-%                 end
-%             end
-%         end
-
-
     else
-        error('CHEBOP2:CONSTRUCTOR:INPUT','Operator should be @(u) or @(x,y,u).')
+        error('CHEBOP2:CONSTRUCTOR:INPUT',...
+                         'Operator should be @(u) or @(x,y,u).')
     end
 else
-    error('CHEBOP2:CONSTRUCTOR:INPUT','First argument should be an operator')
+    error('CHEBOP2:CONSTRUCTOR:INPUT',...
+                          'First argument should be an operator')
 end
 
 % Often the coefficients are obtained with small rounding errors
@@ -222,21 +209,22 @@ end
 N.domain = domain;
 N.op = op;
 N.coeffs = A;
-%N.coeffcell = C;
 
 % Calculate xorder and yorder of PDE. 
 % Find the differential order of the PDE operator.
-if iscell(A) 
-    xorder = size(A,1)-1; 
-    yorder = size(A,2)-1; 
-elseif ( min(size(A)) > 1 )
+if ( iscell( A ) )
+    xorder = size(A, 1)-1; 
+    yorder = size(A, 2)-1; 
+elseif ( min( size( A ) ) > 1 )
     xorder = find( sum( abs( A ), 2 ) > 100*tol, 1, 'last') - 1;
     yorder = find( sum( abs( A ) ) > 100*tol, 1, 'last' ) - 1;
 else
-    if ( size(A,1) == 1 )
-        yorder = length(A) - 1; xorder = 0;
+    if ( size(A, 1) == 1 )
+        yorder = length(A) - 1; 
+        xorder = 0;
     else
-        xorder = length(A) - 1; yorder = 0;
+        xorder = length(A) - 1; 
+        yorder = 0;
     end
 end
 N.xorder = xorder; 
