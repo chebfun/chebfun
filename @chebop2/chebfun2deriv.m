55393e9c6f977ae8c5f7586629d92accf23aa822
Townsend <townsend@maths.ox.ac.uk>
2014-06-02 10:23:55 +0100

    help text for chebfun2deriv

--- @chebop2/chebfun2deriv.m
+++ @chebop2/chebfun2deriv.m
@@ -1,14 +1,18 @@
 function deriv = chebfun2deriv( op )
-%CHEBFUN2DDERIV Summary of this function goes here
-%   Detailed explanation goes here
+%CHEBFUN2DDERIV   
+% 
+% A few operations in Chebfun2 store AD information (see plus, diff, uminus)
+% This allows us to pull out the coefficients to a partial differential
+% operator. 
 
 % For now, only work with linear operators. Hence, we can take any function on
 % [-1,1] to evaluate the function handle to get the derivative information
 
+% Evaluate the operator at a chebfun2 and use the AD information to 
+% determine the coefficients of the partial differential operator: 
+
 u = chebfun2(@(x,y) x.*y);
-
 opu = op(u);
-
 deriv = opu.deriv;
 
 end
