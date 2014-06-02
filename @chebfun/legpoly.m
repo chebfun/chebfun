7917e61cdf119f18594a24601fcdf014659032eb
Nick Hale <nick.p.hale@gmail.com>
2014-06-02 12:53:08 +0200

    Adding a CHEBFUN/JACPOLY() method.

--- @chebfun/legpoly.m
+++ @chebfun/legpoly.m
@@ -3,12 +3,12 @@ function out = legpoly(f, varargin)
 %   A = LEGPOLY(F, N) returns the first N+1 coefficients in the Legendre series
 %   expansion of the CHEBFUN F, so that such that F approximately equals A(1)
 %   P_N(x) + ... + A(N) P_1(x) + A(N+1) P_0(x) where P_N(x) denotes the N-th
-%   Legendre polynomial.
+%   Legendre polynomial. A is a row vector.
 %
 %   If F is smooth (i.e., numel(f.funs) == 1), then A = LEGPOLY(F) will assume
 %   that N = length(F) - 1;
 %
-%   There is also a LEGPOLY command in the Chebfun trunk directory, which
+%   There is also a LEGPOLY command in the Chebfun trunk directory which
 %   computes the CHEBFUN corresponding to the Legendre polynomial P_n(x).
 %
 %   LEGPOLY does not support quasimatrices.
