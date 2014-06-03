08784edbc88c89d2247741248aa8eed6500fa35a
Mohsin Javed <mhsnjvd@gmail.com>
2014-06-03 11:15:59 +0100

    removing a typo.

--- @deltafun/mat2cell.m
+++ @deltafun/mat2cell.m
@@ -1,7 +1,7 @@
 function f = mat2cell(f, varargin)
 %MAT2CELL   Wrap a DELTAFUN in a cell.
-%   DELTAFUN objects are not array-valued, so G = MAT2CELL(F) simply wraps F is a
-%   cell array and is equivalent to G = {F}.
+%   DELTAFUN objects are not array-valued, so G = MAT2CELL(F) simply wraps F 
+%   in a cell array and is equivalent to G = {F}.
 
 % Copyright 2014 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
