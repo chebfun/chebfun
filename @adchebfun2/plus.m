ae1247e780193a7d69d2fd852cc49b833576bc61
AT <townsend@maths.ox.ac.uk>
2014-06-04 14:53:06 +0100

    Minor changes to ADChebfun2.

--- @adchebfun2/plus.m
+++ @adchebfun2/plus.m
@@ -1,24 +1,24 @@
 function h = plus ( f, g )
-%+	  Plus.
+%+	  Plus for ADCHEBFUN2
 %
-% F + G adds ADchebfun2s F and G, or a scalar to a chebfun2 if either F or G
-% is a scalar.
+% F + G adds ADCHEBFUN2 objects F and G, or a scalar to a 
+% ADCHEBFUN2 if either F or G is a scalar.
 
 % Copyright 2013 by The University of Oxford and The Chebfun Developers.
 % See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
 
-if isempty(f) || isempty(g)  % check for empty ADchebfun2
+if ( isempty(f) || isempty(g) )  % check for empty ADchebfun2
    h = ADchebfun2;  % just return an empty ADchebfun2. 
    return; 
 end
 
-if ( ~isa(f,'adchebfun2') ) % First argument was not an ADchebfun2
+if ( ~isa(f, 'adchebfun2') ) % First argument was not an ADchebfun2
     % Swap arguments.
     h = plus(g, f);
     return
 end
 
-if isa(g,'adchebfun2')     % ADCHEBFUN2 + ADCHEBFUN2
+if isa(g, 'adchebfun2')     % ADCHEBFUN2 + ADCHEBFUN2
     h = f;
     
     % Add the chebfun2s
@@ -32,38 +32,5 @@ elseif ( isa(g, 'chebfun2') || isa(g, 'double') ) % ADCHEBFUN2 + DOUBLE/SCALAR
 else
     error('ADCHEBFUN2:plus:type','Cannot add these two objects together');
 end
-
-
-
-% if ( isa(f,'chebfun2') && isa(g,'double') )  
-% %% chebfun2 + double
-%     if isempty(f) % check for empty chebfun2.
-%         return
-%     end
-%     h = f;
-%     h.scl = f.scl;
-%     h.fun2 = plus(f.fun2,g);
-% elseif ( isa(f,'double') && isa(g,'chebfun2') )  
-% %% double + chebfun2
-%     if isempty(g) % check for empty chebfun2.
-%         return
-%     end
-%     h = g;
-%     h.scl = g.scl;
-%     h.fun2 = plus(f,g.fun2);
-% elseif ( isa(f,'chebfun2') && isa(g,'chebfun2') )  
-% %% chebfun2 + chebfun2
-%     if isempty(g) % check for empty chebfun2.
-%         h = f; return
-%     end
-%     if isempty(f) % check for empty chebfun2.
-%         h = g; return
-%     end
-%     h = f;
-%     h.scl = f.scl;
-%     h.fun2 = plus(f.fun2,g.fun2);
-%     
-
-% end
 
 end
