68a4cee2152280203b2b3df5f87d270dbd8437ab
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2014-06-05 11:32:36 +0100

    Adding test methods, now we try exporting all demos to .m files.

---
+++ tests/chebgui/test_toFilePDE.m
@@ -0,0 +1,40 @@
+function pass = test_toFilePDE(pref)
+%TEST_TOFILE    Test exporting all BVP demos to an .m-file.
+%
+% This test only checks whether nothing breaks, it does not try to solve the
+% problems.
+% Find the folders which demos are stored in. The chebguiDemos folder lives in
+% the trunk folder, find the path of the Chebfun trunk.
+trunkPath = fileparts(which('chebguiWindow'));
+
+% Append directory information
+bvppath = fullfile(trunkPath, 'chebguiDemos', 'pdedemos');
+
+% Set up file to exporting to
+tempPath = fullfile(trunkPath, 'tests', 'chebguiExporter');
+tempFileName = 'tempExporterTest.m';
+
+% Obtain directory information:
+D = dir(bvppath);
+
+% Create a BVP exporter object:
+exporter = chebguiExporter.constructor('pde');
+
+% Loop through the demos and export
+for demoCounter = 3:length(D)
+    demoPath = fullfile(bvppath, D(demoCounter).name);
+    
+    % Load demo
+    cg = chebgui.demo2chebgui(demoPath);
+        
+    % Export the demo!
+    toFile(exporter, cg, tempFileName, tempPath)
+end
+
+% Delete the temporary file we wrote to:
+delete(fullfile(tempPath, tempFileName))
+
+% Got here without crashing == success!
+pass = 1;
+
+end
