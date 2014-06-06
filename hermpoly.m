ca9721ca04c3cec7b98dd3a00edfd831745b61c7
Nick Hale <nick.p.hale@gmail.com>
2014-06-06 11:21:58 +0200

    Tidying up.

--- hermpoly.m
+++ hermpoly.m
@@ -1,50 +1,49 @@
 function H = hermpoly(n, type)
-% HERMPOLY   Hermite polynomial of degree n.
-% H = HERMPOLY(N) returns the chebfun corresponding to the 'physicist'-type 
-% Hermite polynomials H_N(x) on [-inf,inf] (orthogonal with respect to the 
-% weight exp(-x.^2)), where N may be a vector of positive integers.
+%HERMPOLY   Hermite polynomial of degree n.
+%   H = HERMPOLY(N) returns the chebfun corresponding to the 'physicist'-type
+%   Hermite polynomials H_N(x) on [-inf,inf] (orthogonal with respect to the
+%   weight exp(-x.^2)). N may be a vector of positive integers.
 %
-% H = HERMPOLY(N,'PROB') normalises instead by the probablist's definition
-% (with weight exp(-x.^2/2)), which gives rise to monic polynomials.
+%   H = HERMPOLY(N, 'PROB') normalises instead by the probablist's definition
+%   (with weight exp(-x.^2/2)), which gives rise to monic polynomials.
 %
-% Note, this is currently just a toy to play with the construction of
-% Hermite polynomials using a combination of Chebfun's barycentric,
-% mapping, and 'blowup' technologies. See chebfun/chebtests/unbndpolys.m
-% for some testing.
-%
-% See also chebpoly, legpoly, jacpoly, and lagpoly.
+%   Note, this is currently just a toy to play with the construction of Hermite
+%   polynomials using a combination of Chebfun's barycentric, mapping, and
+%   'blowup' technologies.
+% See also CHEBPOLY, LEGPOLY, JACPOLY, and LAGPOLY.
 
-% Copyright 2013 by The University of Oxford and The Chebfun Developers. 
-% See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
+% Copyright 2014 by The University of Oxford and The Chebfun Developers. 
+% See http://www.chebfun.org/ for Chebfun information.
 
 if ( nargin == 1 )
-    % By default we take the physicist's definition
+    % By default we take the physicist's definition:
     type = 'phys';
 end
 
-if ( strcmpi(type,'prob') )
+if ( strcmpi(type, 'prob') )
     normtype = 1;
 else
     normtype = 2;
 end
 
-H = chebfun(@(x) 1+0*x, [-inf inf]);
-xinf = chebfun(@(x) x, [-inf inf] );
-if ( normtype == 1 )    % Probabilist type
-    H = [H xinf]; 
+x = chebfun(@(x) x,[-inf, inf]);        % X
+H = chebfun(@(x) 1 + 0*x, [-inf, inf]); % H_0(x)
+
+if ( normtype == 1 )   % Probabilist type
+    H = [H, x];     % H_1(x)
     for k = 2:max( n ) % Recurrence relation
-        Hk = xinf.*H(:,k) - (k-1)*H(:,k-1);
-        H = [H Hk]; 
+        Hk = x.*H(:,k) - (k-1)*H(:,k-1);
+        H = [H, Hk];   %#ok<AGROW>
     end
-else                 % Physicist type
-    H = [H 2*xinf];
-    for k = 2:max(n) % Recurrence relation
-        Hk = 2.*xinf.*H(:,k)-2*(k-1)*H(:,k-1);
-        H = [H Hk];
+else                   % Physicist type
+    H = [H, 2*x];   % H_1(x)
+    for k = 2:max(n)   % Recurrence relation
+        Hk = 2.*x.*H(:,k) - 2*(k-1)*H(:,k-1);
+        H = [H, Hk];   %#ok<AGROW>
     end
 end
 
-% Take only the ones we want
+% Take only the ones we want:
 H = H(:,n+1);
 
 end
--- lagpoly.m
+++ lagpoly.m
@@ -1,25 +1,27 @@
-function L = lagpoly( n )
-% LAGPOLY   Laguerre polynomial of degree n.
-% L = LAGPOLY(N) returns the chebfun corresponding to the Laguerre polynomials 
-% L_N(x) on [0,inf], where N may be a vector of positive integers.
+function L = lagpoly(n)
+%LAGPOLY   Chebfun representation of Laguerre polynomials.
+%   L = LAGPOLY(N) returns the CHEBFUN corresponding to the Laguerre polynomials
+%   L_N(x) on [0,inf]. N may be a vector of positive integers.
 %
-% Note, this is currently just a toy to play with the construction of
-% Hermite polynomials using a combination of Chebfun's barycentric,
-% mapping, and 'blowup' technologies. See chebfun/chebtests/unbndpolys.m
-% for some testing.
+%   Note, this is currently just a toy to play with the construction of Laguerre
+%   polynomials using a combination of Chebfun's barycentric, mapping, and
+%   'blowup' technologies.
 %
-% See also chebpoly, legpoly, jacpoly, and hermpoly.
+% See also CHEBPOLY, LEGPOLY, JACPOLY, and HERMPOLY.
 
-% Copyright 2011 by The University of Oxford and The Chebfun Developers. 
-% See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
+% Copyright 2014 by The University of Oxford and The Chebfun Developers. 
+% See http://www.chebfun.org/ for Chebfun information.
 
-xinf = chebfun(@(x) x,[0 inf]);
-L = chebfun(@(x) 1+0*x,[0 inf]);
-L = [L chebfun(@(x) 1-x, [0 inf] )];
+x = chebfun(@(x) x, [0, inf]);          % X on [0, inf]
+L = chebfun(@(x) 1 + 0*x, [0, inf]);    % L_0(x)
+L = [L, chebfun(@(x) 1 - x, [0, inf])]; % L_1(x)
+
 for k = 2:max(n) % Recurrence relation
-    Lk = ((2*k-1-xinf).*L(:,k) - (k-1)*L(:,k-1))/k;
-    L = [L Lk]; 
+    Lk = ((2*k-1-x).*L(:,k) - (k-1)*L(:,k-1))/k;
+    L = [L, Lk]; %#ok<AGROW>
 end
 
-% Take only the ones we want
+% Take only the ones we want:
 L = L(:,n+1);
+
+end
