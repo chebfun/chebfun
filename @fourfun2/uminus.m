74954d537f3e566dc3a929d0ce2ab8d97f86edf1
gradywright <gradywright@boisestate.edu>
2014-06-09 10:16:35 +0100

    Removed fourfun2 as everything is now handled in chebfun2.

--- @fourfun2/uminus.m
+++
@@ -1,9 +0,0 @@
-function F = uminus( F )
-%UMINUS   Unary minus for a CHEBFUN2. 
-
-% Copyright 2013 by The University of Oxford and The Chebfun Developers.
-% See http://www.chebfun.org/ for Chebfun information. 
-
-F.pivotValues = -F.pivotValues;
-
-end
