e2a1739cb586adf06be9961a86e0f582030ff5fc
Anthony P. Austin <austin@maths.ox.ac.uk>
2014-06-10 09:30:45 +0100

    Fix miscellaneous spelling errors.
    Most of these were pointed out by LNT.  Closes #542.

--- @chebfun/get.m
+++ @chebfun/get.m
@@ -2,7 +2,7 @@ function out = get(f, prop)
 %GET   GET method for the CHEBFUN class
 %   P = GET(F, PROP) returns the property P specified in the string PROP from
 %   the CHEBFUN F. Valid entries for the string PROP are:
-%       'DOMAIN'         - The domain of definintion of F.
+%       'DOMAIN'         - The domain of definition of F.
 %       'FUNS'           - The piecewise smooth components of F.
 %       'VSCALE'         - Vertical scale of F.
 %       'VSCALE-LOCAL'   - Local vertical scales of F.
@@ -11,8 +11,8 @@ function out = get(f, prop)
 %       'ISHAPPY'        - Is F happy?
 %       'EPSLEVEL'       - Approximate accuracy estimate of F.
 %       'EPSLEVEL-LOCAL' - Approximate accuracy estimate of F's components.
-%       'LVAL'           - Value(s) of F at lefthand side of domain.
-%       'RVAL'           - Value(s) of F at righthand side of domain.
+%       'LVAL'           - Value(s) of F at left-hand side of domain.
+%       'RVAL'           - Value(s) of F at right-hand side of domain.
 %       'LVAL-LOCAL      - Value(s) of F's FUNs at left sides of their domains.
 %       'RVAL-LOCAL'     - Value(s) of F's FUNs at right sides of their domains.
 %       'EXPS'           - Exponents in a CHEBFUN, a two vector.
@@ -21,11 +21,11 @@ function out = get(f, prop)
 %       'DELTAFUNS'        as a single matrix with the first row corresponding to
 %       'DELTAFUNCTIONS'   the locations of the delta functions, and the
 %                          magnitudes appended below the first row.
-%       'IMPS'           - Same as DELTAS, supported for backward compatability.
-%   The following are also supported for backward compatabiilty, and really only
+%       'IMPS'           - Same as DELTAS, supported for backward compatibility.
+%   The following are also supported for backward compatibility, and really only
 %   make sense when the CHEBFUN is represented by a CHEBTECH-type object. Note
 %   that in these cases a cell is always returned, even if the Chebfun has only
-%   a sngle FUN.
+%   a single FUN.
 %       'POINTS'         - The Chebyshev grid used to represent F.
 %       'VALUES'         - The values of the CHEBFUN on the grid above.
 %       'COEFFS'         - The corresponding Chebyshev coefficients.
--- @domain/ode45.m
+++ @domain/ode45.m
@@ -1,13 +1,13 @@
 function varargout = ode45(varargin)
 %ODE45   Solve non-stiff ODEs. Output a CHEBFUN.
 %   
-% This syntax is depricated. Please use chebfun.ode45(...) instead.
+% This syntax is deprecated. Please use chebfun.ode45(...) instead.
 
 % Copyright 2014 by The University of Oxford and The Chebfun Developers. 
 % See http://www.chebfun.org/ for Chebfun information.
 
-warning('CHEBFUN:ode113:depricated', ...
-    ['Usage of ODE45 via the @DOMAIN class is depricated and may be ', ...
+warning('CHEBFUN:ode113:deprecated', ...
+    ['Usage of ODE45 via the @DOMAIN class is deprecated and may be ', ...
     'removed from future releases. Please use chebfun.ode45(...) instead.']);
 
 varargin = domain.toDouble(varargin{:});
