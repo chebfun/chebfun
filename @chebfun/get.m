d92987f90588eb14d139950f16aae76aa506b41e
Anthony P. Austin <austin@maths.ox.ac.uk>
2014-06-11 08:10:41 +0100

    Clean up the chebfun get() method.
    Lots of changes here.  Normally, I'd break this thing up into separate
    commits, but these sort of all happened at once and don't easily split
    apart from one another.
    
      1.  Just about everything has been refactored.  The get() calls for
      many properties are now handled through some subfunctions instead of
      being coded inline.  Unfortunately, the logic in these functions can get
      kind of complicated and hard to follow.  There are two sources of
      complexity here:
    
        A.  We need to ensure that get(f, prop), where f is an array-valued
        chebfun gives the same results as it would if f is a quasimatrix.
        Moreover, we need to do this without just converting every array-valued
        chebfun input into a quasimatrix, which would be too expensive.
    
        B.  We need to try and return "simpler" outputs if possible, e.g., a
        numeric matrix instead of a cell array of cell arrays of values.
    
      2.  With everything rewritten from scratch, several bugs in get()
      which had it acting differently for array-valued chebfuns and
      quasimatrices have been fixed.
    
      3.  Instead of returning a 2D cell arrays with empty cells, get(f,
      prop), where f is a quasimatrix whose columns have differing number of
      funs, returns a cell array of cell arrays C such that C{J}{K} gives
      the value of the property for fun K in column J.  C{J} has only as
      many cells as there are funs in column K.
    
      4.  chebfun.get() is now set up to potentially allow you to get()
      arbitrary properties from the funs, e.g., if your tech supports property
      foo, you could do get(f, 'foo') to get a cell array of cell arrays as in
      the previous paragraph.  I have not activated this capability yet; for
      the moment, we still issue an error if the input property is not
      recognized.  But in principle, it should now be quite easy to activate.
    
      5.  In conjunction with the previous commit, this commit eliminates
      get(f, 'deltafunctions') and get(f, 'deltafuns').  get(f, 'deltas') is
      always more convenient to use than these, and we aren't constrained by
      backwards compatibility to keep the other syntaxes, so let's not bind
      ourselves in the future by introducing them now.
    
      6.  The behavior of get(f, 'deltas') is different for array-valued
      chebfuns and quasimatrices.  Previously, get(f, 'deltas') would return a
      2D cell array with the J-K entry containing the value of the 'deltas'
      property for fun J of column K.  Now it returns a one-dimensional cell
      array whose K'th entry contains the output of get(f(:, K), 'deltas').
      The local 'deltas' properties can now be gotten through get(f,
      'deltas-local').
    
    The only parts of the non-get() code that needed to be altered to
    accommodate these changes were a few places where the code ended up
    relying on the difference in behavior of get() for array-valued chebfuns
    and quasimatrices.  All chebfun tests pass for me at the moment.
    
    This method is now sufficiently complex that it warrants a test.  I'll
    do this if others think that the changes made here are worth keeping.
    
    We may want to consider exposing the doSimplify flag used by some of the
    functions to the user:  the simplification tricks are convenient when
    messaing around at the command prompt but are much more irritating if
    you're trying to write a routine that accepts general inputs.

--- @chebfun/get.m
+++ @chebfun/get.m
@@ -2,174 +2,307 @@ function out = get(f, prop)
 %GET   GET method for the CHEBFUN class
 %   P = GET(F, PROP) returns the property P specified in the string PROP from
 %   the CHEBFUN F. Valid entries for the string PROP are:
-%       'DOMAIN'         - The domain of definition of F.
-%       'FUNS'           - The piecewise smooth components of F.
-%       'VSCALE'         - Vertical scale of F.
-%       'VSCALE-LOCAL'   - Local vertical scales of F.
-%       'HSCALE'         - Horizontal scale of F.
-%       'HSCALE-LOCAL'   - Local horizontal scales of F.
-%       'ISHAPPY'        - Is F happy?
-%       'EPSLEVEL'       - Approximate accuracy estimate of F.
-%       'EPSLEVEL-LOCAL' - Approximate accuracy estimate of F's components.
-%       'LVAL'           - Value(s) of F at left-hand side of domain.
-%       'RVAL'           - Value(s) of F at right-hand side of domain.
-%       'LVAL-LOCAL      - Value(s) of F's FUNs at left sides of their domains.
-%       'RVAL-LOCAL'     - Value(s) of F's FUNs at right sides of their domains.
-%       'EXPS'           - Exponents in a CHEBFUN, a two vector.
-%       'EXPONENTS'
-%       'DELTAS'         - Return the locations and magnitude of delta functions
-%       'DELTAFUNS'        as a single matrix with the first row corresponding to
-%       'DELTAFUNCTIONS'   the locations of the delta functions, and the
+%       'domain'         - The domain of definition of F.
+%       'ends'
+%       'funs'           - The piecewise smooth components of F.
+%       'vscale'         - Vertical scale of F.
+%       'vscale-local'   - Local vertical scales of F.
+%       'hscale'         - Horizontal scale of F.
+%       'hscale-local'   - Local horizontal scales of F.
+%       'ishappy'        - Is F happy?
+%       'epslevel'       - Approximate accuracy estimate of F.
+%       'epslevel-local' - Approximate accuracy estimate of F's components.
+%       'lval'           - Value(s) of F at left-hand side of domain.
+%       'rval'           - Value(s) of F at right-hand side of domain.
+%       'lval-local      - Value(s) of F's FUNs at left sides of their domains.
+%       'rval-local'     - Value(s) of F's FUNs at right sides of their domains.
+%       'exps'           - Exponents in a CHEBFUN, a two vector.
+%       'exponents'
+%       'deltas'         - Return the locations and magnitude of delta functions
+%                          as a single matrix with the first row corresponding
+%                          to the locations of the delta functions, and the
 %                          magnitudes appended below the first row.
-%       'IMPS'           - Same as DELTAS, supported for backward compatibility.
+%       'imps'           - Same as DELTAS, supported for backward compatibility.
 %   The following are also supported for backward compatibility, and really only
 %   make sense when the CHEBFUN is represented by a CHEBTECH-type object. Note
 %   that in these cases a cell is always returned, even if the Chebfun has only
 %   a single FUN.
-%       'POINTS'         - The Chebyshev grid used to represent F.
-%       'VALUES'         - The values of the CHEBFUN on the grid above.
-%       'COEFFS'         - The corresponding Chebyshev coefficients.
+%       'points'         - The Chebyshev grid used to represent F.
+%       'values'         - The values of the CHEBFUN on the grid above.
+%       'coeffs'         - The corresponding Chebyshev coefficients.
 
 % Copyright 2014 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
 
-% TODO: Figure out what to do for quasimatrices.
-% TODO: Convert help text to lower case.
-
 switch prop
     case {'domain', 'ends'}
         out = domain(f);
-        
-    case fieldnames(f)
-        % Allow access to any of F's properties via GET.
-        if ( numel(f) > 1 )
-            % TODO: Is this the right thing to do?
-            warning('CHEBFUN:get:quasi', ...
-                'CHEBFUN/GET() returns only the property information of the first column.')
-        end
-        out = f(1).(prop);
-        
-    case 'lval'
-        out = cell(1, numColumns(f));
-        for k = 1:numel(f)
-            out{k} = get(f(k).funs{1}, 'lval');
-        end
-        out = cell2mat(out);
-        if ( f(1).isTransposed )
-            out = out.';
-        end
-            
-    case 'rval'
-        out = cell(1, numColumns(f));
-        for k = 1:numel(f)
-            out{k} = get(f(k).funs{end}, 'rval');
-        end
-        out = cell2mat(out);
-        if ( f(1).isTransposed )
-            out = out.';
-        end
-        
-    case {'exps', 'exponents'}
-        out = cell(1, numColumns(f));
-        for k = 1:numel(f)
-            for j = 1:numel(f(k).funs)
-                out{j,k} = get(f(k).funs{j}, 'exponents');
-            end
-        end
-        if ( min(size(out)) == 1 )
-            out = cell2mat(out);
-        end
-        if ( f(1).isTransposed )
-            out = out.';
-        end        
-        
-    case {'deltas', 'deltafunctions', 'deltafuns'}
-        out = cell(1, numColumns(f));
-        for k = 1:numel(f)
-            for j = 1:numel(f(k).funs)
-                out{k, j} = get(f(k).funs{j}, 'deltas');
-            end
-        end
-        
-        if ( min(size(out)) == 1 )
-            out = cell2mat(out);
-            % Make sure to merge delta functions at same locations.
-            if ( ~isempty(out) )
-                [mag, loc] = deltafun.mergeColumns(out(2:end, :), out(1, :));
-                out = [loc; mag];
-            end
-        end
-        if ( f(1).isTransposed )
-            out = out.';
-        end        
-        
-    case 'imps'
-        out = get(f, 'deltas');
-        warning('CHEBFUN:get:imps', 'IMPS field is deprecated, use DELTAS')        
-        
-    case {'lval-local', 'rval-local'}
-        if ( isempty(f) )
-            out = [];
-            return
-        end
-        lrval = prop(1:4);
-        numFuns = numel(f.funs);
-        numCols = size(f.funs{1}, 2);
-        out = zeros(numFuns, numCols);
-        for k = 1:numFuns
-            out(k,:) = get(f.funs{k}, lrval);
-        end
-        if ( f(1).isTransposed )
-            out = out.';
-        end 
-        
-    case 'ishappy'
-        for j = 1:numel(f)
-            for k = 1:numel(f(j).funs)
-                out = all(get(f(j).funs{k}, 'ishappy'));
-                if ( ~out ), return, end
-            end
-        end
-        
-    case 'hscale'
-        out = hscale(f);   
-
     case 'vscale'
-        out = vscale(f); 
-
+        out = vscale(f);
+    case 'hscale'
+        out = hscale(f);
     case 'epslevel'
         out = epslevel(f);
-        
-    case {'values', 'coeffs', 'points', 'epslevel-local', 'vscale-local', 'hscale-local'}
-        % Adjust the PROP variable in the case of -local:
-        idx = strfind(prop, '-');
-        if ( ~isempty(idx) )
-            prop = prop(1:idx-1);
+    case 'ishappy'
+        out = ishappy(f);
+    case fieldnames(f)
+        out = cell(1, numel(f));
+        for k = 1:1:numel(f)
+            out{k} = f(k).(prop);
         end
-        
-        % Initialise the output:
-        m = numel(f);
-        n = 0;
-        for j = 1:m
-            n = max(n, numel(f(j).funs));
+
+        if ( numel(out) == 1 )
+            out = out{1};
         end
-        out = cell(n, m);
-        
-        % Loop over each of the columns and each of the funs:
-        for k = 1:m
-            for j = 1:numel(f(k).funs)
-                out{j,k} = get(f(k).funs{j}, prop);
-            end
-        end
-        if ( ~isempty(idx) && min(m, n) == 1 )
+    case 'lval'
+        dom = domain(f);
+        out = feval(f, dom(1));
+    case 'rval'
+        dom = domain(f);
+        out = feval(f, dom(end));
+    case 'lval-local'
+        out = getSimpleNumericLocalProp(f, 'lval', true);
+    case 'rval-local'
+        out = getSimpleNumericLocalProp(f, 'rval', true);
+    case 'vscale-local'
+        out = getSimpleNumericLocalProp(f, 'vscale', true);
+    case 'hscale-local'
+        out = getSimpleNumericLocalProp(f, 'hscale', true);
+    case 'epslevel-local'
+        out = getSimpleNumericLocalProp(f, 'epslevel', true);
+    case {'values', 'coeffs', 'points'}
+        out = getSimpleNumericLocalProp(f, prop, true);
+    case {'exps', 'exponents'}
+        out = getArbitraryLocalProp(f, 'exponents', true);
+        if ( isvector(out) )
             out = cell2mat(out);
         end
-        if ( f(1).isTransposed )
-            out = out.';
+    case 'deltas'
+        out = getArbitraryLocalProp(f, 'deltas', false);
+        for k = 1:numColumns(f)
+            colDeltaData = out{k}.';
+            colDeltaData = horzcat(colDeltaData{:});
+            if ( ~isempty(colDeltaData) )
+                [mag, loc] = deltafun.mergeColumns(colDeltaData(2:end, :), ...
+                    colDeltaData(1, :));
+                out{k} = [loc ; mag];
+            else
+                out{k} = [];
+            end
         end
-        
+
+        if ( numel(out) == 1 )
+            out = out{1};
+        end
+    case 'deltas-local'
+        out = getArbitraryLocalProp(f, 'deltas', true);
+    case 'imps'
+        warning('CHEBFUN:get:imps', ...
+            '''imps'' property is deprecated.  Use ''deltas'' instead.');
+        out = get(f, 'deltas');
     otherwise
-        error('CHEBFUN:CHEBFUN:GET:propname', ...
-            'Unknown property name ''%s'' for object of type CHEBFUN.', prop);
-        
+        error('CHEBFUN:get:badProp', ...
+            '''%s'' is not a recognized CHEBFUN property name.', prop);
+end
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function out = getSimpleNumericLocalProp(f, prop, doSimplify)
+%GETSIMPLENUMERICLOCALPROP   Get simple numeric FUN properties.
+%   OUT = GETSIMPLENUMERICLOCALPROP(F, PROP, DOSIMPLIFY) retrieves the values
+%   of the "simple numeric local" property PROP from each of the FUNs of F and
+%   assembles them in some sensible way.  A property is a "simple numeric
+%   local" property if (1) it is numeric, (2) it is a FUN property, and (3)
+%   either one of the following conditions holds:
+%
+%     A.  If F has N columns, then the property also has N columns, and the
+%     values in column K of the property are associated with column K of F.
+%
+%     B.  The property consists of a single column, the values of which are
+%     associated with _all_ columns of F.
+%
+%   If DOSIMPLIFY is TRUE, the function will try to represent the property in a
+%   simpler form.  The hierarchy of "simplicity", from least simplest to
+%   simplest, is as follows:
+%
+%     1.  Cell array of cell arrays.  out{j}{k} gives the value of the property
+%     for FUN j of column k.
+%
+%     2.  2D cell array.  The output can be simplified to this if all columns
+%     of F have the same number of FUNs.  out{j, k} gives the value of the
+%     property for FUN j of column k.
+%
+%     3.  Numeric matrix.  If the output can be simplified to a 2D cell array,
+%     we can sometimes convert it further into a numeric matrix if the result
+%     can be unambiguously interpreted.  See CANCONVERT2DCELLARRAYTONUMERIC.
+%
+%   The reason this funciton exists is that simple numeric local properties can
+%   be extracted from array-valued CHEBFUNs without needing to convert them to
+%   quasimatrices first, which can potentially save a lot in computational
+%   overhead costs.
+
+% Get properties for each fun and assemble into cell arrays.
+if (numel(f) == 1)  % Single column or array-valued CHEBFUN.
+    out = getSimpleNumericLocalPropArrayValued(f, prop);
+else                % Quasimatrix / array of CHEBFUNs.
+    out = getArbitraryLocalPropQuasimatrix(f, prop);
+end
+
+% Try to convert from a cell array of cell arrays to a 2D cell array.
+if ( doSimplify && canConvertCellOfCellsTo2DCellArray(out) )
+    out = horzcat(out{:});
+
+    % Try to go one step further and convert to a numeric matrix.
+    if ( canConvert2DCellArrayToNumeric(out) )
+        out = cell2mat(out);
+    end
+end
+
+if ( f(1).isTransposed )
+    out = out.';
+end
+
+end
+
+function out = getArbitraryLocalProp(f, prop, doSimplify)
+%GETSIMPLENUMERICLOCALPROP   Get simple numeric FUN properties.
+%   OUT = GETSIMPLENUMERICLOCALPROP(F, PROP, DOSIMPLIFY) retrieves the values
+%   of the property PROP from each of the FUNs of F and %   assembles them in
+%   some sensible way.
+%
+%   If DOSIMPLIFY is TRUE, the function will try to represent the property in a
+%   simpler form.  The hierarchy of "simplicity", from least simplest to
+%   simplest, is as follows:
+%
+%     1.  Cell array of cell arrays.  out{j}{k} gives the value of the property
+%     for FUN j of column k.
+%
+%     2.  2D cell array.  The output can be simplified to this if all columns
+%     of F have the same number of FUNs.  out{j, k} gives the value of the
+%     property for FUN j of column k.
+%
+%     3.  Single cell contents.  If the output can be simplified to a 2D cell
+%     array, with only one cell, we just return the contents of that cell.
+
+% The only way to correctly support access to general properties of
+% array-valued CHEBFUNs is to convert them to quasimatrices first.  (Example:
+% If f is an array-valued CHEBFUN with n columns and has a tech with a property
+% that is a 1 x n row vector, we don't know if each element in that row vector
+% corresponds to a different column or if the whole vector needs to be a
+% property of each column.  Only the tech's MAT2CELL knows how to split the
+% property appropriately amongst the columns.)
+if ( (numel(f) == 1) && (numColumns(f) > 1) )
+    out = getArbitraryLocalProp(cheb2quasi(f), prop);
+    return
+end
+
+out = getArbitraryLocalPropQuasimatrix(f, prop);
+
+% Try to convert from a cell array of cell arrays to a 2D cell array.
+if ( doSimplify && canConvertCellOfCellsTo2DCellArray(out) )
+    out = horzcat(out{:});
+
+    % If our 2D cell array has only one cell (i.e., if f has only one column,
+    % and that column has only fun), return the contents of that cell.
+    if ( numel(out) == 1 )
+        out = out{1};
+    end
+end
+
+if ( f(1).isTransposed )
+    out = out.';
+end
+
+end
+
+function out = getSimpleNumericLocalPropArrayValued(f, prop)
+%GETSIMPLENUMERICLOCALPROPARRAYVALUED   Get simple numeric FUN properties from
+%   an array-valued CHEBFUN.
+%
+%   OUT = GETSIMPLENUMERICLOCALPROPARRAYVALUED(F, PROP) returns a cell array of
+%   cell arrays OUT such that OUT{J}{K} is the value of the simple numeric
+%   local property PROP (see GETSIMPLENUMERICLOCALPROP) for FUN J of column K
+%   of the array-valued CHEBFUN F.
+
+nCols = numColumns(f);
+nFuns = numel(f.funs);
+
+tmp = cell(nFuns, nCols);
+for j = 1:nFuns
+    funPropVal = get(f.funs{j}, prop);
+    [funPropValRows, funPropValCols] = size(funPropVal);
+
+    if ( funPropValCols == 1 )
+        funPropVal = repmat(funPropVal, 1, nCols);
+    elseif ( funPropValCols ~= nCols )
+        error('CHEBFUN:get:notSimple', 'Property is not simple.');
+    end
+
+    tmp(j, :) = mat2cell(funPropVal, funPropValRows, ones(nCols, 1));
+end
+
+out = cell(1, nCols);
+for k = 1:nCols
+    out{k} = vertcat(tmp(:, k));
+end
+
+end
+
+function out = getArbitraryLocalPropQuasimatrix(f, prop)
+%GETARBITRARYLOCALPROPQUASIMATRIX   Get FUN properties from a quasimatrix.
+%   OUT = GETARBITRARYLOCALPROPQUASIAMTRIX(F, PROP) returns a cell array of
+%   cell arrays OUT such that OUT{J}{K} is the value of the local property PROP
+%   for FUN J of column K of the quasimatrix F.
+
+nCols = numColumns(f);
+out = cell(1, nCols);
+for k = 1:nCols
+    nFuns = numel(f(k).funs);
+    out{k} = cell(nFuns, 1);
+    for j = 1:nFuns
+        out{k}{j} = get(f(k).funs{j}, prop);
+    end
+end
+
+end
+
+function canConvert = canConvertCellOfCellsTo2DCellArray(out)
+%CANCONVERTCELLOFCELLSTO2DCELLARRAY   Self-explanatory.
+%   CANCONVERT = CANCONVERTCELLOFCELLTO2DCELLARRAY(OUT) function treturns TRUE
+%   of the cell array of cell arrays OUT can be converted to a 2D cell array
+%   (i.e., if OUT{J} has the same number of columns for each J) and FALSE
+%   otherwise.
+
+    canConvert = ~any(diff(cellfun(@(C) size(C, 1), out)));
+end
+
+function canConvert = canConvert2DCellArrayToNumeric(out)
+%CANCONVERT2DCELLARRAYTONUMERIC   Self-explanatory.
+%   CANCONVERT = CANCONVERT2DCELLARRAYTONUMERIC(OUT) function treturns TRUE of
+%   the 2D cell array OUT can be converted to a numeric matrix.
+
+[nFuns, nCols] = size(out);
+
+% If each column has multiple funs, the property values for each fun must
+% all have only one row.  If each column has only one fun, the property
+% values must all have the same number of rows.
+if ( nFuns > 1 )
+    nRowsNeeded = 1;
+else
+    nRowsNeeded = size(out{1}, 1);
+end
+
+for ( j = 1:1:nFuns )
+    for ( k = 1:1:nCols )
+        if ( size(out{j, k}, 1) ~= nRowsNeeded )
+            canConvert = false;
+            return;
+        end
+    end
+end
+
+canConvert = true;
+
 end
