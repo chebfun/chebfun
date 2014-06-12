5113eaf6d3633e26076485fcd767bb1e73a05799
Nick Hale <nick.p.hale@gmail.com>
2014-06-12 12:38:08 +0200

    Reorganise the way the fixed-length hack is implemented.

--- @unbndfun/sum.m
+++ @unbndfun/sum.m
@@ -11,7 +11,7 @@ function out = sum(g)
 % Get the domain.
 dom = g.domain;
 
-%% Cancel vanishing boundary values with negative exponents:
+% Cancel vanishing boundary values with negative exponents:
 g.onefun = cancelExponents(g.onefun);
 
 % Get the function values at the end of the domain. Note that the end point of
@@ -38,28 +38,30 @@ end
 
 % Check 2: Check the speed of decay at infinity/ties. The integrand is
 % integrable only when it decays faster than 1/x towards infinity/ties.
-decay = isdecay(g.onefun);
-maskInf = isinf(dom);
-if ( any(~decay & maskInf) )
+if ( any(~isdecay(g.onefun) & isinf(dom)) )
     warning('CHEBFUN:UNBNDFUN:sum:slowdecay', ...
         ['Result may not be accurate ' ...
-        'as the function decays slowly at infinity.'])
+         'as the function decays slowly at infinity.'])
 end
 
 % If we reach here, the function decays sufficiently fast. Construct a ONEFUN
 % for the integrand and integrate it.
-%
-% TODO:  Using an exact-length construction here is a hack.  It only works if
-% g.onefun is a chebtech, and, even then, the idea that it "works" is merely
-% heuristic; there's no a priori reason that it should.  We really would like
-% to do an adaptive construction, but the fact that the nonlinear map
-% compresses very wide intervals near +/-Inf into very small ones near +/-1,
-% means that the filtered function produced by unbndfunIntegrand will appear to
-% exhibit sharp transitions to zero when sampled on a Chebyshev grid of the
-% usual sizes, causing the constructor to fail.  Until we can solve this
-% problem, there doesn't seem to be much else we can do here.
-techPrefs.exactLength = length(g);
+
 techConstructor = get(g.onefun, 'techConstructor');
+if ( isa(techConstructor(), 'chebtech') )
+    techPrefs.exactLength = length(g);
+    % TODO: Using an exact-length construction here is a hack. It only works if
+    % g.onefun is a CHEBTECH, and, even then, the idea that it "works" is merely
+    % heuristic; there's no a priori reason that it should. We really would like
+    % to do an adaptive construction, but the fact that the nonlinear map
+    % compresses very wide intervals near +/-Inf into very small ones near +/-1,
+    % means that the filtered function produced by unbndfunIntegrand() will
+    % appear to exhibit sharp transitions to zero when sampled on a Chebyshev
+    % grid of the usual sizes, causing the constructor to fail. Until we can
+    % solve this problem there doesn't seem to be much else we can do here.
+else
+    techPrefs = [];
+end
 integrand = techConstructor(@(x) unbndfunIntegrand(x, g), [], techPrefs);
 out = sum(integrand);
 
