4e995be09c7f948c86588b86e51eb32e704f2a54
Townsend <townsend@maths.ox.ac.uk>
2014-06-12 09:26:15 +0100

    Adding a test to check that Chebfun2 can do repeated arithmetic. (Plus is a difficult operation in Chebfun2.)

---
+++ tests/chebfun2/test_repeatedArithmetic.m
@@ -0,0 +1,39 @@
+function pass = test_repeatedArithmetic( prefs )
+% In version 4 repeated arithmetric lost quite a bit of accurate. 
+% With PLUS based on QR, we are in better shape. Test this. 
+
+if ( nargin < 1 ) 
+    prefs = chebfunpref(); 
+end
+tol = 1e4*prefs.cheb2Prefs.eps; 
+
+% Add 100 functions together: 
+f = chebfun2(@(x,y) cos(x.*y)); 
+g = chebfun2(0);
+for jj = 1:100 
+    g = g + f; 
+end
+pass(1) = ( norm( g - 100*f ) < 10 * tol ); 
+
+% Multiply 10 functions together: 
+f = chebfun2(@(x,y) cos(x.*y)); 
+g = f;
+for jj = 1:10 
+    g = g .* f; 
+end
+pass(2) = ( norm( g - f.^11 ) < tol );
+
+% Multiply 30 functions together: 
+f = chebfun2(@(x,y) sin(x.*y)); 
+g = f;
+for jj = 1:30 
+    g = g .* f; 
+end
+pass(3) = ( norm( g - f.^31 ) < tol );
+
+% Near cancellation not causing problems: 
+f = chebfun2(@(x,y) cos(x.*y)); 
+g = f + 1e-15*chebfun2(@(x,y) sin(x.*y.^2)); 
+pass(4) = ( norm( f - g ) <  tol );
+
+end
