ce0d56932c8ae46fb1d2367888e430502bc8f688
Townsend <townsend@maths.ox.ac.uk>
2014-06-13 10:49:24 +0100

    Tidy up minandmax2.

--- @chebfun2/minandmax2.m
+++ @chebfun2/minandmax2.m
@@ -4,8 +4,9 @@ function [Y, X] = minandmax2( f )
 %   its domain. Y is a vector of length 2 such that Y(1) = min(f(x,y)) and Y(2)
 %   = max(f(x,y)).
 %
-%   [Y, X] = minandmax2(F) also returns the position of the minimum and maximum.
-%   That is,
+%   [Y, X] = minandmax2(F) also returns the position of the minimum and 
+%   maximum. For example,
+%
 %       F(X(1,1),X(1,2)) = Y(1)     and      F(X(2,1),X(2,2)) = Y(2)
 %
 % See also MAX2, MIN2, NORM.
@@ -13,7 +14,8 @@ function [Y, X] = minandmax2( f )
 % Copyright 2014 by The University of Oxford and The Chebfun Developers.
 % See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
 
-if ( isempty( f ) ) % check for empty CHEBFUN2.
+% check for empty CHEBFUN2.
+if ( isempty( f ) )
     Y = []; 
     X = [];
     return
