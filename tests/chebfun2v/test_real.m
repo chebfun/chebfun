73199d5fdb5642db655954eacf35baf40ea91126
Nick Hale <nick.p.hale@gmail.com>
2014-06-15 15:38:23 +0200

    Tweaking some tolerances.

--- tests/chebfun2v/test_real.m
+++ tests/chebfun2v/test_real.m
@@ -5,7 +5,7 @@ if ( nargin == 0 )
     pref = chebfunpref; 
 end
 
-tol = 10*pref.cheb2Prefs.eps; 
+tol = 100*pref.cheb2Prefs.eps; 
 
 f = chebfun2v(@(x,y) cos(x.*y),@(x,y) cos(x.*y)); 
 g = real( f ); 
@@ -13,11 +13,11 @@ pass(1) = ( norm( f - g ) < tol );
 
 f = chebfun2v(@(x,y) cos(x.*y),@(x,y) cos(x.*y)); 
 g = real( 1i*f ); 
-pass(2) = ( norm( g ) < 100*tol ); 
+pass(2) = ( norm( g ) < tol ); 
 
 f1 = chebfun2v(@(x,y) cos(x.*y),@(x,y) cos(x.*y)); 
 f2 = chebfun2v(@(x,y) sin(x + y.^2),@(x,y) sin(x + y.^2)); 
 g = real( f1 + 1i*f2 ); 
-pass(3) = ( norm( f1 - g ) < 100*tol ); 
+pass(3) = ( norm( f1 - g ) < tol ); 
 
 end
