25c4d2e34242229671bc824409d71f5b7acb5432
Hadrien Montanelli <montanelli@maths.ox.ac.uk>
2014-06-17 14:31:35 +0100

    Tidy SOLVEPDE and DISPLAY.

--- @chebop2/display.m
+++ @chebop2/display.m
@@ -1,4 +1,4 @@
-function display( N )
+function display(N)
 %DISPLAY   Print to command line for CHEBOP2.
 % DISPLAY is called automatically when a statement that results in a
 % CHEBOP2 output is not terminated with a semicolon.
@@ -6,34 +6,33 @@ function display( N )
 % Copyright 2014 by The University of Oxford and The Chebfun Developers.
 % See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
 
-loose = ~isequal(get(0,'FormatSpacing'),'compact');
+loose = ~isequal(get(0, 'FormatSpacing'), 'compact');
 
 if ( loose ) 
     fprintf('\n') 
 end
 
-% compact version
-if ( isempty( N ) )
+% Compact version.
+if ( isempty(N) )
     fprintf('    empty chebop2\n\n')
     return
 end
 
-% Get information that we want to display
-dom = N.domain;                           % Domain
+% Get information that we want to display.
+dom = N.domain;                           
 op = N.op; 
 
-% Display the information: 
-
+% Display the information.
 disp('chebop2 object: (partial differential equation)')
 fprintf('         domain               operator  \n');
 
 fprintf(['[%4.2g,%4.2g] x [%4.2g,%4.2g]    ' func2str(op)  '\n'], dom(1),...
                                           dom(2), dom(3), dom(4));
      
-PrintBoundaryConditions(N.lbc, 'left');  
-PrintBoundaryConditions(N.rbc, 'right');
-PrintBoundaryConditions(N.ubc, 'top');
-PrintBoundaryConditions(N.dbc, 'bottom');
+printBoundaryConditions(N.lbc, 'left');  
+printBoundaryConditions(N.rbc, 'right');
+printBoundaryConditions(N.ubc, 'top');
+printBoundaryConditions(N.dbc, 'bottom');
 
 if ( loose ) 
     fprintf('\n');
@@ -41,8 +40,8 @@ end
 
 end
 
-function PrintBoundaryConditions(BC, string)
-% Display the boundary conditions: 
+function printBoundaryConditions(BC, string)
+% Display the boundary conditions.
 
     exists = ~isempty(BC);
     if ( exists )
@@ -53,7 +52,7 @@ function PrintBoundaryConditions(BC, string)
             n = length(BC); 
             if ( n == 1 )
                 val = BC.values{:};
-                PrintBoundaryConditions(val, string)
+                printBoundaryConditions(val, string)
                 return
             end
             fprintf(['boundary ' string ' = function']);
@@ -62,4 +61,5 @@ function PrintBoundaryConditions(BC, string)
         end
         fprintf('\n');
     end
+    
 end
--- @chebop2/solvepde.m
+++ @chebop2/solvepde.m
@@ -1,19 +1,18 @@
-function u = solvepde(N, f, varargin )
+function u = solvepde(N, f, varargin)
 %SOLVEPDE   Solve CHEBOP2 partial differential equation.
+%    U = SOLVEPDE(N, F) is equivalent to U = N \ F and solves the linear partial
+%    differential equation N(U) = F. 
+% 
+%    U = SOLVEPDE(N, F, M, N) solves the PDE with a discretization size of
+%    M-by-N. 
+% 
+%    U = SOLVEPDE(N, F, M, INF) solves the PDE adaptively in the 2nd variable
+%    and a discretization size of M in the 1st. 
+% 
+%    U = SOLVEPDE(N, F, INF, N) solves the PDE adaptively in the 1st variable
+%    and a discretization size of N in the 2nd. 
 %
-% U = SOLVEPDE(N, F) is equivalent to U = N \ F and solves the linear partial
-% differential equation N(U) = F. 
-% 
-% U = SOLVEPDE(N, F, M, N) solves the PDE with a discretization size of
-% M-by-N. 
-% 
-% U = SOLVEPDE(N, F, M, INF) solves the PDE adaptively in the 2nd variable
-% and a discretization size of M in the 1st. 
-% 
-% U = SOLVEPDE(N, F, INF, N) solves the PDE adaptively in the 1st variable
-% and a discretization size of N in the 2nd. 
-%
-% U = SOLVEPDE(N, F, INF, INF) is equivalent to U = SOLVEPDE(N, F).
+%    U = SOLVEPDE(N, F, INF, INF) is equivalent to U = SOLVEPDE(N, F).
 %
 % For further details about the PDE solver, see: 
 % A. Townsend and S. Olver, The automatic solution of partial differential
@@ -25,27 +24,27 @@ function u = solvepde(N, f, varargin )
 % Copyright 2014 by The University of Oxford and The Chebfun2 Developers.
 % See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
 
-% Try and make a chebfun2 out of right hand side.
-if isa(f,'chebfun')
+% Try and make a CHEBFUN2 out of right hand side.
+if isa(f, 'chebfun')
     f = @(x,y) f(x) + 0*y;
     warning('Chebop2:mldivide:rhs','Univariate righthand side.');
 end
-if isa(f,'double')
+if isa(f, 'double')
     f = @(x,y) f + 0*x;
 end
 
-%%% Go get stuff we will need: 
+% Go get stuff we will need: 
 rect = N.domain; 
 f = chebfun2(f, rect);
 prefs = chebfunpref();
-tol = max( prefs.techPrefs.eps, 1e-14 );     % Be gentle!    
-maxDiscretise_x = 2*prefs.cheb2Prefs.maxRank; % Use maxRank to get max disc 
+tol = max(prefs.techPrefs.eps, 1e-14); % Be gentle!    
+maxDiscretise_x = 2*prefs.cheb2Prefs.maxRank; % Use maxRank to get maxDisc. 
 maxDiscretise_y = maxDiscretise_x;
 minsample = 9;
 
-%%% Find out what grid to start on and which directions to do adaptivity. %%%
+% Find out what grid to start on, and which directions to do adaptivity.
 if ( nargin == 3 && isa(varargin{1}, 'double') )
-    % Nonadaptive solve with an m-by-m discretization
+    % Nonadaptive solve with an m-by-m discretization.
     m = varargin{1};
     n = m;
     adaptive_x = 0;
@@ -54,7 +53,7 @@ if ( nargin == 3 && isa(varargin{1}, 'double') )
     maxDiscretise_y = m + 1; 
 elseif ( nargin == 4 && isa(varargin{1},'double') && ~isinf(varargin{1}) ...
         && isa(varargin{2},'double') && ~isinf(varargin{2}) )
-    % Nonadaptive solve with an m-by-n discretization
+    % Nonadaptive solve with an m-by-n discretization.
     m = varargin{1};
     n = varargin{2};
     adaptive_x = 0;
@@ -79,7 +78,7 @@ elseif ( nargin == 4 && isa(varargin{2},'double') && ~isinf(varargin{2})...
     adaptive_x = 0;
     adaptive_y = 1;
     maxDiscretise_x = n + 1;
-elseif ( nargin == 2 || (nargin == 4 && isinf(varargin{2}) && isinf(varargin{1})))
+elseif ( nargin == 2 || ( nargin == 4 && isinf(varargin{2}) && isinf(varargin{1}) ) )
     % This code does the solver with adaptive calls in both the x- and
     % y-direction.
     n = minsample;
@@ -87,16 +86,19 @@ elseif ( nargin == 2 || (nargin == 4 && isinf(varargin{2}) && isinf(varargin{1})
     adaptive_x = 1;
     adaptive_y = 1;
 else
-    error('CHEBOP2:MLDIVIDE:SYNTAX','Unrecognized input syntax')
+    error('CHEBOP2:MLDIVIDE:SYNTAX', 'Unrecognized input syntax.')
 end
 
-% Check the discreizations make sense: 
-if ( abs( round(n) - n ) > 0 || abs( round(m) - m ) > 0)
-    error('CHEBOP2:MLDIVIDE:DISC','Discretization size should be an integer');
+% Check if the discreizations make sense: 
+if ( abs(round(n) - n) > 0 || abs(round(m) - m) > 0)
+    error('CHEBOP2:MLDIVIDE:DISC', 'Discretization size should be an integer.');
 end
 
-%%% Adaptive solver %%%
-Resolved_x = 0; Resolved_y = 0; Resolved = Resolved_x & Resolved_y;
+% Adaptive solver.
+Resolved_x = 0; 
+Resolved_y = 0; 
+Resolved = Resolved_x & Resolved_y;
+
 while ( ( ~Resolved ) && ( m < maxDiscretise_y ) &&...
                          ( n < maxDiscretise_x ) )
                      
@@ -105,52 +107,54 @@ while ( ( ~Resolved ) && ( m < maxDiscretise_y ) &&...
     
     if ( adaptive_y ) 
         % Resolved in y-direction?
-        [Resolved_y, m] = ResolveCheck( X, m, tol );
+        [Resolved_y, m] = resolveCheck(X, m, tol);
     else
         Resolved_y = 1; 
     end
     
     if ( adaptive_x ) 
         % Resolved in x-direction?
-        [Resolved_x, n] = ResolveCheck( X.', n, tol );
+        [Resolved_x, n] = resolveCheck(X.', n, tol);
     else 
         Resolved_x = 1; 
     end
     
-    % update tolerances:
+    % Update tolerances:
     tol = updateTolerance(tol, m, n);
     
     % Resolved in both directions:
     Resolved = Resolved_x & Resolved_y;
     
-    % Check we do not have nans:
-    if ( any( isnan( X ) ) | any( isinf( X ) ) )
-        error('CHEBOP2:MLDIVIDE:NANINF', 'Nonunique solution to PDE')
+    % Check we do not have NANs/INFs:
+    if ( any(isnan(X)) || any(isinf(X)) )
+        error('CHEBOP2:MLDIVIDE:NANINF', 'Nonunique solution to PDE.')
     end
+    
 end
 
 % Did we stop without resolving:
 if ( ( m >= maxDiscretise_y ) || ( n >= maxDiscretise_x ) )
     warning('CHEBOP2:MAXDISC',...
-        'Maximum discretization reached. Solution may not accurate.')
+        'Maximum discretization reached. Solution may not be accurate.')
 end
 
 % Truncate at the end:
-X = cutTrailingCoefficients( X );
+X = cutTrailingCoefficients(X);
 
-% Form a chebfun2 object to represent the solution:
-u = chebfun2( rot90( X, 2 ), 'coeffs', rect); 
+% Form a CHEBFUN2 object to represent the solution:
+u = chebfun2(rot90(X, 2), 'coeffs', rect); 
+
 end
 
-function [Resolved, newDisc] = ResolveCheck( coeffs, oldDisc, tol )
+function [Resolved, newDisc] = resolveCheck(coeffs, oldDisc, tol)
 % Basic Resolution check:
 
 tail = max(abs(coeffs(:,end-8:end)));
-Resolved = all( ( tail < 20*oldDisc*tol ) );
+Resolved = all(tail < 20*oldDisc*tol);
 
 % If unresolved, then increase the grid:
 if ( ~Resolved )
-    newDisc = 2^(floor(log2(oldDisc))+1)+1;
+    newDisc = 2^(floor(log2(oldDisc))+1) + 1;
 else
     newDisc = oldDisc;
 end
@@ -164,16 +168,18 @@ function tol = updateTolerance(tol, m, n)
 if ( max(m, n) > 250 )
     tol = max(tol, 1e-10);
 end
+
 end
 
-function X = cutTrailingCoefficients( X )
-%TODO:
-%     % cut trailing coefficients
-%     idy = find( max(abs(X))/max(abs(X(:))) > eps, 1, 'last');
-%     idx = find( max(abs(X),[],2)/max(abs(X(:))) > eps, 1, 'last');
-%     if isempty(idx) && ~isempty(idy)
+function X = cutTrailingCoefficients(X)
+%[TODO]: Do we want to do this? Bad effects for hyperbolic/parabolic PDEs.
+%
+%     % Cut trailing coefficients.
+%     idy = find(max(abs(X))/max(abs(X(:))) > eps, 1, 'last');
+%     idx = find(max(abs(X), [], 2)/max(abs(X(:))) > eps, 1, 'last');
+%     if ( isempty(idx) && ~isempty(idy) )
 %         X = X(1:idy,:);
-%     elseif ~isempty(idx) && isempty(idy)
+%     elseif ( ~isempty(idx) && isempty(idy) )
 %         X = X(:,1:idx);
 %     else
 %         X = X(1:idy,1:idx);
