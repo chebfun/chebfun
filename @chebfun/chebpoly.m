94fba6339effe6795f75a4c742319949e1cb7fbb
Nick Hale <nick.p.hale@gmail.com>
2014-06-18 14:10:58 +0200

    Adding a deprecated CHEBPOLY().

---
+++ @chebfun/chebpoly.m
@@ -0,0 +1,16 @@
+function varargout = chebpoly(varargin)
+%CHEBPOLY   Chebyshev polynomial coefficients
+%   CHEBPOLY(F) is deprecated. Please use CHEBCOEFFS().
+%
+% See also PLOTCOEFFS.
+
+% Copyright 2014 by The University of Oxford and The Chebfun Developers.
+% See http://www.chebfun.org/ for Chebfun information.
+
+warning('CHEBFUN:chebpoly:deprecated', ...
+    'CHEBPOLY is deprecated. Please use CHEBCOEFFS instead.');
+warning('off', 'CHEBFUN:CHEBFUN:chebpoly:deprecated');
+
+[varargout{1:nargout}] = chebcoeffscoeffs(varargin{:});
+
+end
