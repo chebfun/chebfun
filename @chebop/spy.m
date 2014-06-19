75126df4c50efb6a0d2d305ec671490b88daa72d
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2014-06-19 16:00:36 +0100

    First go at v4 spy plot for CHEBOPs.

--- @chebop/spy.m
+++ @chebop/spy.m
@@ -1,7 +1,12 @@
-function spy(N, varargin)
+function spy(A, c)
 %SPY    Visualize a linear CHEBOP.
-%   SPY(A) creates a picture of the nonzero pattern of the default
-%   discretization of the linear CHEBOP A. See LINOP/SPY for details.
+%   SPY(A) creates an infographic picture of the structure of the linear CHEBOP
+%   A.
+%
+%   SPY(A, C) is the same above, using the colour C.
+%
+%   To see the actual nonzero pattern of A under the current discretization, use
+%   SPY(LINOP(A)).
 %
 % See also LINOP/SPY.
 
@@ -9,9 +14,43 @@ function spy(N, varargin)
 % See http://www.chebfun.org/ for Chebfun information.
 
 % Try to create a LINOP. An error will be thrown if n is not linear.
-L = linop(N);
+A = linop(A);
 
-% SPY plot of the LINOP:
-spy(L, varargin{:});
+dom = A.domain;
+ends = dom;
+% ends = A.domain.endsandbreaks;
+de = dom(end)-dom(1);
+LW = 'linewidth'; lw = 3;
+C = 'color'; EC = 'EdgeColor';
+if nargin < 2, c = 'b'; end
+
+N = 3;
+ish = ishold;
+
+Ab = A.blocks;
+for j = 1:size(Ab, 1)
+    for k = 1:size(Ab, 2)
+        isz = iszero(Ab{j, k});
+        isd = ~isz && Ab{j,k}.diffOrder == 0;
+        if ~A.iszero(j,k)
+            if isd
+                plot(ends([end 1])+(k-1)*de,-ends([end 1])-(j-1)*de, ...
+                    LW, lw, C, c);
+                hold on
+            else
+                for l = 1:numel(ends)-1
+                    fill(ends([l+1 l l l+1])+(k-1)*de, ...
+                        -ends([l+1 l+1 l l])-(j-1)*de, c, EC, c); 
+                    hold on
+                end
+            end
+        end
+    end
+end
+
+set(gca,'xTick',[])
+set(gca,'yTick',[])
+
+if ~ish, hold off, axis equal, axis tight, end
 
 end
