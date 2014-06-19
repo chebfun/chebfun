7331b5c1ae5f09da61a30a802cba8e09db278ceb
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2014-06-19 17:04:00 +0100

    Tidying up of code for @chebop/spy.

--- @chebop/spy.m
+++ @chebop/spy.m
@@ -1,9 +1,7 @@
-function spy(A, c)
+function spy(A)
 %SPY    Visualize a linear CHEBOP.
 %   SPY(A) creates an infographic picture of the structure of the linear CHEBOP
 %   A.
-%
-%   SPY(A, C) is the same above, using the colour C.
 %
 %   To see the actual nonzero pattern of A under the current discretization, use
 %   SPY(LINOP(A)).
@@ -16,31 +14,34 @@ function spy(A, c)
 % Try to create a LINOP. An error will be thrown if n is not linear.
 A = linop(A);
 
+% Domain information:
 dom = A.domain;
-ends = dom;
-% ends = A.domain.endsandbreaks;
-de = dom(end)-dom(1);
-LW = 'linewidth'; lw = 3;
-C = 'color'; EC = 'EdgeColor';
-if nargin < 2, c = 'b'; end
+domDiff = dom(end) - dom(1);
 
-N = 3;
+% Were we holding when we entered this method?
 ish = ishold;
 
+% Begin by looping through the DE blocks:
 Ab = A.blocks;
 for j = 1:size(Ab, 1)
     for k = 1:size(Ab, 2)
-        isz = iszero(Ab{j, k});
-        isd = ~isz && Ab{j,k}.diffOrder == 0;
-        if ~A.iszero(j,k)
-            if isd
-                plot(ends([end 1])+(k-1)*de,-ends([end 1])-(j-1)*de, ...
-                    LW, lw, C, c);
+        % Is the {j,k} block a zero operator?
+        isZero = iszero(Ab{j, k});
+        % Is the {j,k} block a multiplication operator?
+        isMult = (~isZero && ( Ab{j,k}.diffOrder == 0) );
+        if ~isZero
+            if isMult
+                % Draw a diagonal line for multiplication operators.
+                plot(dom([end 1]) + (k - 1)*domDiff, ...
+                    -dom([end 1]) - (j - 1)*domDiff, ...
+                    'Linewidth', 3, 'Color', 'b');
                 hold on
             else
-                for l = 1:numel(ends)-1
-                    fill(ends([l+1 l l l+1])+(k-1)*de, ...
-                        -ends([l+1 l+1 l l])-(j-1)*de, c, EC, c); 
+                % Drow a filled rectangle for other operators.
+                for l = 1:numel(dom)-1
+                    fill(dom([l+1 l l l+1]) + (k - 1)*domDiff, ...
+                        -dom([l+1 l+1 l l]) - (j - 1)*domDiff, ...
+                        'b', 'EdgeColor', 'b'); 
                     hold on
                 end
             end
@@ -48,9 +49,17 @@ for j = 1:size(Ab, 1)
     end
 end
 
+% TODO: Plot lines for constraints?
+
+% Remove the ticks from the axes
 set(gca,'xTick',[])
 set(gca,'yTick',[])
 
-if ~ish, hold off, axis equal, axis tight, end
+% If we were not holding, make the plot look nice
+if ~ish
+    hold off
+    axis equal
+    axis tight
+end
 
 end
