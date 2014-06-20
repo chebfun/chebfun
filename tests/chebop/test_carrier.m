be079a7e393b4138f0b065960d30caeec55cb615
Toby Driscoll <driscoll@udel.edu>
2014-06-20 18:34:27 -0400

    All discretizations for carrier.

--- tests/chebop/test_carrier.m
+++ tests/chebop/test_carrier.m
@@ -11,30 +11,34 @@ pref.errTol = tol;
 dom = [-1 1];
 
 %%
-N = chebop(@(x,u) 0.01.*diff(u,2)+2.*(1-x.^2).*u+u.^2-1, dom);
-rhs = 0;
-N.bc = @(x,u) [u(-1); u(1)];
-x = chebfun(@(x) x, dom);
-N.init =  2.*(x.^2-1).*(1-2./(1+20.*x.^2));
-
-u = solvebvp(N, rhs, pref);
-
-xx = (-1:.25:1)';
-hiquality_ans = [
-                  0
-  -1.487429807540814
-  -1.785617248281071
-   1.572366197526305
-  -1.539652044363185
-   1.572366197526230
-  -1.785617248281089
-  -1.487429807540795
-                   0
-                   ];
-               
-pass(1) = norm(u(xx) - hiquality_ans) < tol;
-pass(2) = norm(u([-1 1])) < tol ;
+discs = {@colloc1,@ultraS,@colloc2};
+for disc = 1:3
+    pref.discretization = discs{disc};
+    N = chebop(@(x,u) 0.01.*diff(u,2)+2.*(1-x.^2).*u+u.^2-1, dom);
+    rhs = 0;
+    N.bc = @(x,u) [u(-1); u(1)];
+    x = chebfun(@(x) x, dom);
+    N.init =  2.*(x.^2-1).*(1-2./(1+20.*x.^2));
+    
+    u = solvebvp(N, rhs, pref);
+    
+    xx = (-1:.25:1)';
+    hiquality_ans = [
+        0
+        -1.487429807540814
+        -1.785617248281071
+        1.572366197526305
+        -1.539652044363185
+        1.572366197526230
+        -1.785617248281089
+        -1.487429807540795
+        0
+        ];
+    
+    pass(2*disc-1) = norm(u(xx) - hiquality_ans) < tol;
+    pass(2*disc) = norm(u([-1 1])) < tol ;
+end
 
 end
 
-              
+
