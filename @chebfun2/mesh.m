64b3274f62358d651d5fa2a7dd14f9b4d60957e6
Townsend <townsend@maths.ox.ac.uk>
2014-07-02 14:14:50 +0100

    Adding a MESH() command in Chebfun2, so that an appropriate error is thrown when a user tries mesh( f ).

---
+++ @chebfun2/mesh.m
@@ -0,0 +1,11 @@
+function varargout = mesh( f, varargin )
+%MESH   3-D mesh surface of a chebfun2.
+%
+% This is NOT supported so returns an error.
+
+% Copyright 2013 by The University of Oxford and The Chebfun Developers.
+% See http://www.maths.ox.ac.uk/chebfun/ for Chebfun information.
+
+error('CHEBFUN2:MESH', 'This is not supported by Chebfun2.'); 
+
+end
