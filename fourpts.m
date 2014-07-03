526e4b6d628f28be777ea10946a715e03b7ad410
Hadrien Montanelli <montanelli@maths.ox.ac.uk>
2014-07-03 19:01:51 +0100

    Make possible [x w] = FOURPTS(N).

--- fourpts.m
+++ fourpts.m
@@ -1,14 +1,25 @@
-function x = fourpts(n, dom)
+function [x, w] = fourpts(n, dom)
 %FOURPTS    Fourier points.
 %   FOURPTS(N) returns N equispaced points in [-1, 1).
 %
 %   FOURPTS(N, D), where D is vector of length 2 and N is a scalar integer,
 %   returns N equispaced points in the interval [D(1),D(2)). 
 %
+%   [X, W] = FOURPTS(N) or [X, W] = FOURPTS(N, D) returns also a row vector 
+%   of the weights for the trapezoidal rule.
+%
 % See also CHEBPTS, LEGPTS, JACPTS, LAGPTS, and HERMPTS.
 
 % Copyright 2014 by The University of Oxford and The Chebfun Developers. 
 % See http://www.chebfun.org/ for Chebfun information.
+
+% Special case (no points).
+if ( n <= 0 )     
+    x = []; 
+    w = [];
+    return
+    
+end
 
 x = linspace(-pi, pi, n+1).';
 x = x./pi;
@@ -16,10 +27,15 @@ x(end) = [];
 
 % Parse inputs:
 if ( nargin == 1 )
-    dom = [-1, 1]; 
+    dom = [-1, 1];
 end
 
-% Map to the right domain: 
-x = diff(dom(1:2))*x/2 + mean(dom(1:2)); 
-    
+% Map to the right domain:
+x = diff(dom(1:2))*x/2 + mean(dom(1:2));
+
+% Scaled quadrature weights:
+if ( nargout > 1 )
+    w = fourtech.quadwts(n) * diff(dom(1:2))/2;
+end
+
 end
