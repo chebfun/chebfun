aa493bf8dd9a453d80104651fd5b2afcd3b06c40
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2014-07-16 13:06:59 +0100

    Initial import of path-following methods.

---
+++ @chebop/followPath.m
@@ -0,0 +1,130 @@
+function [uquasi, lamvec, mvec] = followPath(H, A, g, BCstruct, u0, lam0, measure, varargin)
+% H  -- chebop
+% bcFun -- anonymous function
+% u0 -- initial solution on path
+% lam0 -- initial value of lambda
+% measure -- anonymous function which value we plot
+% varargin -- options
+
+% Set default values
+plotOn = 0; % Option for plotting
+sl0 = .1; % Initial steplength
+slmax = 4; slmin = 0.0001; % Maximum/min steplength
+maxCounter = 7;
+
+% Parse varargin
+while ~isempty(varargin)  % Recurse
+    if ~ischar(varargin{1}) && ~isnumeric(varargin{2})
+        error('followpath:inputArgument','Incorrect input arguments');
+    end
+    val = varargin{2};
+    switch varargin{1}
+        case 'plotOn'
+            plotOn = val;
+        case 'sl0'
+            sl0 = val;
+        case 'maxCounter'
+            maxCounter = val;
+        case 'slmax'
+            slmax = val;
+    end
+    
+    % Throw away arguments and move on
+    varargin(1:2) = [];
+end
+
+
+% Store all the solutions to be returned
+uquasi = u0;
+
+
+% Constraint for tangent
+J = @(u,lam) sum(u).^2+lam.^2;
+
+% Iterate along path.
+% Begin by finding a tangent direction, then set steplength, then compute
+% Newton correction, and repeat.
+counter = 1;
+uold = u0; lamold = lam0;
+if isa(lam0,'chebconst')
+    lamvec = lam0.vals;
+else
+    lamvec = lam0;
+end
+mvec = measure(u0); sl = sl0;
+
+% Before starting path following, plot initial information
+% Obtain a nice string to set on ylabel of bifurcation diagram
+mstring = func2str(measure);
+mstring = mstring(min(strfind(mstring,')'))+1:end); % Throw away the @(u) part
+% mstring = 
+if plotOn
+    subplot(1,2,1);
+    plot(u0), title(['Solution for \lambda =' num2str(lamvec)]), xlabel('x'),ylabel('u(x)')
+    subplot(1,2,2)
+    plot(lamvec,mvec,'-*')
+    title('Bifurcation diagram'), xlabel('\lambda'), ylabel(mstring)
+    drawnow, shg
+end
+
+% Set up initial tangent and tau we want to be orthogonal to.
+told = chebfun(0,domain(u0)); tauold = 1;
+
+retract = 0; % retract == 1 if Newton told us to go back along the tangent.
+fprintf('No. path iter    Newton iter   Steplength\n')
+fprintf('-------------------------------------------\n')
+while counter <= maxCounter
+    % Find a tangent direction, but only if we were told by Newton not to
+    % retract
+    if ~retract
+        [t, tau] = tangentBVP(H,A,g,BCstruct,uold, lamold, told,tauold);
+        % Move in the direction of the tangent
+        uinit = uold+sl*t;
+        laminit = lamold+sl*tau;
+    end
+
+    % Find a Newton correction
+    [u, lam, iter, retract] = newtonBVP(H,A,g,BCstruct,uinit,laminit,t,tau);
+    
+    if retract % Newton told us we were trying to take too long tangent steps
+        disp('retracted')
+        % Move in the direction of the current tangent, but only with
+        % quarter of the steplength
+        sl = sl/4;
+        uinit = uold+sl*t;
+        laminit = lamold+sl*tau;       
+        if sl < slmin
+            disp('FAILED: sl < slmin')
+            return
+        end
+        continue
+    end
+    fprintf('%7i \t   %2i \t\t %6.4f \n',counter,iter,sl)    
+    
+    % Store values for plotting
+    mvec = [mvec; measure(u)]; lamvec = [lamvec;lam];
+
+    if plotOn
+        subplot(1,2,1);
+        plot(u),title(['Solution for \lambda =' num2str(lam)]), xlabel('x'),ylabel('u(x)')
+        subplot(1,2,2)
+        plot(lamvec,mvec,'-*'), 
+        title('Bifurcation diagram'), xlabel('\lambda'), ylabel(mstring)
+        drawnow, shg
+    end
+    
+    counter = counter + 1;
+    if iter >= 5
+        sl = max(sl/2,slmin); % Half steplength
+    else
+        sl = min(sl*2,slmax); % Try to increase steplength
+    end
+    
+    % If successful, update old values
+    told = t; tauold = tau; uold = u; lamold = lam;
+    
+    % Update quasimatrix to be returned
+    uquasi = [uquasi, u];
+    
+end
+
---
+++ @chebop/tangentBVP.m
@@ -0,0 +1,72 @@
+function [t,tau] = tangentbvp(H,A,g,BCstruct,u,lambda,told,tauold)
+
+% Find a tangent to the curve H(u,lambda)=0 at the given point, by solving
+% a boundary-value problem.
+
+bcFun = H.bc;
+
+% if nargin < 7
+%     bcFun = [];
+% end
+
+d = domain(u);
+x = chebfun(@(x) x, d);
+
+% Differentiate to get H' = [A | f ]. 
+lam = lambda;   % to get autodiff data
+v = H.op(x,u,lam);              % should be zero, approximately!
+
+Au = A(u,lam);
+gu = g(u,lam);
+% A = diff(v,u,'linop');
+% g = diff(v,lam);
+
+% u = set(u,'funreturn',1);
+
+% Create a superlinop for the constraint which goes at the bottom
+Jm = [told'*diag(sum(d)) tauold];
+
+% Jul = J(u,lam);
+
+% Jlin = diff(Jul,u,'linop');
+% m = 1;%diff(Jul,lam);
+% m = diff(Jul,lam);
+S = linop([Au gu;Jm]);
+
+% Need to linearise and obtain superlinops to use for BCs if bcFun is not
+% empty
+if ~isempty(bcFun)
+    % Evaluate bcFun
+%     bcVals = bcFun(x,u,lam);
+    % Differentiate each result to get a superlinop, consisting of a row
+    % linop and a scalar
+        
+    % Assign rhs of BCs. The op fields have been passed down.
+    S = addConstraint(S, BCstruct(1,:), 0);
+    S = addConstraint(S, BCstruct(2,:), 0);
+    
+%     for bcCounter = 1:numel(bcVals)
+%        rowlin = diff(bcVals(:,bcCounter),u,'linop');
+%        rowsca = diff(bcVals(:,bcCounter),lam,'linop');
+%        % Convert each part into a superlinop
+%        rowop = [rowlin rowsca];
+%        rowval = bcVals(:,bcCounter).vals;
+%        BCstruct.other(bcCounter).op = rowop;
+%        BCstruct.other(bcCounter).val = 0; % Tangent problem always has conditions == 0
+%     end
+end
+% BCstruct(2).val = -1;
+rhs = [chebfun(0,d);1];
+
+ttau = S\rhs;
+
+% Extract function and scalar
+t = ttau{1};
+tau = ttau{2};
+
+scale = sqrt(t'*t + tau^2);
+
+t = t/scale;
+tau = tau/scale;
+
+end
