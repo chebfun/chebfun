77e5f1f756eab4c900c6c47b445557718b46ba9a
Kuan Xu <xukuan2011@gmail.com>
2014-07-23 23:08:42 +0100

    A typo.

--- @chebtech2/angles.m
+++ @chebtech2/angles.m
@@ -1,6 +1,6 @@
 function out = angles(n)
 %ANGLES   Return the angles of the Chebyshev points of 2nd kind in [-1, 1].
-%   CHEBTECH2,ANGLES(N) returns ACOS(X), where X are the N Chebyshev points of
+%   CHEBTECH2.ANGLES(N) returns ACOS(X), where X are the N Chebyshev points of
 %   the 2nd kind in [-1, 1].
 %
 % See also POINTS, CHEBPTS, LENGTH.
