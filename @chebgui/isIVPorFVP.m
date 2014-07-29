0029f3ec38e3958a0a166e5c4c1c71c63b50b328
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2014-07-29 12:31:35 +0100

    Making IVP systems work in chebgui.

--- @chebgui/isIVPorFVP.m
+++ @chebgui/isIVPorFVP.m
@@ -1,29 +1,54 @@
-function isIorF = isIVPorFVP(guifile, bcInput, allVarString)
-%%
+function isIorF = isIVPorFVP(guifile, bcInput, allVarNames)
+% The domain of the problem
 dom = guifile.domain;
+% Find what the left end right endpoint of the interval is. We look to the left
+% of the first whitespace, and to the right of the last whitespace.
 spaceLoc = strfind(dom, ' ');
 firstSpace = min(spaceLoc);
 lastSpace = max(spaceLoc);
-
+% LEFTDOMSTR and RIGHTDOMSTR are strings that contain information about the
+% endpoint of the domain. E.g., if dom = '[0 10]', we'll have LEFTDOMSTR = '0'
+% and RIGHTDOMSTR = '10'.
 leftDomStr = dom(2:firstSpace-1);
 rightDomStr = dom(lastSpace+1:end-1);
 
+% NUMSTR is a regular expression for representing general numbers.
 numStr = '[+-]?[0-9]*\.?[0-9]*(e[0-9]+)*';
+
+% Initialize cells for storing evaluation information.
+% For storing information about left boundary conditions.
+leftConditionLocs = cell(size(bcInput));
+% For storing information about right boundary conditions.
+rightConditionLocs = leftConditionLocs;
+% For any parts of the strings where condition of the form u(0) are imposed.
+pointEvalConditionLocs = leftConditionLocs;
+% For storing information about where variables appear at places other than
+% where point evaluation takes place. This happens e.g. in conditions such as
+% 'sum(u) = 1':
+globalConditionLocs = leftConditionLocs;
+
+% Anonymous function for combinging information about where conditions are
+% evaluated for different variable names.
+unionFun = @(c1, c2) cellfun(@union, c1, c2, 'uniformOutput', false);
+
 % Find parts of the boundary conditions input where condition are imposed on the
-% left or right ends of the interval.
-leftConditionLocs = ...
-    regexp(bcInput,[allVarString, '''*\(', leftDomStr, '\)']);
-rightConditionLocs = ...
-    regexp(bcInput,[allVarString, '''*\(', rightDomStr, '\)']);
-
-% Find any parts of the string where condition of the form u(0) are imposed.
-pointEvalConditionLocs = ...
-    regexp(bcInput,[allVarString, '''*\(', numStr, '\)']);
-
-% Find where the variables appear at places other then where point evaluation
-% takes place. This happens e.g. in conditions such as 'sum(u) = 1':
-globalConditionLocs = ...
-    regexp(bcInput, [allVarString, '[^''*\(', numStr, '\)]']);
+% left or right ends of the interval. In case of systems, ALLVARNAMES will be a
+% cell-array that contains the variable names that appear in the problem. So
+% need to loop through the ALLVARNAMES cell.
+for varCounter = 1:size(allVarNames,1)
+    % The current dependent variable name we're looking at.
+    varName = allVarNames{varCounter};
+    tempLeft = regexp(bcInput,[varName, '''*\(', leftDomStr, '\)']);
+    tempRight = regexp(bcInput,[varName, '''*\(', rightDomStr, '\)']);
+    tempPoint = regexp(bcInput,[varName, '''*\(', numStr, '\)']);
+    tempGlobal =  regexp(bcInput, [varName, '[^''*\(', numStr, '\)]']);
+    
+    % Combine with previously obtained information.
+    leftConditionLocs = unionFun(leftConditionLocs, tempLeft);
+    rightConditionLocs = unionFun(rightConditionLocs, tempRight);
+    pointEvalConditionLocs = unionFun(pointEvalConditionLocs, tempPoint);
+    globalConditionLocs = unionFun(globalConditionLocs, tempGlobal);
+end
 
 % Below, we like to work with cells which arise in the case where more than one
 % condition is imposed. So ensure LEFTCONDITIONLOCS, RIGHTCONDITIONLOCS and
