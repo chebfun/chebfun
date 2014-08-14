f5b3ce571f36390afdc7bfe30906607e19ecc6f1
Mohsin Javed <mhsnjvd@gmail.com>
2014-08-14 11:00:36 +0100

    chebcoeffs now returns a column vector with the zeroth order coefficient at the top and highest at the bottom.

--- @chebfun/chebcoeffs.m
+++ @chebfun/chebcoeffs.m
@@ -1,8 +1,8 @@
 function out = chebcoeffs(f, varargin)
 %CHEBCOEFFS   Chebyshev polynomial coefficients of a CHEBFUN.
 %   A = CHEBCOEFFS(F, N) returns the first N Chebyshev coefficients of F, i.e.,
-%   the row vector such that F = ... + A(1) T_N(x) + ... + A(N) T_1(x) +
-%   A(N+1) T_0(x), where T_M(x) denotes the M-th Chebyshev polynomial.
+%   the column vector such that F = A_0 +  A_1 T_1(x) + ... + 
+%   A_(N-1) T_(N-1)(x), where T_M(x) denotes the M-th Chebyshev polynomial.
 %
 %   If F is a smooth CHEBFUN (i.e., with no breakpoints), then CHEBCOEFFS(F) is
 %   equivalent to CHEBCOEFFS(F, LENGTH(F)).
@@ -76,7 +76,7 @@ end
 if ( numFuns == 1 )
     
     % CHEBCOEFFS() of a smooth piece:
-    out = chebcoeffs(f.funs{1}, N).';    
+    out = flipud(chebcoeffs(f.funs{1}, N));    
     
 else
     % CHEBCOEFFS() of a piecewise smooth CHEBFUN:
@@ -86,24 +86,24 @@ else
     x = chebfun('x', d);
     w = 1./sqrt((x - d(1)).*(d(2) - x));
     numCols = numColumns(f);
-    out = zeros(numCols, N);
+    out = zeros(N, numCols);
     f = mat2cell(f);
     for j = 1:numCols
         for k = 1:N
             T = chebpoly(k-1, d);
             I = (f{j}.*T).*w;
-            out(j, N-k+1) = 2*sum(I)/pi;
+            out(k,j) = 2*sum(I)/pi;
         end
     end
-    out(:,N) = out(:,N)/2;
+    out(1,:) = out(1,:)/2;
     
 end
 
 % Return 2nd-kind coefficients:
 if ( (kind == 2) && (numel(out) > 1) )
-    out(:,end) = 2*out(:,end);
+    out(end,:) = 2*out(end,:);
     % Recurrence relation / conversion matrix:
-    out = .5*[out(:,1:2), out(:,3:end) - out(:,1:end-2)];
+    out = .5*[out(1:2,:); out(3:end,:) - out(1:end-2,:)];
 end
 
 end
