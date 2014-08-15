1efd2c53822350a9fe0e97ad352181b0d81f0112
Mohsin Javed <mhsnjvd@gmail.com>
2014-08-15 11:32:42 +0100

    making sure legcoeffs returns a vector of length n+1 as advertised.

--- @chebtech/legcoeffs.m
+++ @chebtech/legcoeffs.m
@@ -1,14 +1,14 @@
 function b = legcoeffs(f, n)
 %LEGCOEFFS   Compute Legendre series coefficients of a CHEBTECH object.
 %   B = LEGCOEFFS(F) returns the Legendre series coefficients of CHEBTECH F, so
-%   that F = B(N+1)*P_N + ... + B(1)*P_0, where P_k is the kth Legendre
-%   polynomial.
+%   that F = B(1)*P_0 + ... + B(N+1)*P_N, where P_k is the kth Legendre
+%   polynomial. B is a vector of the same length as that of F.
 %
 %   B = LEGCOEFFS(F, N) returns the first N+1 coefficients. If length(F) < N + 1
 %   then the additional entries of B are padded with zeros.
 %
 %   If F is an array-valued CHEBTECH, then a matrix of coefficients is returned
-%   so that F(:,k) = B(N+1,k)*P_N + ... + B(1,k)*P_0.
+%   so that F(:,k) = B(1,k)*P_0 + ... + B(N+1,k)*P_N.
 %
 % See also CHEBCOEFFS.
 
@@ -20,11 +20,10 @@ b = cheb2leg(flipud(f.coeffs));
 if ( nargin > 1 )
     s = size(b);
     if ( s(1) > n + 1 )
-        b = b(1:n, :);
+        b = b(1:n+1, :);
     else
-        b = [b; zeros(n-s(1), s(2))];
+        b = [b; zeros(n+1-s(1), s(2))];
     end
 end
 
 end
-
