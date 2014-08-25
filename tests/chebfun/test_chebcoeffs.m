6e960ce3f949e937b500a88876407ba8c16db91c
Anthony P. Austin <austin@maths.ox.ac.uk>
2014-08-25 13:20:12 +0100

    Improve the test for chebfun.chebcoeffs().
    The call to merge() in chebfun.chebcoeffs() prevented this test from testing
    extraction of coefficients for a function with a breakpoint like it said it
    was.  This is now fixed, and we've also added tests to check that things are
    working properly with array-valued chebfuns.

--- tests/chebfun/test_chebcoeffs.m
+++ tests/chebfun/test_chebcoeffs.m
@@ -4,16 +4,38 @@ if ( nargin == 0 )
     pref = chebfunpref();
 end
 
+% Test on a smooth chebfun.
+f = chebfun(@(x) cos(x));
+n = 5;
+c = chebcoeffs(f, n);
+c_exact = [besselj(0, 1) ;
+           0 ;
+           -2*besselj(2, 1) ;
+           0 ;
+           2*(-23*besselj(0, 1) + 40*besselj(1, 1))];
+err = c - c_exact;
+pass(1) = norm(err, inf) < 10*vscale(f).*epslevel(f);
+
+% Test on a smooth array-valued chebfun.
+f = [f f];
+c = chebcoeffs(f, n);
+c_exact = [c_exact c_exact];
+err = c - c_exact;
+pass(2) = norm(err(:), inf) < 10*vscale(f).*epslevel(f);
+
 % Test on piecewise-smooth chebfun
-f = chebfun(@(x)sin(100*x), 'splitting', 'on');
-n = 20;
-p = chebcoeffs(f, n);
+f = chebfun(@(x) abs(x), [-1 0 1]);
+n = 7;
+c = chebcoeffs(f, n);
+c_exact = [2/pi ; 0 ; 4/(3*pi) ; 0 ; -4/(15*pi) ; 0 ; 4/(35*pi)];
+err = c - c_exact;
+pass(3) = norm(err, inf) < 10*vscale(f).*epslevel(f);
 
-g = chebfun(@(x)sin(100*x));
-c = g.funs{1}.onefun.coeffs;
-cc = flipud(c(end-n+1:end));
-
-err = cc-p;
-pass(1) = norm(err, inf) < vscale(f).*epslevel(f);
+% Test on a piecewise-smooth array-valued chebfun.
+f = [f f];
+c = chebcoeffs(f, n);
+c_exact = [c_exact c_exact];
+err = c - c_exact;
+pass(4) = norm(err(:), inf) < 10*vscale(f).*epslevel(f);
 
 end
