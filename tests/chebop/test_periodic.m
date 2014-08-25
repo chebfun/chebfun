ebb9ad109c31627d0cd1f8d2e0942279dfacbff3
Hadrien Montanelli <montanelli@maths.ox.ac.uk>
2014-08-25 16:03:05 +0100

    Adding a test.

--- tests/chebop/test_periodic.m
+++ tests/chebop/test_periodic.m
@@ -69,6 +69,61 @@ err(3) = norm(real(e) - [0 0 1 1 1].', inf) + ...
     norm(imag(e) - [-1 1 -1 0 1].', inf);
 err(4) = norm(V{1}(pi) - V{1}(pi), inf) + norm(V{2}(pi) - V{2}(pi), inf);
 
+%% Nonlinear ODE.
+% u'' - sin(u) = cos(2x).
+
+% Domain.
+dom = [-pi pi];
+
+% Define the rhs, and the intial guess.
+f = chebfun(@(x) cos(2*x), dom);
+u0 = f;
+
+% Define the non-linear operator.
+N = chebop(@(u) diff(u, 2) - sin(u), dom);
+
+% Solve using the periodic tag.
+N.bc = 'periodic';
+N.init = u0;
+u = N \ f;
+
+% Solve imposing directly the periodic boundary condition.
+N.bc = @(x, u) [u(dom(2)) - u(dom(1)); ...
+   feval(diff(u), dom(2)) - feval(diff(u), dom(1))];
+N.init = u0;
+v = N \ f;
+
+% Compare.
+err(5) = norm(u - v, inf);
+
+%% System of nonlinear ODEs.
+%  u - v' + v = 0, u'' - cos(v) = cos(x).
+
+% Define the domain.
+dom = [-pi pi];
+
+% Define the rhs, and the intial guesses.
+f = [ chebfun(0, dom) ; chebfun(@(x) cos(x), dom) ];
+u0 = f;
+
+% Define the non-linear operator.
+N = chebop(@(x, u, v) [ u - diff(v) + v ; diff(u, 2) - cos(v) ], dom);
+
+% Solve using the periodic tag.
+N.bc = 'periodic';
+N.init = u0;
+u = N \ f;
+
+% Solve imposing directly the periodic boundary condition.
+N.bc = @(x, u, v) [ u(dom(1)) - u(dom(2)); ...
+                    feval(diff(u), dom(1)) - feval(diff(u), dom(2)) ; ...
+                    v(dom(1)) - v(dom(2)) ];
+N.init = u0;
+v = N \ f;
+
+% Compare.
+err(6) = norm(u - v, inf);
+
 %%
 
 pass = err < tol;
