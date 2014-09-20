c944281e318247c1c03c669b1c61f2a967c4a5c7
Mohsin Javed <mhsnjvd@gmail.com>
2014-09-20 16:12:11 +0100

    cumsum in chebtech now works.

--- @chebtech/flipud.m
+++ @chebtech/flipud.m
@@ -6,8 +6,7 @@ function f = flipud(f)
 % See http://www.chebfun.org/ for Chebfun information.
 
 % Negate the odd coefficients:
-f.coeffs(end-1:-2:1,:) = -f.coeffs(end-1:-2:1,:);
-% Note: we use end-1:-2:1 rather than 1:2:end-1 as we only want to change the
-% odd coefficients (and this avoids checking the length of the coeffs).
+f.coeffs(2:2:end,:) = -f.coeffs(2:2:end,:);
+% Note: the first odd coefficient is at index 2.
 
 end
