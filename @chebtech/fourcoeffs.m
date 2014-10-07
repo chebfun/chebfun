80d50aaec38f8e211de6796c634eee13aaa06db7
Hadrien Montanelli <montanelli@maths.ox.ac.uk>
2014-10-07 10:15:02 +0100

    Changed @chebtech/fourcoeffs to @chebtech/trigcoeffs.

--- @chebtech/fourcoeffs.m
+++ @chebtech/fourcoeffs.m
@@ -1,6 +1,6 @@
-function out = fourcoeffs(f, N)
-%FOURCOEFFS   Fourier coefficients of a CHEBTECH.
-%   C = FOURCOEFFS(F) returns a column vector with the Fourier
+function out = trigcoeffs(f, N)
+%TRIGCOEFFS   Trigonometric coefficients of a CHEBTECH.
+%   C = TRIGCOEFFS(F) returns a column vector with the trignometric
 %   coefficients of F using complex-exponential form. Specifically for
 %   N = length(F):
 %   If N is odd
@@ -11,7 +11,7 @@ function out = fourcoeffs(f, N)
 %                + C(N)*z^(N/2-1) + 
 %   where z = exp(1i*pi*x).
 %
-%   A = FOURCOEFFS(F, N) truncates or pads the vector C so that N coefficients
+%   A = TRIGCOEFFS(F, N) truncates or pads the vector C so that N coefficients
 %   of the CHEBTECH F are returned.
 %
 %   If F is array-valued with M columns, then C is an MxN matrix.
@@ -21,7 +21,7 @@ function out = fourcoeffs(f, N)
 % Copyright 2014 by The University of Oxford and The Chebfun Developers. 
 % See http://www.chebfun.org/ for Chebfun information.
 
-% TODO: Is there a fast transfrom from Fourier to Chebyshev?
+% [TODO]: Is there a fast transfrom from Fourier to Chebyshev?
 
 if ( nargin == 1 )
     N = length(f);
