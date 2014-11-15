212c61d697904d35e46a1574f1bec805e7a98a9e
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2014-11-15 16:24:53 +0000

    Changes so that we still respect TRIGTECH discretization.

--- @chebop/mldivide.m
+++ @chebop/mldivide.m
@@ -1,4 +1,4 @@
-function varargout = mldivide(N, rhs, pref, varargin)
+function varargout = mldivide(N, rhs, varargin)
 %\    MLDIVIDE   Solve CHEBOP BVP or IVP system.
 %
 % MLDIVIDE is a convenient wrapper for CHEBOP/SOLVEBVP and CHEBOP/SOLVEIVP. It
@@ -11,12 +11,6 @@ function varargout = mldivide(N, rhs, pref, varargin)
 % Copyright 2014 by The University of Oxford and The Chebfun Developers. 
 % See http://www.chebfun.org/ for Chebfun information.
 
-
-% If we did not get preferences passed in, create a cheboppref:
-if ( nargin < 3 )
-    pref = cheboppref();
-end
-
 % Are we dealing with an initial or a final value problem. In that case, either
 % we have  
 %   - N.LBC is nonempty, but both N.RBC and N.BC are empty. Here, we are dealing
@@ -25,7 +19,15 @@ end
 %     with a final value problem, where all conditions are imposed via N.RBC.
 isIVP =  ( ~isempty(N.lbc) && isempty(N.rbc) && isempty(N.bc) ) || ...
      ( isempty(N.lbc) && ~isempty(N.rbc) && isempty(N.bc) );
- 
+
+% If we did not get preferences passed in, we need to create a cheboppref so
+% that we know wheter we want to solve the IVP via time-stepping or globally:
+if ( nargin < 3 )
+    pref = cheboppref();
+else
+    pref = varargin{1};
+end
+
 % Look at what solver we're using for IVPs:
 ivpSolver = func2str(pref.ivpSolver);
 
@@ -33,11 +35,16 @@ ivpSolver = func2str(pref.ivpSolver);
 % (ode113, ode15s, ode45) as our choice, call the solveIVP method. Otherwise,
 % solve problems globally:
 if ( isIVP && ~isempty(strfind(ivpSolver, 'chebfun.ode')) )
+    % Pass PREF here, as we'd have to start by constructing a CHEBOPPREF anyway
+    % that start of solveIVP otherwise.
     [varargout{1:nargout}] = solveIVP(N, rhs, pref, varargin{:});
 else
     % We have conditions in other fields, or we want to solve IVPs globablly,
-    % call CHEBOP/SOLVEBVP:
-    [varargout{1:nargout}] = solvebvp(N, rhs, pref, varargin{:});
+    % call CHEBOP/SOLVEBVP. However, here, we don't want to pass PREF if it
+    % wasn't included in VARARGIN, as that'd mean we couldn't to an automatic
+    % switch to Fourier methods in the periodic case (i.e. change the
+    % discretization to TRIGTECH).
+    [varargout{1:nargout}] = solvebvp(N, rhs, varargin{:});
 end
 
 end
