6816765a86243176dd602fc2e2593297821cfbe8
Hrothgar <hrothgar@maths.ox.ac.uk>
2014-11-25 14:28:29 +0000

    Formatting changes to univatiate.

--- @treeVar/univariate.m
+++ @treeVar/univariate.m
@@ -1,22 +1,21 @@
 function treeOut = univariate(treeIn, method)
-%UNIVARIATE     Construct a syntax tree for univariate functions.
+%UNIVARIATE   Construct a syntax tree for univariate functions.
+%   The UNIVARIATE method is called by most univariate methods of the TREEVAR
+%   class, such as COS, EXP and SIN. It constructs the corresponding syntax
+%   tree, where the appropriate method is at the top and the syntax tree up to
+%   that point becomes the center node.
 %
-% The UNIVARIATE() method is called by most univariate methods of the TREEVAR
-% class, such as cos, exp and sin. It constructs the corresponding syntax tree,
-% where the appropriate method is at the top, and the current syntax tree up to
-% that point becomes the center node.
+%   Calling sequence:
+%      TREEOUT = UNIVARIATE(TREEIN, METHOD)
 %
-% Calling sequence:
-%   TREEOUT = UNIVARIATE(TREEIN, METHOD)
+%   The inputs to this method are:
+%      TREEIN: A MATLAB struct for representing the syntax tree of a mathematical
+%              expressions that the METHOD operates on.
+%      METHOD: The name of the method that invokes the call to UNIVARIATE.
 %
-% The inputs to this method are:
-%   TREEIN: A MATLAB struct for representing the syntax tree of a mathematical
-%           expressions that the METHOD operates on.
-%   METHOD: The name of the method that invokes the call to UNIVARIATE().
-%
-% The output of this method is:
-%   TREEOUT: A MATLAB struct that represents the syntax tree of the mathematical
-%            expression, obtained once METHOD has operated on TREEIN.
+%   The output of this method is:
+%      TREEOUT: A MATLAB struct that represents the syntax tree of the mathematical
+%               expression, obtained once METHOD has operated on TREEIN.
 
 % Copyright 2014 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
