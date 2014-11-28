29d6625e417d26ec69affce7be598d085a103659
Hrothgar <hrothgar@maths.ox.ac.uk>
2014-11-28 23:12:56 +0000

    Added bump function and Blasius function, and alphabetized.

--- +cheb/gallery.m
+++ +cheb/gallery.m
@@ -3,24 +3,26 @@ function [f,fa] = gallery(name)
 %   GALLERY(NAME) returns a chebfun corresponding to NAME. See the listing
 %   below for available function names.
 %
-%   [F,FA] = GALLERY(NAME, PARAM1, PARAM2, ...) also returns the anonymous
-%   function used to define the test function.
+%   [F,FA] = GALLERY(NAME) also returns the anonymous function used to define
+%   the test function.
 %
 %   airy         Airy Ai function on [-40, 40]
 %   bessel       Bessel function with parameter 0 on [-100, 100]
+%   bump         A bump function
+%   blasius      The Blasius function on [0, 10]
 %   chirp        Sine with exponentially increasing frequency
 %   erf          The error function on [-10, 10]
 %   fishfilet    Wild oscillations from Extreme Extrema example
 %   gamma        The gamma function on [-4, 4]
 %   jitter       A piecewise constant function
 %   kahaner      Challenging integrand with four spikes
-%   motto        (scribbled) Chebfun motto by Gilbert Strang
+%   motto        (Scribbled) Chebfun motto by Gilbert Strang
 %   runge        The Runge function
+%   seismograph  Tanh plus growing oscillation
+%   si           Sine integral on [-50, 50]
 %   sinefun1     As smooth as it looks
 %   sinefun2     Not as smooth as it looks
-%   si           Sine integral on [-50, 50]
 %   spikycomb    25 peaks, each sharper than the last
-%   seismograph  Tanh plus growing oscillation
 %   wiggly       One of the Chebfun team's favorites
 %   zigzag       Degree 10000 polynomial that looks piecewise linear
 
@@ -40,7 +42,7 @@ end
 % The main switch statement.
 switch name
 
-    % the Airy function on [-40, 40]
+    % The Airy function on [-40, 40]
     case 'airy'
         fa = @airy;
         f = chebfun(fa, [-40 40]);
@@ -50,58 +52,78 @@ switch name
         fa = @(x) besselj(0, x);
         f = chebfun(fa, [-100 100]);
 
-    % sine with exponentially increasing frequency
+    % The Blasius function on [0, 10]
+    case 'blasius'
+        op = @(u) 2*diff(u,3) + u.*diff(u,2);
+        bc  = @(x,u) [u(0); feval(diff(u),0); feval(diff(u),10)-1];
+        N = chebop(op, [0 10], bc);
+        N.init = chebfun([3.55542; 4.25907; 0.43669; -0.21367;
+                0.06382; -0.00118; -0.00865; 0.00306], [0 10], 'coeffs');
+        f = N\0;
+        fa = @(x) f(x);
+
+    % A bump function
+    case 'bump'
+        fa = @(x) (abs(x) < 1).*exp(-1./(1-x.^2));
+        f = chebfun(fa, [-2 2]);
+
+    % Sine with exponentially increasing frequency
     case 'chirp'
         fa = @(x) sin(x.*exp(x));
         f = chebfun(fa, [0 5]);
 
-    % the error function on [-10, 10]
+    % The error function on [-10, 10]
     case 'erf'
         fa = @erf;
         f = chebfun(fa, [-10 10]);
 
-    % wild oscillations from Extreme Extrema example
+    % Wild oscillations from Extreme Extrema example
     case 'fishfilet'
         fa = @(x) cos(x).*sin(exp(x));
         f = chebfun(fa, [0 6]);
 
-    % the gamma function on [-4, 4]
+    % The gamma function on [-4, 4]
     case 'gamma'
         fa = @gamma;
         f = chebfun(fa, [-4 4], 'blowup', 'on', 'splitting', 'on');
 
-    % a piecewise constant function
+    % A piecewise constant function
     case 'jitter'
         fa = @(x) round(exp(x)*2.*sin(8*x));
         f = chebfun(fa, 'splitting', 'on');
 
-    % challenging integrand with four spikes
+    % Challenging integrand with four spikes
     case 'kahaner'
         fa = @(x) sech(10*(x-0.2)).^2 + sech(100*(x-0.4)).^4 + ...
          sech(1000*(x-0.6)).^6 + sech(1000*(x-0.8)).^8;
         f = chebfun(fa, [0 1]);
 
-    % (scribbled) Chebfun motto by Gilbert Strang
+    % (Scribbled) Chebfun motto by Gilbert Strang
     case 'motto'
         f = exp(3i*scribble('there is no fun like chebfun'));
         fa = @(x) f(x);
 
-    % the Runge function
+    case 'rose'
+        m = 5; n = 4;
+        fa = @(t) cos(m/n*t).*cos(t) + 1i*cos(m/n*t).*sin(t);
+        f = chebfun(fa, [0, 8*pi], 'trig');
+
+    % The Runge function
     case 'runge'
         fa = @(x) 1./(1 + x.^2);
         f = chebfun(fa, [-5 5]);
 
-    % as smooth as it looks
+    % As smooth as it looks
     case 'sinefun1'
         fa = @(x) (2 + sin(50*x));
         f = chebfun(fa);
 
-    % not as smooth as it looks
+    % Not as smooth as it looks
     case 'sinefun2'
         fa = @(x) (2 + sin(50*x)).^1.0001;
         f = chebfun(fa);
 
-    % sine integral
+    % Sine integral
     case 'si'
         f = cumsum(chebfun(@(x) sin(x)./(x)), [-50, 50]);
         fa = @(x) f(x);
@@ -112,24 +134,24 @@ switch name
         fa = @(x) exp(x).*sech(4*sin(40*x)).^exp(x);
         f = chebfun(fa);
 
-    % tanh plus growing oscillation
+    % Tanh plus growing oscillation
     % from ATAP, Chapter 5
     case 'seismograph'
         fa = @(x) tanh(20*sin(12*x)) + .02*exp(3*x).*sin(300*x);
         f = chebfun(fa);
 
-    % one of the Chebfun team's favorites
+    % One of the Chebfun team's favorites
     case 'wiggly'
         fa = @(x) sin(x) + sin(x.^2);
         f = chebfun(fa, [0 10]);
 
-    % degree 10000 polynomial that looks piecewise linear
+    % Degree 10000 polynomial that looks piecewise linear
     % from ATAP appendix
     case 'zigzag'
         f = cumsum(chebfun(@(t) sign(sin(100*t./(2-t))), 10000));
         fa = @(x) f(x);
 
-    % unknown function
+    % Error if the input is unknown.
     otherwise
         error('CHEB:GALLERY:unknown:unknownFunction', ...
             'Unknown function.')
