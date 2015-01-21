202a548bab676f8b4e74d015adc4142a55500663
nickhale <nick.p.hale@gmail.com>
2015-01-21 17:06:45 +0200

    Previous test (using lam = .5) was dumb.

--- tests/misc/test_ultrapoly.m
+++ tests/misc/test_ultrapoly.m
@@ -38,13 +38,13 @@ JJ = J(x,:);
 A = UU./JJ; 
 pass(4) = norm( A - ones(100,1)*A(1,:) ) < 1e5*tol;
 
-lambda = .5;
+lambda = .25;
 U = ultrapoly([0 2 3], lambda);
 C = chebcoeffs(U);
-tru = [1 0.25 0
-       0 0    0.375
-  	   0 0.75 0
-       0 0    0.625];
+tru = [1 0.0625 0
+       0 0      0.078125
+  	   0 0.3125 0
+       0 0      0.234375];
 pass(5) = ( all(size(C) == size(tru)) ) && ( norm(C(:) - tru(:)) < tol );
 
 end
