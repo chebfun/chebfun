2f6be188982dc74234f0e57cda4d7a6bb7c3255a
Hrothgar <hrothgar@maths.ox.ac.uk>
2015-02-20 01:38:06 +0000

    Fixing docs.

--- @chebfun/gamma.m
+++ @chebfun/gamma.m
@@ -8,13 +8,6 @@ function F = gamma(F, varargin)
 % Copyright 2014 by The University of Oxford and The Chebfun Developers. See
 % http://www.chebfun.org/ for Chebfun information.
 
-% [TODO]:  Restore or change this once we have decided the proper behavior or
-% isfinite() and defined that function.
-%if ( ~isfinite(f) )
-%    error('CHEBFUN:CHEBFUN:cos:inf',...
-%        'COS is not defined for functions which diverge to infinity');
-%end
-
 % Call the compose method:
 F = compose(F, @gamma, varargin{:});
 
