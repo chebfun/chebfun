73e637cb5838506fe3782407fb69130096054e07
jared <aurentz@maths.ox.ac.uk>
2015-03-08 10:50:49 +0000

    simplified the toggles

--- v5b.m
+++ v5b.m
@@ -1,26 +1,11 @@
-% This function allows for a user defined
-% epslevel that is enforced at every call
-% of HAPPINESSCHECK in both the CHEBTECH
-% and TRIGTECH classes.
-%
-% Jared L. Aurentz, March 2015
-
-function v5b(epsilon)
+% V5B  Chebfun v5 with prescribed epslevel.
+%   This function forces the epslevel returned
+%   by HAPPINESSCHECK in both the CHEBTECH
+%   and TRIGTECH classes to be Matlab EPS.
+function v5b
 
    % global variable for eps
    global myeps
-
-   % no input
-   if nargin == 0
-      myeps = eps;
-
-   % user defined epsilon
-   else if nargin == 1
-      myeps = epsilon;
-
-   % too many inputs
-   else
-      error('Maximum of one input for v5b!')
-   end
-
+   myeps = eps;
+   
 end
