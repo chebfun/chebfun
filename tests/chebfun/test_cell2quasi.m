f0bc85949381d471d9cb3a03ddbe185662bf9078
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2015-03-12 17:46:46 +0000

    Cleaning up the test.

--- tests/chebfun/test_cell2quasi.m
+++ tests/chebfun/test_cell2quasi.m
@@ -1,5 +1,8 @@
 function pass = test_cell2quasi(pref)
-
+%TEST_CELL2QUASI
+%
+% Test that a quasimatrix created from a chebfun and a scalar has the correct
+% domain.
 if ( nargin == 0 )
     pref = chebfunpref();
 end
@@ -9,8 +12,5 @@ x35 = chebfun(@(x) x, [3, 5], pref);
 Q = quasimatrix({x35, 2});
 pass(1) = all( Q(:,2).domain == [3, 5] );
 
-% x = chebfun(@(x) x, [2, 4]);
-% x18 = [x ; 1.8];
-% plot(x18)
 
 end
