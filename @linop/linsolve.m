7f2bdb8b507edf6796742a9852960cb59d046e10
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2015-03-21 16:43:44 +0000

    More meaningful errors and warnings when we encounter incorrect no. of BCs

--- @linop/linsolve.m
+++ @linop/linsolve.m
@@ -115,9 +115,10 @@ for dim = [dimVals inf]
     else
         [A, P] = matrix(disc);
         if ( size(A, 1) ~= size(A, 2) )
-            % [TODO]: Improve this warning.
             warning('CHEBFUN:LINOP:linsolve:notSquare', ...
-                'Matrix is not square!');
+                ['Operator may not have the correct number of boundary ' ...
+                'conditions.\n' ...
+                'Matrix is not square. Problem may be ill-posed.']);
         end
     end
     
