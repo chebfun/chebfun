6d85848586fe918f7975deffe7aa00e930b56c75
Kuan Xu <xukuan2011@gmail.com>
2015-04-02 14:29:02 +0100

    Loosening the tolerances used in plus@singfun and sum@unbndfun. Note that both the tweaked fudge constant in front of the tolerance have no theoretical ground and are only justified empirically.

--- @singfun/plus.m
+++ @singfun/plus.m
@@ -60,7 +60,7 @@ end
 fExps = f.exponents;
 gExps = g.exponents;
 tolExps = chebfunpref().blowupPrefs.exponentTol;
-tolSmth = 1e2*eps;
+tolSmth = 1e3*eps; % tolerance loosened in epslevel-dependent test
 
 %%
 if ( all(abs(fExps - gExps) < tolExps) )
