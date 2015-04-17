d6811f515c2e8e8daaf815fa425f89db6644b3fa
Alex Townsend <ajt@mit.edu>
2015-04-17 15:29:59 -0400

    Make compress into a SVD code.

--- @spherefun/compress.m
+++ @spherefun/compress.m
@@ -11,19 +11,30 @@ tol = 50*eps;
 C = f.Cols.values;
 R = f.Rows.values;
 D = f.BlockDiag;
-for j = 1:size(D,1)/2
+
+for j = 1 : size(D, 1)/2
     
+    % Look at 2x2 blocks at one time. 2x2 blocks separate. 
     ii = 2*j-1:2*j;
-    % Grady's memo tells us this preserves the BMC structure:
-    [UC, SC, VC] = svd( C(:,ii) );
-    [UR, SR, VR] = svd( R(:,ii) );
     
+    % Grady's memo tells us this preserves the BMC structure:  Does it?
+    % (Yes(?), because C(:,ii)D(ii,ii)R(:,ii)' is a BMC matrix.)
+    % IDEA:     C D R  = ( UC SC VC' ) * D * ( UR SR VR' )'
+    %                  =  UC  ( SC VC' D VR SR )  UR' 
+    %                  =  UC ( U S V' ) UR' 
+    %                  =  ( UC U ) S ( UR V )'
+    
+    [UC, SC, VC] = svd( C(:,ii), 0 );   % We could also use QR here, if it preserves the structure. 
+    [UR, SR, VR] = svd( R(:,ii), 0 );
+    
+    % Now do the middle part: 
     [U, S, V] = svd( SC * VC' * D(ii,ii) * VR * SR ,0 );
     
     % Push back into CDR structure:
     D(ii,ii) = S;
     C(:,ii) = UC * U;
     R(:,ii) = UR * V;
+    
 end
 
 % COMPRESS: Remove zeros on diag of B
@@ -36,6 +47,5 @@ R = R(:, idx);
 f.Cols = trigtech( C );
 f.Rows = trigtech( R );
 f.BlockDiag = D;
-
 
 end
--- @spherefun/svd.m
+++ @spherefun/svd.m
@@ -5,6 +5,13 @@ function varargout = svd( f )
 % 
 % [U, S, V] = svd( F ) returns the singular value decomposition of F. 
 
+% COMPRESS is actually doing the SVD: 
+f = compress( f ) ; 
 
+if ( nargout <= 1 ) 
+    varargout = { full( diag( f.BlockDiag ) ) }; 
+elseif ( nargout == 3 ) 
+    varargout = { f.Cols, f.BlockDiag, f.Rows }; 
+end
 
 end
