3521820c65663467a29e89dde3e47d503ff64a76
Alex Townsend <ajt@mit.edu>
2015-04-22 10:58:32 -0400

    Move moving of commands.

--- @lowrankapprox/power.m
+++ @lowrankapprox/power.m
@@ -1,37 +1,38 @@
 function f = power( f, n )
-%.^	CHEBFUN2 power. 
+%.^	     Pointwise power of a LOWRANKAPPROX. 
 %
-% F.^G returns a CHEBFUN2 F to the scalar power G, a scalar F to the
-% CHEBFUN2 power G, or a CHEBFUN2 F to the CHEBFUN2 power G.
+% F.^G returns a LOWRANKAPPROX F to the scalar power G, a scalar F to the
+% CHEBFUN2 power G, or a LOWRANKAPPROX F to the LOWRANKAPPROX power G.
 
 % Copyright 2014 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
 
 if ( isempty( f ) || isempty( n ) )    % Check for empty objects.
-    f = chebfun2();
     
-elseif ( isa (f, 'double') )           % double.^CHEBFUN2
+    f = chebfun2( );
+    
+elseif ( isa (f, 'double') )           % double.^LOWRANKAPPROX
     
     op = @(x,y) f .^ ( feval( n, x, y ) );
     f = chebfun2( op, n.domain );
     
-elseif ( isa( n, 'double' ) )          % CHEBFUN2.^double
+elseif ( isa( n, 'double' ) )          % LOWRANKAPPROX.^double
 
    if ( abs(round( n ) - n) > eps )
        % Positive/negative test.
        [bol, wzero] = singleSignTest(f);
        if ( bol == 0 || wzero == 1 )
-           error('CHEBFUN:CHEBFUN2:power:fractional', ...
+           error('CHEBFUN:LOWRANKAPPROX:power:fractional', ...
                'A change of sign/zero has been detected, unable to represent the result.');
        end
    end
     op = @(x,y) feval( f, x, y ) .^ n;
     f = chebfun2( op, f.domain );
     
-else                                   % CHEBFUN2.^CHEBFUN2
+else                                   % LOWRANKAPPROX.^LOWRANKAPPROX
     
     if ( ~domainCheck(f, n) ) % check they're on the same domain.
-        error('CHEBFUN:CHEBFUN2:power:domain','Domains must be the same');
+        error('CHEBFUN:LOWRANKAPPROX:power:domain','Domains must be the same');
     end
     op = @(x,y) feval( f, x, y ) .^ ( feval( n, x, y ) );
     f = chebfun2( op, f.domain );      % Resample and call constructor.
