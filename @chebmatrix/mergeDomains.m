8a4fb114afb2c1731a7833bc7615fcf78c5b5fbe
nickhale <nick.p.hale@gmail.com>
2015-05-13 13:00:09 +0200

    Make logic clearer.

--- @chebmatrix/mergeDomains.m
+++ @chebmatrix/mergeDomains.m
@@ -2,14 +2,16 @@ function d = mergeDomains(varargin)
 %MERGEDOMAINS  Merge domains of CHEBMATRIX blocks.
 %   D = MERGEDOMAINS(D1,D2,...) merges domains (union of breakpoints, while
 %   checking endpoints). Each input is inspected.
-%       If numeric scalar, it's ignored.
-%       If a numeric vector, it's taken as a domain.
+%       If numeric scalar, it is ignored.
+%       If a numeric vector, it is taken as a domain.
 %       If a chebfun or linBlock, its domain is extracted.
+%
+% See also DOMAIN.MERGE.
 
 % Copyright 2014 by The University of Oxford and The Chebfun Developers. 
 % See http://www.chebfun.org/ for Chebfun information.
 
-% Start by getting all the domains from VARARGIN
+% Start by getting all the domains from VARARGIN:
 doms = cell(size(varargin));
 for argCounter = 1:nargin
     item = varargin{argCounter};
@@ -34,38 +36,40 @@ elseif ( isempty(doms) )
     return
 end
 
-% Often in the CHEBMATRIX case, we're dealing with objects that live on the same
+%%
+% Often in the CHEBMATRIX case we're dealing with objects that live on the same
 % domains. The DOMAIN.MERGE() method is pretty expensive, so we check whether we
 % can get away with a cheaper version of merging the domains -- namely, if
 % they're all of the same size, we check their diffs, and if they agree, we're
 % happy!
 
-% Start off by assuming that the domains don't match:
-domainMatch = false;
-% 
 % Get the lengths of all the domains:
 domLengths = cellfun(@length, doms);
+
 % Do the length of the domains agree?
 lengthMatch = ~any(domLengths - domLengths(1));
 if ( lengthMatch )
     % Compare the other entries of DOMS with the first entry.
-    % The row vector with the first domain:
+    
+    % A row vector containing the first domain:
     dom1 = doms{1};
-    % A matrix with the rest of the domains:
+    % A matrix containing the rest of the domains:
     domMat = cell2mat(doms(2:end)');
     
-    % Subtract the first row from the matrix, and check whether we get any
-    % non-zero entries:
+    % Subtract the first row from the rest of the matrix, and check whether we
+    % get any non-zero entries. If so, the domains do not match. Otherwise they
+    % match.
     domainMatch = ~any(any(bsxfun(@minus, domMat, dom1)));
+    if ( domainMatch )
+        % The domains match! Return the first entry of DOMS:
+        d = dom1;
+    else
+        % Domains do not match, so we need to merge all the domains:
+        d = domain.merge(doms{:});
+    end
     
-end
-
-% Did the domains match?
-if ( domainMatch )
-    % Return the first entry of DOMS:
-    d = dom1;
 else
-    % We need to merge all the domains:
+    % Lengths do not match, so we need to merge all the domains:
     d = domain.merge(doms{:});
 end
 
