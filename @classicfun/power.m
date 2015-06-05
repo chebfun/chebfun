1e7e44893955311ab8d698b1ae1c1a1866064a5c
nickhale <nick.p.hale@gmail.com>
2015-06-05 16:46:25 +0200

    bndfun/power and unbndfun/power were identical.

--- @classicfun/power.m
+++ @classicfun/power.m
@@ -1,7 +1,8 @@
 function f = power(f, b)
-%.^   BNDFUN power.
-%   F.^G returns a BNDFUN F to the scalar power G, a scalar F to the BNDFUN
-%   power G, or a BNDFUN F to the BNDFUN power G. F and or G may be complex. 
+%.^   CLASSICFUN power.
+%   F.^G returns a CLASSICFUN F to the scalar power G, a scalar F to the
+%   CLASSICFUN power G, or a CLASSICFUN F to the CLASSICFUN power G. F and or G
+%   may be complex.
 %
 %   This function assumes that the curve traced out by F in the complex plane
 %   both (1) does not come too close to zero except at the domain boundaries 
@@ -21,7 +22,7 @@ function f = power(f, b)
 % If there are roots at the end of the domain, then make the f.onefun a singfun:
 lval = get(f, 'lval');                          % Value at left of domain.
 rval = get(f, 'rval');                          % Value at right of domain.
-tol = 1e3*get(f, 'epslevel').*get(f, 'vscale'); % Tolerance for a root.
+tol = 1e2*get(f, 'epslevel').*get(f, 'vscale'); % Tolerance for a root.
 if ( any(abs(lval) < tol) || any(abs(rval) < tol) ) && ...
         ( ~isa(f.onefun, 'singfun') )
     f.onefun = singfun(f.onefun);               % Cast f.onefun to a SINGFUN.
