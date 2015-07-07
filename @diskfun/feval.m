f1b7df2c242beb6caf91c61e09f7f7f4967a4634
heatherwilber <heatherwilber@boisestate.edu>
2015-07-07 10:55:53 -0600

    added description of double fourier method

--- @diskfun/feval.m
+++ @diskfun/feval.m
@@ -1,35 +1,39 @@
 function y = feval( f, c1, c2, c3)
-%FEVAL  Evaluate a SPHEREFUN at one or more points.
-%   Y = FEVAL( F, LAMBDA, THETA )  evaluates a spherefun F at (LAMBDA,
-%   THETA) where LAMBDA and THETA are doubles representing the longitudinal
-%   (or azimuthal) and latitudinal (or elevation) angles.
+%FEVAL  Evaluate a DISKFUN at one or more points.
+%   Y = FEVAL( F, THETA, r, 0)  evaluates a diskfun F at (THETA,
+%   r) where THETA and r are doubles representing central angle in radians and radius on the disk.
+%   
+%   
 %
-%   Y = FEVAL( F, X, Y, Z )  evaluates a spherefun F at a point (X,Y,Z) in
-%   Cartesian cooridnates on the surface of a sphere.  
+%   Y = FEVAL( F, X, Y, 1 )  evaluates a diskfun F at a point (X,Y) in
+%   Cartesian cooridnates.   
 
 % See also SUBSREF.
 
-if nargin == 3      % Spherical coordinates used.
-    lambda = c1;
-    theta = c2;
-elseif nargin == 4  % Cartesian coordinates used.
-    if ~isnumeric(c1) || ~isnumeric(c1) || ~isnumeric(c1)
-        if strcmpi(c1, ':') || strcmpi(c2, ':') || strcmpi(c3, ':') 
-            error('SPHEREFUN:feval:colon','Colon operator not allowed when using Cartesian coordinates');
-        else
-            error('SPHEREFUN:feval:unknown','Unkown input feval(%s,%s,%s)',c1,c2,c3);
-        end
-    end
-    % Convert to spherical coordinates
-    [lambda,theta] = cart2sph(c1,c2,c3);
-
+%if nargin == 3      % Spherical coordinates used.
+   % lambda = c1;
+  %  theta = c2;
+%elseif nargin == 4  % Cartesian coordinates used.
+  %  if ~isnumeric(c1) || ~isnumeric(c1) || ~isnumeric(c1)
+      %  if strcmpi(c1, ':') || strcmpi(c2, ':') || strcmpi(c3, ':') 
+       %     error('SPHEREFUN:feval:colon','Colon operator not allowed when using Cartesian coordinates');
+      %  else
+       %     error('SPHEREFUN:feval:unknown','Unkown input feval(%s,%s,%s)',c1,c2,c3);
+      %  end
+    %end
+    % Convert to polar coordinates
+    if c3 ==1
+    [theta,r] = cart2pol(c1,c2);
+    else 
+     theta = c1;
+     r = c2; 
     % Check latitudinal coordinate system to set the elevation angle
     % appropriately.
-    if iscolat( f )
-        theta = pi/2 - theta;
-    end
-end
+    %if iscolat( f )
+     %   theta = pi/2 - theta;
+    %end
+%end
 
-y = feval@separableApprox( f, lambda, theta );
+y = feval@separableApprox( f, theta, r);
 
 end 
--- @diskfun/partition.m
+++ @diskfun/partition.m
@@ -10,14 +10,14 @@ function [fp,fm] = partition(f)
 %
 % See also COMBINE
 
-if ~isa(f,'spherefun')
+if ~isa(f,'diskfun')
     error('SPHEREFUN:partition:unknown',['Undefined function ''partition'' for ' ...
         'input argument of type %s.'], class(f));
 end
 
 if isempty(f)
-    fp = spherefun();
-    fm = spherefun();
+    fp = diskfun();
+    fm = diskfun();
     return
 end
 
@@ -25,7 +25,7 @@ end
 id = f.idxPlus;
 
 if isempty(id)
-    fp = spherefun();
+    fp = diskfun();
 else
     fp = f;
     fp.cols = fp.cols(:,id);
@@ -39,7 +39,7 @@ end
 id = f.idxMinus;
 
 if isempty(id)
-    fm = spherefun();
+    fm = diskfun();
 else
     fm = f;
     fm.cols = fm.cols(:,id);
