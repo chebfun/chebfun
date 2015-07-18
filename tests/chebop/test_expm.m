1d7b146b8747fb61b455d38e506b0e72155dc58b
Jared Aurentz <aurentz@maths.ox.ac.uk>
2015-07-18 15:40:31 +0100

    fixed plotting bug

--- tests/chebop/test_expm.m
+++ tests/chebop/test_expm.m
@@ -62,7 +62,6 @@ v = expm(A, t, u0, pref);
 pass(4) = isequal(get(v{1}.funs{1}, 'tech'), @chebtech2);
 
 % Compare solutions at final time.
-plot([u{6},v{6}])
-%pass(5) = norm(u{6} - v{6}, inf) < tol;
+pass(5) = norm(u{6} - v{6}, inf) < tol;
 
 end
