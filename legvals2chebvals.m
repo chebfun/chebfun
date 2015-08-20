c4f9362d424b62c41c154148807c46a4cf29b0a1
Townsend <townsend@maths.ox.ac.uk>
2015-08-20 10:21:33 +0100

    More help text.

--- legvals2chebvals.m
+++ legvals2chebvals.m
@@ -1,4 +1,11 @@
 function chebvals = legvals2chebvals( legvals )
+% LEGVALS2CHEBVALS       Convert Legendre values to Chebyshev values
+% 
+%  CHEBVALS = LEGVALS2CHEBVALS( LEGVALS ), converts the vector of
+%  LEGVALS representing values of a polynomial at LEGPTS to a
+%  vector CHEBVALS representing the same polynomial evaluated at CHEBPTS.  
+% 
+% See also chebvals2legvals
 
 legcoeffs = chebfun.idlt( legvals ); 
 chebvals = legcoeffs2chebvals( legcoeffs ); 
