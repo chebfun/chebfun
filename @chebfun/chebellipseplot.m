8380f30036f95fc1b3b33e1b9c3d3b657b1ebb1f
Anthony P. Austin <austin@maths.ox.ac.uk>
2015-09-04 14:42:26 +0100

    Reinstate chebellipseplot() for backward compatibility.
    Marked as deprecated.  Users are referred to plotregion().

---
+++ @chebfun/chebellipseplot.m
@@ -0,0 +1,21 @@
+function varargout = chebellipseplot(varargin)
+%CHEBELLIPSEPLOT   Plot estimated regions of analyticity (deprecated).
+%   CHEBELLIPSEPLOT(...) is equivalent to PLOTREGION(...).
+%
+%   This function is deprecated.  Please use PLOTREGION instead.
+%
+% See also:  PLOTREGION.
+
+% Copyright 2015 by The University of Oxford and The Chebfun Developers.
+% See http://www.chebfun.org/ for Chebfun information.
+
+warning('CHEBFUN:CHEBFUN:chebellipseplot:deprecated', ...
+    'CHEBELLIPSEPLOT is deprecated.  Please use PLOTREGION instead.');
+
+if ( nargout > 0 )
+    varargout{:} = plotregion(varargin{:});
+else
+    plotregion(varargin{:});
+end
+
+end
