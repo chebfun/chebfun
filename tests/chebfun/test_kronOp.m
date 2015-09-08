a7b37ef62016ddd6d8f46976c290359b9522e218
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2015-09-08 17:34:02 +0100

    Fixing inconsistent behaviour of chebfun/kron with op output.
    Closes #1419.

--- tests/chebfun/test_kronOp.m
+++ tests/chebfun/test_kronOp.m
@@ -51,4 +51,27 @@ pass(8) = norm( Au - A*u ) < tol;
 pass(9)  = norm( Au(xx1) - matrix(AC, 200, @chebcolloc1)*u(xx1) ) < tol;
 pass(10) = norm( Au(xx2) - matrix(AC, 200, @chebcolloc2)*u(xx2) ) < tol;
 
+%% Invalid calls to kron with op output:
+
+try
+    kron(f, g, 'invalid')
+    pass(11) = 0;
+catch ME
+    pass(11) = strcmp(ME.identifier, 'CHEBFUN:CHEBFUN:kron:sizes');
+end
+
+try
+    kron(f.', g, 'op')
+    pass(12) = 0;
+catch ME
+    pass(12) = strcmp(ME.identifier, 'CHEBFUN:CHEBFUN:kron:columnsAndRows');
+end
+
+try
+    kron(quasimatrix(f), g', 'op')
+    pass(13) = 0;
+catch ME
+    pass(13) = strcmp(ME.identifier, 'CHEBFUN:CHEBFUN:kron:quasimatrix');
+end
+
 end
