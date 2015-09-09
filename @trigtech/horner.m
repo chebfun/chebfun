9fda5f02628afee24e5683d24097b5fe7fd7af29
gradywright <gradywright@boisestate.edu>
2015-09-09 07:28:11 -0600

    Evaluate a constant trigtech at the correct number of points passed in

--- @trigtech/horner.m
+++ @trigtech/horner.m
@@ -140,6 +140,7 @@ n = ceil((N+1)/2);
 c = c(n:-1:1,:);
 a = real(c);
 b = imag(c);
+e = ones(nValsX,1);
 
 % Adjust the last coefficient which corresponds to the pure cos(pi*n*x) 
 % mode in the case that N is even.
@@ -150,7 +151,7 @@ end
 
 % Just return the constant term.
 if N == 1
-    q = a;
+    q = e*a;
     return
 end
     
