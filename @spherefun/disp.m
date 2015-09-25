6218cf5143357bc1517040b45b35f893b02b8a7c
gradywright <gradywright@boisestate.edu>
2015-09-25 15:42:42 -0600

    Updated Software section. Updated the way spherefuns are displayed.

--- @spherefun/disp.m
+++ @spherefun/disp.m
@@ -15,24 +15,13 @@ if ( isempty(F) )
 end
 
 % Get information that we want to display:
-dom = F.domain;                           % Domain
 len = length(F);                          % Numerical rank
-[xx, yy] = meshgrid(dom(1:2), dom(3:4));   
-vals = feval(F, xx, yy ).';               % Corner values
-vals = vals(:);
 vscl = vscale(F);                         % vertical scale
 
 % Display the information: 
 disp('   spherefun object: (1 smooth surface)')
-fprintf('       domain                 rank       corner values\n');
-if ( isreal(vals) )
-    fprintf('[%4.2g,%4.2g] x [%4.2g,%4.2g]   %6i     [%4.2g %4.2g %4.2g %4.2g]\n', ...
-        dom(1), dom(2), dom(3), dom(4), len, vals);
-else
-    fprintf('[%4.2g,%4.2g] x [%4.2g,%4.2g]   %6i     [  complex values  ]\n', ...
-        dom(1), dom(2), dom(3) , dom(4), len);
-end
-fprintf('vertical scale = %3.2g \n', vscl)
+fprintf('       domain        rank    vertical scale\n');
+fprintf('     unit sphere  %6i          %3.2g\n', len, vscl);
 
 if ( loose )
     fprintf('\n');
