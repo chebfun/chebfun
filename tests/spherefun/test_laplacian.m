f334501b545a75c10a2a9d7eafab4e0d1767b5f7
ajt60gaibb <townsend@maths.ox.ac.uk>
2015-12-08 19:54:32 -0500

    Fix Lap test.

--- tests/spherefun/test_laplacian.m
+++ tests/spherefun/test_laplacian.m
@@ -29,7 +29,7 @@ function sample_error = SampleError( h, g )
 m = 6; n = m;  
 [x, y] = getPoints( m, n ); 
 [L2, T2] = meshgrid(x, y);
-F = h(L2, T2);
+F = feval(h, L2, T2);
 % hn = pi/length(g.cols);  % Have to adjust for the shift in y points. Ugly!
 % approx = feval(g.cols,(y-hn)/pi-.5) * g.blockDiag * feval(g.rows,x/pi)';
 % sample_error = norm( F - approx , inf );
