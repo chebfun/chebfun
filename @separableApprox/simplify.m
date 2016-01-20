94e779efb8c34403504ce7b2dff7a6b154ffa52d
ajt60gaibb <townsend@maths.ox.ac.uk>
2016-01-20 18:26:12 -0500

    Add tolerance to 2D simplify.

--- @separableApprox/simplify.m
+++ @separableApprox/simplify.m
@@ -1,17 +1,22 @@
-function f = simplify( f ) 
+function f = simplify( f, tol ) 
 % Simplify a SEPARABLEAPPROX
 % 
 % F = SIMPLIFY( F ) compressed the representation of F to one that is
 % numerically the same, but requires fewer parameters to store. Currently this
 % simplifies the polynomial degree of F, but not the rank. 
+%
+% G = SIMPLIFY(F, TOL) does the same as above but uses the scalar TOL instead
+% of the default simplification tolerance as the relative threshold level.
 
 % Copyright 2015 by The University of Oxford and The Chebfun2 Developers.
 % See http://www.chebfun.org/ for Chebfun2 information.
 
-% Simplify the column and row slices. 
-f.cols = simplify( f.cols, [], 'globaltol' ); 
-f.rows = simplify( f.rows, [], 'globaltol' ); 
+if ( nargin < 2 )
+    tol = []; 
+end
 
-% TODO: Simplify the rank 
+% Simplify the column and row slices. 
+f.cols = simplify( f.cols, tol, 'globaltol' ); 
+f.rows = simplify( f.rows, tol, 'globaltol' ); 
 
 end 
