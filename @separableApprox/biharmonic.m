3ceafa33837f53ff0e60cc15ae0d8fa5437f7472
Hadrien Montanelli <montanelli@maths.ox.ac.uk>
2016-01-29 14:58:44 +0000

    Implement BIHARMONIC operator for CHEBFUN2, and minor help text changes.

---
+++ @separableApprox/biharmonic.m
@@ -0,0 +1,14 @@
+function B = biharmonic(f)
+%BIHARMONIC   Biharmonic operator of a SEPARABLEAPPROX.
+%   B = BIHARMONIC(F) returns a SEPARABLEAPPROX representing the biharmonic 
+%   operator applied to F.
+%
+% See also SEPARABLEAPPROX/BIHARM.
+
+% Copyright 2016 by The University of Oxford and The Chebfun Developers.
+% See http://www.chebfun.org/ for Chebfun information.
+
+% biharmonic(f) = f_xxxx + f_yyyy + 2*f_xxyy:
+B = diff(f, 4, 2) + diff(f, 4, 1) + 2*diff(diff(f, 2, 1), 2, 2);
+
+end
