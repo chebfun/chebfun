66d1cf09db89a49a8146779add8aeaa7f7368832
Anthony P. Austin <austin@maths.ox.ac.uk>
2016-02-02 08:01:26 +0000

    Implement lebesgue() for trigonometric interpolation.
    This only works for odd-length grids.  Even-length trigonometric
    interpolation has theoretical issues associated with it (namely
    nonuniqueness) that make it not worth bothering about.

--- lebesgue.m
+++ lebesgue.m
@@ -84,17 +84,18 @@ elseif ( nargin == 4 ) % LEBESGUE(X, A, B, 'trig')
         error('CHEBFUN:lebesgue:parseInputs:badArg3', 'Invalid argument.');
     end
 else
-    error('CHEBFUN:lebesgue:tooManyArgs', 'Too many input arguments.');
+    error('CHEBFUN:lebesgue:parseInputs:tooManyArgs', ...
+          'Too many input arguments.');
 end
 
 if ( ~isequal(size(d), [1 2]) || (d(1) >= d(2)) )
-    error('CHEBFUN:lebesgue:badDom', ...
+    error('CHEBFUN:lebesgue:parseInputs:badDom', ...
           ['Domain input must be either two numbers or a row vector with ' ...
            'two elements, listed in ascending order.']);
 end
 
 if ( any(x < d(1) - 10*eps(d(1))) || any(x > d(2) + 10*eps(d(2))) )
-    error('CHEBFUN:lebesgue:pointsOutsideDomain', ...
+    error('CHEBFUN:lebesgue:parseInputs:pointsOutsideDomain', ...
           sprintf(['Interpolation points must be inside domain ' ...
                    '([%.6f %.6f]).'], d(1), d(2)));
 end
@@ -155,9 +156,59 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function L = trigLebesgue(x, d)
-L = [];
+%TRIGLEBESGUE   Compute Lebesgue function for trigonometric interpolation.
+%  X - Interpolation nodes (odd-length vector).
+%  D - Interpolation domain.
+
+% NB:  This code only works if the number of points is odd.
+
+% Map from the domain in d to [-pi, pi].
+map = @(t) pi/(d(2) - d(1))*(2*t - d(1) - d(2));
+
+% Map the points to [-pi, pi], since trigBaryWeights() assumes the points
+% come from there.
+xm  = map(x);
+
+% Remove a periodic endpoint.  This will make it so that, e.g.,
+% lebesgue(linspace(-pi, pi, 17), 'trig') does the "expected" thing instead
+% of treating -pi and pi as two distinct points that are really, really
+% close to each other.
+if ( norm([xm(1), xm(end)] - [-pi, pi], Inf) < 2*pi*eps )
+    xm(end) = [];
+end
+
+% We can't deal with even-length grids.
+if ( mod(length(xm), 2) == 0 )
+    error('CHEBFUN:lebesgue:trigLebesgue:evenLengthGrid', ...
+          ['LEBESGUE for trigonometric interpolation requires an ' ...
+           'odd-length grid.']);
+end
+
+% Evaluate the barycentric weights.
+w = trigBaryWeights(xm);
+
+% Set breakpoints at the interpolation nodes.
+dom = unique([x(:) ; d.']).';
+L = chebfun(@(t) trigLebesgueFun(map(t), xm(:), w), dom);
+
 end
 
 function L = trigLebesgueFun(t, x, w)
-L = [];
+%TRIGLEBESGUEFUN:  Evaluate Lebesgue func. for trig. interp. grid at a point.
+%  T - Evaluation points.
+%  X - Interpolation nodes (odd-length vector).
+%  W - Trigonometric barycentric weights.
+
+% NB:  This code only works if the number of points is odd.
+
+% Based on the trigonometric barycentric formula.
+L = ones(size(t)); % Note: L(x) = 1
+mem = ismember(t, x);
+for i = 1:numel(t)
+    if ( ~mem(i) )
+        xx = w./sin((t(i) - x)/2);
+        L(i) = sum(abs(xx))/abs(sum(xx));
+    end
+end
+
 end
