d3f00a6dc04f54a365485f52160a306b2c081a97
Nick Hale <nick.p.hale@gmail.com>
2016-02-05 14:46:40 +0000

    Give a second output in quasi2cheb (pass/fail).

--- @chebfun/quasi2cheb.m
+++ @chebfun/quasi2cheb.m
@@ -1,7 +1,10 @@
-function F = quasi2cheb(F)
+function [F, pass] = quasi2cheb(F)
 %QUASI2CHEB   Convert a quasimatrix to an array-valued CHEBFUN.
 %   QUASI2CHEB(F) converts the quasimatrix F to an array-valued CHEBFUN by
-%   taking the union of the domains of each of the columns:
+%   taking the union of the domains of each of the columns.
+%
+%   [G PASS] = QUASI2CHEB(F) returns a second output with the value TRUE if G is
+%   a CHEBFUN (i.e,. numel(G) == 1), and FALSE if G remains a quasimatrix.
 %
 % See also QUASIMATRIX, CHEB2QUASI, NUM2CELL.
 
@@ -9,7 +12,7 @@ function F = quasi2cheb(F)
 % See http://www.chebfun.org/ for Chebfun information.
 
 if ( numel(F) < 2 )
-    % F is already be a quasimatrix!
+    % F is already a quasimatrix!
     return
 end
 
@@ -21,5 +24,7 @@ F = cheb2cell(F);
 
 % Call CAT to collate the columns:
 F = cat(2 - F{1}.isTransposed, F{:});
+
+pass = numel(F) == 1;
 
 end
