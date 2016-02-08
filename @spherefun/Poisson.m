b924d599d986366a2a9de6b12c1f19f96b835aca
ajt60gaibb <alex.townsend1987@gmail.com>
2016-02-08 13:48:50 -0500

    Improve the Spherefun Poisson solver.

--- @spherefun/Poisson.m
+++ @spherefun/Poisson.m
@@ -1,18 +1,18 @@
 function u = Poisson( f, int_const, m, n )
-% POISSON  Fast Poisson solver for the sphere
-% 
-% POISSON( f, const, n ) solves  
-%  
-%  sin(th)^2u_thth   + sin(th)cos(th)u_th +  u_{lam,lam}  =  sin(th)^2 * f   
+% POISSON              Fast Poisson solver for the sphere
 %
-% on the unit sphere written in spherical coordinates (lam, th) 
-% with integral condition  sum2( u ) = const with a discretization of 
+% POISSON( f, const, n ) solves
+%
+%  sin(th)^2u_thth   + sin(th)cos(th)u_th +  u_{lam,lam}  =  sin(th)^2 * f
+%
+% on the unit sphere written in spherical coordinates (lam, th)
+% with integral condition  sum2( u ) = const with a discretization of
 % size N x N.
 %
 % POISSON( f, const, m, n ) same as POISSON( f, const, n), but with a
-% discretization of size m x n. 
+% discretization of size m x n.
 %
-% EXAMPLE: 
+% EXAMPLE:
 %  f = @(lam,th) -6*(-1+5*cos(2*th)).*sin(lam).*sin(2*th);
 %  exact = @(lam,th) -2*sin(lam).*sin(2*th).*sin(th).^2 -...
 %            sin(lam).*sin(th).*cos(th) + .5*sin(lam).*sin(2*th).*cos(2*th);
@@ -21,89 +21,80 @@ function u = Poisson( f, int_const, m, n )
 %  norm( spherefun(exact) - u )
 
 % DEVELOPERS NOTE:
-% 
-% METHOD: Spectral method (in coeff space). We use the Fourier basis in 
+%
+% METHOD: Spectral method (in coeff space). We use the Fourier basis in
 % the theta- and lambda-direction.
 %
-% LINEAR ALGEBRA: Matrix equations.  The matrix equation decouples into n 
-% linear systems. This form banded matrices. 
+% LINEAR ALGEBRA: Matrix equations.  The matrix equation decouples into n
+% linear systems. This form banded matrices.
 %
 % SOLVE COMPLEXITY:    O( m*n )  N = m*n = total degrees of freedom
 %
-%  Alex Townsend, July 2015. 
+%  Alex Townsend, July 2015.
 
-if ( nargin < 4 ) 
-    n = m; 
+if ( nargin < 4 )
+    n = m;
+end
+% Construct useful spectral matrices:
+
+DF1m = trigspec.diffmat( m, 1 );
+DF2m = trigspec.diffmat( m, 2 );
+DF2n = trigspec.diffmat( n, 2 );
+Mcossin = spdiags( .25i*[-ones(m,1) ones(m,1)], [-2 2], m ,m ); % multiplication for sin(theta).*cos(theta)
+Msin2 = spdiags( .5*[-.5*ones(m,1) ones(m,1) -.5*ones(m,1)], [-2 0 2], m ,m );% multiplication for sin(theta)^2
+Im = speye( m );
+
+% There is a factor of 4 sped up here, by taking account of real 
+% solution, and even/odd symmetry.
+
+% Underlying discretization grids:
+lam0 = pi*trigpts( n );
+th0 = pi*trigpts( m );
+
+% Forcing term:
+if ( isa(f, 'function_handle') || isa(f, 'spherefun') )
+    [rhs_lam, rhs_theta] = meshgrid( lam0, th0 );
+    F = feval( f, rhs_lam, rhs_theta );
+    F = trigtech.vals2coeffs( F );
+    F = Msin2 * trigtech.vals2coeffs( F.' ).';
+elseif ( isa( f, 'double' ) )
+    F = Msin2 * f;       % Get trigcoeffs2 of rhs.
 end
 
-% Double up to do DFS method: 
-n = 2*n; 
+% Matrix for solution's coefficients:
+CFS = zeros( m, n );
 
-% Discretization grid: 
-lam0 = pi*trigpts( m );  
-th0 = pi*trigpts( n );      
+% Form discretization of the theta-dependent operator:
+L = Msin2*DF2m + Mcossin*DF1m;
 
-% Calculate trigcoeffs of the forcing term: 
-[ll, tt] = meshgrid( lam0, th0 ); 
-vals = sin(tt).^2 .* feval(f, ll, tt );
-G = trigtech.vals2coeffs( trigtech.vals2coeffs( vals ).' ).'; 
-
-% Construct useful spectral discretization matrices: 
-DF1n = 1i*spdiags((-n/2:n/2-1)', 0, n, n);  % 1st order Fourier diffmat
-DF2n = spdiags( -(-n/2:n/2-1)'.^2, 0, n, n);% 2nd order Fourier diffmat
-DF2m = spdiags( -(-m/2:m/2-1)'.^2, 0, m, m);
-% Multiplication operator for sin(theta):
-Msin = spdiags( .25i*[-ones(n,1) ones(n,1)], [-2 2], n ,n ); 
-% Multiplication operator for sin(theta).*cos(theta):
-Msin2 = spdiags( .5*[-.5*ones(n,1) ones(n,1) -.5*ones(n,1)], [-2 0 2], n ,n );
-
-% Form discretization of the theta-dependent operator: 
-L = Msin2*DF2n + Msin*DF1n;
-
-% We want to solve 
-%     L * X  +  X * DF2^T  = (sin(th))^2*g  
-% such that X(m/2+1, m/2+1) = int_const.  
-
-% Notice that the matrix equation decouples and we can solve for X one
-% column at a time: 
-CFS = zeros( n, m ); 
-I = speye( n );
-% We leave out the (m/2+1)th column because that needs a constraint
-for k = [1:m/2 m/2+2:m]  
-    CFS(:, k) = ( L +  DF2m(k, k)*I ) \ G(:, k);
+% All these modes are even:
+k_even = [floor(n/2)-1:-2:1 floor(n/2)+3:2:n];
+for k = k_even
+    
+    CFS(:, k) = ( L + scl(k)*Im ) \ F(:,k);
+    
 end
 
-% Now do the equation where we need the integral constraint. 
-% We ensure that X_{n/2+1,:}*en = int_const. 
-en = 2*pi*(1+exp(1i*pi*(-n/2:n/2-1)))./(1-(-n/2:n/2-1).^2);
-en([n/2, n/2 + 2]) = 0;
-CFS(:, m/2+1) = [ en ;  L([1:n/2 n/2+2:n], :)] \ ... 
-                                 [ int_const ; G([1:n/2 n/2+2:n], m/2+1) ];
+% All these modes are odd:
+k_odd = [floor(n/2):-2:1 floor(n/2)+2:2:n];
+for k = k_odd
+    
+    CFS(:, k) = ( L + scl(k)*Im ) \ F(:,k);
+    
+end
 
-% Convert to VALUES on the underlying grid: 
-VALS = trigtech.coeffs2vals( trigtech.coeffs2vals( CFS ).' ).'; 
+% Now do the equation where we need the integral constraint:
+% We will take  X_{n/2+1,:} en = int_const.
+% First, let's project the rhs to have mean zero:
+k = floor(n/2)+1;
+floorm = floor(m/2);
+mm = (-floorm:ceil(m/2)-1);
+en = 2*pi*(1+exp(1i*pi*mm))./(1-mm.^2);
+en([floorm, floorm + 2]) = 0;
+ii = [1:floorm floorm+2:m];
+F(floor(m/2)+1,k) = -en(ii) * F(ii, k )./en(floor(m/2)+1);
+CFS(:, k) = [ en ; L( ii, :) ] \ [ int_const ; F(ii, k) ];
 
-% Now restrict down to region of interest:
-VALS = VALS([n/2+1:n 1], :);
-
-% Finally, make a spherefun obxject out of the values: 
-u = spherefun( real( VALS ) ); 
-
-% - DEBUG
-% % Plot: 
-% subplot(1,3,1)
-% surf(ll, tt, real(VALS),'edgealpha',0,'facecolor','interp'), 
-% ylim([-pi pi])
-% norm( imag( VALS ) )
-% 
-% % Compare to exact solution:
-% subplot(1,3,2)
-% EXACT = exact(ll,tt);
-% EXACT_CFS = trigtech.vals2coeffs( trigtech.vals2coeffs( EXACT ).' ).'; 
-% surf(ll, tt ,EXACT ,'edgealpha',0,'facecolor','interp'), 
-% ylim([-pi pi])
-% 
-% subplot(1,3,3)
-% surf(ll, tt, abs( VALS - EXACT ),'edgealpha',0,'facecolor','interp' )
+u = spherefun.coeffs2spherefun( CFS ); 
 
 end
