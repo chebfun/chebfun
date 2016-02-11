752749db9dae378b3228451bf9eee0ed469ac5d1
gradywright <gradywright@boisestate.edu>
2016-02-11 11:53:22 -0700

    Update help text.

--- @spherefun/norm.m
+++ @spherefun/norm.m
@@ -1,13 +1,13 @@
 function [ normF, normloc ] = norm( f, p )
 %NORM   Norm of a SPHEREFUN
 % 
-%    SPHEREFUN(F) = sqrt(integral of abs(F)^2).
-%    SPHEREFUN(F, 2) = largest singular value of F.
-%    SPHEREFUN(F,'fro') is the same as NORM(F).
-%    SPHEREFUN(F, 1) = NOT SUPPORTED.
-%    SPHEREFUN(F, inf) = global maximum in absolute value.
-%    SPHEREFUN(F, max) = global maximum in absolute value.
-%    SPHEREFUN(F, min) = NOT SUPPORTED
+%    NORM(F) = sqrt(integral of abs(F)^2).
+%    NORM(F, 2) = same as NORM(F).
+%    NORM(F,'fro') is the same as NORM(F).
+%    NORM(F, 1) = NOT SUPPORTED.
+%    NORM(F, inf) = global maximum in absolute value.
+%    NORM(F, max) = global maximum in absolute value.
+%    NORM(F, min) = NOT SUPPORTED
 %
 % Furthermore, the inf norm for SPHEREFUN objects also returns a second output,
 % giving a position where the max occurs.
