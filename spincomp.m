32bdf32f328af0cd4d7a7b139f8894a3cacf06f8
Hadrien Montanelli <montanelli@maths.ox.ac.uk>
2016-03-28 19:19:32 +0100

    A few modifications in SPINCOMP.

--- spincomp.m
+++ spincomp.m
@@ -44,7 +44,7 @@ nSchemes = length(schemes);
 timesteps = pref.dt;
 nTimesteps = length(timesteps);
 
-% Convert to CHEBMATRIX:
+% Convert the initial conditiona to a CHEBMATRIX:
 if ( isa(u0, 'chebfun') == 1 || isa(u0, 'chebfun2') == 1 || ...
     isa(u0, 'chebfun3') == 1)
     u0 = chebmatrix(u0);
@@ -57,7 +57,7 @@ elseif ( isa(u0, 'chebfun2v') == 1 || isa(u0, 'chebfun3v') == 1 )
 end
 nVars = length(u0);
 
-% Create a grid:
+% Create a grid to compare exact and computed solutions:
 N = 32;
 dom = u0{1}.domain;
 xx = trigpts(N, dom);
@@ -71,8 +71,8 @@ elseif ( isa(u0{1}, 'chebfun3') == 1 )
     [xx, yy, zz] = meshgrid(xx);
 end
     
-% We estimate the exact solution using a very small time step (half the smallest
-% time step given) and using PECEC635 (6th-order multistep scheme):
+% First, estimate the exact solution using a very small time step (half the 
+% smallest time-step) and with PECEC736 (7th-order multistep scheme):
 if ( isa(pref, 'spinpref') == 1 )
     prefuexact = spinpref();
 elseif ( isa(pref, 'spinpref2') == 1 )
@@ -80,7 +80,7 @@ elseif ( isa(pref, 'spinpref2') == 1 )
 elseif ( isa(pref, 'spinpref3') == 1 )
     prefuexact = spinpref3();
 end
-prefuexact.scheme = 'pecec635';
+prefuexact.scheme = 'pecec736';
 prefuexact.dt = min(timesteps)/2;
 prefuexact.plot = 'off';
 prefuexact.M = pref.M;
@@ -89,6 +89,8 @@ uexact = spinoperator.solvepde(pdechar, tspan, u0, prefuexact);
 if ( isa(uexact, 'chebmatrix') == 0 )
     uexact = chebmatrix(uexact);
 end
+
+% Scale (i.e., maximum amplitude) of the exact solution:
 scale = 0;
 for i = 1:nVars
     if ( dim == 1 )
@@ -100,7 +102,7 @@ for i = 1:nVars
     end
 end
 
-% Now, compute the solutions for the different schemes and different time-steps:
+% Second, compute solutions for different schemes and time-steps:
 prefu = spinpref();
 prefu.plot = 'off';
 prefu.M = pref.M;
@@ -116,15 +118,19 @@ for k = 1:nTimesteps
         if ( isa(u, 'chebmatrix') == 0 )
             u = chebmatrix(u);
         end
-        for i = 1:nVars
-            if ( dim == 1 )
-                temp = max(abs(u{i}(xx) - uexact{i}(xx)));
-                err(k,l) = max(err(k,l), temp);
-            elseif( dim == 2 )
-                temp = max(max(abs(u{i}(xx,yy) - uexact{i}(xx,yy))));
-                err(k,l) = max(err(k,l), temp);
-            elseif ( dim == 3 )
-                temp = max(max(max(abs(u{i}(xx,yy,zz) - uexact{i}(xx,yy,zz)))));
+        isNan = cell2mat(cellfun(@(C) isnan(C), u.blocks, 'UniformOutput', 0));
+        if ( any(isNan) == 1 )
+            err(k,l) = NaN;
+        else
+            for i = 1:nVars
+                if ( dim == 1 )
+                    temp = abs(u{i}(xx) - uexact{i}(xx));
+                elseif( dim == 2 )
+                    temp = abs(u{i}(xx,yy) - uexact{i}(xx,yy));
+                elseif ( dim == 3 )
+                    temp = abs(u{i}(xx,yy,zz) - uexact{i}(xx,yy,zz));
+                end
+                temp = max(temp(:));
                 err(k,l) = max(err(k,l), temp);
             end
         end
@@ -152,8 +158,8 @@ for l = 1:nSchemes
 end
 left = 10^floor(log10(min(timesteps)));
 right = 10^ceil(log10(max(timesteps)));
-down = min(1e-12, 10^floor(log10(min(err(:)))));
-up = min(max(1, 10^ceil(log10(max(err(:))))), 1e2);
+down = max(min(1e-10, 10^floor(log10(min(err(:))))), 1e-12);
+up = min(max(1e0, 10^ceil(log10(max(err(:))))), 1e2);
 set(gca, 'fontsize', 16), axis([left right down up])
 xlabel('Relative time-step'), ylabel(sprintf('Relative error at t = %.3f', TF))
 legend(labels, 'Location', 'NorthWest')
