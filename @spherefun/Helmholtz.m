81882dd1cbf7f25043697fa5f0e16fc7df65e91c
Hadrien Montanelli <montanelli@maths.ox.ac.uk>
2016-03-29 18:35:45 +0100

    Various coding style and documentation improvments in POISSION and HELMOLTZ.

--- @spherefun/Helmholtz.m
+++ @spherefun/Helmholtz.m
@@ -1,12 +1,12 @@
 function u = Helmholtz( f, K, m, n )
-%HELMHOLTZ  Fast Helmholtz solver for the sphere
+%HELMHOLTZ   Fast Helmholtz solver for the sphere.
 %
-% U = HELMHOLTZ( F, K, N ) solves U_xx + U_yy + U_zz + K^2U = F on the sphere 
-% for U with a discretization of size NxN. F should be a SPHEREFUN and the
-% solution is returned as a SPHEREFUN. 
+%    U = HELMHOLTZ(F, K, N) solves U_xx + U_yy + U_zz + K^2U = F on the sphere 
+%    for U with a discretization of size N x N. F should be a SPHEREFUN and the
+%    solution is returned as a SPHEREFUN. 
 %
-% HELMHOLTZ( F, K, M, N) same as HELMHOLTZ( F, K, N ), but with a
-% discretization of size M x N.
+%    HELMHOLTZ(F, K, M, N) same as HELMHOLTZ(F, K, N), but with a
+%    discretization of size M x N.
 
 % Copyright 2016 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
@@ -16,40 +16,47 @@ function u = Helmholtz( f, K, m, n )
 % METHOD: Spectral method (in coeff space). We use the Fourier basis in
 % the theta- and lambda-direction.
 %
-% LINEAR ALGEBRA: Matrix equations.  The matrix equation decouples into n
+% LINEAR ALGEBRA: Matrix equations. The matrix equation decouples into N
 % linear systems. This form banded matrices.
 %
-% SOLVE COMPLEXITY:    O( m*n )  N = m*n = total degrees of freedom
+% SOLVE COMPLEXITY:    O(M*N)  with M*N = total degrees of freedom
 
-% Solve standard Helmholtz equation.  This parameter is kept for developers.
+% Solve standard Helmholtz equation. This parameter is kept for developers.
 c = 1;  
 
 if ( K == 0 )
-    u = spherefun.Poisson( f, 0, m, n );
+    u = spherefun.Poisson(f, 0, m, n);
     return
 end
 
 % Construct useful spectral matrices:
-Im = speye( m );
+Im = speye(m);
+
 % Please note that DF1m here is different than trigspec.diff(m,1) because we 
 % take the coefficient space point-of-view and set the (1,1) entry to be 
 % nonzero.
 DF1m = (1i)*spdiags((-floor(m/2):1:ceil((m-2)/2))', 0, m, m);
 DF2m = (1i)^2*spdiags((-floor(m/2):1:ceil((m-2)/2))', 0, m, m).^2;
 DF2n = (1i)^2*spdiags((-floor(n/2):1:ceil((n-2)/2))', 0, n, n).^2;
-Mcossin = spdiags( .25i*[-ones(m,1) ones(m,1)], [-2 2], m ,m ); % multiplication for sin(theta).*cos(theta)
-Msin2 = spdiags( .5*[-.5*ones(m,1) ones(m,1) -.5*ones(m,1)], [-2 0 2], m ,m );% multiplication for sin(theta)^2
+
+% Multiplication for sin(theta).*cos(theta):
+Mcossin = spdiags(.25i*[-ones(m, 1) ones(m,1)], [-2 2], m, m); 
+
+% Multiplication for sin(theta)^2:
+Msin2 = spdiags(.5*[-.5*ones(m, 1) ones(m,1) -.5*ones(m,1)], [-2 0 2], m, m);
 
 % Underlying discretization grid:
-lam0 = linspace(-pi, pi, n+1)'; lam0( end )=[];
-th0 = linspace(-pi, pi, m+1)'; th0( end )=[];
+lam0 = linspace(-pi, pi, n+1)'; 
+lam0( end )=[];
+th0 = linspace(-pi, pi, m+1)'; 
+th0( end )=[];
 
 % Forcing term:
 if ( isa(f, 'function_handle') )
-    [rhs_lam, rhs_th] = meshgrid( lam0, th0 );
-    F = feval( f, rhs_lam, rhs_th );      % Get (trigvals,trigvals) of rhs
-    F = trigtech.vals2coeffs( F );         % Get in Fourier basis
-    F = trigtech.vals2coeffs( F.' ).';
+    [rhs_lam, rhs_th] = meshgrid(lam0, th0);
+    F = feval(f, rhs_lam, rhs_th);      % Get (trigvals,trigvals) of rhs
+    F = trigtech.vals2coeffs(F);        % Get in Fourier basis
+    F = trigtech.vals2coeffs(F.').';
 elseif ( isa(f, 'spherefun') )
     F = coeffs2(f, n, m);
 end
@@ -65,23 +72,18 @@ int_const = en*F(:,k)/K^2;
 % Multiple rhs by sin(th)^2 and divide by K^2: 
 F = Msin2 * F / K^2;
 
-% Want to solve        X L^T   +    X DF^T = F
-%
-%   subject to     zero integral constraints, i.e.,  w^T X_0  = 0
+% Want to solve        
+%    X L^T + X DF^T = F
+% subject to zero integral constraints, i.e.,  w^T X_0  = 0.
 
 % Note that the matrix equation decouples because DF is diagonal.
 
 % Solve decoupled matrix equation for X, one row at a time:
 CFS = zeros(m, n);
-
 L = c*(Msin2*DF2m + Mcossin*DF1m)/K^2 + Msin2;
-
-scl = c*diag( DF2n )/K^2;
-
+scl = c*diag(DF2n)/K^2;
 for k = [floor(n/2):-1:1 floor(n/2)+2:n]
-    
-    CFS(:,k) = ( L + scl(k)*Im ) \ F(:,k);
-    
+    CFS(:,k) = (L + scl(k)*Im) \ F(:,k);
 end
 
 % Now, do zeroth mode:
@@ -90,6 +92,6 @@ ii = [1:floorm floorm+2:m];
 CFS(:, k) = [ en ; L( ii, :) ] \ [ int_const ; F(ii, k) ];
 
 % Now, convert to a spherefun object: 
-u = spherefun.coeffs2spherefun( CFS ); 
+u = spherefun.coeffs2spherefun(CFS); 
 
 end
--- @spherefun/Poisson.m
+++ @spherefun/Poisson.m
@@ -1,24 +1,23 @@
-function u = Poisson( f, int_const, m, n )
-%POISSON     Fast Poisson solver for the sphere
+function u = Poisson(f, const, m, n)
+%POISSON   Fast Poisson solver for the sphere.
 %
-% POISSON(f, const, n ) solves
+%   POISSON(F, CONST, N) solves
 %
-%  sin(th)^2u_thth   + sin(th)cos(th)u_th +  u_{lam,lam}  =  sin(th)^2 * f
+%     sin(th)^2u_{th,th} + sin(th)cos(th)u_th + u_{lam,lam} = sin(th)^2*f
 %
-% on the unit sphere written in spherical coordinates (lam, th)
-% with integral condition  sum2( u ) = const with a discretization of
-% size N x N.
+%   on the unit sphere written in spherical coordinates (lam, th)
+%   with integral condition sum2(u) = CONST with a discretization of size N x N.
 %
-% POISSON(f, const, m, n ) same as POISSON(f, const, n), but with a
-% discretization of size m x n.
+%   POISSON(F, CONST, M, N) is the same as POISSON(F, CONST, N), but with a
+%   discretization of size M x N.
 %
 % EXAMPLE:
-%  f = @(lam,th) -6*(-1+5*cos(2*th)).*sin(lam).*sin(2*th);
-%  exact = @(lam,th) -2*sin(lam).*sin(2*th).*sin(th).^2 -...
-%            sin(lam).*sin(th).*cos(th) + .5*sin(lam).*sin(2*th).*cos(2*th);
-%  int_const = 0;
-%  u = spherefun.Poisson( f, int_const, 100);
-%  norm( spherefun(exact) - u )
+%   f = @(lam,th) -6*(-1+5*cos(2*th)).*sin(lam).*sin(2*th);
+%   exact = @(lam,th) -2*sin(lam).*sin(2*th).*sin(th).^2 -...
+%             sin(lam).*sin(th).*cos(th) + .5*sin(lam).*sin(2*th).*cos(2*th);
+%   const = 0;
+%   u = spherefun.Poisson(f, const, 100);
+%   norm(spherefun(exact) - u)
 
 % Copyright 2016 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
@@ -28,14 +27,11 @@ function u = Poisson( f, int_const, m, n )
 % METHOD: Spectral method (in coeff space). We use the Fourier basis in
 % the theta- and lambda-direction.
 %
-% LINEAR ALGEBRA: Matrix equations.  The matrix equation decouples into n
+% LINEAR ALGEBRA: Matrix equations. The matrix equation decouples into n
 % linear systems. This form banded matrices.
 %
-% SOLVE COMPLEXITY:    O( m*n )  N = m*n = total degrees of freedom
+% SOLVE COMPLEXITY:    O(M*N)  with M*N = total degrees of freedom
 %
-%  Alex Townsend, July 2015.
-
-% DEVELOPERS NOTES 
 % This is designed to be almost a standalone script to reduce overhead 
 % so that it can be used for the numerical simulation of time-dependent 
 % PDEs, where this comment is executed hundreds of times. 
@@ -43,6 +39,7 @@ function u = Poisson( f, int_const, m, n )
 if ( nargin < 4 )
     n = m;
 end
+
 % Construct useful spectral matrices:
 % Please note that DF1m is different than trigspec.diff(m,1) because we 
 % take the coefficient space point-of-view and set the (1,1) entry to be 
@@ -50,66 +47,68 @@ end
 DF1m = (1i)*spdiags((-floor(m/2):1:ceil((m-2)/2))', 0, m, m);
 DF2m = (1i)^2*spdiags((-floor(m/2):1:ceil((m-2)/2))', 0, m, m).^2;
 DF2n = (1i)^2*spdiags((-floor(n/2):1:ceil((n-2)/2))', 0, n, n).^2;
-% multiplication for sin(theta).*cos(theta)
-Mcossin = spdiags( .25i*[-ones(m,1) ones(m,1)], [-2 2], m ,m ); 
-% multiplication for sin(theta)^2
-Msin2 = spdiags( .5*[-.5*ones(m,1) ones(m,1) -.5*ones(m,1)], [-2 0 2], m ,m );
-Im = speye( m );
-scl = diag( DF2n ); 
+
+% Multiplication for sin(theta).*cos(theta):
+Mcossin = spdiags(.25i*[-ones(m, 1) ones(m, 1)], [-2 2], m, m); 
+
+% Multiplication for sin(theta)^2:
+Msin2 = spdiags(.5*[-.5*ones(m, 1) ones(m, 1) -.5*ones(m, 1)], [-2 0 2], m, m);
+Im = speye(m);
+scl = diag(DF2n); 
 
 % There is a factor of 4 speed up here, by taking account of real 
 % solution, and even/odd symmetry.
 
 % Underlying discretization grid:
-lam0 = linspace(-pi, pi, n+1)'; lam0( end )=[];
-th0 = linspace(-pi, pi, m+1)'; th0( end )=[];
+lam0 = linspace(-pi, pi, n+1)'; 
+lam0(end) = [];
+th0 = linspace(-pi, pi, m+1)'; 
+th0(end) = [];
 
 % Forcing term:
 if ( isa(f, 'function_handle') )
-    [rhs_lam, rhs_theta] = meshgrid( lam0, th0 );
-    F = feval( f, rhs_lam, rhs_theta );
-    F = trigtech.vals2coeffs( F );
-    F = Msin2 * trigtech.vals2coeffs( F.' ).';
+    [rhs_lam, rhs_theta] = meshgrid(lam0, th0);
+    F = feval(f, rhs_lam, rhs_theta);
+    F = trigtech.vals2coeffs(F);
+    F = Msin2*trigtech.vals2coeffs(F.').';
 elseif ( isa(f, 'spherefun') )
-    F = Msin2 * coeffs2(f, n, m);
+    F = Msin2*coeffs2(f, n, m);
 elseif ( isa( f, 'double' ) )
-    F = Msin2 * f;       % Get trigcoeffs2 of rhs.
+    F = Msin2*f;       % Get trigcoeffs2 of rhs.
 end
 
 % Matrix for solution's coefficients:
-CFS = zeros( m, n );
+CFS = zeros(m, n);
 
 % Form discretization of the theta-dependent operator:
 L = Msin2*DF2m + Mcossin*DF1m;
 
-% All these modes are even:
+% Solve for the even modes:
 k_even = [floor(n/2)-1:-2:1 floor(n/2)+3:2:n];
 for k = k_even
-    
-    CFS(:, k) = ( L + scl(k)*Im ) \ F(:,k);
-    
+    CFS(:,k) = (L + scl(k)*Im) \ F(:,k);
 end
 
-% All these modes are odd:
+% Solve for the odd modes:
 k_odd = [floor(n/2):-2:1 floor(n/2)+2:2:n];
 for k = k_odd
-    
-    CFS(:, k) = ( L + scl(k)*Im ) \ F(:,k);
-    
+    CFS(:,k) = (L + scl(k)*Im) \ F(:,k);
 end
 
 % Now do the equation where we need the integral constraint:
-% We will take  X_{n/2+1,:} en = int_const.
+% We will take X_{n/2+1,:} en = const.
+
 % First, let's project the rhs to have mean zero:
-k = floor(n/2)+1;
+k = floor(n/2) + 1;
 floorm = floor(m/2);
 mm = (-floorm:ceil(m/2)-1);
 en = 2*pi*(1+exp(1i*pi*mm))./(1-mm.^2);
 en([floorm, floorm + 2]) = 0;
 ii = [1:floorm floorm+2:m];
-F(floor(m/2)+1,k) = -en(ii) * F(ii, k )./en(floor(m/2)+1);
-CFS(:, k) = [ en ; L( ii, :) ] \ [ int_const ; F(ii, k) ];
+F(floor(m/2)+1,k) = -en(ii)*F(ii, k)./en(floor(m/2)+1);
 
-u = spherefun.coeffs2spherefun( CFS ); 
+% Second, solve: 
+CFS(:, k) = [ en ; L( ii, :) ] \ [ const ; F(ii, k) ];
+u = spherefun.coeffs2spherefun(CFS); 
 
 end
