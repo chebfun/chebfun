8109ff862b5dc2ef10316be2041670e3efd433b0
gradywright <gradywright@boisestate.edu>
2016-03-31 16:27:33 -0600

    Test the spherefunv deals with empty spherefunv objects.

---
+++ tests/spherefunv/test_empty.m
@@ -0,0 +1,29 @@
+function pass = test_empty( pref ) 
+% For empty spherfunv objects, does each command deal with them
+% appropriately?
+
+%% 
+% Check that the spherefunv commands work for empty Spherefunv objects. 
+
+pass = 1; 
+F = spherefunv;
+try 
+    cross(F,F);
+    F.';
+    curl(F);
+    divergence(F);
+    dot(F,F);
+    F(1,1);
+    isempty(F);
+    F-F;
+    F.*F;
+    norm(F);
+    F+F;
+    F.^2;
+    F*F;
+    F';
+catch
+   pass = 0;  
+end
+
+end
