f5465276532a54967fff1f571318e7989483e759
bhashemi <hashemi@maths.ox.ac.uk>
2016-04-15 16:57:28 +0100

    added 'outerProd' for outer-product of 1D chebfuns to form a chebfun3 object

--- @chebfun2/sample.m
+++ @chebfun2/sample.m
@@ -30,8 +30,12 @@ end
 
 % Use CDR decomposition so we can keep it in low rank form: 
 [C, D, R] = cdr( f ); 
-Cvals = sample(chebfun(C), m);
-Rvals = sample(chebfun(R), n);
+% Cvals = sample(chebfun(C), m);
+% Rvals = sample(chebfun(R), n);
+% See https://github.com/chebfun/chebfun/issues/1808
+Cvals = sample(C, m);
+Rvals = sample(R, n);
+
 
 % Evaluate: 
 if ( nargout <= 1 )
