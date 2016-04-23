6e38a0a531397bfb041ad2d5778425110d18829d
gradywright <gradywright@boisestate.edu>
2016-04-23 09:59:02 -0600

    New roots test.

--- tests/spherefun/test_roots.m
+++ tests/spherefun/test_roots.m
@@ -4,7 +4,7 @@ function pass = test_roots( pref )
 if ( nargin < 1 ) 
     pref = chebfunpref; 
 end
-tol = 10*pref.cheb2Prefs.chebfun2eps;
+tol = 1e5*pref.cheb2Prefs.chebfun2eps;
 
 % More extensive testing is needed.  Here we just test that errors aren't
 % thrown and the form of the output is reasonable.
@@ -26,5 +26,19 @@ if isempty(r)
 else
     pass(2) = 0;
 end
+
+% Zero contour at the equator
+f = spherefun(@(x,y,z) z);
+r = roots(f);
+% First component of roots should be cos(pi*lam) (or it's negative).
+r1true = chebfun(@(lam) cos(pi*lam));
+pass(3) = ( ( norm(r{1}(:,1)-r1true) < tol ) || ...
+            ( norm(r{1}(:,1)+r1true) < tol ) );
+% Second component of roots should be sin(pi*lam) (or it's negative).
+r2true = chebfun(@(lam) sin(pi*lam));
+pass(4) = ( ( norm(r{1}(:,2)-r2true) < tol ) || ...
+            ( norm(r{1}(:,2)+r2true) < tol ) );
+% Third component of roots should be zero.
+pass(5) = ( norm(r{1}(:,3)) < tol );
 
 end
