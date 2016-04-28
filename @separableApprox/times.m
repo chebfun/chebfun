75b21356beca4c80cfffe96fcd8504da0143cb7a
ajt60gaibb <alex.townsend1987@gmail.com>
2016-04-28 15:30:47 -0400

    Let's only use Grady's idea for rank1.*chebfun2.

--- @separableApprox/times.m
+++ @separableApprox/times.m
@@ -14,21 +14,23 @@ if ( isa(f, 'separableApprox') )    % SEPARABLEAPPROX .* ???
     elseif ( isa( g, 'separableApprox') )
         bol = domainCheck(f, g);
         if ( bol )
-            [mf, nf] = length(f); 
-            [mg, ng] = length(g);
-            m = max(mf, mg);
-            n = max(nf, ng); 
-            [Cf, Df, Rf] = coeffs2(f, m, n); 
-            [Cg, Dg, Rg] = coeffs2(g, m, n);
-            Cf = chebtech2.coeffs2vals( Cf ); 
-            Rf = chebtech2.coeffs2vals( Rf ); 
-            Cg = chebtech2.coeffs2vals(Cg)*sqrt(Dg);
-            Rg = chebtech2.coeffs2vals(Rg)*sqrt(Dg); 
-            H = zeros(m, n); 
-            for k = 1:size(Cg,2)
-               H = H + (bsxfun(@times, Cf, Cg(:,k)))*Df*(bsxfun(@times, Rf, Rg(:,k)))'; 
+            % Grady's faster times for rank 1 functions: 
+            if ( length( f ) == 1 ) 
+                [C, D, R] = cdr( f ); 
+                C = C*sqrt(D); 
+                R = R*sqrt(D);
+                h = g; 
+                for k = 1:length( g ) 
+                    h.cols(:,k) = C.*(g.cols(:,k)); 
+                    h.rows(:,k) = R.*(g.rows(:,k)); 
+                end
+
+            elseif ( length( g ) == 1 ) 
+                 h = times(g, f);
+            else
+                % Give up, call the constructor: 
+                h = compose(f, @times, g); 
             end
-            h = chebfun2( H );
         else
             error('CHEBFUN:SEPARABLEAPPROX:times:domain', 'Inconsistent domains');
         end
