3c08315ccd529f3fa1b336aa1406c2a5df43682e
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2016-05-09 15:42:46 +0100

    lots of little changes to gallery3

--- +cheb/gallery3.m
+++ +cheb/gallery3.m
@@ -1,41 +1,40 @@
 function varargout = gallery3(name)
 %CHEB.GALLERY3   CHEBFUN3 example functions.
 %
-%   [F, FA] = CHEB.GALLERY3(NAME) also returns the anonymous function FA 
-%   used to define the function. 
-%   CHEB.GALLERY3 with no output argument creates a plot of the selected
-%   function:
+%   F = CHEB.GALLERY3(NAME) returns a chebfun3 corresponding to NAME.
+%   The domain is often but not always [-1,1]^3.
+%   See the listing below for available names.
 %
-%   genz1        An oscillatory integration test function
-%   genz2        An integration test function with a corner peak
-%   genz3        Another integration test function with a Gaussian peak
-%   genz4        An integration test function with a product peak
-%   griewank     A global minimization problem with many widespread local 
-%                minima. It's global minimum is at origin with value zero.
-%  rastrigin     Another global minimization test problem with lots of
-%                local minima. It's global minimum is at origin with value zero.
-%   runge        A 3D Runge function
-%   wagon        A challenging optimization problem suggested by Wagon
-%   shubert      Shubert function from global optimization
-%   rosenbrock   Rosenbrock function from global optimization. It's global
-%                minimum is zero at (1,1,1).
-%   levy         Levy function. Another global minimization test problem
-%                with minimum value zero at (1,1,1).
-%   aurentz      A 3D radial bump function suggested by Aurentz
-%   clebsch      Clebsch Diagonal Cubic from algebraic geometry
+%   [F, FA] = CHEB.GALLERY3(NAME) also returns the anonymous function FA 
+%   used to define the chebfun3.
+%
+%   CHEB.GALLERY3 with no output argument creates a plot of the selected
+%   function. 
+%
+%   Below, the functions are listed with suggestions for good
+%   viewing in the form e.g. [plot(f)].  For more details about
+%   the functions, see the code and associated comments.
+%
+%   aurentz      A 3D radial bump function [scan(f)]
+%   barth10      Barth decic from algebraic geometry
+%   bessel       Bessel function of radius r
 %   cassini      Cassini Surface from algebraic geometry
-%   barth10      Barth Decic from algebraic geometry
-%   wave1        A standing wave function
-%   wave2        A travelling wave function
-%   slevinsky    A function related to the heat and wave equations 
-%                suggested by Slevinsky
-%   Also, the following three functions 
-%   3dxy, 
-%   4px,
-%   32m1,
-%   5gz3y, 
-%   are a few of the complete wave functions of the Hydrogen atom which 
-%   represent its orbitals.
+%   clebsch      Clebsch diagonal cubic from alg. geom. [isosurface(f,0)]
+%   genz1        Oscillatory integration test fun. [plot(f)]
+%   genz2        Cubature test function with corner peak [surf(f)]
+%   genz3        Cubature test function with Gaussian peak [surf(f)]
+%   genz4        Cubature test function with product peak [surf(f)]
+%   griewank     Function with many local minima.  The global
+%                min. is 0 at (0,0,0). [plot(f)]
+%   levy         Function with global min. 0 at (1,1,1) [surf(f)]
+%   rastrigin    Function with many local minima.  The global
+%                min. is 0 at (0,0,0).  [isosurface(f)]
+%   rosenbrock   Function with global min 0 at (1,1,1).
+%                [contour(f(:,:,1),0:10:100)]
+%   runge        3D Runge function [slice(f)]
+%   shubert      Function with many local minima.  [scan(f)]
+%   wagon        3D generalization of "SIAM 100 Digit Challenge" function
+%                [contourf(f(-.16,:,:))]
 %
 %   Gallery functions are subject to change in future releases of Chebfun.
 %
@@ -49,8 +48,7 @@ function varargout = gallery3(name)
 if ( nargin == 0 )
     names = {'wagon', 'runge', 'clebsch', 'cassini', 'barth10', ...
         'shubert', 'rosenbrock', 'aurentz', 'genz1', 'genz2', 'genz3', ...
-        'genz4', 'griewank', 'rastrigin', 'wave1', 'wave2', 'slevinsky', ...
-        '3dxy', '4px', '32m1', '5gz3y'};
+        'genz4', 'griewank', 'rastrigin', 'bessel'};
     name = names{randi(length(names))};
 end
 
@@ -91,6 +89,7 @@ switch lower(name)
     case 'wagon'
         % Function from Page 99 of F. Bornemann, D. Laurie, S. Wagon and J. 
         % Waldvogel, The SIAM 100-Digit Challenge, SIAM, 2004.
+        % This function was proposed by Stan Wagon.
         fa = @(x,y,z) exp(sin(50*x)) + sin(60*exp(y)).*sin(60*z) + ...
             sin(70*sin(x)).*cos(10*z) + sin(sin(80*y)) - sin(10*(x+z)) +...
             (x.^2 + y.^2 + z.^2)/4;
@@ -129,19 +128,7 @@ switch lower(name)
         f = chebfun3(fa);
         % Try e.g., scan(f), or surf(f)  
         
-    case 'wave1'
-        % A standing wave function
-        fa = @(x,y,z) 3*sin(2*x) .* sin(3*y) .* sin(z);
-        f = chebfun3(fa);
-        
-    case 'wave2'
-        % A travelling wave function
-        fa = @(x,y,t) 3*sin(t + 2*x - 3*y);
-        f = chebfun3(fa);
-        
-    case 'slevinsky'
-        % Function suggested by Mikael Slevinsky related to the heat and
-        % wave equations
+    case 'bessel'
         fa = @(x,y,z) besselj(0,10*sqrt(x.^2+y.^2+z.^2));
         f = chebfun3(fa);
         % Try e.g., surf(f)
@@ -155,7 +142,7 @@ switch lower(name)
         f = chebfun3(fa, [-10 10 -10 10 -10 10]);
         % Try e.g. surf(f)
 
-       case 'rastrigin'
+    case 'rastrigin'
            % Rastrigin Function (http://www.sfu.ca/~ssurjano/rastr.html)
            fa = @(x,y,z) 30 + (x.^2-10*cos(2*pi*x)) + ...
                (y.^2-10*cos(2*pi*y)) + (z.^2-10*cos(2*pi*z));
@@ -192,54 +179,6 @@ switch lower(name)
         f = chebfun3(fa);
         % Try e.g. isosurface(f,0)           
            
-    case '3dxy'
-        % http://csi.chemie.tu-darmstadt.de/ak/immel/script/redirect.cgi?filename=http://csi.chemie.tu-darmstadt.de/ak/immel/tutorials/orbitals/index.html
-        const = (1/sqrt(4*pi))*sqrt(60/4)*(1*(9*sqrt(30)));
-        z0 = 31;
-        %z0 = 2;
-        fa = @(x,y,z) const * 4./9 * x.*y .* exp(-sqrt(x.^2 + y.^2 + (z-z0).^2)./3);
-        %f = chebfun3(fa);
-        f = chebfun3(fa, [-30 30 -30 30 -30 30]);
-        % Try e.g. isosurface(f) or isosurface(f, [+0.0005 -0.0005])
-        
-    case '4px'
-        % http://csi.chemie.tu-darmstadt.de/ak/immel/script/redirect.cgi?filename=http://csi.chemie.tu-darmstadt.de/ak/immel/tutorials/orbitals/index.html
-        const = sqrt(3/(4*pi))*(1/(64*sqrt(15)));
-        z0 = 31;
-        fa = @(x,y,z) const * x .* (20 - 5 * sqrt(x.^2 + y.^2 + (z-z0).^2) + ...
-            ((x.^2 + y.^2 + (z-z0).^2))/4) .* ...
-                    exp(-sqrt(x.^2 + y.^2 + (z-z0).^2)./4);
-        f = chebfun3(fa, [-30 30 -30 30 -30 30]);
-        % Try e.g. isosurface(f, [0.0005 -0.0005])
-    
-    case '32m1'
-        % psi_{3,2,-1}, i.e., n = 3, l = 2, m = -1:
-        % https://en.wikiversity.org/wiki/Quantum_mechanics/The_hydrogen_atom#Eigenvalues_and_eigenfunctions_of_the_hydrogen_atom
-        %psi32_1 = @(r, theta, phi) const * r.^2  .* exp(-r/(3*a0)) .* 
-        %                           sin(theta) .* cos(theta) .* exp(-i*phi)
-        a0 = 0.0529; % The Bohr radius in nanometer
-        %z0 = 31;
-        z0 = 2;
-        const = 2*sqrt(2)/(81*sqrt(15*a0^7))*sqrt(15/(8*pi));
-        fa = @(x, y, z) const * z .* (x + i*y) .* ...
-            exp(-sqrt(x.^2 + y.^2 + (z-z0).^2)/(3*a0));
-        f = chebfun3(fa);
-        %f = chebfun3(fa, [-30 30 -30 30 -30 30]);
-        % Try e.g. isosurface(f, [-0.005 0.005])
-        
-    case '5gz3y'
-        % Cartesian Wave Function - 5gz3y
-        % http://csi.chemie.tu-darmstadt.de/ak/immel/script/redirect.cgi?filename=http://csi.chemie.tu-darmstadt.de/ak/immel/tutorials/orbitals/index.html
-        n = 5; 
-        z0 = 31;
-        const = 1/(900*sqrt(70));
-        fa = @(x,y,z) const * y.*z .* (4*(z-z0).^2 - 3*x.^2 - 3*y.^2)...
-            ./sqrt(x.^2 + y.^2 + (z-z0).^2).^4 .* ...
-            (2./n .* sqrt(x.^2 + y.^2 + (z-z0).^2)).^4 .* ...
-            exp(-2/n*sqrt(x.^2 + y.^2 + (z-z0).^2)./2);
-        f = chebfun3(fa, [-30 30 -30 30 -30 30]);
-        % Try e.g., plot(f) or isosurface(f, [-0.005 0.005])
-        
     % Throw an error if the input is unknown.
     otherwise
         %error('CHEB:GALLERY3:unknown:unknownFunction', ...
@@ -256,108 +195,4 @@ else
 end
 
 end
-
-%% TODO: Add the following functions:
-
-% 3D Franke
-% animation(chebfun3s(@(x,y,z) 3/4*exp(-( (9*x-2).^2 + (9*y-2).^2 + (9*z-2).^2)/4) + 3/4*exp( -((9*x+1).^2)/49 - (9*y+1)/10 - (9*z+1)/10)...
-%     +1/2*exp(-( (9*x-7).^2 + (9*y-3).^2 + (9*z-5).^2)/4) - 1/5*exp( -(9*x-4).^2 - (9*y-7).^2 - (9*z-5).^2)))
-
-%%close all; animation(chebfun3s(@(x,y,z) x.^2 .* y.^2 .* z.^5),3) % (https://www.youtube.com/watch?v=w4ixBow28nc)
-
-
-% close all; animation(chebfun3s(@(x,y,z) exp(-10*(x.^2 + y.^2 + z.^2)) + exp(-2*((x-0.4).^2 + (y-0.3).^2 + (z-0.7).^2)) + exp(-20*((x+0.4).^2 + (y+0.8).^2 + (z-0.85).^2) )))
-
-%http://www.sfu.ca/~ssurjano/easom.html
-%ff = @(x,y,z) -cos(x).*cos(y).*cos(z).* exp(-(x-0.5).^2 -(y-0.5).^2 -(z-0.5).^2);
-
-% http://www.sfu.ca/~ssurjano/michal.html
-% m = 10; ff = @(x,y,z) -(sin(x).*(sin(x.^2)).^(2*m)) -(sin(y).*(sin(y.^2)).^(2*m)) -(sin(z).*(sin(z.^2)).^(2*m));
-
-%http://www.sfu.ca/~ssurjano/stybtang.html
-
-%http://www.sfu.ca/~ssurjano/detpep10curv.html
-%ff = @(x,y,z) 4 * (x - 2 + 8*y - 8*y.^2).^2 + (3 - 4*y).^2 + 16 * sqrt(z+1) .* (2*z-1).^2;
-
-%close all; animation(chebfun3s(@(x,y,z) sin(4*(x.^2 + y.^2 + z.^2))))
-
-%%
-%http://www.sfu.ca/~ssurjano/hart3.html
-% function y = hart3(x)
-% % 
-% % Hartmann function 
-% % Matlab Code by A. Hedar (Sep. 29, 2005).
-% % The number of variables n = 3.
-% % 
-% a(:,2)=10.0*ones(4,1);
-% for j=1:2;
-%    a(2*j-1,1)=3.0; a(2*j,1)=0.1; 
-%    a(2*j-1,3)=30.0; a(2*j,3)=35.0; 
-% end
-% c(1)=1.0;c(2)=1.2;c(3)=3.0;c(4)=3.2;
-% p(1,1)=0.36890;p(1,2)=0.11700;p(1,3)=0.26730;
-% p(2,1)=0.46990;p(2,2)=0.43870;p(2,3)=0.74700;
-% p(3,1)=0.10910;p(3,2)=0.87320;p(3,3)=0.55470;
-% p(4,1)=0.03815;p(4,2)=0.57430;p(4,3)=0.88280;
-% s = 0;
-% for i=1:4;
-%    sm=0;
-%    for j=1:3;
-%       sm=sm+a(i,j)*(x(j)-p(i,j))^2;
-%    end
-%    s=s+c(i)*exp(-sm);
-% end
-% y = -s;
-%%
-
-%% [TODO]: 
-% 1) Spherical harmonics: Choose some which have all 3 of theta, phi and r
-% and not only theta and phi.
-% https://en.wikipedia.org/wiki/Table_of_spherical_harmonics
-% Some plots are here:
-% https://en.wikipedia.org/wiki/Spherical_harmonics
-% As Jared suggests a reasonable way to plot these complex-valued functions 
-% Y_L^M is to plot abs((Y_L^M).^2) to get rid of complex values and also 
-% plot a physically meaningful concept. 
-% In this context we have x = r sin(theta) cos(phi), 
-%                         y = r sin(theta) sin(phi), and 
-%                         z = r cos(theta).
-% A good work to do is to first write an m-file which creates a function 
-% handle in terms of X, Y and Z for each Y_L^M (like output of gallery)
-% and then call chebfun3 on them.
-
-% 2) Hydrogen atom eigenfunctions as suggested by Mikael
-% https://en.wikipedia.org/wiki/Schr%C3%B6dinger_equation#Three-dimensional_examples#Hydrogen_atom
-
-% Each wave function can be factorized into the radial part R(r) and an 
-% angular part Y(theta, phi) which is a spherical harmonic, i.e.,
-%                                 psi(r, theta, phi) = R(r) * Y(theta, phi)
-%
-% The eigenvalue problem associated with the hydrogen atom is the following
-%               \hat{H} psi(r, theta, phi) = E psi(r, theta, phi)
-% where \hat{H} is the Hamiltonian ...
-% E := E_n = -0.5 \alpha^2 \mu c^2 / n^2
-% \alpha = fine structure constant = 1/137
-
-% a0 = 0.529; % The Bohr radius in angstroms
-%a0 = 0.529e-10; % The Bohr radius in meters
-
-% Each wave function has three quantum numbers:
-% n = principal quantum number (quantum number for the radial part r)
-% l = azimuthal quantum number
-% m = magnetic quantum number
-
-% nucleeus (i.e., proton in the case of Hydrogen atom) is fixed at the 
-% origin (x0, y0, z0), because it is much more heavier.
-% r = sqrt(x.^2 + y.^2 + z.^2); distance of the electron from the nucleus
-% (i.e, proton in case of Hydrogen atom)
-% theta = elevation
-% phi = azimuthal angle
-
-% x = r * sin(theta) * cos(phi)
-% y = r * sin(theta) * sin(phi)
-% z = r * cos(phi)
-% tan(theta) = sqrt(x^2+y^2) / z
-% tan(phi) = y / x
-%%
 
