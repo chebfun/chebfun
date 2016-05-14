10a1170cacd95d9323f4571f1e947c9ba0760889
bhashemi <hashemi@maths.ox.ac.uk>
2016-05-14 23:10:59 +0100

    added `test_laplacian` to `chebfun3v`
    and removed the wrong syntax in `chebfun3v/laplacian`

--- @chebfun3v/laplacian.m
+++ @chebfun3v/laplacian.m
@@ -1,28 +1,40 @@
-function L = laplacian( F )
-%LAPLACIAN Vector Laplacian of a CHEBFUN3V.
+function L = laplacian(F)
+%LAPLACIAN Vector Laplacian of a CHEBFUN3V object.
 %   LAPLACIAN(F) returns a CHEBFUN3V representing the vector Laplacian of F.
 %
-% See also CHEBFUN3V/LAP.
+%   See also CHEBFUN3V/LAP.
 
 % Copyright 2016 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
 
 % Empty check: 
-if ( isempty( F ) )
+if ( isempty(F) )
     L = chebfun3v;
     return
 end
 
-diff1 = diff(F,2,1); diff2 = diff(F,2,2); diff3 = diff(F,2,3);
-%G = diff(Fc{1}, 2, 1) + diff(Fc{2}, 2, 2) + diff(Fc{3}, 2,3); 
+diff1 = diff(F, 2, 1);
+diff2 = diff(F, 2, 2); 
+diff3 = diff(F, 2, 3);
 
 % laplacian = F_xx + F_yy + F_zz
 L = chebfun3v;
-[~, f_fiberDim1] = min( rank(diff1.components{1}) );
-[~, f_fiberDim2] = min( rank(diff1.components{2}) );
-[~, f_fiberDim3] = min( rank(diff1.components{3}) );
-L.components{1} = chebfun3(@(x,y,z) feval(diff1.components{1},x,y,z) + feval(diff2.components{1},x,y,z) + feval(diff3.components{1},x,y,z), 'fiberDim',f_fiberDim1(1));
-L.components{2} = chebfun3(@(x,y,z) feval(diff1.components{2},x,y,z) + feval(diff2.components{2},x,y,z) + feval(diff3.components{2},x,y,z), 'fiberDim',f_fiberDim2(1));
-L.components{3} = chebfun3(@(x,y,z) feval(diff1.components{3},x,y,z) + feval(diff2.components{3},x,y,z) + feval(diff3.components{3},x,y,z), 'fiberDim',f_fiberDim3(1));
+vscales1 = [vscale(diff1.components{1}) + vscale(diff2.components{1}), ...
+    vscale(diff3.components{1})];
+L.components{1} = chebfun3(@(x,y,z) feval(diff1.components{1},x,y,z) ...
+    + feval(diff2.components{1},x,y,z) + feval(diff3.components{1},x,y,z), ...
+    'vscaleBnd', vscales1, 'fiberDim', 3);
+
+vscales2 = [vscale(diff1.components{2}) + vscale(diff2.components{2}), ...
+    vscale(diff3.components{2})];
+L.components{2} = chebfun3(@(x,y,z) feval(diff1.components{2},x,y,z) + ...
+    feval(diff2.components{2},x,y,z) + feval(diff3.components{2},x,y,z),...
+    'vscaleBnd', vscales2, 'fiberDim', 3);
+
+vscales3 = [vscale(diff1.components{3}) + vscale(diff2.components{3}), ...
+    vscale(diff3.components{3})];
+L.components{3} = chebfun3(@(x,y,z) feval(diff1.components{3},x,y,z) + ...
+    feval(diff2.components{3},x,y,z) + feval(diff3.components{3},x,y,z), ...
+    'vscaleBnd', vscales3, 'fiberDim', 3);
 
 end
