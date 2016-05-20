555bcb2270a55669be97f9f4287d851599c92443
ajt60gaibb <alex.townsend1987@gmail.com>
2016-05-20 09:27:59 -0400

    Test to see if K = sqrt(l*(l+1)).

--- @spherefun/helmholtz.m
+++ @spherefun/helmholtz.m
@@ -1,15 +1,15 @@
 function u = helmholtz(f, K, m, n)
 %HELMHOLTZ   Fast Helmholtz solver for the sphere.
-%    U = HELMHOLTZ(F, K, N) solves U_xx + U_yy + U_zz + K^2U = F on the sphere 
+%    U = HELMHOLTZ(F, K, N) solves U_xx + U_yy + U_zz + K^2U = F on the sphere
 %    for U with a discretization of size N x N. F should be a SPHEREFUN and the
-%    solution is returned as a SPHEREFUN. 
+%    solution is returned as a SPHEREFUN.
 %
 %    HELMHOLTZ(F, K, M, N) same as HELMHOLTZ(F, K, N), but with a
 %    discretization of size M x N.
 %
-%  Example: 
-%    K = 100; m = 1000; n = m; 
-%    f = spherefun( @(x,y,z) cos(x.*y.*z) ); 
+%  Example:
+%    K = 100; m = 1000; n = m;
+%    f = spherefun( @(x,y,z) cos(x.*y.*z) );
 %    u = spherefun.helmholtz(f, K, m, n);
 %    plot( u )
 
@@ -27,16 +27,24 @@ function u = helmholtz(f, K, m, n)
 % SOLVE COMPLEXITY:    O(M*N)  with M*N = total degrees of freedom
 
 % Solve standard Helmholtz equation. This parameter is kept for developers.
-c = 1;  
+c = 1;
 
 % If the call is helmholtz(f, K, m), then set n
-if ( nargin < 4 ) 
+if ( nargin < 4 )
     n = m;
 end
 
 if ( K == 0 )
     u = spherefun.poisson(f, 0, m, n);
     return
+end
+
+% Check for eigenvalues:
+e = eig( [ -1 K^2 ; 1 0 ] ); % Is K = sqrt(l*(l+1)), where l is an integer?
+e = e(e>0); e = e(abs( e - round(e) ) < 1e-13 );
+if ( ~isempty( e ) )
+    error('SPHEREFUN:HELMHOLTZ:EIGENVALUE',...
+          'There are %u possible solutions.',2*e(1)+1)
 end
 
 % If m or n are non-positive then throw an error
@@ -59,18 +67,18 @@ m = m + mod(m,2);
 % Construct useful spectral matrices:
 Im = speye(m);
 
-% Please note that DF1m here is different than trigspec.diff(m,1) because we 
-% take the coefficient space point-of-view and set the (1,1) entry to be 
+% Please note that DF1m here is different than trigspec.diff(m,1) because we
+% take the coefficient space point-of-view and set the (1,1) entry to be
 % nonzero.
 DF1m = trigspec.diffmat(m, 1, 1);
-DF2m = trigspec.diffmat(m, 2); 
+DF2m = trigspec.diffmat(m, 2);
 DF2n = trigspec.diffmat(n, 2);
 
 % Multiplication for sin(theta).*cos(theta):
-% Below is equivalent to 
-% Mcossin = spdiags(.25i*[-ones(m, 1) ones(m, 1)], [-2 2], m, m); 
+% Below is equivalent to
+% Mcossin = spdiags(.25i*[-ones(m, 1) ones(m, 1)], [-2 2], m, m);
 cfs = trigtech(@(theta) sin(pi*theta).*cos(pi*theta));
-Mcossin = trigspec.multmat(m, cfs.coeffs); 
+Mcossin = trigspec.multmat(m, cfs.coeffs);
 
 % Multiplication for sin(theta)^2:
 % Below is equivalent to
@@ -79,9 +87,9 @@ cfs = trigtech(@(theta) sin(pi*theta).^2);
 Msin2 = trigspec.multmat(m, cfs.coeffs);
 
 % Underlying discretization grid:
-lam0 = linspace(-pi, pi, n+1)'; 
+lam0 = linspace(-pi, pi, n+1)';
 lam0( end )=[];
-th0 = linspace(-pi, pi, m+1)'; 
+th0 = linspace(-pi, pi, m+1)';
 th0( end )=[];
 
 % Forcing term:
@@ -94,7 +102,7 @@ elseif ( isa(f, 'spherefun') )
     F = coeffs2(f, n, m);
 end
 
-% Calculate the integral constraint constant: 
+% Calculate the integral constraint constant:
 k = floor(n/2)+1;
 floorm = floor(m/2);
 mm = (-floorm:ceil(m/2)-1);
@@ -102,10 +110,10 @@ en = 2*pi*(1+exp(1i*pi*mm))./(1-mm.^2);
 en([floorm, floorm + 2]) = 0;
 int_const = en*F(:,k)/K^2;
 
-% Multiple rhs by sin(th)^2 and divide by K^2: 
+% Multiple rhs by sin(th)^2 and divide by K^2:
 F = Msin2 * F / K^2;
 
-% Want to solve        
+% Want to solve
 %    X L^T + X DF^T = F
 % subject to zero integral constraints, i.e.,  w^T X_0  = 0.
 
@@ -124,7 +132,7 @@ k = floor(n/2)+1;
 ii = [1:floorm floorm+2:m];
 CFS(:, k) = [ en ; L( ii, :) ] \ [ int_const ; F(ii, k) ];
 
-% Now, convert to a spherefun object: 
-u = spherefun.coeffs2spherefun(CFS); 
+% Now, convert to a spherefun object:
+u = spherefun.coeffs2spherefun(CFS);
 
 end
