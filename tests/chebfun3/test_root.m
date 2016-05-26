15b0e4c778650991ddc9fdfc43dda7459fab5b12
bhashemi <hashemi@maths.ox.ac.uk>
2016-05-26 14:23:14 +0100

    added a test from #1900 to `chebfun3/root`.

--- tests/chebfun3/test_root.m
+++ tests/chebfun3/test_root.m
@@ -18,4 +18,14 @@ pass(1) = f(r(1), r(2), r(3)) < tol;
 pass(2) = g(r(1), r(2), r(3)) < tol;
 pass(3) = h(r(1), r(2), r(3)) < tol;
 
+
+%% Another trivial test. See #1900:
+tol = 1e-2;
+f = chebfun3(@(x,y,z) x.^2-tol);
+g = chebfun3(@(x,y,z) y.^2-tol);
+h = chebfun3(@(x,y,z) z.^2-tol);
+r = root(f, g, h);
+pass(4) = f(r(1), r(2), r(3)) < tol;
+pass(5) = g(r(1), r(2), r(3)) < tol;
+pass(6) = h(r(1), r(2), r(3)) < tol;
 end
