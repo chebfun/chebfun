dfb3a843f0593f9b5eb1daec4f3dad6a07bf0f3c
Zuoxin (Klaus) Wang <wangzuoxin@hotmail.com>
2016-05-30 23:12:23 +0100

    Rename variables and change documentation
    Rename variables and change documentation

--- @chebfun2/sumdisk.m
+++ @chebfun2/sumdisk.m
@@ -1,28 +1,37 @@
 function [Integral] = sumdisk(f)
 %SUM2DISK   Double integral of a CHEBFUN2 over the unit disk.
 %   I = SUM2DISK(F) returns the double integral of a CHEBFUN2 over the unit
-%   disk. Chebyshev polynomial approximation formula of maximal degree N is
-%   used
-%   
+%   disk. The integral is evaluated using a truncated 2D Chebyshev
+%   polynomial with certain maximal degree that resolve the function to
+%   machine precision
+%
 % See also INTEGRAL2, INTEGRAL, QUAD2D, SUM2.
 
 % Copyright 2016 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
 
-% Note for developers: This method supposes that the CHEBFUN2 is based on 
+% Note for developers: This method supposes that the CHEBFUN2 is based on
 % Chebyshev polynomials.
 
+% Empty check:
+if ( isempty( f ) ) 
+    Integral = 0;
+    return
+end
 
-ChebCoeff_Mat = chebcoeffs2(f);
-ChebCoeff_Mat_RowNum = size(ChebCoeff_Mat,1);
-ChebCoeff_Mat_ColNum = size(ChebCoeff_Mat,2);
+ChebCoeffMat = chebcoeffs2(f);                  % Matrix of Chebyshev coefficients
+ChebCoeffMatRowNum = size(ChebCoeffMat,1);      
+ChebCoeffMatColNum = size(ChebCoeffMat,2);
 
 Integral = 0;
 
-for I = 1:ChebCoeff_Mat_RowNum
-   for J = 1:ChebCoeff_Mat_ColNum
-       Integral = Integral + ChebCoeff_Mat(I,J)*TixTjy_DiscIntegral_Fn(I-1,J-1);
-   end
+% Loop through the entries off the matrix of Chebyshev coefficients
+for I = 1:ChebCoeffMatRowNum
+    for J = 1:ChebCoeffMatColNum
+        Integral = Integral + ChebCoeffMat(I,J)*GetTixTjyDiscIntegral(I-1,J-1);
+        % use the local function GETIXTJYDISCINTEGRAL() to compute the
+        % integral of T_i(x)T_j(y) over the unit disk.
+    end
 end
 
 
@@ -32,41 +41,45 @@ end
 % This local function computes the integral of T_i(x)T_j(y) over the unit
 % disk.
 
-function [ DiscIntegral ] = TixTjy_DiscIntegral_Fn(I,J)
-%TixTjy_DiscIntegral_Fn Computes the integral of product of Chebyshev
-%polynomials T_i(x)T_j(y) in the unit disc. Slevinsky formula is used
+function [ DiscIntegral ] = GetTixTjyDiscIntegral(I,J)
+%TixTjy_DiscIntegral_Fn   Computes the integral of product of Chebyshev
+%   polynomials T_i(x)T_j(y) in the unit disc. Slevinsky formula is used
+%   here.
+%
 %   Detailed explanation goes here
+
+
 if mod(I,2) == 1
     DiscIntegral = 0;
 elseif mod(J,2) == 1
     DiscIntegral = 0;
 else
     
-        if J == 0
-           if I == 0 
-               DiscIntegral = pi;
-           elseif I == 2
-               DiscIntegral = -pi/2;
-           else
-               DiscIntegral = 0;
-           end
-            
+    if J == 0
+        if I == 0
+            DiscIntegral = pi;
+        elseif I == 2
+            DiscIntegral = -pi/2;
+        else
+            DiscIntegral = 0;
         end
         
-        
-        if J>0
-            if I == J-2 && J~=2
-                DiscIntegral = (pi/2)*((-1)^(J/2))/(2*(J-1));
-            elseif I == J
-                DiscIntegral = (pi/2)*(((-1)^(J/2))/(2*(J+1)) - ((-1)^(J/2))/(2*(J-1)));
-            elseif I == J+2
-                DiscIntegral = -(pi/2)*((-1)^(J/2))/(2*(J+1));
-            elseif I == 0 && J == 2
-                DiscIntegral = -(pi/2);
-            else
-                DiscIntegral = 0;
-            end
+    end
+    
+    
+    if J>0
+        if I == J-2 && J~=2
+            DiscIntegral = (pi/2)*((-1)^(J/2))/(2*(J-1));
+        elseif I == J
+            DiscIntegral = (pi/2)*(((-1)^(J/2))/(2*(J+1)) - ((-1)^(J/2))/(2*(J-1)));
+        elseif I == J+2
+            DiscIntegral = -(pi/2)*((-1)^(J/2))/(2*(J+1));
+        elseif I == 0 && J == 2
+            DiscIntegral = -(pi/2);
+        else
+            DiscIntegral = 0;
         end
+    end
     
     
 end
