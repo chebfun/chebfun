458491c952a47d562b30919a7ec3569d0db4d603
oliviersete <olivier.sete@web.de>
2016-06-07 13:39:05 +0100

    Fix typo in help text of chebfun3/ldivide.m

--- @chebfun3/ldivide.m
+++ @chebfun3/ldivide.m
@@ -2,7 +2,7 @@ function h = ldivide(f, g)
 %.\   Pointwise left array divide for CHEBFUN3 objects.
 %   F.\G returns (1/F)*G, if G is a CHEBFUN3 and F is a double.
 %
-%   F.\G returns G\F, if G is double and F is a CHEBFUN3, but this does
+%   F.\G returns G/F, if G is double and F is a CHEBFUN3, but this does
 %   not work if F becomes numerically close to zero.
 % 
 %   F.\G is the same as the command ldivide(F,G).
