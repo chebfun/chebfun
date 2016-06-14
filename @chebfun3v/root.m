fb7c224935897d5cc1b2d01835f2f11000ee5471
oliviersete <olivier.sete@web.de>
2016-06-14 20:34:35 +0100

    Fixed a bug in chebfun3v/root and chebfun3v/roots, simplified the code and adjusted the help text.

--- @chebfun3v/root.m
+++ @chebfun3v/root.m
@@ -1,16 +1,16 @@
-function varargout = root(F, varargin)
+function varargout = root(F)
 %ROOT   Find one common zero of a CHEBFUN3V object.
 %   R = ROOT(F) finds one common zero of the three CHEBFUN3 objects F(1), 
-%   F(2) and F(3) in their domain of definition under the assumption 
-%   that the solution set is zero-dimensional. R is a matrix with three 
-%   columns storing the x-value, y-value, and z-value of the common root.
+%   F(2) and F(3) in their domain of definition under the assumption that
+%   the solution set is zero-dimensional. R is a row vector storing the
+%   x-value, y-value, and z-value of the common root.
 %   This function is also called by the syntax ROOT(F, G, H), where F, G and
 %   H are CHEBFUN3 objects.
 %
 %   [x, y, z] = ROOT(F) returns the x-value, y-value and z-value as 
 %   three separate columns.
 %
-% See also CHEBFUN3/ROOTS and CHEBFUN2/ROOTS.
+% See also CHEBFUN3/ROOT and CHEBFUN2/ROOTS.
 
 % Copyright 2016 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
@@ -25,17 +25,12 @@ f = F.components{1};
 g = F.components{2};
 h = F.components{3};
 
-if ( isempty(varargin) )
-        r = root(f, g, h);
-        xroot = r(:, 1); 
-        yroot = r(:, 2);
-        zroot = r(:, 3);
-end
+r = root(f, g, h);
 
 if ( nargout <= 1 )
-    varargout{1} = [xroot, yroot, zroot];
+    varargout{1} = r;
 else
-    varargout = {xroot, yroot, zroot};
+    varargout = {r(:, 1), r(:, 2), r(:, 3)};
 end
 
 end
