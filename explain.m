9dcfd59d9d4db5baa04b8eca14d482a8dc4b15a3
Jared <aurentz@maths.ox.ac.uk>
2016-06-16 16:35:18 +0100

    switched plotting order

--- explain.m
+++ explain.m
@@ -66,18 +66,9 @@ if ( ng<100 )
     ms0 = 5; 
 end
 
-% Plot Chebyshev coeffs of f and g
+% get Chebyshev coeffs of f and g
 gc = abs(chebcoeffs(g));
-semilogy(0:ng-1,gc,'.k',MS,ms), hold on
 fc = abs(chebcoeffs(f)); 
-semilogy(0:nf-1,fc,'or',MS,ms0)
-
-% Attempt to adjust axes for consistency for comparisons:
-a = axis;
-a(2) = max(a(2),ng);
-a(4) = 1e03*a(4);
-a(3) = 1e-3*a(3);
-axis(a), set(gca,FS,9)
 
 % create envelope from gc as in Step 1 of standardChop algorithm
 m = gc(end)*ones(ng, 1);
@@ -87,7 +78,18 @@ end
 
 % Plot envelope
 m(m == 0) = a(3);
-semilogy(0:ng-1,m,'-g')
+semilogy(0:ng-1,m,'-g'), hold on
+
+% Plot Chebyshev coeffs of f and g
+semilogy(0:ng-1,gc,'.k',MS,ms)
+semilogy(0:nf-1,fc,'or',MS,ms0)
+
+% Attempt to adjust axes for consistency for comparisons:
+a = axis;
+a(2) = max(a(2),ng);
+a(4) = 1e03*a(4);
+a(3) = 1e-3*a(3);
+axis(a), set(gca,FS,9)
 
 % plot epsval
 semilogy(0:ng-1,0*m+epsval*m(1),'k--')
