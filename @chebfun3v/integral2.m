6722570aad37af762e6df613d471b5689a36e523
bhashemi <hashemi@maths.ox.ac.uk>
2016-06-20 09:17:39 +0100

    obvious changes to chebfun3v/integral2

--- @chebfun3v/integral2.m
+++ @chebfun3v/integral2.m
@@ -1,7 +1,8 @@
 function v = integral2(F, S)
 %INTEGRAL2   Flux integral of a Chebfun3v object through a 2D-surface.
-%   INTEGRAL2(F, S) computes the flux integral of the Chebfun3v object F through
-%   the parametric surface S defined as a Chebfun2v object (with 3 components):
+%   INTEGRAL2(F, S) computes the flux integral of the Chebfun3v object F 
+%   through the parametric surface S defined as a Chebfun2v object (with 3 
+%   components):
 %                           /
 %       INTEGRAL2(F, S) =  |   < F, dS >
 %                          /S
@@ -15,49 +16,42 @@ function v = integral2(F, S)
 % Copyright 2016 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
 
-% Developer Note: If F = F(x,y,z) is a CHEBFUN3v and S : DOM -> R^3 is a
-% chebfun2v object that parametrizes the surface, then
-% \iint_S dot(F, \vec{dS})
-%       = \iint_DOM dot(F(S_1(u,v), S_2(u,v), S_3(u,v))), cross(S_u, S_v)) dudv.
-% Note that the domain of f should contain the range of S.
-% TODO: Check for this in the code.
-
-
 % Check that F is a chebfun3v object with 3 components:
-if ( isa(F, 'chebfun3v') )
-    if ( F.nComponents ~= 3 )
-        error('CHEBFUN:CHEBFUN3V:fluxintegral', ...
-            'The chebfun3v object must have 3 components.')
-    end
-else
+if ( F.nComponents ~= 3 )
     error('CHEBFUN:CHEBFUN3V:fluxintegral', ...
-        'First input must be a chebfun3v object.')
+        'The chebfun3v object must have 3 components.')
 end
 
 % Check that the second input is a chebfun2v object with 2 components:
 if ( isa(S, 'chebfun2v') )
     if ( S.nComponents ~= 3 )
         error('CHEBFUN:CHEBFUN3V:fluxintegral', ...
-            'The parametrisation must habe 3 components.')
+            'The parametrisation must have 3 components.')
     end
 else
     error('CHEBFUN:CHEBFUN3V:fluxintegral', ...
         'The parametrisation must be a chebfun2v object.')
 end
 
+% Get components of F:
+F1 = F(1);
+F2 = F(2);
+F3 = F(3);
 
-% Get components of Phi:
+% Get components of the surface S:
 S1 = S(1);
 S2 = S(2);
 S3 = S(3);
 
 % Build the composition F(S):
-FS1_handle = @(x,y) feval(F.components{1}, feval(S1,x,y), feval(S2,x,y), ...
+FS1_handle = @(x,y) feval(F1, feval(S1, x, y), feval(S2, x, y), ...
     feval(S3,x,y));
-FS2_handle = @(x,y) feval(F.components{2}, feval(S1,x,y), feval(S2,x,y), ...
-    feval(S3,x,y));
-FS3_handle = @(x,y) feval(F.components{3}, feval(S1,x,y), feval(S2,x,y), ...
-    feval(S3,x,y));
+
+FS2_handle = @(x,y) feval(F2, feval(S1, x, y), feval(S2, x, y), ...
+    feval(S3, x, y));
+
+FS3_handle = @(x,y) feval(F3, feval(S1, x, y), feval(S2, x, y), ...
+    feval(S3, x, y));
 
 FS = chebfun2v(FS1_handle, FS2_handle, FS3_handle, S1.domain);
 
