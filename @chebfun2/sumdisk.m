68b37eb0ea8c2b41b2f70779af57888a213b9dc9
Klaus Wang <zuoxin.wang@maths.ox.ac.uk>
2016-06-20 16:35:23 +0100

    sumdisk code simplified

--- @chebfun2/sumdisk.m
+++ @chebfun2/sumdisk.m
@@ -12,7 +12,7 @@ function I = sumdisk(f)
 % See http://www.chebfun.org/ for Chebfun information.
 
 % Empty check:
-if ( isempty( f ) ) 
+if ( isempty(f) ) 
     I = 0;
     return
 end
@@ -22,60 +22,50 @@ Domainf = f.domain;
 colsTechf = get(f.cols.funs{1}, 'tech');
 
 if ( isequal(colsTechf,@trigtech) )
-    f = chebfun2(@(x,y) feval(f,x,y), Domainf, 'vectorize');         % not as clean as we would like
-elseif ( isequal(colsTechf,@chebtech2) ~= 1 )
-    error('CHEBFUN:CHEBFUN2:sumdisk:notChebfun2orTrigfun2', ...
-                  'The input argument is not chebfun2 or trigfun2.'); 
+    % not as clean as we would like
+    f = chebfun2(@(x,y) feval(f, x, y), Domainf, 'vectorize');
 end
 
 % Compute the integral
-
 coeffs = chebcoeffs2(f);                 % matrix of Chebyshev coefficients
-coeffs = coeffs(1:2:end, 1:2:end);          % Extract the even entries
+coeffs = coeffs(1:2:end, 1:2:end);       % Extract the even entries
 [nRow, nCol] = size(coeffs);
 
 
 % Entires on the main diagonal
-
 if ( nRow == 1 )
-    
-    CoeffsDiag0 = coeffs(1,1);
+    CoeffsDiag0 = coeffs(1, 1);
     Diag0Int = pi;
     
 elseif ( nCol == 1 ) 
-    
     CoeffsDiag0 = coeffs(1,1);
     Diag0Int = pi;
     
 else    % nRow > 1 or nCol > 1
-    
     % Extract 0 diagonal of the coeff matrix
-    CoeffsDiag0 = diag(coeffs,0);               
+    CoeffsDiag0 = diag(coeffs, 0);               
     Diag0Length = length(CoeffsDiag0);
     
     % Compute the integral of T_i(x)T_j(y) over the unit disk for the
     % appropriate i,j
-    
     kVec = 2*(0:(Diag0Length-1))';
     Diag0Int = (pi*(-1).^(kVec/2))./(2-2*kVec.^2);
-    Diag0Int(1,1) = pi;
-    
+    Diag0Int(1, 1) = pi;
 end
 
 % Entires on the +2 diagonal
 
 if ( nCol == 1 )
-    
     CoeffsDiag2 = 0;
     Diag2Int = 0;
     
 else   % nCol > 1
     
     if ( nRow == 1 )
-        CoeffsDiag2 = coeffs(1,2);
+        CoeffsDiag2 = coeffs(1, 2);
         Diag2Int = -pi/2;
     else        % nRow > 1
-        CoeffsDiag2 = diag(coeffs,1);
+        CoeffsDiag2 = diag(coeffs, 1);
         Diag2Length = length(CoeffsDiag2);
         kVec = 2*(0:(Diag2Length - 1))';
         Diag2Int = (pi*(-1).^(1+kVec/2))./(4*kVec + 4);
@@ -85,10 +75,7 @@ else   % nCol > 1
 end
 
 % Entires on the -2 diagonal
-
-
 if ( nRow == 1 )
-    
     CoeffsDiagm2 = 0;
     Diagm2Int = 0;
     
@@ -107,18 +94,13 @@ end
 
 
 % Sum up all the terms
-
-I = (Diag0Int')*CoeffsDiag0 + (Diag2Int')*CoeffsDiag2 + (Diagm2Int')*CoeffsDiagm2;
+I = (Diag0Int')*CoeffsDiag0 + (Diag2Int')*CoeffsDiag2 + ...
+    (Diagm2Int')*CoeffsDiagm2;
 
 
 % Rescale the integral for non-default domain
-
-
 if ( any(Domainf ~= [-1 1 -1 1]) )
     I = I * (Domainf(2) - Domainf(1))*(Domainf(4) - Domainf(3))/4;
 end
 
-
-
 end
-
