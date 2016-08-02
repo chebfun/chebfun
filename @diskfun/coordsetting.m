7762bbf59ea55bdc96891010de9ae5908fe823ff
ajt60gaibb <alex.townsend1987@gmail.com>
2016-08-02 09:39:25 -0400

    Tidy help text for coordsetting.

--- @diskfun/coordsetting.m
+++ @diskfun/coordsetting.m
@@ -1,26 +1,36 @@
-function iscart = coordsetting(varargin)
-% COORDSETTING: Given input to a function, determine whether the
-% function evaluation should occur with respect to polar or Cartesian coordinates. 
-% The first input element must be a diskfun. 
+function iscart = coordsetting( varargin )
+% COORDSETTING     Decide if the function is in polar or Cartesian
+%   ISCART = COORDSETTING( F ) returns ISCART = 1 if F.COORDS is set to
+%   'CART'; otherwise ISCART = 0. 
+%  
+%   ISCART = COORDSETTING( F, STR ) returns ISCART = 1 if STR = 'cart' and 
+%   returns ISCART = 0 if STR = 'polar; 
+%
+%   ISCART = COORDSETTING( F, ..., STR ) is the same as COORDSETTING( F,
+%   STR ).
 
-%figure out if Cartesian or polar
-f = varargin{1}; 
+% Copyright 2016 by The University of Oxford and The Chebfun Developers.
+% See http://www.chebfun.org/ for Chebfun information.
 
-%first check global setting and apply
-if strcmpi(f.coords,'cart')
-    iscart =1; 
-elseif strcmpi(f.coords, 'polar')
-    iscart =0; 
-end
+% Grab diskfun:
+f = varargin{ 1 }; 
 
-%second search for user-supplied 'polar' setting in arguments: 
-isPolar = find(strcmp(varargin,'polar'));
-if ( any(isPolar) )
+% First check global setting and apply flag:
+if ( strcmpi(f.coords, 'cart') )
+    iscart = 1; 
+elseif ( strcmpi(f.coords, 'polar') )
     iscart = 0; 
 end
-%third search for user-supplied 'cart' setting in feval:
-isCart = find(strcmp(varargin,'cart'));
-if ( any(isCart) )
+
+% Second search for user-supplied 'polar' flag in arguments: 
+isPolar = find(strcmp(varargin, 'polar'));
+if ( any( isPolar ) )
+    iscart = 0; 
+end
+
+% Third search for user-supplied 'cart' setting in feval:
+isCart = find(strcmp(varargin, 'cart'));
+if ( any( isCart ) )
     iscart = 1; 
 end
 
