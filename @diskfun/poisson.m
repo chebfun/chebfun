f9fc8d71688dc44bf03466594cf972a8e743e224
gradywright <gradywright@boisestate.edu>
2016-08-02 07:45:23 -0600

    Modification to bring inline with coding standards.

--- @diskfun/poisson.m
+++ @diskfun/poisson.m
@@ -1,5 +1,5 @@
 function u = poisson( f, bc, m, n )
-% POISSON      Fast Poisson solver for the disk. 
+% POISSON    Fast Poisson solver with Dirichlet boundary conditions for the disk. 
 %   U = POISSON(F, BC, N) solves laplacian(U) = F on the unit disk, which
 %   in polar coordinates (theta, r) is 
 %  
@@ -36,6 +36,10 @@ function u = poisson( f, bc, m, n )
 % SOLVE COMPLEXITY:    O( m*n )  N = m*n = total degrees of freedom
 %
 %   Alex Townsend, July 2015. (Updated Feb 2016,  Heather Wilber)
+%
+% For more details, see 
+% H. Wilber, Numerical computing with functions on the sphere and disk,
+% Master?s thesis, Boise State University, 2016.
 
 % Copyright 2016 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
@@ -55,7 +59,7 @@ D1 = ultraS.diffmat( m, 1 );              % 1st order ultraS diffmat
 D2 = ultraS.diffmat( m, 2 );              % 2nd order ultraS diffmat
 Mr = ultraS.multmat(m,[0;1],1);           % multiplication of r in ChebU 
 Mr2 = ultraS.multmat(m,[.5;0;.5],2);      % multiplication of r^2 in ultra2
-Mr2c = ultraS.multmat(m,[.5;0;.5],0);      % multiplication of r^2 in Cheb
+Mr2c = ultraS.multmat(m,[.5;0;.5],0);     % multiplication of r^2 in Cheb
 S1 = ultraS.convertmat( m, 0, 1 );        % convert chebT coeffs -> ultra2
 S12 = ultraS.convertmat( m, 1, 1);        % convert chebU coeffs -> ultra2
 
@@ -71,71 +75,67 @@ if ( isa(f, 'function_handle') )
     F = (S1*chebtech2.vals2coeffs( F.' )).';        % Get (C^{(2)},trigvals) basis
     F = trigtech.vals2coeffs( F );
 elseif ( isa(f, 'diskfun') )
-%     tol = 1e5*vscale(f)*chebfunpref().cheb2Prefs.chebfun2eps;
     F = coeffs2(f, n, m);
-    F = S1*Mr2c*F; %r.^2*rhs in C^{2}
+    F = S1*Mr2c*F; % r.^2*rhs in C^{2}
     F = F.';  
-elseif ( isa( f, 'double' ) ) %assume these are chebyshev coeffs
-%     tol = 1e5*chebfunpref().cheb2Prefs.chebfun2eps; 
-    f = chebtech2.alias(trigtech.alias(f.', n).',m); %get correct size
-    F = S1*Mr2c*f; %r.^2*rhs in C^{2}
+elseif ( isa( f, 'double' ) ) % assume these are chebyshev coeffs
+    f = chebtech2.alias(trigtech.alias(f.', n).',m); % get correct size
+    F = S1*Mr2c*f; % r.^2*rhs in C^{2}
     F = F.';
-    
 end
 
-%if F is real-valued, we will use symmetry to reduce # computations
+% If F is real-valued, we will use symmetry to reduce # computations
 realValued = isreal(F); 
-d = (-n/2)*realValued+n +realValued; %how many Fourier coeffs we need to solve for? 
-                     %(n if rv=0, n/2 +1 if rv=1)
+% How many Fourier coeffs we need to solve for? n if rv=0, n/2 +1 if rv=1
+d = (-n/2)*realValued+n +realValued; 
 
-% set up  LHS
+% Set up LHS
 L = Mr2*D2 + S12*Mr*D1;
-L = L(1:end-2, :); %eliminate last two rows to make room for BCs.
+L = L(1:end-2, :); % Eliminate last two rows to make room for BCs.
 
 % Boundary conditions 
 bcvals = feval(bc, th0);
 bc = trigtech.vals2coeffs(bcvals); 
-S1 = S1(1:end-2, :); %make S1 the right size for including BCs.
+S1 = S1(1:end-2, :); % Make S1 the right size for including BCs.
 
-%set up for Sherman-Morrison solve
+% Set up for Sherman-Morrison solve
 W = [1 ;zeros((m-1)/2, 1)]; 
 
 CFS = zeros(m,n);
-%main loop
+% Main loop
 for k = 1 : d
-    j = -n/2 + k-1; %wave number 
-    a = mod(j,2)+1; %even a=1  or odd a=2 wave number
+    j = -n/2 + k-1; % Wave number 
+    a = mod(j,2)+1; % Even a=1 or odd a=2 wave number
     B= L-j^2*S1 ; 
     
-    %we will keep the odd or even contributions only;  
+    % We will keep the odd or even contributions only;  
     kB = [zeros(1,(m-1)/2+abs(mod(j,2)-1)); B(a:2:end,a:2:end)];
     kB(1,1)=1;
 
-    %correct size of vectors (different size system depending
-    %on wave number)
+    % Correct size of vectors (different size system depending on wave
+    % number)
     w = W(1:end-(a-1));
     
-    %set up RHS
+    % Set up RHS
     b = [bc(k); F(k,a:2:end-2).'];
 
-    %we solve Ax=b by Sherman Morrison formula: inv(A)b = inv(B + wv^t)b =
-    %inv(B)b - ( inv(B)wv^t * inv(B)b)/(1+v^t*inv(B)w)
-    
+    % We solve Ax=b by Sherman Morrison formula: 
+    % inv(A)b = inv(B + wv^t)b = inv(B)b - ( inv(B)wv^t * inv(B)b)/(1+v^t*inv(B)w)
     invBb = kB\b; 
     invBw = kB\w; 
     
-    %CFS(a:2:end, k) = invBb-(invBw*v*invBb)/(1+v*invBw); 
-    %rewrite using the fact that  v=[0 1 1 1 ... 1]: 
-    CFS(a:2:end,k) = invBb-sum(invBb(2:end))*invBw/(1+sum(invBw(2:end)));
-    
+    % We want to compute
+    % CFS(a:2:end, k) = invBb-(invBw*v*invBb)/(1+v*invBw); 
+    % But this can be made faster by using the fact that v=[0 1 1 1 ... 1]: 
+    CFS(a:2:end,k) = invBb-sum(invBb(2:end))*invBw/(1+sum(invBw(2:end)));    
 end
 
-%when F is real-valued, this gets the rest of the Fourier coeffs
+% When F is real-valued, this gets the rest of the Fourier coeffs
 if ( realValued == 1 )
     CFS(:,d+1:n) = flip(conj(CFS(:,2:d-1)));
 end
 
-%solution returned as a diskfun:
+% Solution returned as a diskfun:
 u = diskfun.coeffs2diskfun( CFS ); 
 
 end
