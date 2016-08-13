6f3a56a2de7bddd704618b38411fc4002972e341
oliviersete <olivier.sete@web.de>
2016-08-13 23:10:01 +0100

    Add composition G(F) where G is a CHEBFUN2V and F can be a Chebfun2v, two Chebfun2, Chebfun3v, two Chebfun3.  If F is a Chebfun2 or Chebfun3, G(F) is interpreted as G(real(F), imag(F)).  Add corresponding tests.

--- @chebfun2v/subsref.m
+++ @chebfun2v/subsref.m
@@ -1,37 +1,47 @@
-function varargout = subsref( F, ref )
+function varargout = subsref(F, ref)
 %SUBSREF   CHEBFUN2V subsref.
-% 
+%
 % ( )
 %   F(X,Y) returns the values of the CHEBFUN2 F evaluated on the array (X,Y).
+%
 %   F(k) returns the first component of F if k=1, the second if k=2, and
-%   the third if k=3. 
+%   the third if k=3.
+%
+%   F(G) where G is a CHEBFUN2V with two components returns the CHEBFUN2V
+%   representing the composition F(G).  If G is a CHEBFUN3V, then F(G) is a
+%   CHEBFUN3V.  If G is a CHEBFUN2 or CHEBFUN3, then F(G) is interpreted as
+%   F([real(G); imag(G)]), regardless whether G is real or complex.
+%
+%   F(X, Y) where X and Y are CHEBFUN2 objects returns the CHEBFUN2V
+%   representing the composition.  If X and Y are CHEBFUN3 objects, then
+%   F(X, Y) is a CHEBFUN3V.
 %
 %  .
 %   F.PROP returns the property PROP of F as defined by GET(F,'PROP').
-%  
+%
 % { }
 %    Throws an error.
 
 % Copyright 2016 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
 
-% check for empty CHEBFUN2V object. 
-if ( isempty( F ) )
-   varargout = {[]};
-   return
+% check for empty CHEBFUN2V object.
+if ( isempty(F) )
+    varargout = {[]};
+    return
 end
 
-% Recursicve going through the ref structure: 
+% Recursive going through the ref structure:
 indx = ref(1).subs;
 
 switch ( ref(1).type )
     
     case '.'
-        if ( numel( ref ) == 1 )
-            % This is a get call to get a property. 
-            varargout = { get(F, indx) };
+        if ( numel(ref) == 1 )
+            % This is a get call to get a property.
+            varargout = {get(F, indx)};
         else
-            %  
+            %
             t2 = ref(2).type;
             if ( strcmp(t2,'.') )
                 out = get(F, indx, ref(2).subs{:});
@@ -42,18 +52,38 @@ switch ( ref(1).type )
             if ( numel(ref) > 2 )
                 varargout = {subsref(out, ref(3:end))};
             else
-                varargout = { out };
+                varargout = {out};
             end
         end
         
     case '()'
         if ( length(indx) > 1 )
-            x = indx{1}; 
-            y = indx{2}; % where to evaluate
-            vals = feval(F, x, y); 
-            varargout = { vals }; 
+            % where to evaluate
+            x = indx{1};
+            y = indx{2};
+            % If x, y are Chebfun2 or Chebfun3, call compose; else feval.
+            if ( isa(x, 'chebfun2') && isa(y, 'chebfun2') )
+                out = compose([x; y], F);
+            elseif ( isa(x, 'chebfun3') && isa(y, 'chebfun3') )
+                out = compose([x; y], F);
+            else
+                out = feval(F, x, y);
+            end
+            varargout = {out};
+            
+        elseif ( isa(indx{1}, 'chebfun2v') || isa(indx{1}, 'chebfun3v') )
+            % Composition F(CHEBFUN2V) or F(CHEBFUN3V):
+            out = compose(indx{1}, F);
+            varargout = {out};
+            
+        elseif ( isa(indx{1}, 'chebfun2') || isa(indx{1}, 'chebfun3') )
+            % Composition F(G) where G is a CHEBFUN2 or CHEBFUN3, interpreted as
+            % [real(G), imag(G)], regardless whether G is real or complex.
+            out = compose(indx{1}, F);
+            varargout = {out};
+            
         else
-            if ( isa(indx{1},'double') )
+            if ( isa(indx{1}, 'double') )
                 if all( indx{1} == 1  )
                     varargout = F.components(1);
                 elseif ( all( indx{1} == 2 ) )
@@ -66,18 +96,17 @@ switch ( ref(1).type )
                 end
             end
         end
-         
-         
+        
     otherwise
         error('CHEBFUN:CHEBFUN2V:subsref:unexpectedType', ...
             ['??? Unexpected index.type of ' index(1).type]);
         
 end
 
-% Recurse down: 
-if ( numel( ref ) > 1 )
-   ref(1) = []; 
-   varargout = { subsref( varargout{ : }, ref ) }; 
+% Recurse down:
+if ( numel(ref) > 1 )
+    ref(1) = [];
+    varargout = { subsref( varargout{ : }, ref ) };
 end
 
 end
--- tests/chebfun2v/test_subsref.m
+++ tests/chebfun2v/test_subsref.m
@@ -1,22 +1,114 @@
 function pass = test_subsref( pref )
-% Test Chebfun2v subsref() command. 
+% Test Chebfun2v subsref() command.
 
-if ( nargin == 0) 
-    pref = chebfunpref; 
+if ( nargin == 0)
+    pref = chebfunpref;
 end
 
 tol = 1000*pref.cheb2Prefs.chebfun2eps;
 
+j = 1;
 
-% Test recursive subsref:  
+% Test recursive subsref:
 f = chebfun2(@(x, y) sin(x.*y));
 F = [f ; f ; f];
 
-G = F(1); 
-exact = G.pivotValues; 
+G = F(1);
+exact = G.pivotValues;
 
-pass(1) = norm( exact - F(1).pivotValues ) < tol; 
+pass(j) = norm( exact - F(1).pivotValues ) < tol;
+j = j+1;
 
+% Composition of a CHEBFUN2V (2 components) with two CHEBFUN2 objects.
+f1 = chebfun2(@(x,y) x + 1);
+f2 = chebfun2(@(x,y) 2*y);
+G = chebfun2v(@(x,y) x + y, @(x,y) x - y);
+H = G(f1, f2);
+H_true = chebfun2v(@(x,y) x + 2*y + 1, @(x,y) x - 2*y + 1);
+pass(j) = ( norm(H - H_true) < tol );
+j = j+1;
+pass(j) = ( norm(H(1, 1) - [4; 0]) < tol );
+j = j+1;
 
+% Composition of a CHEBFUN2V (3 components) with two CHEBFUN2 objects.
+f1 = chebfun2(@(x,y) x + 1);
+f2 = chebfun2(@(x,y) 2*y);
+G = chebfun2v(@(x,y) x + y, @(x,y) x - y, @(x,y) x.*y );
+H = G(f1, f2);
+H_true = chebfun2v(@(x,y) x + 2*y + 1, @(x,y) x - 2*y + 1, ...
+    @(x,y) 2*(x + 1).*y);
+pass(j) = ( norm(H - H_true) < tol );
+j = j+1;
+
+% Composition of a CHEBFUN2V with two CHEBFUN3 objects.
+f1 = chebfun3(@(x,y,z) x);
+f2 = chebfun3(@(x,y,z) y + z);
+G = chebfun2v(@(x,y) x, @(x,y) y, @(x,y) x + y);
+H = G(f1, f2);
+H_true = chebfun3v(@(x,y,z) x, @(x,y,z) y + z, @(x,y,z) x + y + z);
+pass(j) = ( norm(H - H_true) < tol );
+j = j+1;
+
+% Composition of a CHEBFUN2V (2 components) with a CHEBFUN2V.
+F = chebfun2v(@(x,y) x, @(x,y) y);
+G = chebfun2v(@(x,y) x + y, @(x,y) x - y);
+H = G(F);
+pass(j) = ( norm(H - G) < tol );
+j = j+1;
+
+% Composition of a CHEBFUN2V (3 components) with a CHEBFUN2V.
+F = chebfun2v(@(x,y) x, @(x,y) y);
+G = chebfun2v(@(x,y) x + y, @(x,y) x - y, @(x,y) x.*y);
+H = G(F);
+pass(j) = ( norm(H - G) < tol );
+j = j+1;
+
+% Composition of a CHEBFUN2V (2 components) with a CHEBFUN3V.
+F = chebfun3v(@(x,y,z) x, @(x,y,z) y + z);
+G = chebfun2v(@(x,y) x + y, @(x,y) x - y);
+H = G(F);
+H_true = chebfun3v(@(x,y,z) x + y + z, @(x,y,z) x - y - z);
+pass(j) = ( norm(H - H_true) < tol );
+j = j+1;
+
+% Composition of a CHEBFUN2V (3 components) with a CHEBFUN3V.
+F = chebfun3v(@(x,y,z) x, @(x,y,z) y + z);
+G = chebfun2v(@(x,y) x + y, @(x,y) x - y, @(x,y) x.*y);
+H = G(F);
+H_true = chebfun3v(@(x,y,z) x + y + z, @(x,y,z) x - y - z, @(x,y,z) x.*(y + z));
+pass(j) = ( norm(H - H_true) < tol );
+j = j+1;
+
+% Composition of a CHEBFUN2V with a CHEBFUN2, which will be interpreted as the
+% CHEBFUN2V [real(f); imag(f)].  (Regardless if f is real or complex.)
+f = chebfun2(@(x,y) x + 1i*y);
+G = chebfun2v(@(x,y) x, @(x,y) y, @(x,y) x + y);
+H = G(f);
+pass(j) = ( norm(H - G) < tol );
+j = j+1;
+
+% With real f:
+f = chebfun2(@(x,y) x);
+G = chebfun2v(@(x,y) x, @(x,y) y, @(x,y) x + y);
+H = G(f);
+H_true = chebfun2v(@(x,y) x, @(x,y) 0*x, @(x,y) x);
+pass(j) = ( norm(H - H_true) < tol );
+j = j+1;
+
+% Composition of a CHEBFUN2V with a CHEBFUN3, which will be interpreted as the
+% CHEBFUN3V [real(f); imag(f)].  (Regardless if f is real or complex.)
+f = chebfun3(@(x,y,z) x + y + 1i*z);
+G = chebfun2v(@(x,y) x, @(x,y) y, @(x,y) x + y);
+H = G(f);
+H_true = chebfun3v(@(x,y,z) x + y, @(x,y,z) z, @(x,y,z) x + y + z);
+pass(j) = ( norm(H - H_true) < tol );
+j = j+1;
+
+% With real f:
+f = chebfun3(@(x,y,z) x + y);
+G = chebfun2v(@(x,y) x, @(x,y) y, @(x,y) x + y);
+H = G(f);
+H_true = chebfun3v(@(x,y,z) x + y, @(x,y,z) 0*y, @(x,y,z) x + y);
+pass(j) = ( norm(H - H_true) < tol );
 
 end
