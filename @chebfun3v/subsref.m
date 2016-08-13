eff0a86ead76c765f18e61c27b61f7c3c5efaaa9
oliviersete <olivier.sete@web.de>
2016-08-13 21:24:01 +0100

    Add composition G(F) where G is a CHEBFUN3V and F a Chebfun2v, Chebfun3v, or three Chebfun2, or Chebfun3 objects.  Add corresponding tests.

--- @chebfun3v/subsref.m
+++ @chebfun3v/subsref.m
@@ -1,21 +1,28 @@
 function varargout = subsref(F, ref)
-%SUBSREF   CHEBFUN3V subsref. 
-%   F(X, Y, Z) returns the values of the CHEBFUN3V object F evaluated on the 
-%   array (X, Y, Z). 
-%   
-%   F(k) returns the first component of the chebfun3v object F if k=1, the 
+%SUBSREF   CHEBFUN3V subsref.
+%   F(X, Y, Z) returns the values of the CHEBFUN3V object F evaluated on the
+%   array (X, Y, Z).
+%
+%   F(k) returns the first component of the chebfun3v object F if k=1, the
 %   second if k=2, and the third if k=3.
-% 
+%
+%   F(G) where G is a CHEBFUN3V returns the CHEBFUN3V representing the
+%   composition F(G).  If G is a CHEBFUN2V, then F(G) is a CHEBFUN2V.
+%
+%   F(X, Y, Z) where X, Y, Z are CHEBFUN3 objects is a CHEBFUN3V representing
+%   the composition.  If X, Y, Z are CHEBFUN2 objects, F(X, Y, Z) is a
+%   CHEBFUN2V.
+%
 %   F.PROP returns the property PROP of F as defined by GET(F,'PROP').
 %   Throws an error.
 
 % Copyright 2016 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
 
-% check for empty CHEBFUN3V object. 
+% check for empty CHEBFUN3V object.
 if ( isempty(F) )
-   varargout = {[]};
-   return
+    varargout = {[]};
+    return
 end
 
 indx = ref(1).subs;
@@ -23,7 +30,7 @@ indx = ref(1).subs;
 switch ( ref(1).type )
     case '.'
         if ( numel(ref) == 1 )
-            % This is a get call to get a property. 
+            % This is a get call to get a property.
             varargout = {get(F, indx)};
         else
             % Probably F.components{1} or ...
@@ -44,10 +51,23 @@ switch ( ref(1).type )
     case '()'
         if ( length(indx) > 1 )
             x = indx{1}; % Where to evaluate?
-            y = indx{2}; 
-            z = indx{3}; 
-            vals = feval(F, x, y, z); 
-            varargout = {vals};
+            y = indx{2};
+            z = indx{3};
+            % If x,y,z are Chebfun2 or Chebfun3, call compose; else feval.
+            if ( isa(x, 'chebfun2') && isa(y, 'chebfun2') && isa(z, 'chebfun2') )
+                out = compose([x; y; z], F);
+            elseif ( isa(x, 'chebfun3') && isa(y, 'chebfun3') && isa(z, 'chebfun3') )
+                out = compose([x; y; z], F);
+            else
+                out = feval(F, x, y, z);
+            end
+            varargout = {out};
+            
+        elseif ( isa(indx{1}, 'chebfun2v') || isa(indx{1}, 'chebfun3v') )
+            % Composition F(CHEBFUN2V) or F(CHEBFUN3V):
+            out = compose(indx{1}, F);
+            varargout = {out};
+            
         else
             if ( isa(indx{1}, 'double') )
                 if all( indx{1} == 1  )
@@ -71,8 +91,8 @@ end
 
 % Recurse down? E.g. for "F(1).core" where F is a CHEBFUN3V object.
 if ( numel(ref) > 1 )
-   ref(1) = []; 
-   varargout = { subsref( varargout{ : }, ref ) }; 
+    ref(1) = [];
+    varargout = { subsref( varargout{ : }, ref ) };
 end
 
 end
--- tests/chebfun3v/test_subsref.m
+++ tests/chebfun3v/test_subsref.m
@@ -1,18 +1,74 @@
 function pass = test_subsref(pref)
 % Test Chebfun3v subsref() command.
 
-if ( nargin == 0) 
-    pref = chebfunpref; 
+if ( nargin == 0)
+    pref = chebfunpref;
 end
 tol = 1000*pref.cheb3Prefs.chebfun3eps;
 
-% Test recursive subsref:  
+j = 1;
+
+% Test recursive subsref:
 f = chebfun3(@(x,y,z) sin(x.*y.*z));
 F = [f; f; f];
 
 G = F(1);
-exactCore = G.core; 
+exactCore = G.core;
 
-pass(1) = norm(exactCore(:) - F(1).core(:)) < tol;
+pass(j) = norm(exactCore(:) - F(1).core(:)) < tol;
+j = j+1;
+
+% Composition of a CHEBFUN3V (2 components) with three CHEBFUN3.
+f1 = chebfun3(@(x,y,z) x);
+f2 = chebfun3(@(x,y,z) y);
+f3 = chebfun3(@(x,y,z) z);
+G = chebfun3v(@(x,y,z) x + y, @(x,y,z) y);
+H = G(f1, f2, f3);
+pass(j) = ( norm(H - G) < tol );
+j = j+1;
+
+% Composition of a CHEBFUN3V (3 components) with three CHEBFUN3.
+f1 = chebfun3(@(x,y,z) x);
+f2 = chebfun3(@(x,y,z) y);
+f3 = chebfun3(@(x,y,z) z);
+G = chebfun3v(@(x,y,z) x + y, @(x,y,z) y, @(x,y,z) z);
+H = G(f1, f2, f3);
+pass(j) = ( norm(H - G) < tol );
+j = j+1;
+
+% Composition of a CHEBFUN3V (2 components) with three CHEBFUN2.
+f1 = chebfun2(@(x,y) x);
+f2 = chebfun2(@(x,y) y);
+f3 = chebfun2(@(x,y) x+y);
+G = chebfun3v(@(x,y,z) x + y, @(x,y,z) y + z);
+H = G(f1, f2, f3);
+H_true = chebfun2v(@(x,y) x + y, @(x,y) x + 2*y);
+pass(j) = ( norm(H - H_true) < tol );
+j = j+1;
+
+% Composition of a CHEBFUN3V (3 components) with three CHEBFUN2.
+f1 = chebfun2(@(x,y) x);
+f2 = chebfun2(@(x,y) y);
+f3 = chebfun2(@(x,y) x+y);
+G = chebfun3v(@(x,y,z) x + y, @(x,y,z) y + z, @(x,y,z) x + z);
+H = G(f1, f2, f3);
+H_true = chebfun2v(@(x,y) x + y, @(x,y) x + 2*y, @(x,y) 2*x + y);
+pass(j) = ( norm(H - H_true) < tol );
+j = j+1;
+
+% Composition of a CHEBFUN3V with a CHEBFUN2V.
+G = chebfun3v(@(x,y,z) 2*x, @(x,y,z) y - x, @(x,y,z) z);
+F = chebfun2v(@(x,y) x, @(x,y) y, @(x,y) x + y);
+H = G(F);
+H_true = chebfun2v(@(x,y) 2*x, @(x,y) y - x, @(x,y) x + y);
+pass(j) = ( norm(H - H_true) < tol );
+j = j+1;
+
+% Composition of a CHEBFUN3V with a CHEBFUN3V
+G = chebfun3v(@(x,y,z) x + y + z, @(x,y,z) x - y);
+F = chebfun3v(@(x,y,z) 2*x, @(x,y,z) x + y, @(x,y,z) z + 1);
+H = G(F);
+H_true = chebfun3v(@(x,y,z) 3*x + y + z + 1, @(x,y,z) x - y);
+pass(j) = ( norm(H - H_true) < tol );
 
 end
