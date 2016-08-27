6257ea8a10c735dbe45afd47ac726a8556e013a9
ajt60gaibb <alex.townsend1987@gmail.com>
2016-08-27 09:06:15 -0400

    Add diskfun/sum command so help text contain DISKFUN instead of SEPARABLEAPPROX.

---
+++ @diskfun/sum.m
@@ -0,0 +1,15 @@
+function varargout = sum( varargin )
+%SUM   Definite Integration of a DISKFUN.
+%   G = sum(F,DIM) where DIM is 1 or 2 integrates only over Y or X respectively,
+%   and returns as its output a chebfun in the remaining variable.
+%
+%   G = sum(F) is the same as sum(F,1)
+%
+% See also SUM2. 
+
+% Copyright 2016 by The University of Oxford and The Chebfun Developers.
+% See http://www.chebfun.org/ for Chebfun information.
+
+[varargout{1:nargout}] = sum@separableApprox(varargin{:});
+
+end
