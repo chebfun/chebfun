a4049ebc1787d9057e607497895bef224e81dc51
Hadrien Montanelli <montanelli@maths.ox.ac.uk>
2016-09-17 00:12:36 +0100

    Polish solvepde.

--- @spinoperator/solvepde.m
+++ @spinoperator/solvepde.m
@@ -263,11 +263,6 @@ NcInit{1} = coeffs;
 if ( q > 1 )
     [cInit, NcInit] = startMultistep(K, dt, L, Nc, Nv, pref, S, cInit, NcInit);
 end
-% vInit = ifftn(cInit{1}(1:N,:,:));
-% for k = 1:nVars-1
-%     idx = k*N + 1;
-%     vInit = [vInit; ifftn(cInit{1}(idx:idx+N-1,:,:))];
-% end
 
 % Compute the coefficients of the scheme:
 schemeCoeffs = computeCoeffs(K, dt, L, M, S);
