d66d29d1f46b16b9c13b1168dbf6edaa626b46ce
Nakatsukasa <nakatsukasayuji@gmail.com>
2016-11-07 16:51:06 +0000

    fix help text in chebfun2-pd

--- @chebfun2/pd.m
+++ @chebfun2/pd.m
@@ -1,14 +1,14 @@
 function [U,H] = pd(f)
 %PD   Polar Decomposition of a CHEBFUN2.
-%   U = EIG(F) returns the unitary polar factor of F. 
+%   U = PD(F) returns the unitary polar factor of F. 
 %   This is the partial isometry with same x-, and y-space as F. 
 %   (Note that strictly speaking, U is not unitary but a partial isometry, 
 %   with rank(F) copies of singular values at 1. 
 %   (as in the so-called "canonical polar decomposition"). 
 %
-%   [U,H] = EIG(F) returns the unitary polar factor, along with H, a
+%   [U,H] = PD(F) returns the unitary polar factor, along with H, a
 %   symmetric positive semidefinite bivariate function 
-%   (its eigenvalues are all positive). 
+%   (its eigenvalues are all positive). They should satisfy F = U*H. 
 %
 
 % Copyright 2016 by The University of Oxford and The Chebfun Developers.
