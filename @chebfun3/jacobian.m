2ef7b914a10e1c519c3b710572a4774670b80884
bhashemi <hashemi@maths.ox.ac.uk>
2016-11-10 11:16:31 +0000

    address #2002

---
+++ @chebfun3/jacobian.m
@@ -0,0 +1,23 @@
+function J = jacobian(f, g, h)
+%JACOBIAN   Jacobian determinant of three CHEBFUN3 objects.
+%   J = JACOBIAN(F,G,H) returns the Jacobian determinant of the Jacobian 
+%   matrix.
+%
+%   Note we return the determinant of the Jacobian matrix and not the 
+%   Jacobian matrix itself.
+%
+% See also CHEBFUN3V/JACOBIAN.
+
+% Copyright 2016 by The University of Oxford and The Chebfun Developers.
+% See http://www.chebfun.org/ for Chebfun information.
+
+% Empty check: 
+if ( isempty(f) || isempty(g) || isempty(h))  
+    J = [];
+    return
+end
+
+% Call CHEBFUN3V/JACOBIAN():
+J = jacobian(chebfun3v({f, g, h}));
+
+end
