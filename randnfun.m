15179d2751ffb7441b1e23dc9d885f6e477f0498
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2017-02-14 17:18:59 +0000

    changing DT to DX in randnfun/randnfuntrig

--- randnfun.m
+++ randnfun.m
@@ -1,14 +1,14 @@
-function f = randnfun(dt, n, dom)
+function f = randnfun(dx, n, dom)
 %RANDNFUN   Random smooth function
-%   F = RANDNFUN(DT) returns a smooth chebfun on [-1,1] with maximum
-%   wave number about 2pi/DT and standard normal distribution N(0,1)
+%   F = RANDNFUN(DX) returns a smooth chebfun on [-1,1] with maximum
+%   wave number about 2pi/DX and standard normal distribution N(0,1)
 %   at each point.  F can be regarded as one sample path of a Gaussian
 %   process.  F is obtained by calling RANDNFUNTRIG on an interval
 %   of about twice the length and restricting the result to [-1,1].
 %
-%   F = RANDNFUN(DT, N) returns a quasimatrix with N independent columns.
+%   F = RANDNFUN(DX, N) returns a quasimatrix with N independent columns.
 %
-%   F = RANDNFUN(DT, N, DOM) returns a result with domain DOM = [A, B].
+%   F = RANDNFUN(DX, N, DOM) returns a result with domain DOM = [A, B].
 %
 % Examples:
 %
@@ -33,7 +33,7 @@ end
 % Call RANDNFUNTRIG on interval of approximately double length.
 % and then restrict the result to the prescribed interval.
 
-m = 2*round(diff(dom)/dt)+1;
-dom2 = dom(1) + [0 m*dt];
-f = randnfuntrig(dt, n, dom2);
+m = 2*round(diff(dom)/dx)+1;
+dom2 = dom(1) + [0 m*dx];
+f = randnfuntrig(dx, n, dom2);
 f = f{dom(1),dom(2)};
--- randnfuntrig.m
+++ randnfuntrig.m
@@ -1,15 +1,15 @@
-function f = randnfuntrig(dt, n, dom)
+function f = randnfuntrig(dx, n, dom)
 %RANDNFUNTRIG   Random smooth periodic function
-%   F = RANDNFUNTRIG(DT) returns a smooth periodic chebfun (trigfun)
-%   on [-1,1] with maximum wave number about 2pi/DT and standard normal
+%   F = RANDNFUNTRIG(DX) returns a smooth periodic chebfun (trigfun)
+%   on [-1,1] with maximum wave number about 2pi/DX and standard normal
 %   distribution N(0,1) at each point.  F can be regarded as one sample
 %   path of a Gaussian process.  It is obtained from a finite Fourier series
 %   with independent normally distributed coefficients of equal variance.
 %   (This code is preliminary and the definition may change in the future.)
 %
-%   F = RANDNFUNTRIG(DT, N) returns a quasimatrix with N independent columns.
+%   F = RANDNFUNTRIG(DX, N) returns a quasimatrix with N independent columns.
 %
-%   F = RANDNFUNTRIG(DT, N, DOM) returns a result with domain DOM = [A, B].
+%   F = RANDNFUNTRIG(DX, N, DOM) returns a result with domain DOM = [A, B].
 %
 % Examples:
 %
@@ -35,7 +35,7 @@ end
 % construction since the 'trig', 'coeffs' mode is only documented
 % in this case.
 
-m = round(diff(dom)/dt);
+m = round(diff(dom)/dx);
 c = randn(2*m+1, n) + 1i*randn(2*m+1, n);
 c = (c + flipud(conj(c)))/2;
 f = chebfun(c/sqrt(2*m+1), dom, 'trig', 'coeffs');
