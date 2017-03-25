c9a60fbe515130c1798e4bf2cdfb986f0de6268b
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2017-03-25 14:45:16 +0000

    improved syntax for randnfuntrig

--- randnfuntrig.m
+++ randnfuntrig.m
@@ -1,4 +1,4 @@
-function f = randnfuntrig(dx, n, dom)
+function f = randnfuntrig(varargin)
 %RANDNFUNTRIG   Random smooth periodic function
 %   F = RANDNFUNTRIG(DX) returns a smooth periodic chebfun (trigfun)
 %   on [-1,1] with maximum wave number about 2pi/DX and standard normal
@@ -7,9 +7,13 @@ function f = randnfuntrig(dx, n, dom)
 %   with independent normally distributed coefficients of equal variance.
 %   (This code is preliminary and the definition may change in the future.)
 %
+%   F = RANDNFUNTRIG(DX, DOM) returns a result with domain DOM = [A, B].
+%
 %   F = RANDNFUNTRIG(DX, N) returns a quasimatrix with N independent columns.
 %
-%   F = RANDNFUNTRIG(DX, N, DOM) returns a result with domain DOM = [A, B].
+%   Combinations RANDNFUNTRIG(DX, N, DOM) or RANDNFUNTRIG(DX, DOM, N) are
+%   also allowed.  Commands RANDNFUNTRIG() or RANDNFUNTRIG(DOM) use
+%   the default value DX = 1.
 %
 % Examples:
 %
@@ -18,18 +22,14 @@ function f = randnfuntrig(dx, n, dom)
 %
 %   X = randnfuntrig(.01,2); cov(X)
 %
+%   f = randnfuntrig([0 100]); plot(cumsum(f))
+%
 % See also RANDNFUN.
 
 % Copyright 2017 by The University of Oxford and The Chebfun Developers. 
 % See http://www.chebfun.org/ for Chebfun information.
 
-if nargin < 3
-    dom = [-1 1];     % default domain
-end
-
-if nargin < 2
-    n = 1;            % default number of columns
-end
+[dx, n, dom] = parseInputs(varargin{:});
 
 % Although the output is real, complex arithmetic is used for the
 % construction since the 'trig', 'coeffs' mode is only documented
@@ -39,3 +39,34 @@ m = round(diff(dom)/dx);
 c = randn(2*m+1, n) + 1i*randn(2*m+1, n);
 c = (c + flipud(conj(c)))/2;
 f = chebfun(c/sqrt(2*m+1), dom, 'trig', 'coeffs');
+
+end
+
+function [dx, n, dom] = parseInputs(varargin)
+
+dx = NaN;  
+n = NaN;  
+dom = NaN;
+
+for j = 1:nargin
+    v = varargin{j};
+    if ~isscalar(v)
+        dom = v;
+    elseif isnan(dx) 
+        dx = v;
+    else
+        n = v;
+    end
+end
+
+if isnan(dx)
+   dx = 1;          % default space scale
+end
+if isnan(n)
+   n = 1;           % default number of columns
+end
+if isnan(dom)
+   dom = [-1 1];    % default domain
+end
+
+end
