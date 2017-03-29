b8f2531c69131fc1c8748f022f5770a83e185c2d
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2017-03-29 10:16:37 +0100

    changes to help text

--- @chebop/ctranspose.m
+++ @chebop/ctranspose.m
@@ -4,7 +4,12 @@ function Nstar = ctranspose(N)
 %   endpoint boundary conditions. If N is nonlinear then N is first
 %   linearized around U = 0 and NSTAR is the adjoint of the linearization.
 %
-%   ADJOINT(N) is called for the syntax N'.
+%   N' is calculated by a call to ADJOINT(N).  To linearize about
+%   a nonzero function U, use ADJOINT(N, U).
+%
+%  Example:
+%   L = chebop(-1,1); L.op = @(x,u) diff(u,2) + diff(u,1) + u;
+%   L.lbc = 0; L.rbc = 1, Ls = L'
 %
 % See also CHEBOP/ADJOINT.
 
