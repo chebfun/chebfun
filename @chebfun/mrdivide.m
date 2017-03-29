da224e492e60fd0d664b9ced3174a1016e069b26
Nick Hale <nick.p.hale@gmail.com>
2017-03-29 13:07:27 +0200

    Fix mldivide and mrdivide.

--- @chebfun/mrdivide.m
+++ @chebfun/mrdivide.m
@@ -1,18 +1,32 @@
-function X = mrdivide(B, A)
+function X = mrdivide(B, A, flag)
 %/   Right matrix divide for CHEBFUN objects.
-%   B/A in general gives the least squares solution to X*A = B.
+%   X = B/A or X = mrdivide(B, A) is equivalent to X = B./A.
+%
+%   X = mrdivide(B, A, 'least-squares') or mrdivide(B, A, 'ls')returns the
+%   least squares solution to X*A = B.
 %
 % See also RDIVIDE, MLDIVIDE.
 
 % Copyright 2017 by The University of Oxford and The Chebfun Developers. 
 % See http://www.chebfun.org/ for Chebfun information.
 
-% TODO: Something is wrong with the comment lines below.
+% Parse inputs:
+if ( nargin < 3 )
+    flag = 0;
+else
+    flag = any(strcmpi(flag, {'least-squares', ls}));
+end
+
+if ( ~flag ) % B./A
+    X = rdivide(B, A);
+else         % Least-squares solution
+    X = mrdivide_ls(B, A);    
+end
+
+end
+
+function X = mrdivide_ls(B, A)
 % TODO: Give an example for each of the cases below.
-
-% TODO: Document that 1/f is equivalent to 1./f, NOT the trivial least squares
-% problem.
-
 if ( isscalar(A) )
     if ( A == 0 )
         % TODO:  Return identically Inf/NaN CHEBFUN instead?
@@ -25,28 +39,27 @@ elseif ( size(A, 2) ~= size(B, 2) )
     error('CHEBFUN:CHEBFUN:mrdivide:dimensions', ...
         'Matrix dimensions must agree.')
     
-elseif ( isscalar(B) )
-    X = B./A;
-    
 elseif ( isnumeric(A) )
     % [INF x M] * [M x N] = [INF x N]:
     [Q, R] = qr(B, 0);
-    X = Q * (R/A);
+    X = Q * (R\A);
     % X = B*(eye(size(B,2))/A);
     
 elseif ( ~A(1).isTransposed )
     % [M x INF] * [INF x N] = [M x N]:
     [Q, R] = qr(A, 0);
-    X = (B/R) * Q';
-    
+    X = (B\R) * Q';
+   
 else
     % [M x N] * [N x INF] = [M x INF]:
-    %        XA = B
-    %   A^* X^* = B^*
-    %    QR X^* = B^*
+    % XA = B
+    % A^* X^* = B^*
+    % QR X^* �= B^*
     % X R^* Q^* = B
-    %         X = (BQ)R^{-*}
+    % X = (BQ)R^{-*}
     [Q, R] = qr(A', 0);
     X = (B*Q) / R';
+
+end
 
 end
