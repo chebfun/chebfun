b7b9747b7bc755d7ca79b568c3d2b6ee73d43e1a
gradywright <gradywright@boisestate.edu>
2017-04-28 07:47:05 -0600

    Change code so that it samples the random function over a dense enough polar tensor product grid to resolve the function and then passes these samples to the constructor.  The parameters for determining the density of the tensor product polar grid are still not right.

--- randnfundisk.m
+++ randnfundisk.m
@@ -22,6 +22,29 @@ if nargin == 0
 end
  
 fsquare = randnfun2(dt,1.25*[-1 1 -1 1],'trig');
-f = diskfun(@(x,y) fsquare(x,y));
+
+% Sample fsquare over a polar tensor product grid that is dense enough to
+% resolve fsquare to machine precision.
+[m,n] = length(fsquare);
+m = max(m,n);
+
+% Not sure about this multipliers:  Idea is that Chebyshev expansions
+% require roughly pi/2 times the resolution as a Fourier expansion. The
+% sqrt(2) comes from the maximum length of a Cartesian square grid cell.
+% The 1.25 comes from the fact that the fsquare is over a grid that is 1.25
+% times bigger than a grid from [-1,1] 
+% (this number seems especially suspect).
+n = round(1.25*m*sqrt(2)*pi/2);
+n = n + 1-mod(n,2);
+% Also this multiplier is probably not right for the Fourier expansion.
+m = round(m*1.25*pi*sqrt(2)); m = m + mod(m,2);
+
+% Create the tensor product grid
+r = chebpts(n); r = r((n+1)/2:end); % r should be restricted to [0,1].
+[tt,rr] = meshgrid(trigpts(m,[-pi pi]),r);
+
+% Sample the random function over the square and create the diskfun.
+F = fsquare(rr.*cos(tt),rr.*sin(tt));
+f = diskfun(F);
 
 end
