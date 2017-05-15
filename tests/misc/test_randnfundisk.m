8307e5367d0b235445a335be14794b443612508f
Silviu Filip <silviuioan.filip@ens-lyon.fr>
2017-05-15 13:27:32 +0100

    loosened tolerance for one of the test cases

--- tests/misc/test_randnfundisk.m
+++ tests/misc/test_randnfundisk.m
@@ -10,7 +10,7 @@ pass(1) = (abs(mean2(f.^2)-1) < .1);
 pass(2) = (abs(mean2(f)) < .1);
 
 f = randnfundisk(40);
-d = max2(f) - min2(f)
+d = max2(f) - min2(f);
 pass(3) = (d < 0.5) & (d > 0.001);
 
 
