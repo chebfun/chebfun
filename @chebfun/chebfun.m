c61af26173054044b29d0caed8bc659a564d2757
Silviu Filip <silviuioan.filip@ens-lyon.fr>
2017-05-24 13:30:09 +0100

    vectorizating op instead of transposing it

--- @chebfun/chebfun.m
+++ @chebfun/chebfun.m
@@ -904,12 +904,9 @@ try
             end
         end
         
-        % Try and transpose:
-        op = @(x) op(x).';
-        warning('CHEBFUN:CHEBFUN:vectorCheck:transpose',...
-                ['Chebfun input should return a COLUMN array.\n', ...
-                 'Attempting to transpose.'])
-             
+        % Try and vectorize:
+        op = vectorCheck(op, dom, 1);
+
     elseif ( any(sv == 1) )
         % The operator always returns a scalar:
         op = @(x) repmat(op(x), length(x), 1);
