798a79f421274849806c7de65e273fc7157d33da
Silviu Filip <filips@maths.ox.ac.uk>
2017-06-13 08:27:54 +0100

    added test file stubs

---
+++ tests/chebfun/test_cummax.m
@@ -0,0 +1,7 @@
+% Test file for @chebfun/cummax.m.
+
+function pass = test_cummax(pref)
+
+% TODO:
+
+end
---
+++ tests/chebfun/test_cummin.m
@@ -0,0 +1,7 @@
+% Test file for @chebfun/cummin.m.
+
+function pass = test_cummin(pref)
+
+% TODO:
+
+end
