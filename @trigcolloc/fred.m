c10be67077a8f68c78b2063d748af0a24ac4b853
Nick Hale <nickphale@gmail.com>
2017-06-30 11:14:55 +0200

    Throw sensible warnings for Fred and Volt.

---
+++ @trigcolloc/fred.m
@@ -0,0 +1,6 @@
+function varargout = fred(varargin)
+
+error('CHEBFUN:trigcolloc:fred:noSupport', ...
+    'No support for FRED in @TRIGCOLLOC class.');
+
+end
---
+++ @trigcolloc/volt.m
@@ -0,0 +1,6 @@
+function varargout = volt(varargin)
+
+error('CHEBFUN:trigcolloc:volt:noSupport', ...
+    'No support for VOLT in @TRIGCOLLOC class.');
+
+end
