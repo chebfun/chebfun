ed91880d74a2c49dbf11d76257ba578970f1092a
ajt60gaibb <alex.townsend1987@gmail.com>
2017-08-17 17:17:10 -0400

    Looks like we need vpa for now to compute shifts accurately when n>1500

--- @chebfun2/poisson.m
+++ @chebfun2/poisson.m
@@ -30,6 +30,9 @@ function u = poisson( f, g, m, n )
 %
 % SOLVE COMPLEXITY:  O(M*N*log(MAX(M,N))log(1/eps))  with M*N = total
 % degrees of freedom.
+% 
+% AUTHORS: Dan Fortunato (dan.fortunato@gmail.com ) and Alex 
+%          Townsend (townsend@cornell.edu). 
 
 % Solve for u on the same domain as f, adjust diffmat to including scaling:
 dom = f.domain;
@@ -134,9 +137,19 @@ B = det([-a*alp -alp a; -b -1 b ; c 1 c]);
 C = det([-alp a 1; -1 b 1 ; 1 c 1]);
 D = det([-a*alp -alp 1; -b -1 1; c 1 1]);
 T = @(z) (A*z+B)./(C*z+D);                     % Mobius transfom
-J = ceil( log(16*gam)*log(4/tol)/pi^2 );       % No. of ADI iterations
-K = ellipke( 1-1/alp^2 );
-[~, ~, dn] = ellipj((1/2:J-1/2)*K/J,1-1/alp^2);% ADI shifts for [-1,-1/t]&[1/t,1]
+J = ceil( log(16*gam)*log(4/tol)/pi^2 ); % No. of ADI iterations
+if ( alp > 1e7 )
+    alp = vpa( alp ); 
+    K = double( ellipke( 1-1/alp^2 ) );
+    m1 = 1/alp^2; 
+    u = (1/2:J-1/2)*K/J; 
+    dn = sech(u) + .25*m1*(sinh(u).*cosh(u)+u).*tanh(u).*sech(u); 
+    dn = double( dn );
+    alp = double( alp ); 
+else
+    K = ellipke( 1-1/alp^2 );
+    [~, ~, dn] = ellipj((1/2:J-1/2)*K/J,1-1/alp^2);% ADI shifts for [-1,-1/t]&[1/t,1]
+end
 p = T( -alp*dn ); q = T( alp*dn );             % ADI shifts for [a,b]&[c,d]
 end
 
