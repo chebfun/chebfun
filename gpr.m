7d2b293a0ba20a0e401f31c45cab2c0e39e360dd
Silviu Filip <silviuioan.filip@ens-lyon.fr>
2017-09-20 15:26:44 +0100

    default length scaled parameter based on log marginal likelihood

--- gpr.m
+++ gpr.m
@@ -1,16 +1,17 @@
 function varargout = gpr(x, y, varargin)
 %GPR        Gaussian Process regression
 %
-%   [F, FVAR] = GPR(X, Y) returns a CHEBFUN on [min(X),max(X)] corresponding
-%   to the posterior mean of a Gaussian Process with prior mean 0 and a
+%   [F, FVAR] = GPR(X, Y) returns a CHEBFUN on [min(X),max(X)] representing
+%   the posterior mean of a Gaussian Process with prior mean 0 and 
 %   squared exponential kernel k(x,x') = SIGMAF^2*exp(-1/(2*L^2)*(x-x')^2),
-%   with signal variance SIGMAF^2 = 0.01 and length scale
-%   L = 1/length(X)*(max(X)-min(X)). F interpolates Y at X. FVAR represents
-%   a chebfun estimate of the variance in the posterior.
+%   with signal variance SIGMAF^2 = 1. L is chosen such that it maximizes
+%   the log marginal likelihood (see eq.(2.30) from [1]). F interpolates Y
+%   at X. FVAR represents a chebfun estimate of the variance in the
+%   posterior.
 %
-%   [F, FVAR, SAMPLE] = GPR(X, Y, 'samples', N) also computes N samples from
-%   the posterior distribution, returning them as N independent columns of
-%   the quasimatrix SAMPLE.
+%   [F, FVAR, SAMPLES] = GPR(X, Y, 'samples', N) also computes N samples
+%   from the posterior distribution, returning them as N independent
+%   columns of the quasimatrix SAMPLES.
 %
 %   [...] = GPR(...,'domain', DOM) computes the results on the domain
 %   DOM = [A, B].
@@ -26,11 +27,11 @@ function varargout = gpr(x, y, varargin)
 %
 % Example:
 %
-%   n = 10; x = -2 + 4*rand(n,1);
-%   y = sin(exp(x));
-%   [f,fvar,smpl] = gpr(x,y,'domain',[-2,2],'samples',3,'hyperparams',[1,0.5]);
-%   plot(repmat(f,1,3)-smpl);
-%   hold on, plot(x,zeros(n,1),'.k','markersize',14), hold off
+%       n = 10; x = -2 + 4*rand(n,1);
+%       y = sin(exp(x));
+%       MS = 'markersize';
+%       [f,fvar,smpl] = gpr(x,y,'domain',[-2,2],'samples',3);
+%       plot(f), hold on, plot(smpl), plot(x,y,'.k',MS,14), hold off
 %
 % References:
 %
@@ -107,6 +108,7 @@ else % no data points given
     
     fvar = chebfun(opts.sigmaf^2,opts.dom);
 end
+fvar = simplify(fvar);
 
 % Take samples from the posterior and construct Chebfun representations
 % of them. For the moment, just sample at a large number of points and
@@ -209,12 +211,18 @@ if opts.trig % if domain endpoints are among data points, check to see if
     end
 end
 
-if ~opts.sigmaf && ~opts.lenScale % hyperparameters not specified; for the
-                                  % moment, just give some heuristic values
-                                  % based on the input data
+if ~opts.sigmaf && ~opts.lenScale % hyperparameters not specified
     n = length(x);
-    opts.sigmaf = 0.1;
-    opts.lenScale = 1/n*(opts.dom(end)-opts.dom(1));
+    opts.sigmaf = 1;    % fix the signal variance to 1
+    
+    % Construct a chebfun approximation of the log marginal likelihood
+    % parametrized on the length scale. Use the length scale maximizing
+    % this function.
+    domSize = opts.dom(end)-opts.dom(1);
+    searchDom = [2/(pi*n)*domSize,1/(min(4,n)*pi)*domSize];
+    %opts.lenScale = 1/n*domSize;
+    f = chebfun(@(z) logML(z,x,y,opts),searchDom,'eps',1e-6);
+    [~, opts.lenScale] = max(f);
 end
 
 end
@@ -236,5 +244,33 @@ else
 end
 
 fxEval = Kss*alpha;
+
+end
+
+% Computes the log marginal likelihood estimate for a given array of
+% hyperparameters (i.e., length scales)
+function fxEval = logML(lenScale, x,y, opts)
+
+fxEval = lenScale;
+[r,c] = size(lenScale);
+n = length(x);
+for i = 1:r
+    for j = 1:c
+        if opts.trig
+            K = opts.sigmaf^2*exp(-2/(lenScale(i,j)^2) * ...
+                    sin(pi/(opts.dom(end)-opts.dom(1))*(repmat(x,1,n) - ...
+                    repmat(x',n,1))).^2);
+        else
+            K = opts.sigmaf^2*exp(-1/(2*lenScale(i,j)^2) * ...
+                    (repmat(x,1,n) - repmat(x',n,1)).^2);
+        end
+    
+        % compute the Cholesky decomposition of K
+        L = chol(K+1e-15*n*eye(n), 'lower');
+        alpha = L'\(L\y);
+        % log marginal likelihood (see line 7 from Alg. 2.1 in [1])
+        fxEval(i,j) = -.5*y'*alpha - trace(log(L)) - n/2*log(2*pi);
+    end
+end
 
 end
