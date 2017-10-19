bd8b5fe620a3681a34fadd6e8129e9ddcef1d990
Nick Hale <nickphale@gmail.com>
2017-10-19 09:49:09 +0200

    Add test.

--- tests/chebop/test_intops.m
+++ tests/chebop/test_intops.m
@@ -56,5 +56,18 @@ A.lbc = 0;
 u = A\1;
 pass(6) = norm(A*u - 1, inf) < tol;
 
+%%
+% From #2185 
+A = chebop(@(x,u) diff(u,2) + sin(2*pi*x)*u + sum(u).*u);
+A.bc = 'periodic'; 
+u = A\1;
+pass(7) = norm(A*u-1);
+
+f = chebfun(@(x) sin(5*pi*x));
+K = @(x,y) cos(-pi*(x-y));
+A = chebop(@(x,u) 1/25*diff(u,2) + fred(K, u));
+A.bc = 'periodic'; 
+pass(8) = norm(A*u-f);
+
 end
 
