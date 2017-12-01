5d6bd8943c81f1da2d63f55f5e41180e5cd2b6e9
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2017-12-01 01:57:52 +0000

    revised test_gpr

--- tests/misc/test_gpr.m
+++ tests/misc/test_gpr.m
@@ -3,8 +3,7 @@
 function pass = test_gpr( pref )
 
 % Generate a few random points to use as test values.
-seedRNG(20);
-xx = 2 * rand(10, 1) - 1;
+rng(2), xx = 2 * rand(10, 1) - 1;
 % The values to match
 yy = exp(xx).*sin(2*xx);
 
@@ -22,26 +21,26 @@ err = norm(fsamples(xx,2) - yy, Inf);
 pass(3) = err < 5e-4;
 
 % Test adding noise to the data:
-yy = yy + .1*randn(size(yy));
+rng(2), yy = yy + .1*randn(size(yy));
 f = gpr(xx,yy,'noise',.1);
 pass(4) = std(f(xx)-yy) < .2;
 
 
 % Test horizontal scale (small interval):
-xx = 2e-100*rand(10,1)-1e-100;
+rng(10), xx = 2e-100*rand(10,1)-1e-100;
 yy = randn(10,1);
 f = gpr(xx,yy);
 err = norm(f(xx) - yy,Inf);
 pass(5) = err < 1e-6;
 
 % Test vertical scale (big function) and horizontal scale(small interval):
-yy = 1e100*randn(10,1);
+rng(2), yy = 1e100*randn(10,1);
 f = gpr(xx,yy);
 err = norm((f(xx) - yy)./yy,Inf);
 pass(6) = err < 1e-6;
 
 % Test vertical scale (big function, fixed hyperparameter(s)):
-xx = 2*rand(10,1)-1;
+rng(2), xx = 2*rand(10,1)-1;
 yy = 1e100*rand(10,1);
 f = gpr(xx,yy,'sigma',1e100,'L',.1);
 err = norm((f(xx) - yy)./yy,Inf);
@@ -68,13 +67,13 @@ f = gpr(x,y);
 ybig = S*y;
 fbig = gpr(x,ybig);
 err = norm(f-fbig/S);
-pass(10) = err < 5e-14;
+pass(11) = err < 5e-14;
 
 % Test vertical scale (small function):
 ysmall = y/S;
 fsmall = gpr(x,ysmall);
 err = norm(f-S*fsmall);
-pass(11) = err < 5e-14;
+pass(12) = err < 5e-14;
 
 % Test horizontal scale (big interval):
 x = (1:5).^2; y = sin(x);
@@ -84,18 +83,17 @@ xbig = S*x;
 fbig = gpr(xbig,y);
 xx = linspace(1,25);
 err = norm(f(xx)-fbig(S*xx));
-pass(12) = err < 5e-14;
+pass(13) = err < 5e-14;
 
 % Test horizontal scale (small interval):
 xsmall = x/S;
 fsmall = gpr(xsmall,y);
 xx = linspace(1,25);
 err = norm(f(xx)-fsmall(xx/S));
-pass(13) = err < 5e-14;
-
+pass(14) = err < 5e-14;
 
 % Test periodic version of the code:
-xx = 2 * rand(100, 1) - 1;
+rng(2), xx = 2 * rand(100, 1) - 1;
 % The values to match
 
 yy = exp(xx.^2);
@@ -103,15 +101,14 @@ yy = exp(xx.^2);
 [f,~,fsamples] = gpr(xx,yy,'domain',[-1,1],'trig','samples',2);
 
 err = norm(f(xx) - yy,Inf);
-pass(14) = err < 1e-5;
+pass(15) = err < 1e-5;
 
 % Test the accuracy of the samples:
 err = norm(fsamples(xx,1) - yy, Inf);
-pass(15) = err < 5e-4;
-
-err = norm(fsamples(xx,2) - yy, Inf);
 pass(16) = err < 5e-4;
 
+err = norm(fsamples(xx,2) - yy, Inf);
+pass(17) = err < 5e-4;
 
 % Test vertical scale (big function):
 x = (1:5).^2; y = sin(x);
@@ -120,13 +117,13 @@ f = gpr(x,y,'trig');
 ybig = S*y;
 fbig = gpr(x,ybig,'trig');
 err = norm(f-fbig/S);
-pass(17) = err < 5e-14;
+pass(18) = err < 5e-14;
 
 % Test vertical scale (small function):
 ysmall = y/S;
 fsmall = gpr(x,ysmall,'trig');
 err = norm(f-S*fsmall);
-pass(18) = err < 5e-14;
+pass(19) = err < 5e-14;
 
 % Test horizontal scale (big interval):
 x = (1:5).^2; y = sin(x);
@@ -136,13 +133,13 @@ xbig = S*x;
 fbig = gpr(xbig,y,'trig');
 xx = linspace(1,25);
 err = norm(f(xx)-fbig(S*xx));
-pass(19) = err < 5e-14;
+pass(20) = err < 5e-14;
 
 % Test horizontal scale (small interval):
 xsmall = x/S;
 fsmall = gpr(xsmall,y,'trig');
 xx = linspace(1,25);
 err = norm(f(xx)-fsmall(xx/S));
-pass(20) = err < 5e-14;
+pass(21) = err < 5e-14;
 
 end
