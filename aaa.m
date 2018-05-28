b731b63e2c08d53084f276c7ed0026fca74c3493
Abi Gopal <agopal43@gmail.com>
2018-05-28 09:52:14 +0100

    Improved residue computation

--- aaa.m
+++ aaa.m
@@ -324,9 +324,10 @@ pol = eig(E, B);
 % Remove zeros of denominator at infinity:
 pol = pol(~isinf(pol));
 
-% Compute residues via discretized Cauchy integral:
-dz = 1e-5*exp(2i*pi*(1:4)/4);
-res = r(bsxfun(@plus, pol, dz))*dz.'/4;
+% Compute residues via formula for res of quotient of analytic functions:
+N = @(t)(1./bsxfun(@minus,t,zj.')) * (fj.*wj);
+Ddiff = @(t) -((1./bsxfun(@minus,t,zj.')).^2) * wj;
+res = N(pol)./Ddiff(pol);
 
 % Compute zeros via generalized eigenvalue problem:
 E = [0 (wj.*fj).'; ones(m, 1) diag(zj)];
