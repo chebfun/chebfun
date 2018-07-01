14149ab229dbcea1fed22a52ecc7dbdc08fd37f7
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2018-07-01 09:20:46 +0100

    fix bug in padeapprox for complex inputs

--- tests/misc/test_padeapprox.m
+++ tests/misc/test_padeapprox.m
@@ -7,4 +7,8 @@ f = @(x) (x.^4 - 3)./((x + 3.2).*(x - 2.2));
 pass(1) = (mu == 4) && (nu == 2) && ...
     (max(abs(sort(poles) - [-3.2 ; 2.2])) < 1e-10);
 
+c = [1 1i];
+[r, a, b] = padeapprox(c, 0, 1);
+pass(2) = (abs(a-1) < 1e-10) && (abs(b(2)-(-1i)) < 1e-10); 
+
 end
