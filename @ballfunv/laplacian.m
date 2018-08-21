541db4b1ee00909a80a0c9479a4cc2fbbf9deabf
ajt60gaibb <alex.townsend1987@gmail.com>
2018-08-21 09:44:10 -0400

    Change names of laplace to lapacian

--- @ballfunv/laplacian.m
+++ @ballfunv/laplacian.m
@@ -1,6 +1,6 @@
-function f = laplace(v)
-% LAPLACE Laplacian of a BALLFUNV
-%   LAPLACE(v) is the laplacian of the BALLFUNV v
+function f = laplacian(v)
+% LAPLACIAN Laplacian of a BALLFUNV.
+%   LAPLACIAN(V) is the laplacian of the BALLFUNV V.
 
 % Copyright 2018 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
