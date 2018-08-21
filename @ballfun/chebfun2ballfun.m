ab3d9a9c2336277dfa5609528a2cb4b81786c583
Nicolas Boulle <nicolas.boulle@ens-rennes.fr>
2018-08-21 21:59:52 +0200

    chebfun2ballfun doesn't depend on S

--- @ballfun/chebfun2ballfun.m
+++ @ballfun/chebfun2ballfun.m
@@ -1,17 +1,15 @@
-function g = chebfun2ballfun(f,S)
+function g = chebfun2ballfun(f)
 %CHEBFUN2BALLFUN   Convert a CHEBFUN in the r-variable to a BALLFUN.
 %   CHEBFUN2BALLFUN(f) is the ballfun function g(r,lam,th) = f(r)
 
 % Copyright 2018 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
 
-m = S(1); n = S(2); p = S(3);
-% Approximation of f on the m first chebyshev polynomials
-f = chebfun(f,m);
 % Chebyshev coefficients of f
 F = chebcoeffs(f);
+m = length(F);
 % Construction of the ballfun function
-G = zeros(m,n,p);
-G(:,floor(n/2)+1,floor(p/2)+1) = F;
+G = zeros(m,1,1);
+G(:,1,1) = F;
 g = ballfun(G);
 end
