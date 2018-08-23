8e0cbc990de51ddc44d0dd4bd2c86b3ffa31b2b5
ajt60gaibb <alex.townsend1987@gmail.com>
2018-08-22 20:18:23 -0400

    Other compose commands

--- @ballfun/sqrt.m
+++ @ballfun/sqrt.m
@@ -5,7 +5,6 @@ function g = sqrt(f)
 % Copyright 2018 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
 
-F = f.coeffs;
-G = ballfun.vals2coeffs(sqrt(ballfun.coeffs2vals(F)));
-g = ballfun(G);
+g = compose( f, @sqrt ); 
+
 end
