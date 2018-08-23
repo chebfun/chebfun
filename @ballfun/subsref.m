60b176b9cac2d1b87a75844006d1e5c3ea156805
ajt60gaibb <alex.townsend1987@gmail.com>
2018-08-22 20:42:14 -0400

    Implement subsref

--- @ballfun/subsref.m
+++ @ballfun/subsref.m
@@ -20,11 +20,66 @@ function varargout = subsref(f, index)
 %
 %   F.PROP returns the property of F specified in PROP.
 %
-% See also DISKFUN/FEVAL, BALLFUN/GET. 
+% See also BALLFUN/FEVAL, BALLFUN/GET. 
 
 % Copyright 2018 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
 
-% TODO: Implemented this. 
+idx = index(1).subs;
+switch index(1).type
+    case '()'
+        % FEVAL / COMPOSE
+        if ( numel(idx) == 3 )
+            % Find where to evaluate:
+            x = idx{1};
+            y = idx{2};
+            z = idx{3};
+            % If x, y, z are numeric or ':' call feval().
+            if ( ( isnumeric(x) || strcmpi(x, ':') ) && ...
+                    ( isnumeric(y) || strcmpi(y, ':') ) && ...
+                    ( isnumeric(z) || strcmpi(z, ':') ) )
+                out = feval(f, x, y, z);
+            else
+                % Don't know what to do.
+                error('CHEBFUN:BALLFUN:subsref:inputs3', ...
+                    'Unrecognized inputs.')
+            end            
+        elseif ( numel(idx) == 4 && strcmpi(idx(4),'cart') )
+            x = idx{1};
+            y = idx{2};
+            z = idx{3};
+            out = feval(f, x, y, z);
+        elseif ( numel(idx) == 4 && strcmpi(idx(4),'polar') )
+            r = idx{1};
+            lam = idx{2};
+            th = idx{3};
+            x = @(r,lam,th)r.*sin(th).*cos(lam);
+            y = @(r,lam,th)r.*sin(th).*sin(lam);
+            z = @(r,lam,th)r.*cos(th);
+            out = feval(f, x(r,lam,th), y(r,lam,th), z(r,lam,th));
+        else
+            error('CHEBFUN:CHEBFUN3:subsref:inputs', ...
+                'Can only evaluate at triples (X,Y,Z), a CHEBFUN with 3 columns, a CHEBFUN2V or a CHEBFUN3V.')
+        end
+        varargout = {out};
+        
+    case '.'
+        % Call GET() for .PROP access.
+        out = get(f, idx);
+        if ( numel(index) > 1 )
+            % Recurse on SUBSREF():
+            index(1) = [];
+            out = subsref(out, index);
+        end
+        varargout = {out};
+        
+    case '{}'
+        % RESTRICT
+        error('CHEBFUN:BALLFUN:subsref:restrict', ...
+                ['This syntax is reserved for restricting',...
+                 'the domain of a ballfun. This functionality'...
+                 'is not available in Ballfun.'])
+        
+end
 
 end
