6d53be98cd2270b8b881f3553c3be7c475e32942
ajt60gaibb <alex.townsend1987@gmail.com>
2018-08-27 09:42:21 -0400

    Compose also works on functions of two variables

--- @ballfun/power.m
+++ @ballfun/power.m
@@ -9,21 +9,6 @@ function g = power(f,n)
 % Copyright 2018 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
 
-% Fast power implementation
-if (n >= 0)
-    S = size(f);
-    g = ballfun(@(r,lam,th)1,S);
-    while (n>0)
-        % If power is odd
-        if (mod(n,2) == 1)
-            g = g*f;
-        end
-        n = floor(n/2);
-        f = f*f;
-    end 
-else
-    error('BALLFUN:isequal:unknown', ...
-    ['Undefined function ''power'' for negative power : ' ...
-     '%d.'], n);
-end
+n = ballfun( n );
+g = compose(f, @power, n); 
 end
