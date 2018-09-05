54778b6ec185bf931698906b41c8ea967e1e9df6
Nicolas Boulle <nicolas.boulle@ens-rennes.fr>
2018-09-05 21:18:44 +0200

    Add sum2(f,dim_1,dim_2)

--- @ballfun/sum2.m
+++ @ballfun/sum2.m
@@ -1,38 +1,111 @@
-function ff = sum2(f)
+function ff = sum2(f,dim_1,dim_2)
 %SUM2  Integration of a BALLFUN function over lambda and theta
-%   SUM2(F) is the integration of the BALLFUN function f over lambda
-%   and theta.
+%   SUM2(F,dim_1,dim_2) is the integration of the BALLFUN function f over
+%   dim_1 and dim_2
 %
 % See also SUM, SUM3.
 
 % Copyright 2018 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
 
+% Sort the dimensions
+dim = sort([dim_1,dim_2]);
+dim_1 = dim(1); dim_2 = dim(2);
+
 [m,n,p] = size(f);
 m = m+2; p = p+2;
 F = coeffs3(f,m,n,p);
 
-% Extract the 0-th Fourier mode
-F = reshape(F(:,floor(n/2)+1,:), m, p);
+if dim_1 == 2 && dim_2 == 3
+    % Integrate over lambda and theta
+    % Multiply by F by the measure r^2*sin(theta)
 
-% Multiply f par r^2sin(theta) (= Jacobian)
-% Just do the multiplication for the 0-th Fourier mode
-trig1 = trigtech( @(t) sin(pi*t));
-Msin = trigspec.multmat(p, trig1.coeffs );
-Mr2 = ultraS.multmat(m, [.5;0;.5], 0 );
-F = Mr2*F*(Msin.');
+    % Extract the 0-th Fourier mode
+    F = reshape(F(:,floor(n/2)+1,:), m, p);
 
-% Integration for the Fourier modes in theta
-Listp = (1:p) - floor(p/2) - 1;
-C = -1i*((-1).^Listp-1)./Listp;
-C(floor(p/2)+1) = pi;
+    % Multiply f par r^2sin(theta) (= Jacobian)
+    % Just do the multiplication for the 0-th Fourier mode
+    trig1 = trigtech( @(t) sin(pi*t));
+    Msin = trigspec.multmat(p, trig1.coeffs );
+    Mr2 = ultraS.multmat(m, [.5;0;.5], 0 );
+    F = Mr2*F*(Msin.');
 
-C=2*pi*C.';
-% Vector of coefficients of chebyshev after integration over lambda and
-% theta
-A = F*C;
+    % Integration for the Fourier modes in theta
+    Listp = (1:p) - floor(p/2) - 1;
+    C = -1i*((-1).^Listp-1)./Listp;
+    C(floor(p/2)+1) = pi;
 
-% Return the chebfun function of r after the multiplication by r^2
-% (measure)
-ff = chebfun(A, 'coeffs');
+    C=2*pi*C.';
+    % Vector of coefficients of chebyshev after integration over lambda and
+    % theta
+    A = F*C;
+
+    % Return the chebfun function of r after the multiplication by r^2
+    % (measure)
+    ff = chebfun(A, 'coeffs');
+    
+elseif dim_1 == 1 &&  dim_2 == 2
+    % Integrate over r and lambda
+    
+    % Coefficients of integration between 0 and 1 of the chebyshev polynomials
+    IntChebyshev = zeros(1,m);
+    for i = 0:m-1
+        if mod(i,4)==0
+            IntChebyshev(i+1) = -1/(i^2-1);
+        elseif mod(i,4)==1
+            IntChebyshev(i+1) = 1/(i+1);
+        elseif mod(i,4)==2
+            IntChebyshev(i+1) = -1/(i^2-1);
+        else
+            IntChebyshev(i+1) = -1/(i-1);
+        end
+    end
+    
+    % Integrate over lambda
+    F = 2*pi*reshape(F(:,floor(n/2)+1,:), m, p);
+    
+    % Integrate over r
+    A = IntChebyshev*F;
+    
+    % Return the chebfun function of theta on the domain
+    ff = chebfun(A.', [-pi,pi],'coeffs', 'trig');
+
+elseif dim_1 == 1 && dim_2 == 3
+    % Integrate over r and theta
+    
+    % Coefficients of integration between 0 and 1 of the chebyshev polynomials
+    IntChebyshev = zeros(1,m);
+    for i = 0:m-1
+        if mod(i,4)==0
+            IntChebyshev(i+1) = -1/(i^2-1);
+        elseif mod(i,4)==1
+            IntChebyshev(i+1) = 1/(i+1);
+        elseif mod(i,4)==2
+            IntChebyshev(i+1) = -1/(i^2-1);
+        else
+            IntChebyshev(i+1) = -1/(i-1);
+        end
+    end
+    
+    % Coefficients of integration between 0 and pi of the theta Fourier
+    % function
+    Listp = (1:p).' - floor(p/2)-1;
+    IntTheta = -1i*((-1).^Listp-1)./Listp;
+    IntTheta(floor(p/2)+1) = pi;
+    
+    % Permute F
+    F = permute(F,[1,3,2]);
+    
+    % Integrate over r and theta
+    A = zeros(n,1);
+    
+    for k = 1:n
+        A(k) = IntChebyshev*F(:,:,k)*IntTheta;    
+    end
+        
+    % Return the chebfun function of lambda on the domain
+    ff = chebfun(A, [-pi,pi],'coeffs', 'trig');
+else
+    error('BALLFUN:sum2: Input arguments not supported'); 
+end
 end
--- tests/ballfun/test_sum2.m
+++ tests/ballfun/test_sum2.m
@@ -6,26 +6,36 @@ if ( nargin == 0 )
 end
 tol = 1e4*pref.techPrefs.chebfuneps;
 
-S = [20,21,22];
-m = S(1);
-
 % Example 1
-f = ballfun(@(r,lam,th)1,S);
-ff = sum2(f);
-g = chebfun(@(r)4*pi*r.^2,m);
-pass(1) = max(abs(chebcoeffs(ff-g,m)))<tol;
+f = ballfun(@(r,lam,th)1);
+ff = sum2(f,2,3);
+g = chebfun(@(r)4*pi*r.^2);
+pass(1) =norm( ff - g ) <tol;
 
 % Example 2
-f = ballfun(@(r,lam,th)cos(lam),S);
-ff = sum2(f);
-g = chebfun(@(r)0,m);
-pass(2) = max(abs(chebcoeffs(ff-g,m)))<tol;
+f = ballfun(@(r,lam,th)cos(lam));
+ff = sum2(f,3,2);
+g = chebfun(@(r)0);
+pass(2) =norm( ff - g ) <tol;
 
 % Example 3
-f = ballfun(@(r,lam,th)r.^2.*sin(lam),S);
-ff = sum2(f);
-g = chebfun(@(r)0,m);
-pass(3) = max(abs(chebcoeffs(ff-g,m)))<tol;
+f = ballfun(@(r,lam,th)r.^2.*sin(lam));
+ff = sum2(f,2,3);
+g = chebfun(@(r)0);
+pass(3) =norm( ff - g ) <tol;
+
+% Example 4
+f = ballfun(@(r,lam,th)r.*sin(th));
+ff = sum2(f,1,2);
+g = chebfun(@(th)pi*sin(th),[-pi,pi],'trig');
+pass(4) = norm( ff - g ) <tol;
+
+% Example 5
+f = ballfun(@(r,lam,th)r.^2.*sin(th).*cos(lam));
+ff = sum2(f,1,3);
+g = chebfun(@(lam)2*cos(lam)/3,[-pi,pi],'trig');
+pass(5) = norm( ff - g ) < tol;
+
 
 if (nargout > 0)
     pass = all(pass(:));
