0d5afe67b28846a4660005390db2bf56bcd5198e
Nicolas Boulle <nicolas.boulle@maths.ox.ac.uk>
2018-11-08 15:00:58 +0000

    BMCIII is almost working

--- @ballfun/BMCIII.m
+++ @ballfun/BMCIII.m
@@ -2,16 +2,17 @@ function F = BMCIII(f,m,n,p)
 % Take a function handle and a size and return the coefficients in the
 % CFF basis by evaluating it at only a fourth of the domain
 
-% Remove issues when f is constant
-f1 = @(r,lam,th) f(r,lam,th) + 0*r + 0*lam + 0*th;
-
 % Evaluation points
-r = chebpts(m);
-lam = [pi*trigpts(n); pi];
-th = [pi*trigpts(p); pi];
+r = reshape(chebpts(m),m,1,1);
+lam = reshape([pi*trigpts(n); pi],1,n+1,1);
+th = reshape([pi*trigpts(p); pi],1,1,p+1);
 
 % Array of coefficients
 F = zeros(m,n,p);
+
+f1 = @(r,lam,th)f(r,lam,th) + 0*r + 0*lam + 0*th;
+
+% We assume n is even
 
 %% Chebyshev
 % m even : evaluate at half and then flip it
@@ -23,32 +24,32 @@ F = zeros(m,n,p);
 % [flip(th(floor((p+1)/2)+1:p+1)) ; th(floor((p+1)/2)+1:p)]
 
 %% g
-% Evaluate at r(floor(m/2)+1:m), lam(1:floor(n/2)), th(floor((p+1)/2)+1:p+1)
-g = reshape(f1(r(floor(m/2)+1:m), lam(1:floor((n+1)/2)), th(floor((p+1)/2)+1:p+1)),a,b,c);
+% Evaluate at r(floor(m/2)+1:m), lam(1:n/2+1), th(floor((p+1)/2)+1:p+1)
+g = f1(r(floor(m/2)+1:m), lam(1:n/2+1), th(floor((p+1)/2)+1:p+1));
 
 %% h
-% Evaluate at r(floor(m/2)+1:m), lam(floor(n/2)+1:n), th(floor((p+1)/2)+1:p+1)
-h = reshape(f1(r(floor(m/2)+1:m), lam(floor((n+1)/2)+1:n+1), th(floor((p+1)/2)+1:p+1)),a,b,c);
+% Evaluate at r(floor(m/2)+1:m), lam(n/2+1:end), th(floor((p+1)/2)+1:p+1)
+h = f1(r(floor(m/2)+1:m), lam(n/2+1:end), th(floor((p+1)/2)+1:p+1));
 
 %% Flip g and h on the radial direction
 flip1g = flip(g(1+mod(m,2):end,:,:), 1);
 flip1h = flip(h(1+mod(m,2):end,:,:), 1);
 
 %% Fill in F
-% [0,1] x [-pi,0] x [0,pi]
-F(floor(m/2)+1:m, 1:floor((n+1)/2), floor((p+1)/2)+1:p) = g(:,:,1:end-1);
-% [0,1] x [0,pi] x [0,pi]
-F(floor(m/2)+1:m, floor((n+1)/2)+1:n, floor((p+1)/2)+1:p) = h(:,1:end-1,1:end-1);
-% [-1,0] x [-pi,0] x [0,pi]
-F(1:floor(m/2), 1:floor((n+1)/2), floor((p+1)/2)+1:p) = flip1g(:,:,1:end-1);
-% [-1,0] x [0,pi] x [0,pi]
-F(1:floor(m/2), floor((n+1)/2)+1:n, floor((p+1)/2)+1:p) = flip1h(:,1:end-1,1:end-1);
+% [0,1] x [-pi,0] x [0,pi[
+F(floor(m/2)+1:m, 1:n/2+1, floor((p+1)/2)+1:p) = g(:,:,1:end-1);
+% [0,1] x [0,pi[ x [0,pi[
+F(floor(m/2)+1:m, n/2+1:n, floor((p+1)/2)+1:p) = h(:,1:end-1,1:end-1);
+% [-1,0[ x [-pi,0] x [0,pi[
+F(1:floor(m/2), 1:n/2+1, floor((p+1)/2)+1:p) = flip1h(:,:,1:end-1);
+% [-1,0[ x [0,pi[ x [0,pi[
+F(1:floor(m/2), n/2+1:n, floor((p+1)/2)+1:p) = flip1g(:,1:end-1,1:end-1);
 % [0,1] x [-pi,0] x [-pi,0]
-F(floor(m/2)+1:m, 1:floor((n+1)/2), 1:floor((p+1)/2)) = flip(h,3);
-% [0,1] x [0,pi] x [-pi,0]
-F(floor(m/2)+1:m, 1:floor((n+1)/2), 1:floor((p+1)/2)) = flip(g,3);
-% [-1,0] x [0,pi] x [-pi,0]
-F(1:floor(m/2), floor((n+1)/2)+1:n, 1:floor((p+1)/2)) = flip(flip1h(:,1:end-1,1+mod(p+1,2):end),3);
-% [-1,0] x [-pi,0] x [-pi,0]
-F(1:floor(m/2), 1:floor((n+1)/2), 1:floor((p+1)/2)) = flip(flip1g(:,:,1+mod(p+1,2):end),3);
+F(floor(m/2)+1:m, 1:n/2+1, 1:floor((p+1)/2)) = flip(h(:,:,1+mod(p+1,2):end),3);
+% [0,1] x [0,pi[ x [-pi,0]
+F(floor(m/2)+1:m, n/2+1:n, 1:floor((p+1)/2)) = flip(g(:,1:end-1,1+mod(p+1,2):end),3);
+% [-1,0[ x [0,pi[ x [-pi,0]
+F(1:floor(m/2), n/2+1:n, 1:floor((p+1)/2)) = flip(flip1h(:,1:end-1,1+mod(p+1,2):end),3);
+% [-1,0[ x [-pi,0] x [-pi,0]
+F(1:floor(m/2), 1:n/2+1, 1:floor((p+1)/2)) = flip(flip1g(:,:,1+mod(p+1,2):end),3);
 end
---
+++ tests/ballfun/test_BMCIII.m
@@ -0,0 +1,102 @@
+function pass = test_BMCIII( pref ) 
+
+% Grab some preferences
+if ( nargin == 0 )
+    pref = chebfunpref();
+end
+tol = 1e2*pref.techPrefs.chebfuneps; 
+
+% Example 1
+f = @(r,lam,th)r.*cos(lam).*sin(th);
+m = 4; n = 4; p = 3;
+F = ballfun.BMCIII(f,m,n,p);
+r = reshape(chebpts(m),m,1,1);
+lam = reshape(pi*trigpts(n),1,n,1);
+th = reshape(pi*trigpts(p),1,1,p);
+G = f(r,lam,th);
+pass(1) = norm(F(:)-G(:)) < tol;
+
+% Example 2
+f = @(r,lam,th)r.*cos(lam).*sin(th);
+m = 4; n = 4; p = 3;
+F = ballfun.BMCIII(f,m,n,p);
+r = reshape(chebpts(m),m,1,1);
+lam = reshape(pi*trigpts(n),1,n,1);
+th = reshape(pi*trigpts(p),1,1,p);
+G = f(r,lam,th);
+pass(2) = norm(F(:)-G(:)) < tol;
+
+% Example 3
+f = @(r,lam,th)r.*cos(lam).*sin(th);
+m = 3; n = 6; p = 4;
+F = ballfun.BMCIII(f,m,n,p);
+r = reshape(chebpts(m),m,1,1);
+lam = reshape(pi*trigpts(n),1,n,1);
+th = reshape(pi*trigpts(p),1,1,p);
+G = f(r,lam,th);
+pass(3) = norm(F(:)-G(:)) < tol;
+
+% Example 4
+f = @(r,lam,th)r.*sin(lam).*sin(th);
+m = 4; n = 4; p = 3;
+F = ballfun.BMCIII(f,m,n,p);
+r = reshape(chebpts(m),m,1,1);
+lam = reshape(pi*trigpts(n),1,n,1);
+th = reshape(pi*trigpts(p),1,1,p);
+G = f(r,lam,th);
+pass(4) = norm(F(:)-G(:)) < tol;
+
+% Example 5
+f = @(r,lam,th)r.*sin(lam).*sin(th);
+m = 4; n = 4; p = 3;
+F = ballfun.BMCIII(f,m,n,p);
+r = reshape(chebpts(m),m,1,1);
+lam = reshape(pi*trigpts(n),1,n,1);
+th = reshape(pi*trigpts(p),1,1,p);
+G = f(r,lam,th);
+pass(5) = norm(F(:)-G(:)) < tol;
+
+% Example 6
+f = @(r,lam,th)r.*sin(lam).*sin(th);
+m = 3; n = 6; p = 4;
+F = ballfun.BMCIII(f,m,n,p);
+r = reshape(chebpts(m),m,1,1);
+lam = reshape(pi*trigpts(n),1,n,1);
+th = reshape(pi*trigpts(p),1,1,p);
+G = f(r,lam,th);
+pass(6) = norm(F(:)-G(:)) < tol;
+
+% Example 7
+f = @(r,lam,th)r.*cos(th)+0*lam;
+m = 4; n = 4; p = 3;
+F = ballfun.vals2coeffs(ballfun.BMCIII(f,m,n,p));
+r = reshape(chebpts(m),m,1,1);
+lam = reshape(pi*trigpts(n),1,n,1);
+th = reshape(pi*trigpts(p),1,1,p);
+G = ballfun.vals2coeffs(f(r,lam,th));
+pass(7) = norm(F(:)-G(:)) < tol;
+
+% Example 8
+f = @(r,lam,th)r.*cos(th)+0*lam;
+m = 4; n = 4; p = 3;
+F = ballfun.BMCIII(f,m,n,p);
+r = reshape(chebpts(m),m,1,1);
+lam = reshape(pi*trigpts(n),1,n,1);
+th = reshape(pi*trigpts(p),1,1,p);
+G = f(r,lam,th);
+pass(8) = norm(F(:)-G(:)) < tol;
+
+% Example 9
+f = @(r,lam,th)r.*cos(th)+0*lam;
+m = 3; n = 6; p = 4;
+F = ballfun.BMCIII(f,m,n,p);
+r = reshape(chebpts(m),m,1,1);
+lam = reshape(pi*trigpts(n),1,n,1);
+th = reshape(pi*trigpts(p),1,1,p);
+G = f(r,lam,th);
+pass(9) = norm(F(:)-G(:)) < tol;
+
+if (nargout > 0)
+    pass = all(pass(:));
+end
+end
