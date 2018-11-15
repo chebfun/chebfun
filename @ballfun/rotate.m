2b103f4827fd4af3e0758317ba76e72cef54bcaf
Nicolas Boulle <nicolas.boulle@maths.ox.ac.uk>
2018-11-15 17:23:30 +0000

    Make the evaluation points even in rotate

--- @ballfun/rotate.m
+++ @ballfun/rotate.m
@@ -31,7 +31,8 @@ N = P + mod(P,2);         % Number of columns must be even.
 % Set the number of rows in the sampled grid equal to n/2+2 so that the 
 % doubled up grid will have n rows (n/2+2 because the pole is included in
 % the sampled grid, and the doubled up grid does not contain -pi).
-P = ceil(P/2)+2+mod(P,2);
+P = ceil(P/2)+2;
+P = P + mod(P,2);
 
 % Sampling grid.
 [lam,th] = meshgrid(trigpts(N,[-pi pi]),linspace(0,pi,P));
