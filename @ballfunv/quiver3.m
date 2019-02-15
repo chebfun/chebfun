a5dc9135fab80091eff2d5df721b5f2d5146ad2e
Nicolas Boulle <nicolas.boulle@maths.ox.ac.uk>
2019-02-15 00:40:47 +0000

    Add quiver3

---
+++ @ballfunv/quiver3.m
@@ -0,0 +1,11 @@
+function  quiver3(F, varargin)
+%QUIVER3   Quiver plot of a BALLFUNV.
+%   Same as QUIVER.
+%
+% See also BALLFUNV/QUIVER.
+
+% Copyright 2019 by The University of Oxford and The Chebfun Developers.
+% See http://www.chebfun.org/ for Chebfun information.
+
+quiver(F, varargin{:})
+end
