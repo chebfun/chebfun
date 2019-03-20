05754c2481cbcb26578242aeb58a49dc6c6e0462
Nicolas Boulle <nicolas.boulle@maths.ox.ac.uk>
2019-03-20 12:29:41 +0000

    Update test_abs and test_sqrt

--- tests/ballfun/test_abs.m
+++ tests/ballfun/test_abs.m
@@ -16,6 +16,11 @@ f = abs(ballfun(@(x,y,z)(x+1i*y).^2));
 exact = ballfun(@(x,y,z)x.^2+y.^2);
 pass(2) = norm( f - exact ) < tol;
 
+% Example 3
+exact = ballfun(@(r,lam,th)r.^2,'spherical');
+f = abs(exact);
+pass(3) = norm( f - exact ) < tol;
+
 if (nargout > 0)
     pass = all(pass(:));
 end
--- tests/ballfun/test_sqrt.m
+++ tests/ballfun/test_sqrt.m
@@ -11,9 +11,9 @@ f = sqrt(ballfun(@(x,y,z)2));
 exact = ballfun(@(x,y,z)sqrt(2));
 pass(1) = norm( f - exact ) < tol;
 
-% Example 1
-f = sqrt(ballfun(@(x,y,z)x.^4));
-exact = ballfun(@(x,y,z)x.^2);
+% Example 2
+f = sqrt(ballfun(@(r,lam,th)r.^4,'spherical'));
+exact = ballfun(@(r,lam,th)r.^2,'spherical');
 pass(2) = norm( f - exact ) < tol;
 
 if (nargout > 0)
