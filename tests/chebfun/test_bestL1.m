f7e034a86a38a4293de15ec37dd59756c22da76d
ajt60gaibb <alex.townsend1987@gmail.com>
2019-07-02 11:55:00 +0100

    Tidy up

--- tests/chebfun/test_bestL1.m
+++ tests/chebfun/test_bestL1.m
@@ -1,30 +1,22 @@
+function pass = test_bestL1( pref )
 
-
-function pass = test_bestL1(pref)
+if ( nargin == 0 )
+    pref = chebfunpref();
+end
+tol = 1e-8;
 
 f = chebfun(@(x) exp(x)*sin(10*x));
-%f = chebfun(@(x) abs(x-0.2),'splitting','on'); func = 'abs02';
 n = 5;  
 T = chebpoly(0:n); 
 
-p1best = watson2(f,n); 
+p1best = watson(f, n); 
 err = f-p1best; 
-pass(1) = norm(sum( T.*sign(err) ))<1e-8
-
-
+pass(1) = norm(sum( T.*sign(err) )) < tol;
 
 n = 5;  
 T = chebpoly(0:n); 
-f = chebfun(@(x) abs(x-0.2),'splitting','on');
-p1best = watson2(f,n); 
+f = chebfun(@(x) abs(x-0.2), 'splitting', 'on');
+p1best = watson(f, n); 
 err = f-p1best; 
-pass(2) = norm(sum( T.*sign(err) ))<1e-8
-
-%{
-p1best = L1min_watson(f,n); 
-err = f-p1best; 
-% plot(err),shg
-pass(2) = norm(sum( T.*sign(err) ))<1e-8
-%}
-
+pass(2) = norm(sum( T.*sign(err) )) < tol;
 end
