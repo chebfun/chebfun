edcdb3fe1a4206e77e99d52e009e49cb41c83faa
nakatsukasayuji <nakatsukasayuji@gmail.com>
2019-07-02 12:06:01 +0100

    Revert "test code and watson2"
    This reverts commit 0700d8d8993334b5c3ce28c3a5470297522ead8a.

--- tests/chebfun/test_bestL1.m
+++ tests/chebfun/test_bestL1.m
@@ -2,29 +2,19 @@
 
 function pass = test_bestL1(pref)
 
-f = chebfun(@(x) exp(x)*sin(10*x));
+f = chebfun(@(x) exp(x)*sin(10*x)); func = 'expsin10';
 %f = chebfun(@(x) abs(x-0.2),'splitting','on'); func = 'abs02';
 n = 5;  
-T = chebpoly(0:n); 
-
-p1best = watson2(f,n); 
-err = f-p1best; 
-pass(1) = norm(sum( T.*sign(err) ))<1e-8
-
-
-
-n = 5;  
-T = chebpoly(0:n); 
-f = chebfun(@(x) abs(x-0.2),'splitting','on');
-p1best = watson2(f,n); 
-err = f-p1best; 
-pass(2) = norm(sum( T.*sign(err) ))<1e-8
-
-%{
 p1best = L1min_watson(f,n); 
+
+T = chebpoly(0:n); 
+
 err = f-p1best; 
 % plot(err),shg
+pass(1) = norm(sum( T.*sign(err) ))<1e-8
+
+p1best = watson(f,n); 
+err = f-p1best; 
 pass(2) = norm(sum( T.*sign(err) ))<1e-8
-%}
 
 end
