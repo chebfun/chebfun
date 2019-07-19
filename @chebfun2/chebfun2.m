8c59ae7ef029f615311d6a0ac285835356f2b619
danfortunato <dan.fortunato@gmail.com>
2019-07-19 15:42:20 -0400

    Update chebfun2 help text for mixed technology

--- @chebfun2/chebfun2.m
+++ @chebfun2/chebfun2.m
@@ -13,8 +13,13 @@ classdef chebfun2 < separableApprox
 % CHEBFUN2(F, [A B C D]) specifies a rectangle [A B]x[C D] where the 
 % function is defined. A, B, C, D must all be finite.
 %
-% If F is a matrix, A = (a_ij), the numbers aij are used as function values
-% at tensor Chebyshev points of the 2nd kind.
+% If F is a matrix, F = (f_ij), the numbers f_ij are used as function
+% values at tensor Chebyshev points of the 2nd kind. CHEBFUN2(F, 'equi')
+% assumes the function values f_ij come from an equispaced tensor grid.
+% CHEBFUN2(F, 'equix') assumes the values come from a tensor grid that is
+% equispaced in x and 2nd-kind Chebyshev in y. CHEBFUN2(F, 'equiy') assumes
+% the values come from a tensor grid that is 2nd-kind Chebyshev in x and
+% equispaced in y.
 % 
 % CHEBFUN2(F, k) returns a rank k approximation to F.
 %
@@ -26,8 +31,27 @@ classdef chebfun2 < separableApprox
 % CHEBFUN2(F, k, [A B C D]) or CHEBFUN2(F, [m,n], [A B C D]) is nonadaptive in
 % rank or degrees, as above, returning a chebfun2 on the domain [A B]x[C D]. 
 % 
-% CHEBFUN2(F, 'coeffs') where F is matrix uses the matrix as coefficients in 
-% a Chebyshev tensor expansion.
+% CHEBFUN2(F, 'coeffs') where F is a matrix uses the matrix as coefficients
+% in a Chebyshev tensor expansion. CHEBFUN2(F, 'coeffsx') uses an expansion
+% of coefficients in x and values in y. CHEBFUN2(F, 'coeffsy') uses an
+% expansion of values in x and coefficients in y.
+%
+% CHEBFUN2(F, 'trig') constructs a CHEBFUN2 object representing a smooth
+% and periodic function F on [-1,1]x[-1 1]. The resulting CHEBFUN2 is
+% represented using a bivariate Fourier expansion. CHEBFUN2(F, 'trigx')
+% constructs a CHEBFUN2 that is periodic in x only, represented as a
+% bivariate Fourier-Chebyshev expansion. CHEBFUN2(F, 'trigy') constructs a
+% CHEBFUN2 that is periodic in y only, represented as a bivariate
+% Chebyshev-Fourier expansion.
+%
+% CHEBFUN2(F, 'periodic')  is the same as CHEBFUN2(F, 'trig').
+% CHEBFUN2(F, 'periodicx') is the same as CHEBFUN2(F, 'trigx').
+% CHEBFUN2(F, 'periodicy') is the same as CHEBFUN2(F, 'trigy').
+%
+% CHEBFUN2(F, pref) constructs a CHEBFUN2 object from F with the options
+% determined by the CHEBFUNPREF object pref. CHEBFUN2(F, {prefx, prefy})
+% uses the CHEBFUNPREF objects prefx in x and prefy in y, where one of
+% prefx and prefy may be [].
 %
 % The Chebfun2 software system is based on: 
 %
