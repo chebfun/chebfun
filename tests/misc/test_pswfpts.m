81ae1c5b7edd4b4b4cf2878f87517e62c49c16c9
Nick Hale <nickhale@sun.ac.za>
2020-12-01 09:08:29 +0200

    Add a test.

--- tests/misc/test_pswfpts.m
+++ tests/misc/test_pswfpts.m
@@ -28,7 +28,7 @@ pass(3) = err < 1e-10;
 % Check nodes are roots of P9:
 [x, w] = pswfpts(5, 4, [-1 1], 'ggq');
 
-% CHeck integral of P(0:9):
+% Check integral of P(0:9):
 pass(4) = norm(sum(P) - w*P(x,:)) < 1e-10;
 
 % Check integral of cosine(k*x):
@@ -44,5 +44,10 @@ pass(5) = err < 1e-10;
 pass(6) = (norm(w-fliplr(w)) == 0) & (norm(x+flipud(x)) == 0);
 [x,w] = pswfpts(15,sqrt(2),[-7,7],'ggq');
 pass(7) = (norm(w-fliplr(w)) == 0) & (norm(x+flipud(x)) == 0);
+
+% Check c > N regime:
+[x, w] = pswfpts(21, 80, 'ggq');
+P = pswf(40,80);
+pass(8) = norm(sum(P) - w*P(x,:)) < 1e-10;
 
 end
