60ca604c8210194bc50ffe585fdcc67d221b3c0d
Nick <nick.p.hale@gmail.com>
2021-10-07 14:41:35 +0200

    Allow test_ivp to pass for older MATLAB versions.

--- tests/chebfun/test_ivp.m
+++ tests/chebfun/test_ivp.m
@@ -23,17 +23,21 @@ opts = odeset('RelTol', 1e-6);
 [tm, ym] = ode113(@vdp1, [0, 20], [2 ; 0], opts);       % Matlab's solution
 pass(3) = max(max(abs(ym - feval(y,tm)))) < 1e-5;
 
-% Test ode78 with different tolerance (RelTol = 1e-6)
-opts = odeset('RelTol', 1e-6);
-[t, y] = chebfun.ode78(@vdp1, [0, 20], [2 ; 0], opts); % CHEBFUN solution
-[tm, ym] = ode78(@vdp1, [0, 20], [2 ; 0], opts);       % Matlab's solution
-pass(4) = max(max(abs(ym - feval(y,tm)))) < 1e-5;
-
-% Test ode89 with different tolerance (RelTol = 1e-6)
-opts = odeset('RelTol', 1e-6);
-[t, y] = chebfun.ode89(@vdp1, [0, 20], [2 ; 0], opts); % CHEBFUN solution
-[tm, ym] = ode89(@vdp1, [0, 20], [2 ; 0], opts);       % Matlab's solution
-pass(5) = max(max(abs(ym - feval(y,tm)))) < 1e-5;
+if ( verLessThan('matlab', '9.11') )
+    % Test ode78 with different tolerance (RelTol = 1e-6)
+    opts = odeset('RelTol', 1e-6);
+    [t, y] = chebfun.ode78(@vdp1, [0, 20], [2 ; 0], opts); % CHEBFUN solution
+    [tm, ym] = ode78(@vdp1, [0, 20], [2 ; 0], opts);       % Matlab's solution
+    pass(4) = max(max(abs(ym - feval(y,tm)))) < 1e-5;
+    
+    % Test ode89 with different tolerance (RelTol = 1e-6)
+    opts = odeset('RelTol', 1e-6);
+    [t, y] = chebfun.ode89(@vdp1, [0, 20], [2 ; 0], opts); % CHEBFUN solution
+    [tm, ym] = ode89(@vdp1, [0, 20], [2 ; 0], opts);       % Matlab's solution
+    pass(5) = max(max(abs(ym - feval(y,tm)))) < 1e-5;
+else
+    pass(4:5) = true;
+end
 
 %% Test some trivial complex-valued IVPs:
 
@@ -53,13 +57,17 @@ pass(7) = abs(y(1) - soln) < 2e-2;
 y = chebfun.ode113(f, d, 1);
 pass(8) = abs(y(1) - soln) < 2e-2;
 
-% Test ode78:
-y = chebfun.ode113(f, d, 1);
-pass(9) = abs(y(1) - soln) < 2e-2;
-
-% Test ode89:
-y = chebfun.ode113(f, d, 1);
-pass(10) = abs(y(1) - soln) < 2e-2;
+if ( verLessThan('matlab', '9.11') )
+    % Test ode78:
+    y = chebfun.ode78(f, d, 1);
+    pass(9) = abs(y(1) - soln) < 2e-2;
+    
+    % Test ode89:
+    y = chebfun.ode89(f, d, 1);
+    pass(10) = abs(y(1) - soln) < 2e-2;
+else
+    pass(9:10) = true;
+end
 
 end
 
