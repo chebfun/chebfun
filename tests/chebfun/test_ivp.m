bf312adb7d8c8d3ca0eae55faf66fe6198fa69a8
Anthony P. Austin <anthony.austin@nps.edu>
2021-10-07 10:37:33 -0700

    Swap clauses in ode78(), ode89() IVP tests.
    These tests should happen ***unless*** we're running a MATLAB version
    prior to R2021b, not the other way around.

--- tests/chebfun/test_ivp.m
+++ tests/chebfun/test_ivp.m
@@ -24,19 +24,19 @@ opts = odeset('RelTol', 1e-6);
 pass(3) = max(max(abs(ym - feval(y,tm)))) < 1e-5;
 
 if ( verLessThan('matlab', '9.11') )
+    pass(4:5) = true;
+else
     % Test ode78 with different tolerance (RelTol = 1e-6)
     opts = odeset('RelTol', 1e-6);
     [t, y] = chebfun.ode78(@vdp1, [0, 20], [2 ; 0], opts); % CHEBFUN solution
     [tm, ym] = ode78(@vdp1, [0, 20], [2 ; 0], opts);       % Matlab's solution
     pass(4) = max(max(abs(ym - feval(y,tm)))) < 1e-5;
-    
+
     % Test ode89 with different tolerance (RelTol = 1e-6)
     opts = odeset('RelTol', 1e-6);
     [t, y] = chebfun.ode89(@vdp1, [0, 20], [2 ; 0], opts); % CHEBFUN solution
     [tm, ym] = ode89(@vdp1, [0, 20], [2 ; 0], opts);       % Matlab's solution
     pass(5) = max(max(abs(ym - feval(y,tm)))) < 1e-5;
-else
-    pass(4:5) = true;
 end
 
 %% Test some trivial complex-valued IVPs:
@@ -58,15 +58,15 @@ y = chebfun.ode113(f, d, 1);
 pass(8) = abs(y(1) - soln) < 2e-2;
 
 if ( verLessThan('matlab', '9.11') )
+    pass(9:10) = true;
+else
     % Test ode78:
     y = chebfun.ode78(f, d, 1);
     pass(9) = abs(y(1) - soln) < 2e-2;
-    
+
     % Test ode89:
     y = chebfun.ode89(f, d, 1);
     pass(10) = abs(y(1) - soln) < 2e-2;
-else
-    pass(9:10) = true;
 end
 
 end
