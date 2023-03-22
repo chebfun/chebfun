32ca090f3e0b3f9be02fb0804f7bd0fdbcd53505
Christoph <christoph.stroessner@epfl.ch>
2023-03-22 15:58:58 +0100

    added vectorized flag to compose in order to ensure that imag works properly

--- @chebfun3/compose.m
+++ @chebfun3/compose.m
@@ -78,8 +78,7 @@ elseif ( ( nargin == 2 ) && ( nargin(op) == 1 ) )
         pref.tech = get(f.rows(:,1).funs{1}, 'tech');
     end
     
-    % Call constructor:
-    f = chebfun3(@(x,y,z) op(feval(f, x, y, z)), f.domain, pref);
+    f = chebfun3(@(x,y,z) op(feval(f, x, y, z)), f.domain, pref, 'vectorize');
     
 elseif ( ( nargin == 3 ) && ( nargin(op) == 2 ) )
     % OP has two input variables.
