6fd8dec450218963dcb751a4b87af371a60b26fe
Nick Hale <nick.p.hale@gmail.com>
2024-08-07 12:38:08 +0200

    Minor changes to fix tests.

--- tests/chebop/test_sparse.m
+++ tests/chebop/test_sparse.m
@@ -1,18 +1,20 @@
 function pass = test_sparse
 
+% Test sparse collocation discretization on the same example as test_system3.
+
 % Store preference state:
 savedPrefs = cheboppref;
 
-% Force sparse discretisation:
+% Force sparse discretization:
 cheboppref.setDefaults('sparse', true);
 pref = cheboppref;
 
-tic
-N = chebop(@(x,u,v) [diff(u,2) + sin(v) ; cos(u) + diff(v,2)], [-1:.5:1]);
+N = chebop(@(x,u,v) [diff(u,2) + sin(v) ; cos(u) + diff(v,2)], [-1:1/2:1]);
 N.lbc = @(u,v) [u - 2 ; v - 1]; 
 N.rbc = @(u,v) [u - 2 ; v + 1];
-[u, v, info1] = solvebvp(N, [0 ; 0], pref);
 
+tic
+[u, v, info1] = solvebvp(N, [0 ; 0], pref);
 pass(1) = abs(feval(v, .2) - -0.371250985730553) < 1e-8;
 time_sparse = toc;
 
@@ -23,7 +25,7 @@ pass(2) = issparse(J);
 cheboppref.setDefaults('sparse', false);
 pref = cheboppref;
 
-toc
+tic
 [u2, v2, info1] = solvebvp(N, [0 ; 0], pref);
 time_dense = toc;
 
