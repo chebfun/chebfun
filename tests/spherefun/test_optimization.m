d99cf8b302dc41a845671c60a92e9c073934f328
Colin B. Macdonald <cbm@m.fsf.org>
2025-08-17 22:02:57 -0700

    tests: looser tolerance when optim not available

--- tests/spherefun/test_optimization.m
+++ tests/spherefun/test_optimization.m
@@ -5,7 +5,7 @@ if ( nargin < 1 )
     pref = chebfunpref; 
 end 
 
-tol = 5e4*pref.cheb2Prefs.chebfun2eps;
+tol = 5e4*pref.cheb2Prefs.chebfun2eps
 j = 1; 
 
 Battery = {@(x,y,z) cos(pi*z),...
@@ -45,7 +45,7 @@ for jj = 1:length(Battery)
     [Y, X] = minandmax2(g); 
     t = toc(s);  
     tt(jj) = t;
-    err(jj) = norm(Y(1) - Mini(jj)) + norm(Y(2) - Maxi(jj));
+    err(jj) = norm(Y(1) - Mini(jj)) + norm(Y(2) - Maxi(jj))
 end
 
 %%
@@ -55,11 +55,15 @@ pass = err < tol;
 j = length(pass);
 
 f = spherefun(@(x,y,z) exp(x));
-[m,loc] = max2(f);
-pass(j+1) = (loc(1)==0);
+[m,loc] = max2(f)
+if ( isempty(ver('optim')) )
+  % increase tolerance for last tests if we don't have fmincon
+  tol = 100*tol;
+end
+pass(j+1) = abs(loc(1) - 0) < tol
 
 f = spherefun(@(x,y,z) exp(x));
-[m,loc] = min2(f);
-pass(j+2) = (loc(1)==0);
+[m,loc] = min2(f)
+pass(j+2) = abs(loc(1) - 0) < tol
 
 end
