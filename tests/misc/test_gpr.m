6af0c05a8bcd6a86fde3e96b6be3488dd4dbfdd9
Nick <nick.p.hale@gmail.com>
2025-08-19 12:39:10 +0200

    Polo

--- tests/misc/test_gpr.m
+++ tests/misc/test_gpr.m
@@ -60,8 +60,6 @@ pass(9) = err < 1e-10;
 f = gpr(xx,yy);
 err = norm((f(xx) - yy)./yy,Inf);
 pass(10) = err < 1e-10;
-disp('Blind debugging') % TODO: Delete me
-[err]
 
 % Test vertical scale (big function):
 x = (1:5).^2; y = sin(x);
