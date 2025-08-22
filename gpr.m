a56b058d455619d8f47bb39551dc088f40378fb0
Nick <nick.p.hale@gmail.com>
2025-08-22 09:50:24 +0200

    Disable some failing tests.

--- gpr.m
+++ gpr.m
@@ -308,10 +308,8 @@ if ~opts.lenScale % hyperparameters not specified
     end
     format long
     f = chebfun(@(z) logML(z,x,y,opts),searchDom, ...
-        'eps',1e-6,'splitting','on')
-    f.values{1}
+        'eps',1e-6,'splitting','on');
     [~, opts.lenScale] = max(f);
-    [a,b] = minandmax(f.funs{1}.onefun)
 end
 
 end
